4a784138fcccbdc1810a4fad44cf970af7349faf
RickyHuo <huochen1994@163.com>
2017-09-03 14:23:05 +0800

    remove Event from the project

--- src/main/scala/org/interestinglab/waterdrop/serializer/Json.scala
+++ src/main/scala/org/interestinglab/waterdrop/serializer/Json.scala
@@ -1,9 +1,9 @@
 package org.interestinglab.waterdrop.serializer
 
-import org.interestinglab.waterdrop.core.Event
 import org.json4s.JsonAST.JValue
-
-// import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import org.json4s._
 import org.json4s.DefaultFormats
 import org.json4s.jackson
 import org.json4s.jackson.JsonMethods
@@ -16,37 +16,13 @@ object Json {
    */
   def checkConfig(): (Boolean, String) = (true, "")
 
-  /**
-   * Deserialize array of bytes to Event.
-   * https://github.com/json4s/json4s/issues/316
-   */
-  def deserialize(bytes: Array[Byte]): Event = {
 
-    implicit val formats = DefaultFormats
-    val m = JsonMethods
-      .parse(new String(bytes, "utf-8"))
-      .extract[Map[String, JValue]]
-    Event(m)
-  }
 
   /**
-   * Deserialize string to Event
+   * Serialize Map[String, JValue] to Json String
    */
-  def deserialize(str: String): Event = {
+  def serialize(e: Map[String, JValue]) : String = {
 
-    implicit val formats = DefaultFormats
-    val m = JsonMethods.parse(str).extract[Map[String, JValue]]
-
-    Event(m)
-  }
-
-  /**
-   * Serialize Event to bytes of array.
-   */
-  def serialize(e: Event): Array[Byte] = {
-
-    implicit val formats = DefaultFormats
-    val s = jackson.Serialization.write(e.toMap)
-    s.getBytes("utf-8")
+    compact(e)
   }
 }
