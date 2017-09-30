13b8b95637f625a3f88a9fd73fa07890cb88c21f
garyelephant <garygaowork@gmail.com>
2017-09-30 23:37:56 +0800

    added Date, Json Filter

---
+++ src/main/scala/org/interestinglab/waterdrop/filter/Date.scala
@@ -0,0 +1,19 @@
+package org.interestinglab.waterdrop.filter
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+class Date(var conf: Config) extends BaseFilter(conf) {
+
+  def this() = {
+    this(ConfigFactory.empty())
+  }
+
+  // TODO
+  override def checkConfig(): (Boolean, String) = (true, "")
+
+  override def process(spark: SparkSession, df: DataFrame): DataFrame = {
+    // TODO
+    df
+  }
+}
