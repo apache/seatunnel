197a3b0a22ad3c7bfc2fc3e25f2a14ab94587f32
garyelephant <garygaowork@gmail.com>
2017-10-01 13:26:56 +0800

    added Drop Filter

---
+++ src/main/scala/org/interestinglab/waterdrop/filter/Drop.scala
@@ -0,0 +1,35 @@
+package org.interestinglab.waterdrop.filter
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.catalyst.parser.ParseException
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.functions.not
+
+class Drop(var conf: Config) extends BaseFilter(conf) {
+
+  def this() = {
+    this(ConfigFactory.empty())
+  }
+
+  override def checkConfig(): (Boolean, String) = {
+    val sparkSession = SparkSession.builder().getOrCreate()
+    conf.hasPath("condition") match {
+      case true => {
+        try {
+          sparkSession.sessionState.sqlParser.parseExpression(conf.getString("condition"))
+          (true, "")
+        } catch {
+          case parseEx: ParseException =>
+            (false, "failed to parse [condition], caught exception: " + parseEx.getMessage())
+          case unknownEx: Throwable => (false, unknownEx.getMessage)
+        }
+      }
+      case false => (false, "please specify [condition]")
+    }
+  }
+
+  override def process(spark: SparkSession, df: DataFrame): DataFrame = {
+    val conditionExpr = conf.getString("condition")
+    df.filter(not(new Column(spark.sessionState.sqlParser.parseExpression(conditionExpr))))
+  }
+}
