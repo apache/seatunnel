bf41574edce121a560e1eaac89ea7b488aa72b59
RickyHuo <huochen1994@163.com>
2017-11-03 13:24:55 +0800

    using jackson

--- src/main/scala/org/interestinglab/waterdrop/filter/Json.scala
+++ src/main/scala/org/interestinglab/waterdrop/filter/Json.scala
@@ -4,7 +4,9 @@ import scala.collection.JavaConversions._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.functions._
+import org.json4s.DefaultFormats
+import org.json4s.jackson.JsonMethods
 
 class Json(var conf: Config) extends BaseFilter(conf) {
 
@@ -12,7 +14,12 @@ class Json(var conf: Config) extends BaseFilter(conf) {
     this(ConfigFactory.empty())
   }
 
-  override def checkConfig(): (Boolean, String) = (true, "")
+  override def checkConfig(): (Boolean, String) = {
+    conf.hasPath("table_name") && conf.hasPath("sql") match {
+      case true => (true, "")
+      case false => (false, "please specify [table_name] and [sql] as a non-empty string")
+    }
+  }
 
   override def prepare(spark: SparkSession, ssc: StreamingContext): Unit = {
     super.prepare(spark, ssc)
@@ -27,9 +34,19 @@ class Json(var conf: Config) extends BaseFilter(conf) {
 
   override def process(spark: SparkSession, df: DataFrame): DataFrame = {
 
-    // TODO:
-    df.withColumn("test", col(conf.getString("source_field")))
-    //spark.sqlContext.read.json()
+    val srcField = conf.getString("source_field")
+
+    conf.getString("target_field") match {
+      case Json.ROOT => df
+      case targetField: String => {
+        val func = udf((s: String) => {
+          implicit val formats = DefaultFormats
+          JsonMethods.parse(s).extract[Map[String, String]]
+        })
+
+        df.withColumn(targetField, func(col(srcField)))
+      }
+    }
   }
 }
 
