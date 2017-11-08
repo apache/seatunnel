db2e453a74f4664087e9aacf94b37e35b67f4beb
RickyHuo <huochen1994@163.com>
2017-11-08 13:26:18 +0800

    implemented filter convert

--- src/main/scala/org/interestinglab/waterdrop/filter/Convert.scala
+++ src/main/scala/org/interestinglab/waterdrop/filter/Convert.scala
@@ -1,9 +1,9 @@
 package org.interestinglab.waterdrop.filter
 
-import scala.collection.JavaConversions._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.sql.types._
 
 class Convert(var conf: Config) extends BaseFilter(conf) {
 
@@ -12,25 +12,34 @@ class Convert(var conf: Config) extends BaseFilter(conf) {
   }
 
   override def checkConfig(): (Boolean, String) = {
-
-    conf.hasPath("source_field") match {
-      case true => (true, "")
-      case false => (false, "please specify [source_field] as a non-empty string")
+    if (!conf.hasPath("source_field")) {
+      (false, "please specify [source_field] as a non-empty string")
+    } else if (!conf.hasPath("to")){
+      (true, "please specify [to] as a non-empty string")
+    } else {
+      (true, "")
     }
   }
 
   override def prepare(spark: SparkSession, ssc: StreamingContext): Unit = {
-    super.prepare(spark, ssc)
 
-    val defaultConfig = ConfigFactory.parseMap(
-      Map(
-        "source_field" -> "raw_message",
-        "target_field" -> Json.ROOT)
-    )
-    conf = conf.withFallback(defaultConfig)
+    super.prepare(spark, ssc)
   }
 
   override def process(spark: SparkSession, df: DataFrame): DataFrame = {
-    df
+
+    val srcField = conf.getString("source_field")
+    val to = conf.getString("to")
+
+    to match {
+      //TODO df.withColumn(srcField, df.col(srcField).cast("string"))
+      case "string" => df.withColumn(srcField, df.col(srcField).cast(StringType))
+      case "integer" => df.withColumn(srcField, df.col(srcField).cast(IntegerType))
+      case "double" => df.withColumn(srcField, df.col(srcField).cast(DoubleType))
+      case "float" => df.withColumn(srcField, df.col(srcField).cast(FloatType))
+      case "long" => df.withColumn(srcField, df.col(srcField).cast(LongType))
+      case "boolean" => df.withColumn(srcField, df.col(srcField).cast(BooleanType))
+      case toType: String => df
+    }
   }
 }
