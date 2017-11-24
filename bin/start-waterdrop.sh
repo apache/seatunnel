550fa0364eb8508a7cea16b50d5fcc87d563e0b8
garyelephant <garygaowork@gmail.com>
2017-11-24 22:23:34 +0800

    updated scripts

--- bin/start-waterdrop.sh
+++ bin/start-waterdrop.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-exec $SPARK_HOME/bin/spark-submit \
-    --class org.interestinglab.waterdrop.WaterdropMain \
-    /data/slot6/waterdrop/WaterdropMain-assembly-0.1.0.jar
+SPARK_HOME=${SPARK_HOME:-/opt/spark}
+
+
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APP_DIR=$(dirname $BIN_DIR)
+CONF_DIR=$APP_DIR/config
+LIB_DIR=$APP_DIR/lib
+
+assemblyJarName=$(find $LIB_DIR -name Waterdrop-*.jar)
+
+exec $SPARK_HOME/bin/spark-submit --class org.interestinglab.waterdrop.Waterdrop \
+    --conf spark.driver.extraJavaOptions=-Dconfig.path=$CONF_DIR/application.conf \
+    $assemblyJarName
 
