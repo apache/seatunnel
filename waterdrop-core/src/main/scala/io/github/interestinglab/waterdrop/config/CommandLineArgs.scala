6a8b8fa31ea698fab99d33fae4fc646f9165f96d
garyelephant <garygaowork@gmail.com>
2018-01-31 01:53:33 +0800

    update start-waterdrop.sh

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/config/CommandLineArgs.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/config/CommandLineArgs.scala
@@ -1,6 +1,6 @@
 package io.github.interestinglab.waterdrop.config
 
 case class CommandLineArgs(
-  master: String = "local[2]",
+  deployMode: String = "client",
   configFile: String = "application.conf",
   testConfig: Boolean = false)
