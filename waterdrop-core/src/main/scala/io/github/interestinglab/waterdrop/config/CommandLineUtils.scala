0f68ecb79c1cbe37eba53ee9daf04779a7baf673
garyelephant <garygaowork@gmail.com>
2018-02-15 00:00:14 +0800

    added docs for deployment

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/config/CommandLineUtils.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/config/CommandLineUtils.scala
@@ -10,7 +10,7 @@ object CommandLineUtils {
 
     opt[String]('c', "config").required().action((x, c) => c.copy(configFile = x)).text("config file")
     opt[Unit]('t', "check").action((_, c) => c.copy(testConfig = true)).text("check config")
-    opt[String]('m', "deploy-mode")
+    opt[String]('e', "deploy-mode")
       .required()
       .action((x, c) => c.copy(deployMode = x))
       .validate(x => if (Common.isModeAllowed(x)) success else failure("deploy-mode: " + x + " is not allowed."))
