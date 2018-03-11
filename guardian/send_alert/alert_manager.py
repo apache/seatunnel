9e014adea40fccb8da9f94ef5016167967dfab80
RickyHuo <huochen1994@163.com>
2018-03-11 17:06:50 +0800

    Add send_alert/alert_manager.py

---
+++ guardian/send_alert/alert_manager.py
@@ -0,0 +1,8 @@
+from emails import Emails
+from webhook import  Webhook
+
+def emails():
+    return Emails()
+
+def webhook():
+    return Webhook()
--- guardian/send_alert/webhook.py
+++ guardian/send_alert/webhook.py
@@ -6,23 +6,29 @@ from urlparse import urlparse
 
 from alert_util import _match_alert
 
-def send_alert(config, level, subject, objects, content):
-    url = config['url']
-    params = {
-        'subject': subject,
-        'objects': objects,
-        'content': content
-    }
 
-    headers = {
-        'content-type': 'application/json;charset=UTF-8',
-        'Accept': 'text/plain'
-    }
+class Webhook(object):
+    def send_alert(self, config, level, subject, objects, content):
+        config = config['webhook']
+        url = config['url']
+        params = {
+            'subject': subject,
+            'objects': objects,
+            'content': content
+        }
 
-    if _match_alert(config['routes'], level):
-        url_info = urlparse(url)
+        headers = {
+            'content-type': 'application/json;charset=UTF-8',
+            'Accept': 'text/plain'
+        }
 
-        port = 80 if url_info.port is None else url_info.port
-        http_client = httplib.HTTPConnection(url_info.hostname, port, timeout=5)
+        if _match_alert(config['routes'], level):
+            url_info = urlparse(url)
 
-        http_client.request("POST", url_info.path, json.dumps(params), headers)
+            port = 80 if url_info.port is None else url_info.port
+            http_client = httplib.HTTPConnection(url_info.hostname, port, timeout=5)
+
+            http_client.request("POST", url_info.path, json.dumps(params), headers)
+
+    def check_config(self, config):
+        print config
