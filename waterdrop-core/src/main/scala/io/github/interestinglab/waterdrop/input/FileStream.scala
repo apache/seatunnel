34a9bbd543f1e27c55198dedda90a3ec3872f49a
garyelephant <garygaowork@gmail.com>
2018-08-29 13:21:03 +0800

    rename streaming inputs to <XXX>Stream

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/FileStream.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/FileStream.scala
@@ -5,7 +5,7 @@ import io.github.interestinglab.waterdrop.apis.BaseStreamingInput
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
 
-class File extends BaseStreamingInput {
+class FileStream extends BaseStreamingInput {
 
   var config: Config = ConfigFactory.empty()
 
