8537f90e19c8657a8667b317aa4556ad459e831e
kid-xiong <rid_xiong5991@163.com>
2018-11-04 20:59:53 +0800

    add kudu and mongo

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/output/MongoDB.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/output/MongoDB.scala
@@ -4,7 +4,7 @@ import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.WriteConfig
 import com.typesafe.config.{Config, ConfigFactory}
 import io.github.interestinglab.waterdrop.apis.BaseOutput
-import org.apache.spark.sql.{Dataset, Row}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 import scala.collection.JavaConversions._
 
@@ -12,34 +12,48 @@ class MongoDB extends BaseOutput {
 
   var config: Config = ConfigFactory.empty()
 
+  val confPrefix = "writeConfig"
+
+  var writeConfig :WriteConfig = _
+
   override def setConfig(config: Config): Unit = {
-    var defaultConfig = ConfigFactory.parseMap(
-      Map(
-        "isReplace" -> "false"
-      )
-    )
-    this.config = config.withFallback(defaultConfig)
+    this.config = config
   }
 
   override def getConfig(): Config = {
     this.config
   }
 
+
   override def checkConfig(): (Boolean, String) = {
-    config.hasPath("mongo_uri") && config.hasPath("database") && config.hasPath("collection")  match {
-      case true => (true, "")
-      case false => (false, "please specify [mongo_uri] and [database] and [collection] ")
+
+    config.hasPath(confPrefix)  match {
+      case true => {
+        val read = config.getConfig(confPrefix)
+        read.hasPath("uri") && read.hasPath("database") && read.hasPath("collection") match {
+          case true => (true, "")
+          case false => (false, "please specify [readConfig.uri] and [readConfig.database] and [readConfig.collection]")
+        }
+      }
+      case false => (false, "please specify [readConfig] ")
     }
   }
 
-  override def process(df: Dataset[Row]): Unit = {
+  override def prepare(spark: SparkSession): Unit = {
+    super.prepare(spark)
+    val map = new collection.mutable.HashMap[String, String]
+    config
+      .getConfig(confPrefix)
+      .entrySet()
+      .foreach(entry => {
+        val key = entry.getKey
+        val value = String.valueOf(entry.getValue.unwrapped())
+        map.put(key, value)
+      })
+    writeConfig = WriteConfig(map)
+  }
 
-    val writeConf = WriteConfig(Map(
-      "uri" -> config.getString("mongo_uri"),
-      "database" -> config.getString("database"),
-      "collection" -> config.getString("collection"),
-      "replaceDocument" -> config.getString("isReplace")
-    ))
-    MongoSpark.save(df,writeConf)
+  override def process(df: Dataset[Row]): Unit = {
+    MongoSpark.save(df,writeConfig)
   }
 }
