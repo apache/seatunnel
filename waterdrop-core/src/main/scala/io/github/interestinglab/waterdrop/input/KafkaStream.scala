dbb39adcfa7e7cc10335a8c3e221ce85e5c9a6d4
rickyhuo <huochen1994@163.com>
2018-11-18 19:46:58 +0800

    Remove parameter of zookeeper.connect

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/KafkaStream.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/KafkaStream.scala
@@ -35,13 +35,11 @@ class KafkaStream extends BaseStreamingInput {
     config.hasPath("topics") match {
       case true => {
         val consumerConfig = config.getConfig(consumerPrefix)
-        consumerConfig.hasPath("zookeeper.connect") &&
-          !consumerConfig.getString("zookeeper.connect").trim.isEmpty &&
-          consumerConfig.hasPath("group.id") &&
+        consumerConfig.hasPath("group.id") &&
           !consumerConfig.getString("group.id").trim.isEmpty match {
           case true => (true, "")
           case false =>
-            (false, "please specify [consumer.zookeeper.connect] and [consumer.group.id] as non-empty string")
+            (false, "please specify [consumer.group.id] as non-empty string")
         }
       }
       case false => (false, "please specify [topics] as non-empty string, multiple topics separated by \",\"")
