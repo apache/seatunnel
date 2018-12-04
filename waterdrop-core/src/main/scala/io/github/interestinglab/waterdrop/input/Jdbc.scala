96e30ec506bd9d03a491c0e0a18c3f2ef7bd99c7
rickyhuo <huochen1994@163.com>
2018-12-04 22:03:18 +0800

    Add mysql input

---
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/Jdbc.scala
@@ -0,0 +1,50 @@
+package io.github.interestinglab.waterdrop.input
+
+import com.typesafe.config.{Config, ConfigFactory}
+import io.github.interestinglab.waterdrop.apis.BaseStaticInput
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+import scala.collection.JavaConversions._
+
+class Jdbc extends BaseStaticInput {
+
+  var config: Config = ConfigFactory.empty()
+
+  override def setConfig(config: Config): Unit = {
+    this.config = config
+  }
+
+  override def getConfig(): Config = {
+    this.config
+  }
+
+  override def checkConfig(): (Boolean, String) = {
+
+    val requiredOptions = List("url", "table", "user", "password");
+
+    val nonExistsOptions = requiredOptions.map(optionName => (optionName, config.hasPath(optionName))).filter { p =>
+      val (optionName, exists) = p
+      !exists
+    }
+
+    if (nonExistsOptions.length == 0) {
+      (true, "")
+    } else {
+      (false, "please specify " + nonExistsOptions.map("[" + _._1 + "]").mkString(", ") + " as non-empty string")
+    }
+  }
+
+  override def prepare(spark: SparkSession): Unit = {
+    super.prepare(spark)
+  }
+
+  override def getDataset(spark: SparkSession): Dataset[Row] = {
+    spark.read
+      .format("jdbc")
+      .option("url", config.getString("url"))
+      .option("dbtable", config.getString("table"))
+      .option("user", config.getString("user"))
+      .option("password", config.getString("password"))
+      .load()
+  }
+}
