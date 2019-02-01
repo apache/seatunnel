d505dc2b4d06fcc39fb1ed80f0d98edfb50adb54
garyelephant <garygaowork@gmail.com>
2019-02-01 23:30:23 +0800

    update

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/pipelines/Pipeline.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/pipelines/Pipeline.scala
@@ -3,28 +3,29 @@ package io.github.interestinglab.waterdrop.pipelines
 import java.io.File
 
 import scala.collection.JavaConversions._
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import io.github.interestinglab.waterdrop.apis.{BaseFilter, BaseOutput, BaseStaticInput, BaseStreamingInput}
+import io.github.interestinglab.waterdrop.config.ConfigRuntimeException
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 // TODO:
-// (1) pipeline 生成
-// (2) pipeline 使用
-// (3) pipeline viewer UI
+// [done] (1) pipeline 生成
+// [done] (2) pipeline 使用
+// (3) pipeline viewer UI (+ Spark Tab)
 // (4) pipeline builder UI
 
 class Pipeline(name: String) {
 
-  val execStartingPoint: Pipeline.StartingPoint = Pipeline.Unused // 执行起始点，执行起始点必须比子Pipeline起始点靠前
-  val streamingInputList: List[BaseStreamingInput[Any]] = List()
-  val staticInputList: List[BaseStaticInput] = List()
-  val filterList: List[BaseFilter] = List()
-  val outputList: List[BaseOutput] = List()
+  var execStartingPoint: Pipeline.StartingPoint = Pipeline.Unused // 执行起始点，执行起始点必须比子Pipeline起始点靠前
+  var streamingInputList: List[BaseStreamingInput[Any]] = List()
+  var staticInputList: List[BaseStaticInput] = List()
+  var filterList: List[BaseFilter] = List()
+  var outputList: List[BaseOutput] = List()
 
   // pipeline 起始点 [point1] input [point2] filter [point3] output
-  val subPipelinesStartingPoint: Pipeline.StartingPoint = Pipeline.Unused // 子Pipeline起始点。
-  val subPipelines: List[Pipeline] = List()
+  var subPipelinesStartingPoint: Pipeline.StartingPoint = Pipeline.Unused // 子Pipeline起始点。
+  var subPipelines: List[Pipeline] = List()
 
   // TODO: 数据仓库刷新分区的需求。
   // TODO: 考虑 end-to-end exactly once 实现方案。
@@ -41,18 +42,28 @@ object Pipeline {
   sealed trait PipelineType {}
   case object Streaming extends PipelineType
   case object Batch extends PipelineType
+  case object Unknown extends PipelineType
 
   def main(args: Array[String]): Unit = {
 
-    val (rootPipeline, rootPipelineType) = pipelineBuilder(args)
+    val rootConfig = ConfigFactory.parseFile(new File(args(0)))
+    val sparkConfig = rootConfig.getConfig("spark")
+
+    val (rootPipeline, rootPipelineType, _) = pipelineRecursiveBuilder(rootConfig, "ROOT_PIPELINE")
 
     validatePipeline(rootPipeline)
 
     val spark = SparkSession.builder().getOrCreate()
 
+    preparePipelineRecursively(rootPipeline)
+
     // TODO: 验证pipelineRunnerForStreaming直到执行ssc.start()才阻塞
     // TODO: 验证pipelineRunnerForBatch遇到action立即会阻塞
     rootPipelineType match {
+
+      // TODO: empty dataset
+      // TODO: multiple action need cache() ???
+
       case Streaming => {
 
         val duration = 10
@@ -68,39 +79,117 @@ object Pipeline {
     }
   }
 
-  def pipelineBuilder(args: Array[String]): (Pipeline, PipelineType) = {
+  private def pipelineRecursiveBuilder(config: Config, pname: String): (Pipeline, PipelineType, StartingPoint) = {
 
-    // TODO: build pipeline
+    val pipeline = new Pipeline(pname)
 
-    val r = """^pipeline<([0-9a-zA-Z_]+)>""".r // pipeline<pname> pattern
-    val rootConfig = ConfigFactory.parseFile(new File(args(0)))
-    val sparkConfig = rootConfig.getConfig("spark")
-
-    val rootPipeline = new Pipeline("ROOT_PIPELINE")
-
-    // 如果有input，本身就是一个pipeline
-    // 如果没有input，那么它内层的pipeline必须有input
-    if (rootConfig.hasPath("input")) {
-      val pipeline = new Pipeline("")
+    if (config.hasPath("input")) {
+      pipeline.streamingInputList = createStreamingInputs(config.getConfig("input"))
+      pipeline.staticInputList = createStaticInputs(config.getConfig("input"))
     }
 
-    for (configName <- rootConfig.root.unwrapped.keySet) {
+    if (pipeline.streamingInputList.nonEmpty || pipeline.staticInputList.nonEmpty) {
+      pipeline.execStartingPoint = PreInput
+    }
+
+    if (config.hasPath("filter")) {
+      pipeline.filterList = createFilters(config.getConfig("filter"))
+    }
+
+    if (pipeline.execStartingPoint != PreInput && pipeline.filterList.nonEmpty) {
+      pipeline.execStartingPoint = PreFilter
+    }
+
+    if (config.hasPath("output")) {
+      pipeline.outputList = createOutputs(config.getConfig("output"))
+    }
+
+    if (pipeline.execStartingPoint != PreInput
+      && pipeline.execStartingPoint != PreFilter
+      && pipeline.outputList.nonEmpty) {
+      pipeline.execStartingPoint = PreOutput
+    }
+
+    // subPipelineStartingPoint alignment order: PreInput --> PreFilter --> PreOutput
+    var subPipelineStartingPoint: StartingPoint = Unused
+    var subPipelineType: PipelineType = Unknown
+    val r = """^pipeline<([0-9a-zA-Z_]+)>""".r // pipeline<pname> pattern
+    for (configName <- config.root.unwrapped.keySet) {
       configName match {
         case name if name.startsWith("pipeline") => {
+
           val r(pipelineName) = name
           println("pipeline: " + pipelineName)
-        }
 
+          val (subPipeline, pType, subSP) = pipelineRecursiveBuilder(config.getConfig(name), pipelineName)
+
+          pipeline.subPipelines = pipeline.subPipelines :+ subPipeline
+
+          subPipelineStartingPoint = mergeStartingPoint(subPipelineStartingPoint, subSP)
+
+          subPipelineType = mergePipelineType(subPipelineType, pType)
+        }
         case _ => {}
       }
-
     }
-    val pType = Streaming
-    (rootPipeline, pType)
+
+    val pType = (pipeline.streamingInputList.nonEmpty, pipeline.staticInputList.nonEmpty) match {
+      case (true, _) => Streaming
+      case (false, true) => Batch
+      case _ => subPipelineType
+    }
+
+    (pipeline, pType, pipeline.execStartingPoint)
+  }
+
+  private def mergePipelineType(t1: PipelineType, t2: PipelineType): PipelineType = {
+    (t1, t2) match {
+      case (Streaming, _) => Streaming
+      case (_, Streaming) => Streaming
+      case (Batch, _) => Batch
+      case (_, Batch) => Batch
+      case _ => Unknown
+    }
+  }
+
+  private def mergeStartingPoint(s1: StartingPoint, s2: StartingPoint): StartingPoint = {
+    (s1, s2) match {
+      case (PreInput, _) => PreInput
+      case (_, PreInput) => PreInput
+      case (PreFilter, _) => PreFilter
+      case (_, PreFilter) => PreFilter
+      case (PreOutput, _) => PreOutput
+      case (_, PreOutput) => PreOutput
+      case _ => Unused
+    }
+  }
+
+  private def createStreamingInputs(config: Config): List[BaseStreamingInput[Any]] = {
+    // TODO:
+    List()
+  }
+
+  private def createStaticInputs(config: Config): List[BaseStaticInput] = {
+    // TODO:
+    List()
+  }
+
+  private def createFilters(config: Config): List[BaseFilter] = {
+    // TODO:
+    List()
+  }
+
+  private def createOutputs(config: Config): List[BaseOutput] = {
+    // TODO:
+    List()
+  }
+
+  private def preparePipelineRecursively(pipeline: Pipeline): Unit = {
+    // TODO:
   }
 
   /**
-   * @throws ConfigError
+   * @throws ConfigRuntimeException
    * */
   def validatePipeline(pipeline: Pipeline): Unit = {
     // TODO: validate multiple level pipeline
@@ -168,17 +257,71 @@ object Pipeline {
   }
 
   private def processPreInputForStaticWithPreFilter(pipeline: Pipeline, spark: SparkSession): Unit = {
-    // TODO: init static inputs, filter[this pipeline], subPipeline(in PreFilter)
+
+    initStaticInputs(spark, pipeline.staticInputList)
+
+    if (pipeline.staticInputList.nonEmpty) {
+      var ds = pipeline.staticInputList.head.getDataset(spark)
+
+      for (f <- pipeline.filterList) {
+        if (ds.take(1).length > 0) {
+          ds = f.process(spark, ds)
+        }
+      }
+
+      for (subPipeline <- pipeline.subPipelines) {
+        pipelineRunner(subPipeline, spark, ds)
+      }
+
+    } else {
+      throw new ConfigRuntimeException("Input must be configured at least once.")
+    }
 
   }
 
   private def processPreInputForStaticWithPreOutput(pipeline: Pipeline, spark: SparkSession): Unit = {
-    // TODO: innput, filter, subPipeline(in PreOutput)
+
+    initStaticInputs(spark, pipeline.staticInputList)
+
+    if (pipeline.staticInputList.nonEmpty) {
+      var ds = pipeline.staticInputList.head.getDataset(spark)
+
+      for (f <- pipeline.filterList) {
+        if (ds.take(1).length > 0) {
+          ds = f.process(spark, ds)
+        }
+      }
+
+      for (subPipeline <- pipeline.subPipelines) {
+        pipelineRunner(subPipeline, spark, ds)
+      }
+
+    } else {
+      throw new ConfigRuntimeException("Input must be configured at least once.")
+    }
 
   }
 
   private def processPreInputForStaticWithUnused(pipeline: Pipeline, spark: SparkSession): Unit = {
-    // TODO: input, filter, output
+
+    initStaticInputs(spark, pipeline.staticInputList)
+
+    if (pipeline.staticInputList.nonEmpty) {
+      var ds = pipeline.staticInputList.head.getDataset(spark)
+
+      for (f <- pipeline.filterList) {
+        if (ds.take(1).length > 0) {
+          ds = f.process(spark, ds)
+        }
+      }
+      pipeline.outputList.foreach(p => {
+        p.process(ds)
+      })
+
+    } else {
+      throw new ConfigRuntimeException("Input must be configured at least once.")
+    }
+
   }
 
   /**
@@ -192,7 +335,9 @@ object Pipeline {
         }
       }
       case PreFilter => {
-        // TODO: init static inputs
+
+        initStaticInputs(spark, pipeline.staticInputList)
+
         pipeline
           .streamingInputList(0)
           .start(
@@ -225,7 +370,8 @@ object Pipeline {
     spark: SparkSession,
     ssc: StreamingContext): Unit = {
 
-    // TODO: init static inputs
+    initStaticInputs(spark, pipeline.staticInputList)
+
     pipeline
       .streamingInputList(0)
       .start(
@@ -251,7 +397,7 @@ object Pipeline {
     spark: SparkSession,
     ssc: StreamingContext): Unit = {
 
-    // TODO: init static inputs
+    initStaticInputs(spark, pipeline.staticInputList)
 
     pipeline
       .streamingInputList(0)
@@ -331,6 +477,37 @@ object Pipeline {
       }
       case _ => {
         // must throw exception
+      }
+    }
+  }
+
+  private def initStaticInputs(sparkSession: SparkSession, staticInputs: List[BaseStaticInput]): Unit = {
+
+    // let static input register as table for later use if needed
+    var datasetMap = Map[String, Dataset[Row]]()
+    for (input <- staticInputs) {
+
+      val ds = input.getDataset(sparkSession)
+
+      val config = input.getConfig()
+      config.hasPath("table_name") match {
+        case true => {
+          val tableName = config.getString("table_name")
+
+          datasetMap.contains(tableName) match {
+            case true =>
+              throw new ConfigRuntimeException(
+                "Detected duplicated Dataset["
+                  + tableName + "], it seems that you configured table_name = \"" + tableName + "\" in multiple static inputs")
+            case _ => datasetMap += (tableName -> ds)
+          }
+
+          ds.createOrReplaceTempView(tableName)
+        }
+        case false => {
+          throw new ConfigRuntimeException(
+            "Plugin[" + input.name + "] must be registered as dataset/table, please set \"table_name\" config")
+        }
       }
     }
   }
