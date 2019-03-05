9dd003f87c21984da4df6baecb1da78099b9e0ba
rickyhuo <huochen1994@163.com>
2019-03-05 19:18:48 +0800

    Add filter of join

---
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Join.scala
@@ -0,0 +1,52 @@
+package io.github.interestinglab.waterdrop.filter
+
+import com.typesafe.config.{Config, ConfigFactory}
+import io.github.interestinglab.waterdrop.apis.BaseFilter
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions._
+
+import scala.collection.JavaConversions._
+
+class Join extends BaseFilter {
+
+  var conf: Config = ConfigFactory.empty()
+
+  /**
+   * Set Config.
+   * */
+  override def setConfig(config: Config): Unit = {
+    this.conf = config
+  }
+
+  /**
+   * Get Config.
+   * */
+  override def getConfig(): Config = {
+    this.conf
+  }
+
+  override def checkConfig(): (Boolean, String) = {
+    conf.hasPath("table_name") && conf.hasPath("source_field") match {
+      case true => (true, "")
+      case false => (false, "please specify [table_name] and [sql]")
+    }
+  }
+
+  override def prepare(spark: SparkSession): Unit = {
+
+    val defaultConfig = ConfigFactory.parseMap(
+      Map(
+        "join_type" -> "right_join"
+      )
+    )
+
+    conf = conf.withFallback(defaultConfig)
+  }
+
+  override def process(spark: SparkSession, df: Dataset[Row]): Dataset[Row] = {
+    val staticDf = spark.read.table(this.conf.getString("table_name"))
+    val source_field = this.conf.getString("source_field")
+    val joinType = this.conf.getString("join_type")
+    df.join(staticDf, col(source_field), joinType)
+  }
+}
