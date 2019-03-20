17d743b17ef947768b68f9f6d02af923410b1fde
garyelephant <garygaowork@gmail.com>
2019-03-20 15:32:15 +0800

    added -i --variable option to make variables substitution available

--- bin/start-waterdrop.sh
+++ bin/start-waterdrop.sh
@@ -23,6 +23,13 @@ while (( "$#" )); do
       shift 2
       ;;
 
+    -i|--variable)
+      variable=$2
+      java_property_value="-D${variable}"
+      variables_substitution="${java_property_value} ${variables_substitution}"
+      shift 2
+      ;;
+
     --) # end argument parsing
       shift
       break
@@ -89,6 +96,10 @@ assemblyJarName=$(find ${LIB_DIR} -name Waterdrop-*.jar)
 
 source ${CONF_DIR}/waterdrop-env.sh
 
+string_trim() {
+    echo $1 | awk '{$1=$1;print}'
+}
+
 ## get spark conf from config file and specify them in spark-submit
 function get_spark_conf {
     spark_conf=$(java -cp ${assemblyJarName} io.github.interestinglab.waterdrop.config.ExposeSparkConf ${CONFIG_FILE})
@@ -100,6 +111,21 @@ function get_spark_conf {
 }
 
 sparkconf=$(get_spark_conf)
+
+# Spark Driver Options
+variables_substitution=$(string_trim "${variables_substitution}")
+driverJavaOpts=""
+executorJavaOpts=""
+clientModeDriverJavaOpts=""
+if [ ! -z "${variables_substitution}" ]; then
+  driverJavaOpts="--conf \"spark.driver.extraJavaOptions=${variables_substitution}\""
+  executorJavaOpts="--conf \"spark.executor.extraJavaOptions=${variables_substitution}\""
+  # in local, client mode, driverJavaOpts can not work, we must use --driver-java-options
+  clientModeDriverJavaOpts="--driver-java-options \"${variables_substitution}\""
+fi
+
+
+sparkconf="${sparkconf} ${driverJavaOpts} ${executorJavaOpts} ${clientModeDriverJavaOpts}"
 
 echo "[INFO] spark conf: ${sparkconf}"
 
