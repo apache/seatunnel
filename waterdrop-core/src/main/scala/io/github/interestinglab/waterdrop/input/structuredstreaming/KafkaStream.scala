09c5565d8164d02cef1cde8b26ec51788320bec2
kid-xiong <rid_xiong5991@163.com>
2019-03-22 01:10:57 +0800

    json parse

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/structuredstreaming/KafkaStream.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/structuredstreaming/KafkaStream.scala
@@ -1,13 +1,16 @@
 package io.github.interestinglab.waterdrop.input.structuredstreaming
 
+import com.alibaba.fastjson.JSON
 import com.typesafe.config.{Config, ConfigFactory}
 import io.github.interestinglab.waterdrop.apis.BaseStructuredStreamingInput
+import org.apache.spark.sql.types.{DataType, DataTypes, StructType}
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
-
+import org.apache.spark.sql.functions._
 import scala.collection.JavaConversions._
 
 class KafkaStream extends BaseStructuredStreamingInput {
   var config: Config = ConfigFactory.empty()
+  var schema = new StructType()
 
   override def setConfig(config: Config): Unit = {
     this.config = config
@@ -28,6 +31,16 @@ class KafkaStream extends BaseStructuredStreamingInput {
     }
   }
 
+  override def prepare(spark: SparkSession): Unit ={
+    config.hasPath("schema") match {
+      case true => {
+        val schemaJson = JSON.parseObject(config.getString("schema"))
+        schema = getSchema(schema,schemaJson)
+      }
+      case false =>{}
+    }
+  }
+
   override def getDataset(spark: SparkSession): Dataset[Row] = {
     val topics = config.getString("topics")
     val consumerConfig = config.getConfig(consumerPrefix)
@@ -42,12 +55,48 @@ class KafkaStream extends BaseStructuredStreamingInput {
       val (key, value) = entry
       println("[INFO] \t" + key + " = " + value)
     }
-    spark.readStream
+    val dataFrame = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", config.getString("consumer.bootstrap.servers"))
-      .option("subscribe", config.getString("topics"))
+      .option("subscribe", topics)
       .options(kafkaParams)
       .load()
+    if (schema.size > 0){
+      dataFrame.withColumn("parse_value",from_json(col("value"),schema))
+    }
+    dataFrame
   }
 
+  private def getSchema(schema: StructType,jsonSchema: JSON): StructType ={
+    for(entry <- jsonSchema){
+      val (field,dataType) = entry
+      dataType.isInstanceOf[JSON] match {
+        case true => {
+          val st = getSchema(new StructType(),dataType.asInstanceOf[JSON])
+          schema.add(field,st)
+        }
+        case false => schema.add(field,getType(dataType.asInstanceOf[String]))
+      }
+    }
+    schema
+  }
+
+  private def  getType(typeString:String): DataType = {
+    var dataType = DataTypes.NullType;
+    typeString.toLowerCase() matches {
+      case "string" => dataType = DataTypes.StringType
+      case "integer" => dataType = DataTypes.IntegerType
+      case "long" => dataType = DataTypes.LongType
+      case "double" => dataType = DataTypes.DoubleType
+      case "float" => dataType = DataTypes.FloatType
+      case "short" => dataType = DataTypes.ShortType
+      case "date" => dataType = DataTypes.DateType
+      case "timestamp" => dataType = DataTypes.TimestampType
+      case "boolean" => dataType = DataTypes.BooleanType
+      case "binary" => dataType = DataTypes.BinaryType
+      case "byte" => dataType = DataTypes.ByteType
+      case _ => throw new RuntimeException("")
+    }
+    dataType
+  }
 }
