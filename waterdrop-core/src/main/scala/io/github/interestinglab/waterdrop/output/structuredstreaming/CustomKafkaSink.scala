be68f5cf6d2936aa651de0adaf626b84a81e34b5
kid-xiong <rid_xiong5991@163.com>
2019-03-21 22:04:18 +0800

    ss dev

---
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/output/structuredstreaming/CustomKafkaSink.scala
@@ -0,0 +1,113 @@
+package io.github.interestinglab.waterdrop.output.structuredstreaming
+
+import java.util.Properties
+
+import com.alibaba.fastjson.JSONObject
+import com.typesafe.config.{Config, ConfigFactory}
+import io.github.interestinglab.waterdrop.apis.BaseStructuredStreamingOutput
+import io.github.interestinglab.waterdrop.output.KafkaProducerUtil
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.sql.streaming.DataStreamWriter
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+import scala.collection.JavaConversions._
+
+class CustomKafkaSink extends BaseStructuredStreamingOutput{
+  var config = ConfigFactory.empty()
+  val producerPrefix = "producer"
+  val outputOptionPrefix = "output.option"
+  var options = new collection.mutable.HashMap[String, String]
+  var kafkaSink: Broadcast[KafkaProducerUtil] = _
+  var topic: String = _
+  override def setConfig(config: Config): Unit = this.config = config
+
+  override def getConfig(): Config = config
+
+  override def checkConfig(): (Boolean, String) = {
+    val producerConfig = config.getConfig(producerPrefix)
+
+    config.hasPath("topic") && producerConfig.hasPath("bootstrap.servers") match {
+      case true => (true, "")
+      case false => (false, "please specify [topic] and [producer.bootstrap.servers]")
+    }
+  }
+
+  override def prepare(spark: SparkSession): Unit = {
+    super.prepare(spark)
+    topic = config.getString("topic")
+    val defaultConfig = ConfigFactory.parseMap(
+      Map(
+        "outputMode" -> "Append",
+        "triggerMode" -> "default",
+        producerPrefix + ".retries" -> 2,
+        producerPrefix + ".acks" -> 1,
+        producerPrefix + ".buffer.memory" -> 33554432,
+        producerPrefix + ".key.serializer" -> "org.apache.kafka.common.serialization.StringSerializer",
+        producerPrefix + ".value.serializer" -> "org.apache.kafka.common.serialization.StringSerializer"
+      )
+    )
+    config.withFallback(defaultConfig)
+    val props = new Properties()
+    config
+      .getConfig(producerPrefix)
+      .entrySet()
+      .foreach(entry => {
+        val key = entry.getKey
+        val value = String.valueOf(entry.getValue.unwrapped())
+        props.put(key, value)
+      })
+
+    println("[INFO] Kafka Output properties: ")
+    props.foreach(entry => {
+      val (key, value) = entry
+      println("[INFO] \t" + key + " = " + value)
+    })
+
+    kafkaSink = spark.sparkContext.broadcast(KafkaProducerUtil(props))
+
+    config.hasPath(outputOptionPrefix) match {
+      case true => {
+        config
+          .getConfig(outputOptionPrefix)
+          .entrySet()
+          .foreach(entry => {
+            val key = entry.getKey
+            val value = String.valueOf(entry.getValue.unwrapped())
+            options.put(key, value)
+          })
+      }
+      case false => {}
+    }
+  }
+  /**
+   * Things to do before process.
+   **/
+  override def open(partitionId: Long, epochId: Long): Boolean = true
+
+  /**
+   * Things to do with each Row.
+   **/
+  override def process(row: Row): Unit = {
+    val json = new JSONObject()
+    row.schema.fieldNames
+      .foreach(field=>json.put(field,row.getAs(field)))
+    kafkaSink.value.send(topic,json.toJSONString)
+  }
+
+  /**
+   * Things to do after process.
+   **/
+  override def close(errorOrNull: Throwable): Unit = {}
+
+  /**
+   * Waterdrop Structured Streaming process.
+   **/
+  override def process(df: Dataset[Row]): DataStreamWriter[Row] = {
+    val writer = df.writeStream
+      .foreach(this)
+      .options(options)
+    StructuredUtils.writeWithTrigger(config,writer)
+  }
+
+
+}
--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/output/structuredstreaming/StructuredUtils.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/output/structuredstreaming/StructuredUtils.scala
@@ -2,7 +2,7 @@ package io.github.interestinglab.waterdrop.output.structuredstreaming
 
 import com.typesafe.config.Config
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.streaming.DataStreamWriter
+import org.apache.spark.sql.streaming.{DataStreamWriter, Trigger}
 
 object StructuredUtils {
 
@@ -30,6 +30,15 @@ object StructuredUtils {
         }
       }
       case false => true
+    }
+  }
+
+  def writeWithTrigger(config: Config,writer: DataStreamWriter[Row]): DataStreamWriter[Row] = {
+    config.getString("triggerMode") match {
+      case "default" => writer
+      case "ProcessingTime" => writer.trigger(Trigger.ProcessingTime(config.getString("interval")))
+      case "OneTime" => writer.trigger(Trigger.Once())
+      case "Continuous" => writer.trigger(Trigger.Continuous(config.getString("interval")))
     }
   }
 }
