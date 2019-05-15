90a9eb9aff491290bbc9d86bb576c72fa307961c
garyelephant <garygaowork@gmail.com>
2019-05-15 15:54:03 +0800

    do not programatically enableHiveSupport(), enable/disable hive support by configuration: spark.sql.catalogImplementation = hive

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/Waterdrop.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/Waterdrop.scala
@@ -96,7 +96,7 @@ object Waterdrop extends Logging {
       println("\t" + key + " => " + value)
     })
 
-    val sparkSession = SparkSession.builder.config(sparkConf).enableHiveSupport().getOrCreate()
+    val sparkSession = SparkSession.builder.config(sparkConf).getOrCreate()
 
     // find all user defined UDFs and register in application init
     UdfRegister.findAndRegisterUdfs(sparkSession)
