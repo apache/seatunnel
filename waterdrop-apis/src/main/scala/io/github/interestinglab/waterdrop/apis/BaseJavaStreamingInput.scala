89c1f2c03832ad2a2867805cbd75a5c2ee64db34
rickyhuo <huochen1994@163.com>
2019-05-16 23:21:50 +0800

    Add java streaming input api

---
+++ waterdrop-apis/src/main/scala/io/github/interestinglab/waterdrop/apis/BaseJavaStreamingInput.scala
@@ -0,0 +1,32 @@
+package io.github.interestinglab.waterdrop.apis
+
+import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.api.java.{JavaDStream, JavaStreamingContext}
+import org.apache.spark.streaming.dstream.DStream
+
+/**
+ * Superclass of all streaming input, extends this abstract class to implement a streaming input.
+ * */
+abstract class BaseJavaStreamingInput[T] extends BaseStreamingInput[T] {
+
+  def rdd2dataset(spark: SparkSession, rdd: RDD[T]): Dataset[Row] = {
+    javaRdd2dataset(spark, rdd.toJavaRDD())
+  }
+
+  def getDStream(ssc: StreamingContext): DStream[T] = {
+    getJavaDstream(new JavaStreamingContext(ssc)).dstream
+  }
+
+  /**
+   * Create spark javaDStream from data source, you can specify type parameter.
+   * */
+  def getJavaDstream(jssc: JavaStreamingContext): JavaDStream[T]
+
+  /**
+   * This must be implemented to convert JavaRDD[T] to Dataset[Row] for later processing
+   * */
+  def javaRdd2dataset(sparkSession: SparkSession, javaRDD: JavaRDD[T]): Dataset[Row]
+}
