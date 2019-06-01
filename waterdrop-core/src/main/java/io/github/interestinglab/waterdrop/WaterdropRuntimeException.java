3c6ddd7a9c5b8862d21864fc8ebac38af8e01ea6
rickyhuo <huochen1994@163.com>
2019-06-01 10:32:58 +0800

    Kafka output supports serializer

---
+++ waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/WaterdropRuntimeException.java
@@ -0,0 +1,22 @@
+package io.github.interestinglab.waterdrop;
+
+/**
+ */
+public class WaterdropRuntimeException extends RuntimeException {
+
+    public WaterdropRuntimeException() {
+        super();
+    }
+
+    public WaterdropRuntimeException(String message) {
+        super(message);
+    }
+
+    public WaterdropRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WaterdropRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
