9d48ac841c6ae40310a5213b1633a6e6d78cf812
rickyhuo <huochen1994@163.com>
2019-08-13 22:47:33 +0800

    Fix coding style by p3c-pmd

--- waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/Application.java
+++ waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/Application.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 public class Application {
 
-    public static void main(String[] args) throws ConfigParser.ConfigError {
+    public static void main(String[] args) throws ConfigParser.ConfigErrorException {
 
         File file = new File(args[0]);
 
