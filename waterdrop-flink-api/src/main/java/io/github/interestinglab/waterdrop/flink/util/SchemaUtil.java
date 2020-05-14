e453c466b4201e19c85b3a6b3104b4e8f0bb4055
garyelephant <garygaowork@gmail.com>
2020-05-14 11:26:14 +0800

    update pom.xml's to pass mvn compile

--- waterdrop-flink-api/src/main/java/io/github/interestinglab/waterdrop/flink/util/SchemaUtil.java
+++ waterdrop-flink-api/src/main/java/io/github/interestinglab/waterdrop/flink/util/SchemaUtil.java
@@ -15,7 +15,6 @@ import org.apache.flink.types.Row;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
