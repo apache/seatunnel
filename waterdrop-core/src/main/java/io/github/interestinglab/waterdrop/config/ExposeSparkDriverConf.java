99bb2cc8cf321f535ae619140b7fa3846d0762bf
RickyHuo <huochen1994@163.com>
2020-06-08 21:13:33 +0800

    Support spark.driver.extra*

--- waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/config/ExposeSparkDriverConf.java
+++ waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/config/ExposeSparkDriverConf.java
@@ -1,10 +1,32 @@
 package io.github.interestinglab.waterdrop.config;
 
 
-import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.io.File;
+
 
 public class ExposeSparkDriverConf {
+
+    public static List<String> splitKey(String key) {
+        List<String> keys = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < key.length(); i ++) {
+            char symbol = key.charAt(i);
+            if (symbol >= 'A' && symbol <= 'Z') {
+                keys.add(key.substring(index, i).toLowerCase());
+                index = i;
+            }
+        }
+
+        keys.add(key.substring(index).toLowerCase());
+
+        if (keys.get(0).equals("extra")) {
+            return keys.subList(1, keys.size());
+        }
+        return keys;
+    }
 
     public static void main(String[] args) throws Exception {
         Config appConfig = ConfigFactory.parseFile(new File(args[0]))
@@ -20,7 +42,8 @@ public class ExposeSparkDriverConf {
             Config sparkDriverConfig = TypesafeConfigUtils.extractSubConfig(sparkConfig, driverPrefix, false);
             StringBuilder stringBuilder = new StringBuilder();
             for (Map.Entry<String, ConfigValue> entry: sparkDriverConfig.entrySet()) {
-                String conf = String.format(" --driver-%s=%s ", entry.getKey(), entry.getValue().unwrapped());
+                List<String> keys = splitKey(entry.getKey());
+                String conf = String.format(" --driver-%s=%s ", String.join("-", keys), entry.getValue().unwrapped());
                 stringBuilder.append(conf);
             }
 
