2308e581391d99bc05517e33469d2b1677a520c5
garyelephant <garygaowork@gmail.com>
2020-07-01 14:07:51 +0800

    update

--- waterdrop-flink-api/src/main/java/io/github/interestinglab/waterdrop/flink/util/SchemaUtil.java
+++ waterdrop-flink-api/src/main/java/io/github/interestinglab/waterdrop/flink/util/SchemaUtil.java
@@ -71,8 +71,6 @@ public class SchemaUtil {
             case "orc":
                 break;
             case "avro":
-
-
                 break;
             case "parquet":
                 break;
