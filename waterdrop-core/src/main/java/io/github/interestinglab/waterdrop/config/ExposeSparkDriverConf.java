991558f8589e562a94ce17735e63ff5db433bb5b
RickyHuo <huochen1994@163.com>
2020-07-15 19:46:05 +0800

    Remove useless code

--- waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/config/ExposeSparkDriverConf.java
+++ waterdrop-core/src/main/java/io/github/interestinglab/waterdrop/config/ExposeSparkDriverConf.java
@@ -1,34 +1,12 @@
 package io.github.interestinglab.waterdrop.config;
 
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.io.File;
-
-import io.github.interestinglab.waterdrop.config.SparkDriverSettings;
 
 
 public class ExposeSparkDriverConf {
 
-    public static List<String> splitKey(String key) {
-        List<String> keys = new LinkedList<>();
-        int index = 0;
-        for (int i = 0; i < key.length(); i++) {
-            char symbol = key.charAt(i);
-            if (symbol >= 'A' && symbol <= 'Z') {
-                keys.add(key.substring(index, i).toLowerCase());
-                index = i;
-            }
-        }
-
-        keys.add(key.substring(index).toLowerCase());
-
-        if (keys.get(0).equals("extra")) {
-            return keys.subList(1, keys.size());
-        }
-        return keys;
-    }
 
     public static void main(String[] args) throws Exception {
         Config appConfig = ConfigFactory.parseFile(new File(args[0]))
@@ -47,7 +25,7 @@ public class ExposeSparkDriverConf {
                 String key = entry.getKey();
                 SparkDriverSettings settings = SparkDriverSettings.fromProperty(key);
                 if (settings != null) {
-                    String conf = String.format("%s=%s", settings.option, entry.getValue().unwrapped());
+                    String conf = String.format(" %s=%s ", settings.option, entry.getValue().unwrapped());
                     stringBuilder.append(conf);
                 }
             }
