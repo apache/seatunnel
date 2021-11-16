9fe6437e80fe32c60155a0890d0bca6a1b584cd1
CalvinKirs <kirs@apache.org>
2021-11-16 20:00:25 +0800

    Fix dead-link and rename some modules name

---
+++ seatunnel-config/src/main/java/io/github/interestinglab/waterdrop/config/impl/ConfigInt.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.interestinglab.waterdrop.config.impl;
+
+import io.github.interestinglab.waterdrop.config.ConfigOrigin;
+import io.github.interestinglab.waterdrop.config.ConfigValueType;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+final class ConfigInt extends ConfigNumber implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+
+    final private int value;
+
+    ConfigInt(ConfigOrigin origin, int value, String originalText) {
+        super(origin, originalText);
+        this.value = value;
+    }
+
+    @Override
+    public ConfigValueType valueType() {
+        return ConfigValueType.NUMBER;
+    }
+
+    @Override
+    public Integer unwrapped() {
+        return value;
+    }
+
+    @Override
+    String transformToString() {
+        String s = super.transformToString();
+        if (s == null)
+            return Integer.toString(value);
+        else
+            return s;
+    }
+
+    @Override
+    protected long longValue() {
+        return value;
+    }
+
+    @Override
+    protected double doubleValue() {
+        return value;
+    }
+
+    @Override
+    protected ConfigInt newCopy(ConfigOrigin origin) {
+        return new ConfigInt(origin, value, originalText);
+    }
+
+    // serialization all goes through SerializedConfigValue
+    private Object writeReplace() throws ObjectStreamException {
+        return new SerializedConfigValue(this);
+    }
+}
