9e2e836c5af825532efeafb6a1ded941ceead429
CalvinKirs <kirs@apache.org>
2021-11-26 09:57:46 +0800

    test

--- seatunnel-config/src/main/java/io/github/interestinglab/waterdrop/config/impl/AbstractConfigNode.java
+++ seatunnel-config/src/main/java/io/github/interestinglab/waterdrop/config/impl/AbstractConfigNode.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.github.interestinglab.waterdrop.config.impl;
 
 import io.github.interestinglab.waterdrop.config.parser.ConfigNode;
