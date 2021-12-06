604391e1d5c293f0ff60c56b8ef5236268f9ec8a
wuchunfu <319355703@qq.com>
2021-12-06 23:23:09 +0800

    Add plugin-spark-source-redis

---
+++ plugin-spark-source-redis/src/main/scala/io/github/interestinglab/waterdrop/spark/source/Redis.scala
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.interestinglab.waterdrop.spark.source
+
+import com.redislabs.provider.redis.{RedisConfig, RedisEndpoint, toRedisContext}
+import io.github.interestinglab.waterdrop.common.config.CheckResult
+import io.github.interestinglab.waterdrop.spark.SparkEnvironment
+import io.github.interestinglab.waterdrop.spark.batch.SparkBatchSource
+import org.apache.spark.sql.{Dataset, Row}
+
+class Redis extends SparkBatchSource {
+  val defaultPort: Int = 6379
+  val defaultDb: Int = 0
+  val defaultPartition: Int = 3
+
+  override def checkConfig(): CheckResult = {
+    val hasTableName = config.hasPath("result_table_name") || config.hasPath("table_name")
+    val hasRedisHost = config.hasPath("host")
+    val hasKeys = config.hasPath("key_pattern")
+    val hasRedisPassword = config.hasPath("auth")
+
+    config match {
+      case _ if !hasTableName => new CheckResult(false, "please specify [result_table_name] as non-empty string")
+      case _ if !hasRedisHost => new CheckResult(false, "please specify [host] as non-empty string")
+      case _ if !hasRedisPassword => new CheckResult(false, "please specify [auth] as non-empty string")
+      case _ if !hasKeys => new CheckResult(false, "please specify [key_pattern] as non-empty string, multiple key patterns separated by ','")
+      case _ => new CheckResult(true, "")
+    }
+  }
+
+  /**
+   * Parameter item setting
+   *
+   * @param env Spark environment
+   */
+  override def prepare(env: SparkEnvironment): Unit = {
+    super.prepare(env)
+  }
+
+  /**
+   * Read the data in redis and convert it into dataframe
+   *
+   * @param env Spark environment
+   * @return Return the input dataset
+   */
+  override def getData(env: SparkEnvironment): Dataset[Row] = {
+    val spark = env.getSparkSession
+
+    var regTable = ""
+    if (config.hasPath("result_table_name")) {
+      regTable = config.getString("result_table_name")
+    } else {
+      regTable = config.getString("table_name")
+    }
+
+    val auth = config.getString("auth")
+
+    val host = config.getString("host")
+
+    var port = defaultPort
+    if (config.hasPath("port")) {
+      port = config.getInt("port")
+    }
+
+    val keyPattern = config.getString("key_pattern")
+
+    var partition = defaultPartition
+    if (config.hasPath("partition")) {
+      partition = config.getInt("partition")
+    }
+
+    var dbNum = defaultDb
+    if (config.hasPath("db_num")) {
+      dbNum = config.getInt("db_num")
+    }
+
+    // Get data from redis through keys and combine it into a dataset
+    val redisConfig = new RedisConfig(RedisEndpoint(host = host, port = port, auth = auth, dbNum = dbNum))
+    val stringRDD = spark.sparkContext.fromRedisKV(keyPattern, partition)(redisConfig = redisConfig)
+    import spark.implicits._
+    val ds = stringRDD.toDF("raw_key", "raw_message")
+    ds.createOrReplaceTempView(s"$regTable")
+    ds
+  }
+}
