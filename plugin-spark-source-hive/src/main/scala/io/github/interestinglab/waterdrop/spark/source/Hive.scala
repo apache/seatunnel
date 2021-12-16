4bb70ecb3c148e5e0d6b20ccf525305d3079cfd2
huzekang <1040080742@qq.com>
2021-12-16 16:09:25 +0800

    Revert "refactor: change spark hive source  code from scala to java"
    This reverts commit f8cc6eff

---
+++ plugin-spark-source-hive/src/main/scala/io/github/interestinglab/waterdrop/spark/source/Hive.scala
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.interestinglab.waterdrop.spark.source
+
+import io.github.interestinglab.waterdrop.common.config.CheckResult
+import io.github.interestinglab.waterdrop.spark.SparkEnvironment
+import io.github.interestinglab.waterdrop.spark.batch.BaseSparkBatchSource
+import org.apache.spark.sql.{Dataset, Row}
+
+class Hive extends BaseSparkBatchSource {
+
+  override def prepare(env: SparkEnvironment): Unit = {}
+
+  override def checkConfig(): CheckResult = {
+    config.hasPath("pre_sql") match {
+      case true => new CheckResult(true, "")
+      case false => new CheckResult(false, "please specify [pre_sql]")
+    }
+  }
+
+  override def getData(env: SparkEnvironment): Dataset[Row] = {
+    env.getSparkSession.sql(config.getString("pre_sql"))
+  }
+}
