67432f9113df5e3cd8f2dabd46a6d21ef782bc2b
leo65535@163.com <xielei_yewu@cmss.chinamobile.com>
2021-12-20 14:59:36 +0800

    [SeaTunnel #836] [sql] Support set operation statements in sql configuration

--- seatunnel-core/seatunnel-core-sql/src/main/java/io/github/interestinglab/waterdrop/core/sql/job/Executor.java
+++ seatunnel-core/seatunnel-core-sql/src/main/java/io/github/interestinglab/waterdrop/core/sql/job/Executor.java
@@ -18,6 +18,7 @@
 package io.github.interestinglab.waterdrop.core.sql.job;
 
 import io.github.interestinglab.waterdrop.core.sql.splitter.SqlStatementSplitter;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.StatementSet;
@@ -25,6 +26,7 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.api.internal.TableEnvironmentImpl;
 import org.apache.flink.table.operations.CatalogSinkModifyOperation;
 import org.apache.flink.table.operations.Operation;
+import org.apache.flink.table.operations.command.SetOperation;
 
 import java.util.List;
 
@@ -46,17 +48,34 @@ public class Executor {
 
         StatementSet statementSet = tEnv.createStatementSet();
         TableEnvironmentImpl stEnv = (TableEnvironmentImpl) tEnv;
+        Configuration configuration = tEnv.getConfig().getConfiguration();
 
         List<String> stmts = SqlStatementSplitter.normalizeStatements(workFlowContent);
         for (String stmt : stmts) {
             Operation op = stEnv.getParser().parse(stmt).get(0);
             if (op instanceof CatalogSinkModifyOperation) {
                 statementSet.addInsertSql(stmt);
+            } else if (op instanceof SetOperation) {
+                callSetOperation(configuration, (SetOperation) op);
             } else {
                 tEnv.executeSql(stmt);
             }
         }
         return statementSet;
+    }
+
+    private static void callSetOperation(Configuration configuration, SetOperation setOperation) {
+
+        // set property
+        String key = setOperation.getKey()
+            .orElseThrow(() -> new IllegalArgumentException("key can not be empty!"))
+            .trim();
+
+        String value = setOperation.getValue()
+            .orElseThrow(() -> new IllegalArgumentException("value can not be empty!"))
+            .trim();
+
+        configuration.setString(key, value);
     }
 
 }
