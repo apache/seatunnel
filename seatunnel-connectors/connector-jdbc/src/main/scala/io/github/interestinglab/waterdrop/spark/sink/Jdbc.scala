00fe91dc1b405e3db6af25be1b7a274449869075
chouc <chouc.mei@gmail.com>
2021-12-21 09:24:18 +0800

    fix merge error

--- seatunnel-connectors/connector-jdbc/src/main/scala/io/github/interestinglab/waterdrop/spark/sink/Jdbc.scala
+++ seatunnel-connectors/connector-jdbc/src/main/scala/io/github/interestinglab/waterdrop/spark/sink/Jdbc.scala
@@ -16,6 +16,15 @@
  */
 package io.github.interestinglab.waterdrop.spark.sink
 
+import io.github.interestinglab.waterdrop.common.config.CheckResult
+import io.github.interestinglab.waterdrop.config.ConfigFactory
+import io.github.interestinglab.waterdrop.spark.SparkEnvironment
+import io.github.interestinglab.waterdrop.spark.batch.SparkBatchSink
+import org.apache.spark.sql.execution.datasources.jdbc2.JDBCSaveMode
+import org.apache.spark.sql.{Dataset, Row}
+
+import scala.collection.JavaConversions._
+
 class Jdbc extends SparkBatchSink {
 
   override def output(data: Dataset[Row], env: SparkEnvironment): Unit = {
