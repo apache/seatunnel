ada7b2c0e0b79d2af771fd6c87a4759a87641161
xtr_1993 <38655011+xtr1993@users.noreply.github.com>
2021-12-21 21:29:45 +0800

    [Feature][Spark] Doris Uniform parameters with Doris Connector (#845)
    * Doris Uniform parameters
    
    * move header properties to checkConfig
    
    Co-authored-by: xutengrui_1993 <xutr@yoozoo.com>

--- docs/en/configuration/sink-plugins/Doris.md
+++ docs/en/configuration/sink-plugins/Doris.md
@@ -1,60 +1,3 @@
-# Sink plugin: Doris
-
-### Description
-
-Write Rows to a Doris Table.
-
-### Options
-
-| name | type | required | default value | engine |
-| --- | --- | --- | --- | --- |
-| fenodes | string | yes | - | Flink |
-| db_name | string | yes | - | Flink |
-| table_name | string | yes | - | Flink |
-| username	 | string | yes | - | Flink |
-| password	 | string | yes | - | Flink |
-| doris_sink_batch_size	 | int | no |  100 | Flink |
-| doris_sink_interval	 | int | no |1000 | Flink |
-| doris_sink_max_retries	 | int | no | 1 | Flink |
-| doris_sink_properties.*	 | - | no | - | Flink |
-
-##### fenodes [string]
-
-Doris FE http address
-
-##### db_name [string]
-
-Doris database name
-
-##### table_name [string]
-
-Doris table name
-
-##### username [string]
-
-Doris username
-
-##### password [string]
-
-Doris password
-
-##### doris_sink_batch_size [int]
-
-Maximum number of lines in a single write Doris,default value is 100.
-
-##### doris_sink_interval [int]
-
-The flush interval millisecond, after which the asynchronous thread will write the data in the cache to Doris.Set to 0 to turn off periodic writing.
-
-##### doris_sink_max_retries [int]
-
-Number of retries after writing Doris failed
-
-##### doris_sink_properties.* [string]
-
-The doris stream load parameters.eg:doris_sink_properties.column_separator' = ','
-
-=======
 # Sink plugin: Doirs
 
 ### Description:
@@ -63,26 +6,26 @@ Use Spark Batch Engine ETL Data to Doris.
 ### Options
 | name | type | required | default value | engine |
 | --- | --- | --- | --- | --- |
-| host | string | yes | - | Spark |
+| fenodes | string | yes | - | Spark |
 | database | string | yes | - | Spark |
-| tableName	 | string | yes | - | Spark |
+| table	 | string | yes | - | Spark |
 | user	 | string | yes | - | Spark |
 | password	 | string | yes | - | Spark |
-| bulk_size	 | int | yes | - | Spark |
+| batch_size	 | int | yes | 100 | Spark |
 | doris.*	 | string | no | - | Spark |
 
-##### host [string]
+##### fenodes [string]
 Doris FE address:8030
 
 ##### database [string]
 Doris target database name
-##### tableName [string]
+##### table [string]
 Doris target table name
 ##### user [string]
 Doris user name
 ##### password [string]
 Doris user's password
-##### bulk_size [string]
+##### batch_size [string]
 Doris number of submissions per batch
 ##### doris. [string]
 Doris stream_load properties,you can use 'doris.' prefix + stream_load properties
@@ -92,24 +35,15 @@ Doris stream_load properties,you can use 'doris.' prefix + stream_load propertie
 ### Examples
 
 ```
-
-DorisSparkSink {
-	 fenodes = "127.0.0.1:8030"
-	 db_name = database
-	 table_name = table
-	 username = root
-	 password = password
-	 doris_sink_size = 1
-}
-
-DorisFlinkSink {
-      host="0.0.0.0:8030"
-      database="test"
-      tableName="user"
-      user="doris"
-      password="doris"
-      bulk_size=10000
-      doris.column_separator="\t"
-      doris.columns="id,user_name,user_name_cn,create_time,last_login_time"
-}
- ```
+Doris {
+            fenodes="0.0.0.0:8030"
+            database="test"
+            table="user"
+            user="doris"
+            password="doris"
+            batch_size=10000
+            doris.column_separator="\t"
+            doris.columns="id,user_name,user_name_cn,create_time,last_login_time"
+      
+      }
+```
--- seatunnel-connectors/plugin-spark-sink-doris/src/main/scala/io/github/interestinglab/waterdrop/spark/sink/Doris.scala
+++ seatunnel-connectors/plugin-spark-sink-doris/src/main/scala/io/github/interestinglab/waterdrop/spark/sink/Doris.scala
@@ -20,22 +20,24 @@ package io.github.interestinglab.waterdrop.spark.sink
 import io.github.interestinglab.waterdrop.common.config.CheckResult
 import io.github.interestinglab.waterdrop.spark.SparkEnvironment
 import io.github.interestinglab.waterdrop.spark.batch.SparkBatchSink
+import org.apache.log4j.Logger
 import org.apache.spark.sql.{Dataset, Row}
+
 import scala.collection.{JavaConversions, mutable}
 import scala.collection.mutable.ListBuffer
 
 class Doris extends SparkBatchSink with Serializable {
 
   var apiUrl: String = _
+  var batch_size: Int = 100
   var column_separator: String = "\t"
   var propertiesMap = new mutable.HashMap[String,String]()
 
   override def output(data: Dataset[Row], env: SparkEnvironment): Unit = {
-    val bulkSize: Int = config.getInt(Config.BULK_SIZE)
     val user: String = config.getString(Config.USER)
     val password: String = config.getString(Config.PASSWORD)
     if (propertiesMap.contains(Config.COLUMN_SEPARATOR)) {
-     column_separator =  propertiesMap(Config.COLUMN_SEPARATOR)
+      column_separator =  propertiesMap(Config.COLUMN_SEPARATOR)
     }
     val sparkSession = env.getSparkSession
     import sparkSession.implicits._
@@ -47,7 +49,7 @@ class Doris extends SparkBatchSink with Serializable {
       for (message <- partition) {
         count += 1
         buffer += message
-        if (count > bulkSize) {
+        if (count > batch_size) {
           dorisUtil.saveMessages(buffer.mkString("\n"))
           buffer.clear()
           count = 0
@@ -58,7 +60,7 @@ class Doris extends SparkBatchSink with Serializable {
   }
 
   override def checkConfig(): CheckResult = {
-    val requiredOptions = List(Config.HOST, Config.DATABASE, Config.TABLE_NAME,Config.BULK_SIZE,Config.USER,Config.PASSWORD)
+    val requiredOptions = List(Config.HOST, Config.DATABASE, Config.TABLE_NAME,Config.USER,Config.PASSWORD)
     val nonExistsOptions = requiredOptions.map(optionName => (optionName, config.hasPath(optionName))).filter { p =>
       val (optionName, exists) = p
       !exists
@@ -66,32 +68,33 @@ class Doris extends SparkBatchSink with Serializable {
     if (nonExistsOptions.nonEmpty) {
       new CheckResult(false, "Please specify " + nonExistsOptions
         .map { option =>
-            val (name, exists) = option
-            "[" + name + "]"
-          }.mkString(", ") + " as non-empty string"
+          val (name, exists) = option
+          "[" + name + "]"
+        }.mkString(", ") + " as non-empty string"
       )
     } else if (config.hasPath(Config.USER) && !config.hasPath(Config.PASSWORD) || config.hasPath(Config.PASSWORD) && !config.hasPath(Config.USER)) {
       new CheckResult(false, Config.CHECK_USER_ERROR)
-    } else if (config.hasPath(Config.BULK_SIZE) && config.getInt(Config.BULK_SIZE) < 0) {
-      new CheckResult(false,Config.CHECK_INT_ERROR)
     } else {
       val host: String = config.getString(Config.HOST)
       val dataBase: String = config.getString(Config.DATABASE)
       val tableName: String = config.getString(Config.TABLE_NAME)
       this.apiUrl = s"http://$host/api/$dataBase/$tableName/_stream_load"
+      val httpConfig = JavaConversions.asScalaSet(config.entrySet()).filter(x => x.getKey.startsWith(Config.ARGS_PREFIX))
+      if (httpConfig.nonEmpty) {
+        httpConfig.foreach(tuple => {
+          val split = tuple.getKey.split(".")
+          if (split.size == 2) {
+            propertiesMap.put(split(1),tuple.getValue.render())
+          }
+        })
+      }
       new CheckResult(true,Config.CHECK_SUCCESS)
     }
   }
 
   override def prepare(prepareEnv: SparkEnvironment): Unit = {
-    val httpConfig = JavaConversions.asScalaSet(config.entrySet()).filter(x => x.getKey.startsWith(Config.ARGS_PREFIX))
-    if (httpConfig.nonEmpty) {
-      httpConfig.foreach(tuple => {
-        val split = tuple.getKey.split(".")
-        if (split.size == 2) {
-          propertiesMap += (split(0) -> tuple.getValue.render())
-        }
-      })
+    if (config.hasPath(Config.BULK_SIZE) && config.getInt(Config.BULK_SIZE) > 0) {
+      batch_size = config.getInt(Config.BULK_SIZE)
     }
   }
 }
