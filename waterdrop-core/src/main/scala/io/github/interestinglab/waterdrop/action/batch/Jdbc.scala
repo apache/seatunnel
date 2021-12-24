57431ced0750f212f2617f9740049964b80fbd09
wunan <wunan200609@126.com>
2021-12-24 18:01:12 +0800

    add jdbc action class

---
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/action/batch/Jdbc.scala
@@ -0,0 +1,67 @@
+package io.github.interestinglab.waterdrop.action.batch
+
+import io.github.interestinglab.waterdrop.apis.BaseAction
+import io.github.interestinglab.waterdrop.config.{Config, ConfigFactory}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+ * Jdbc Action is able to specify driver class while Mysql Output's driver is bound to com.mysql.jdbc.Driver etc.
+ * When using Jdbc Action, class of jdbc driver must can be found in classpath.
+ * Jdbc Action supports at least: MySQL, Oracle, PostgreSQL, SQLite
+
+ * */
+class Jdbc extends BaseAction {
+
+  var firstProcess = true
+  var config: Config = ConfigFactory.empty()
+
+  /**
+   * Set Config.
+   * */
+  override def setConfig(config: Config): Unit = {
+    this.config = config
+  }
+
+  /**
+   * Get Config.
+   * */
+  override def getConfig(): Config = {
+    this.config
+  }
+
+  override def checkConfig(): (Boolean, String) = {
+
+    // TODO: are user, password required ?
+    val requiredOptions = List("driver", "url", "table", "user", "password");
+    val nonExistsOptions = requiredOptions.map(optionName => (optionName, config.hasPath(optionName))).filter { p =>
+      val (optionName, exists) = p
+      !exists
+    }
+
+    if (nonExistsOptions.length == 0) {
+      val saveModeAllowedValues = List("overwrite", "append", "ignore", "error");
+      if (!config.hasPath("save_mode") || saveModeAllowedValues.contains(config.getString("save_mode"))) {
+        (true, "")
+      } else {
+        (false, "wrong value of [save_mode], allowed values: " + saveModeAllowedValues.mkString(", "))
+      }
+
+    } else {
+      (
+        false,
+        "please specify " + nonExistsOptions
+          .map { option =>
+            val (name, exists) = option
+            "[" + name + "]"
+          }
+          .mkString(", ") + " as non-empty string"
+      )
+    }
+  }
+
+  override def onExecutionStarted(sparkSession: SparkSession, sparkConf: SparkConf, config: Config): Unit = ???
+
+  override def onExecutionFinished(sparkConf: SparkConf, config: Config): Unit = ???
+
+}
