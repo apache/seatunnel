a92e19b1d0f636d18b9bdb71ce1cb067c8dd1a33
wunan <wunan200609@126.com>
2021-12-26 18:00:24 +0800

    jdbc action is ok

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/action/batch/Jdbc.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/action/batch/Jdbc.scala
@@ -1,15 +1,20 @@
 package io.github.interestinglab.waterdrop.action.batch
 
-import io.github.interestinglab.waterdrop.apis.BaseAction
+import java.sql.{Connection, DriverManager}
+
 import io.github.interestinglab.waterdrop.config.{Config, ConfigFactory}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import io.github.interestinglab.waterdrop.apis.BaseAction
+
+import scala.collection.JavaConverters._
+
 
 /**
  * Jdbc Action is able to specify driver class while Mysql Output's driver is bound to com.mysql.jdbc.Driver etc.
  * When using Jdbc Action, class of jdbc driver must can be found in classpath.
  * Jdbc Action supports at least: MySQL, Oracle, PostgreSQL, SQLite
-
+ * the sql executed can not return anything
  * */
 class Jdbc extends BaseAction {
 
@@ -52,11 +57,35 @@ class Jdbc extends BaseAction {
   }
 
   override def onExecutionStarted(sparkSession: SparkSession, sparkConf: SparkConf, config: Config): Unit = {
-    /*"beforeActions", "afterActions"*/
+    executeSql("beforeActions")
   }
 
   override def onExecutionFinished(sparkConf: SparkConf, config: Config): Unit = {
-    /*"beforeActions", "afterActions"*/
+    executeSql("afterActions")
   }
 
+  def getConnection(driver: String, url: String, user: String, password: String) : Connection = {
+    Class.forName(driver)
+    DriverManager.getConnection(url, user, password)
+  }
+
+  def executeSql(executeType: String): Unit = {
+    val configList = config.getList(executeType)
+    if (configList.size() > 0) {
+      val dataBaseConfig = getDataBaseConfig();
+      val connection = getConnection(dataBaseConfig._1, dataBaseConfig._2, dataBaseConfig._3, dataBaseConfig._4)
+      val statement = connection.createStatement()
+      configList.asScala.foreach(sql => statement.execute(sql.unwrapped().toString))
+      statement.close()
+      connection.close()
+    }
+  }
+
+  def getDataBaseConfig(): (String, String, String, String) = {
+    val driver = config.getString("driver")
+    val url = config.getString("url")
+    val user = config.getString("user")
+    val password = config.getString("password")
+    (driver, url, user, password)
+  }
 }
