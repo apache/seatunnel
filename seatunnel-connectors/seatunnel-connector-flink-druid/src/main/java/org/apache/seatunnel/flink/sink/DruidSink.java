49d77f55c03ba3c3519bb3a0bbcf8df579418198
Benedict Jin <asdf2014@apache.org>
2022-01-10 20:07:34 +0800

    [SeaTunnel#969]Add Druid Flink Sink plugin (#970)
    * Add Druid Flink Sink plugin
    
    * Fix conflicts and improve doc
    
    * Use shaded Config instead

---
+++ seatunnel-connectors/seatunnel-connector-flink-druid/src/main/java/org/apache/seatunnel/flink/sink/DruidSink.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.flink.sink;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.operators.DataSink;
+import org.apache.flink.types.Row;
+import org.apache.seatunnel.common.config.CheckConfigUtil;
+import org.apache.seatunnel.common.config.CheckResult;
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
+import org.apache.seatunnel.flink.FlinkEnvironment;
+import org.apache.seatunnel.flink.batch.FlinkBatchSink;
+
+public class DruidSink implements FlinkBatchSink<Row, Row> {
+
+    private static final String COORDINATOR_URL = "coordinator_url";
+    private static final String DATASOURCE = "datasource";
+
+    private Config config;
+    private String coordinatorURL;
+    private String datasource;
+
+    @Override
+    public DataSink<Row> outputBatch(FlinkEnvironment env, DataSet<Row> dataSet) {
+        return dataSet.output(new DruidOutputFormat(coordinatorURL, datasource));
+    }
+
+    @Override
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    @Override
+    public Config getConfig() {
+        return config;
+    }
+
+    @Override
+    public CheckResult checkConfig() {
+        return CheckConfigUtil.check(config, COORDINATOR_URL, DATASOURCE);
+    }
+
+    @Override
+    public void prepare(FlinkEnvironment env) {
+        this.coordinatorURL = config.getString(COORDINATOR_URL);
+        this.datasource = config.getString(DATASOURCE);
+    }
+}
