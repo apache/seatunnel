1d0b1bc11993f2ed3691d6a60b92d862b015a433
Zonglei Dong <dongzonglei@apache.org>
2022-01-10 22:53:45 +0800

    [Feature][plugin-filter-date] Date filter supports Timestamp. (#973)
    * [Feature][plugin-filter-date] Date filter supports Timestamp.
    
    * [Feature][plugin-filter-date] Date filter supports Timestamp.
    
    * [Feature][plugin-filter-date] Fixes date filter Timestamp format.
    
    * [Feature][plugin-filter-date] Fixes input Timestamp format.
    
    * [Feature][plugin-filter-date] Fixes input Timestamp format.

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Date.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Date.scala
@@ -22,7 +22,7 @@ package io.github.interestinglab.waterdrop.filter
 import io.github.interestinglab.waterdrop.config.{Config, ConfigFactory}
 import io.github.interestinglab.waterdrop.apis.BaseFilter
 import io.github.interestinglab.waterdrop.core.RowConstant
-import io.github.interestinglab.waterdrop.utils.{FormatParser, StringTemplate, UnixMSParser, UnixParser}
+import io.github.interestinglab.waterdrop.utils.{FormatParser, StringTemplate, UnixMSParser, UnixParser, TimestampParser}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
@@ -77,6 +77,7 @@ class Date extends BaseFilter {
     val dateParser = config.getString("source_time_format") match {
       case "UNIX" => new UnixParser(targetTimeFormat)
       case "UNIX_MS" => new UnixMSParser(targetTimeFormat)
+      case "TIMESTAMP" => new TimestampParser(targetTimeFormat)
       case sourceTimeFormat: String => new FormatParser(sourceTimeFormat, targetTimeFormat)
     }
 
