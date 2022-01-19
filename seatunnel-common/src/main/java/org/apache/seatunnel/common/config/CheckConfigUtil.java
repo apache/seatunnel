7717ab2547691eadf0baa9ce8cdbbb8a65e7ba08
MrLiu <1340877767@qq.com>
2022-01-19 11:44:09 +0800

    [SeaTunnel #967][seatunnel-connector] Add neo4j spark source  (#1028)
    * feat(plugin): add neo4j source
    
    * fix(plugin): replace all _
    
    * doc(plugin): change neo4j doc title
    
    * chore(plugin): change build and error message
    
    * chore(plugin): change CheckResult tool and pom
    
    * style(plugin): change CheckConfigUtil code style
    
    * doc(plugin): change neo4j doc
    
    * doc(plugin): change neo4j doc
    
    * chore(plugin): change neo4j config style
    
    * chore(plugin): change neo4j config
    
    Co-authored-by: MrLiu <liuwensheng@linklogis.com>

--- seatunnel-common/src/main/java/org/apache/seatunnel/common/config/CheckConfigUtil.java
+++ seatunnel-common/src/main/java/org/apache/seatunnel/common/config/CheckConfigUtil.java
@@ -21,6 +21,11 @@ import org.apache.seatunnel.shade.com.typesafe.config.Config;
 
 import static org.apache.seatunnel.common.Constants.CHECK_SUCCESS;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CheckConfigUtil {
 
     public static CheckResult check(Config config, String... params) {
@@ -38,5 +43,44 @@ public class CheckConfigUtil {
         } else {
             return new CheckResult(true, CHECK_SUCCESS);
         }
+    }
+
+    /**
+     * check config if there was at least one usable
+     */
+    public static CheckResult checkOne(Config config, String... params) {
+        if (params.length == 0) {
+            return new CheckResult(true, "");
+        }
+
+        List<String> missingParams = new LinkedList();
+        for (String param : params) {
+            if (!config.hasPath(param) || config.getAnyRef(param) == null) {
+                missingParams.add(param);
+            }
+        }
+
+        if (missingParams.size() == params.length) {
+            String errorMsg = String.format("please specify at least one config of [%s] as non-empty",
+                    missingParams.stream().collect(Collectors.joining(",")));
+            return new CheckResult(false, errorMsg);
+        } else {
+            return new CheckResult(true, CHECK_SUCCESS);
+        }
+    }
+
+    /**
+     * merge all check result
+     */
+    public static CheckResult mergeCheckMessage(CheckResult... checkResults) {
+        List<String> list = new LinkedList<>();
+        List<CheckResult> notPassConfig = Arrays.stream(checkResults).filter(item -> !item.isSuccess()).collect(Collectors.toList());
+        if (notPassConfig.isEmpty()) {
+            return new CheckResult(true, CHECK_SUCCESS);
+        } else {
+            String errMessage = notPassConfig.stream().map(it -> it.getMsg()).collect(Collectors.joining(","));
+            return new CheckResult(false, errMessage);
+        }
+
     }
 }
