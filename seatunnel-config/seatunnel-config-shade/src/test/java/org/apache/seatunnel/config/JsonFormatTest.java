97d054fee38fdf2be1caffd821a5f371c001d5cd
simon <zhangshiming@cvte.com>
2022-03-18 09:24:29 +0800

    json

---
+++ seatunnel-config/seatunnel-config-shade/src/test/java/org/apache/seatunnel/config/JsonFormatTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.config;
+
+import org.apache.seatunnel.config.utils.FileUtils;
+
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigFactory;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigRenderOptions;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigResolveOptions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class JsonFormatTest {
+
+    @Test
+    public void testJsonFormat() {
+        File fileFromResources = FileUtils.getFileFromResources("seatunnel/spark.batch.json");
+        StringBuilder jsonString = new StringBuilder();
+        try {
+            InputStream inputStream = new FileInputStream(fileFromResources);
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonString.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Config config = ConfigFactory
+                .parseFile(FileUtils.getFileFromResources("seatunnel/spark.batch.json"))
+                .resolveWith(ConfigFactory.systemProperties(),
+                        ConfigResolveOptions.defaults().setAllowUnresolved(true));
+
+        ConfigRenderOptions options = ConfigRenderOptions.concise().setFormatted(true);
+        String configString = config.root().render(options).replaceAll(" ", "");
+        Assert.assertEquals(configString, jsonString.toString().replaceAll(" ", ""));
+
+    }
+
+}
