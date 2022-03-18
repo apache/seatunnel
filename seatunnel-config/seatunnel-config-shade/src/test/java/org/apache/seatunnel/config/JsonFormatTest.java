90e535216fb1f28faa0dceeb3a15824c3d6cf1f4
simon <zhangshiming@cvte.com>
2022-03-18 10:32:11 +0800

    fix

--- seatunnel-config/seatunnel-config-shade/src/test/java/org/apache/seatunnel/config/JsonFormatTest.java
+++ seatunnel-config/seatunnel-config-shade/src/test/java/org/apache/seatunnel/config/JsonFormatTest.java
@@ -21,44 +21,30 @@ import org.apache.seatunnel.config.utils.FileUtils;
 
 import org.apache.seatunnel.shade.com.typesafe.config.Config;
 import org.apache.seatunnel.shade.com.typesafe.config.ConfigFactory;
-import org.apache.seatunnel.shade.com.typesafe.config.ConfigRenderOptions;
 import org.apache.seatunnel.shade.com.typesafe.config.ConfigResolveOptions;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class JsonFormatTest {
 
     @Test
     public void testJsonFormat() {
-        File file = FileUtils.getFileFromResources("seatunnel/spark.batch.json");
-        StringBuilder jsonString = new StringBuilder();
-        try {
-            InputStream inputStream = new FileInputStream(file);
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while ((line = br.readLine()) != null) {
-                jsonString.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        Config config = ConfigFactory
-                .parseFile(file)
+        Config json = ConfigFactory
+                .parseFile(FileUtils.getFileFromResources("json/spark.batch.json"))
                 .resolveWith(ConfigFactory.systemProperties(),
                         ConfigResolveOptions.defaults().setAllowUnresolved(true));
 
-        ConfigRenderOptions options = ConfigRenderOptions.concise().setFormatted(true);
-        String configString = config.root().render(options).replaceAll(" ", "");
-        Assert.assertEquals(configString, jsonString.toString().replaceAll(" ", ""));
+        Config config = ConfigFactory
+                .parseFile(FileUtils.getFileFromResources("json/spark.batch.conf"))
+                .resolveWith(ConfigFactory.systemProperties(),
+                        ConfigResolveOptions.defaults().setAllowUnresolved(true));
+
+        Assert.assertEquals(config.atPath("transform"), json.atPath("transform"));
+        Assert.assertEquals(config.atPath("sink"), json.atPath("sink"));
+        Assert.assertEquals(config.atPath("source"), json.atPath("source"));
+        Assert.assertEquals(config.atPath("env"), json.atPath("env"));
 
     }
 
