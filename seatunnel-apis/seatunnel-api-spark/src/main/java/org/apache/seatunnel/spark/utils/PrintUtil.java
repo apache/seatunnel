b34bccedbd0a52cd1131d29ac5fb9669895584b5
yuangjiang <759874125@qq.com>
2022-04-16 10:08:22 +0800

    code formated

--- seatunnel-apis/seatunnel-api-spark/src/main/java/org/apache/seatunnel/spark/utils/PrintUtil.java
+++ seatunnel-apis/seatunnel-api-spark/src/main/java/org/apache/seatunnel/spark/utils/PrintUtil.java
@@ -17,12 +17,13 @@
 
 package org.apache.seatunnel.spark.utils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PrintUtil {
     private static final Logger LOG = LoggerFactory.getLogger(PrintUtil.class);
@@ -30,23 +31,22 @@ public class PrintUtil {
     public static void printResult(Map<String, Object> result) {
         List<String> names = new ArrayList<>();
         List<String> values = new ArrayList<>();
-        result.forEach(
-                (name, val) -> {
-                    names.add(name);
-                    values.add(String.valueOf(val));
-                });
+        result.forEach((name, val) -> {
+            names.add(name);
+            values.add(String.valueOf(val));
+        });
 
         int maxLength = 0;
         for (String name : names) {
             maxLength = Math.max(maxLength, name.length());
         }
-        maxLength += 5;
-
-        StringBuilder builder = new StringBuilder(128);
+        final int length = 5;
+        maxLength = maxLength + length;
+        StringBuilder builder = new StringBuilder();
         builder.append("\n**********************Reports***********************\n");
         for (int i = 0; i < names.size(); i++) {
             String name = names.get(i);
-            builder.append(name + StringUtils.repeat(" ", maxLength - name.length()));
+            builder.append(name).append(StringUtils.repeat(" ", maxLength - name.length()));
             builder.append("|  ").append(values.get(i));
 
             if (i + 1 < names.size()) {
