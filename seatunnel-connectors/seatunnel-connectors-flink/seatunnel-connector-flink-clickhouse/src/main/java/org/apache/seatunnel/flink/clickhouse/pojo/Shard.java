e0b5e3eeb3b41abd9d094b21c630d7f36d4e48f1
ruanwenjun <wenjun@apache.org>
2022-04-16 21:30:40 +0800

    fix code style

--- seatunnel-connectors/seatunnel-connectors-flink/seatunnel-connector-flink-clickhouse/src/main/java/org/apache/seatunnel/flink/clickhouse/pojo/Shard.java
+++ seatunnel-connectors/seatunnel-connectors-flink/seatunnel-connector-flink-clickhouse/src/main/java/org/apache/seatunnel/flink/clickhouse/pojo/Shard.java
@@ -19,7 +19,7 @@ package org.apache.seatunnel.flink.clickhouse.pojo;
 
 import java.io.Serializable;
 
-public class Shard implements Serializable,Comparable<Shard> {
+public class Shard implements Serializable {
     private static final long serialVersionUID = -1L;
 
     private final int shardNum;
@@ -128,10 +128,5 @@ public class Shard implements Serializable,Comparable<Shard> {
         result = 31 * result + database.hashCode();
         hashCode = result;
         return hashCode;
-    }
-
-    @Override
-    public int compareTo(Shard o) {
-        return o.shardNum - this.shardNum;
     }
 }
