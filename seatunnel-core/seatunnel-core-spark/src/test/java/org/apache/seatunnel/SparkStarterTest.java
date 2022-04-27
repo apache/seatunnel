da7131eb4e20d4011e17ede9103cbcb9b0a7d2e8
ruanwenjun <wenjun@apache.org>
2022-04-27 11:22:18 +0800

    add license header

--- seatunnel-core/seatunnel-core-spark/src/test/java/org/apache/seatunnel/SparkStarterTest.java
+++ seatunnel-core/seatunnel-core-spark/src/test/java/org/apache/seatunnel/SparkStarterTest.java
@@ -19,13 +19,10 @@ package org.apache.seatunnel;
 
 import static org.junit.Assert.assertEquals;
 
-import com.beust.jcommander.JCommander;
-import org.apache.seatunnel.command.SparkCommandArgs;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Constructor;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
@@ -39,28 +36,5 @@ public class SparkStarterTest {
         Map<String, String> sparkConf = SparkStarter.getSparkConf(file);
         assertEquals("SeaTunnel", sparkConf.get("spark.app.name"));
         assertEquals("1", sparkConf.get("spark.executor.cores"));
-    }
-    @Test
-    public void testChangeFileLocation() throws Exception {
-        URI uri = ClassLoader.getSystemResource("spark_application.conf").toURI();
-        String file = new File(uri).toString();
-        String[] args = {"--master" , "yarn" , "--deploy-mode" , "cluster" ,"-c" ,file};
-        Class SparkStarterClass =  Class.forName ("org.apache.seatunnel.SparkStarter");
-        Constructor con = SparkStarterClass.getDeclaredConstructor(String[].class,SparkCommandArgs.class);
-        con.setAccessible(true);
-        SparkCommandArgs commandArgs = new SparkCommandArgs();
-        JCommander.newBuilder()
-                .programName("start-seatunnel-spark.sh")
-                .addObject(commandArgs)
-                .args(args)
-                .build();
-        SparkStarter starter = (SparkStarter) con.newInstance(args,commandArgs);
-        starter.changeFileLocation();
-        String[] finalArgs = starter.args;
-        for (int i = 1; i < finalArgs.length; i++) {
-            if ("-c".equals(finalArgs[i - 1]) || "--config".equals(finalArgs[i - 1])) {
-                assertEquals("spark_application.conf",finalArgs[i]);
-            }
-        }
     }
 }
