5e9575c30f04defba9108e6e3d266bfcf7ae283f
fanjia <1095948736@qq.com>
2022-04-27 14:46:27 +0800

    fix SparkStarter getConfigFile error

--- seatunnel-core/seatunnel-core-spark/src/main/java/org/apache/seatunnel/SparkStarter.java
+++ seatunnel-core/seatunnel-core-spark/src/main/java/org/apache/seatunnel/SparkStarter.java
@@ -127,10 +127,10 @@ public class SparkStarter implements Starter {
     private static SparkCommandArgs parseCommandArgs(String[] args) {
         SparkCommandArgs commandArgs = new SparkCommandArgs();
         JCommander commander = JCommander.newBuilder()
-            .programName("start-seatunnel-spark.sh")
-            .addObject(commandArgs)
-            .args(args)
-            .build();
+                .programName("start-seatunnel-spark.sh")
+                .addObject(commandArgs)
+                .args(args)
+                .build();
         if (commandArgs.isHelp()) {
             commander.usage();
             System.exit(USAGE_EXIT_CODE);
@@ -154,19 +154,19 @@ public class SparkStarter implements Starter {
      */
     private void setSparkConf() throws FileNotFoundException {
         commandArgs.getVariables()
-            .stream()
-            .filter(Objects::nonNull)
-            .map(variable -> variable.split("=", 2))
-            .filter(pair -> pair.length == 2)
-            .forEach(pair -> System.setProperty(pair[0], pair[1]));
+                .stream()
+                .filter(Objects::nonNull)
+                .map(variable -> variable.split("=", 2))
+                .filter(pair -> pair.length == 2)
+                .forEach(pair -> System.setProperty(pair[0], pair[1]));
         this.sparkConf = getSparkConf(commandArgs.getConfigFile());
         String driverJavaOpts = this.sparkConf.get("spark.driver.extraJavaOptions");
         String executorJavaOpts = this.sparkConf.get("spark.executor.extraJavaOptions");
         if (!commandArgs.getVariables().isEmpty()) {
             String properties = commandArgs.getVariables()
-                .stream()
-                .map(v -> "-D" + v)
-                .collect(Collectors.joining(" "));
+                    .stream()
+                    .map(v -> "-D" + v)
+                    .collect(Collectors.joining(" "));
             driverJavaOpts += " " + properties;
             executorJavaOpts += " " + properties;
             this.sparkConf.put("spark.driver.extraJavaOptions", driverJavaOpts);
@@ -183,13 +183,13 @@ public class SparkStarter implements Starter {
             throw new FileNotFoundException("config file '" + file + "' does not exists!");
         }
         Config appConfig = ConfigFactory.parseFile(file)
-            .resolve(ConfigResolveOptions.defaults().setAllowUnresolved(true))
-            .resolveWith(ConfigFactory.systemProperties(), ConfigResolveOptions.defaults().setAllowUnresolved(true));
+                .resolve(ConfigResolveOptions.defaults().setAllowUnresolved(true))
+                .resolveWith(ConfigFactory.systemProperties(), ConfigResolveOptions.defaults().setAllowUnresolved(true));
 
         return appConfig.getConfig("env")
-            .entrySet()
-            .stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().unwrapped().toString()));
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().unwrapped().toString()));
     }
 
     /**
@@ -202,10 +202,10 @@ public class SparkStarter implements Starter {
         }
         try (Stream<Path> stream = Files.walk(pluginRootDir, PLUGIN_LIB_DIR_DEPTH, FOLLOW_LINKS)) {
             return stream
-                .filter(it -> pluginRootDir.relativize(it).getNameCount() == PLUGIN_LIB_DIR_DEPTH)
-                .filter(it -> it.getParent().endsWith("lib"))
-                .filter(it -> it.getFileName().endsWith("jar"))
-                .collect(Collectors.toList());
+                    .filter(it -> pluginRootDir.relativize(it).getNameCount() == PLUGIN_LIB_DIR_DEPTH)
+                    .filter(it -> it.getParent().endsWith("lib"))
+                    .filter(it -> it.getFileName().endsWith("jar"))
+                    .collect(Collectors.toList());
         }
     }
 
@@ -217,7 +217,7 @@ public class SparkStarter implements Starter {
         if (!Files.exists(pluginRootDir) || !Files.isDirectory(pluginRootDir)) {
             return Collections.emptyList();
         }
-        Config config = new ConfigBuilder<>(commandArgs.getConfigFile(), EngineType.SPARK).getConfig();
+        Config config = new ConfigBuilder<>(Paths.get(commandArgs.getConfigFile()), EngineType.SPARK).getConfig();
         PluginFactory<RuntimeEnv> pluginFactory = new PluginFactory<>(config, EngineType.SPARK);
         return pluginFactory.getPluginJarPaths().stream().map(url -> new File(url.getPath()).toPath()).collect(Collectors.toList());
     }
@@ -280,8 +280,8 @@ public class SparkStarter implements Starter {
     protected void appendPaths(List<String> commands, String option, List<Path> paths) {
         if (!paths.isEmpty()) {
             String values = paths.stream()
-                .map(Path::toString)
-                .collect(Collectors.joining(","));
+                    .map(Path::toString)
+                    .collect(Collectors.joining(","));
             appendOption(commands, option, values);
         }
     }
