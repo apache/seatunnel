33f2ecb3559c1d92ee08a7ab19eb46a518e1cb3f
Wenjun Ruan <wenjun@apache.org>
2022-04-28 14:26:58 +0800

    Remove connector listing (#1765)

--- seatunnel-e2e/seatunnel-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/SparkContainer.java
+++ seatunnel-e2e/seatunnel-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/SparkContainer.java
@@ -156,7 +156,7 @@ public abstract class SparkContainer {
 
     private List<File> getConnectorJarFiles() {
         File jars = new File(PROJECT_ROOT_PATH +
-            "/seatunnel-connectors/seatunnel-connectors-spark-list/seatunnel-connectors-spark-list-current/target/lib");
+            "/seatunnel-connectors/seatunnel-connectors-spark-dist/target/lib");
         return Arrays.stream(
                 Objects.requireNonNull(
                     jars.listFiles(
