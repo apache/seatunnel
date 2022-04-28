73a6124725700d764bee075e6731b0d78a2bedff
wanghuan2054 <499218471@qq.com>
2022-04-28 13:48:01 +0800

    [Feature][seatunnel-transforms] Add Replace transforms for flink #1759

--- seatunnel-transforms/seatunnel-transforms-flink/seatunnel-transform-flink-replace/src/test/java/org/apache/seatunnel/flink/transform/TestReplace.java
+++ seatunnel-transforms/seatunnel-transforms-flink/seatunnel-transform-flink-replace/src/test/java/org/apache/seatunnel/flink/transform/TestReplace.java
@@ -15,33 +15,33 @@ public class TestReplace {
     public void testExample01() {
         String input = "Tom's phone number is 123456789 , he's age is 24";
         String output = "Tom's phone number is * , he's age is 24";
-        Assert.assertEquals("期望输出值和实际输出值不一致", output, new ScalarReplace("\\d+", "*", true, true).eval(input));
+        Assert.assertEquals("The expected output is inconsistent with the actual output", output, new ScalarReplace("\\d+", "*", true, true).eval(input));
 
     }
 
     /**
-     *   Replace the string that all regular expression matches
-     *   Input : Tom's phone number is 123456789 , he's age is 24
-     *   Output : Tom's phone number is * , he's age is *
+     * Replace the string that all regular expression matches
+     * Input : Tom's phone number is 123456789 , he's age is 24
+     * Output : Tom's phone number is * , he's age is *
      */
     @Test
     public void testExample02() {
         String input = "Tom's phone number is 123456789 , he's age is 24";
         String output = "Tom's phone number is * , he's age is *";
-        Assert.assertEquals("期望输出值和实际输出值不一致", output, new ScalarReplace("\\d+", "*", true, false).eval(input));
+        Assert.assertEquals("The expected output is inconsistent with the actual output", output, new ScalarReplace("\\d+", "*", true, false).eval(input));
 
     }
 
     /**
-     *   Replace the string that all regular expression matches
-     *   Input : Tom's phone number is 123456789 , he's age is 24
-     *   Output : Tom is phone number is 123456789 , he is age is 24
+     * Replace the string that all regular expression matches
+     * Input : Tom's phone number is 123456789 , he's age is 24
+     * Output : Tom is phone number is 123456789 , he is age is 24
      */
     @Test
     public void testExample03() {
         String input = "Tom's phone number is 123456789 , he's age is 24";
         String output = "Tom is phone number is 123456789 , he is age is 24";
-        Assert.assertEquals("期望输出值和实际输出值不一致", output, new ScalarReplace("'s", " is", false, false).eval(input));
+        Assert.assertEquals("The expected output is inconsistent with the actual output", output, new ScalarReplace("'s", " is", false, false).eval(input));
 
     }
 
