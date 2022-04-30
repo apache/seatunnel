dce221b8a259cbd6df035be4a82c71d15533c2bd
gleiyu <gleiyu@sina.cn>
2022-04-30 23:49:37 +0800

    get fields from jdbc meta data

--- seatunnel-connectors/seatunnel-connectors-flink/seatunnel-connector-flink-jdbc/src/main/java/org/apache/seatunnel/flink/jdbc/source/JdbcSource.java
+++ seatunnel-connectors/seatunnel-connectors-flink/seatunnel-connector-flink-jdbc/src/main/java/org/apache/seatunnel/flink/jdbc/source/JdbcSource.java
@@ -17,55 +17,30 @@
 
 package org.apache.seatunnel.flink.jdbc.source;
 
-import static org.apache.seatunnel.flink.jdbc.Config.DRIVER;
-import static org.apache.seatunnel.flink.jdbc.Config.PARALLELISM;
-import static org.apache.seatunnel.flink.jdbc.Config.PARTITION_COLUMN;
-import static org.apache.seatunnel.flink.jdbc.Config.PARTITION_LOWER_BOUND;
-import static org.apache.seatunnel.flink.jdbc.Config.PARTITION_UPPER_BOUND;
-import static org.apache.seatunnel.flink.jdbc.Config.PASSWORD;
-import static org.apache.seatunnel.flink.jdbc.Config.QUERY;
-import static org.apache.seatunnel.flink.jdbc.Config.SOURCE_FETCH_SIZE;
-import static org.apache.seatunnel.flink.jdbc.Config.URL;
-import static org.apache.seatunnel.flink.jdbc.Config.USERNAME;
-import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.BIG_INT_TYPE_INFO;
-import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.INT_TYPE_INFO;
-import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.LONG_TYPE_INFO;
-import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.SHORT_TYPE_INFO;
-
-import org.apache.seatunnel.common.config.CheckConfigUtil;
-import org.apache.seatunnel.common.config.CheckResult;
-import org.apache.seatunnel.flink.FlinkEnvironment;
-import org.apache.seatunnel.flink.batch.FlinkBatchSource;
-import org.apache.seatunnel.flink.jdbc.input.DefaultTypeInformationMap;
-import org.apache.seatunnel.flink.jdbc.input.JdbcInputFormat;
-import org.apache.seatunnel.flink.jdbc.input.MysqlTypeInformationMap;
-import org.apache.seatunnel.flink.jdbc.input.PostgresTypeInformationMap;
-import org.apache.seatunnel.flink.jdbc.input.TypeInformationMap;
-
-import org.apache.seatunnel.shade.com.typesafe.config.Config;
-
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.operators.DataSource;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.connector.jdbc.split.JdbcNumericBetweenParametersProvider;
 import org.apache.flink.connector.jdbc.split.JdbcParameterValuesProvider;
 import org.apache.flink.types.Row;
+import org.apache.seatunnel.common.config.CheckConfigUtil;
+import org.apache.seatunnel.common.config.CheckResult;
+import org.apache.seatunnel.flink.FlinkEnvironment;
+import org.apache.seatunnel.flink.batch.FlinkBatchSource;
+import org.apache.seatunnel.flink.jdbc.input.*;
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.*;
+import static org.apache.seatunnel.flink.jdbc.Config.*;
 
 public class JdbcSource implements FlinkBatchSource {
 
@@ -74,14 +49,9 @@ public class JdbcSource implements FlinkBatchSource {
     private static final int DEFAULT_FETCH_SIZE = 10000;
 
     private Config config;
-    private String tableName;
-    private String driverName;
-    private String dbUrl;
-    private String username;
     private String password;
     private int fetchSize = DEFAULT_FETCH_SIZE;
     private int parallelism = -1;
-    private Set<String> fields;
     private Map<String, TypeInformation<?>> tableFieldInfo;
 
     private static final Pattern COMPILE = Pattern.compile("[\\s]*select[\\s]*(.*)from[\\s]*([\\S]+)(.*)",
@@ -115,13 +85,11 @@ public class JdbcSource implements FlinkBatchSource {
 
     @Override
     public void prepare(FlinkEnvironment env) {
-        driverName = config.getString(DRIVER);
-        dbUrl = config.getString(URL);
-        username = config.getString(USERNAME);
+        String driverName = config.getString(DRIVER);
+        String dbUrl = config.getString(URL);
+        String username = config.getString(USERNAME);
         String query = config.getString(QUERY);
-        Tuple2<String, Set<String>> tableNameAndFields = getTableNameAndFields(COMPILE, query);
-        tableName = tableNameAndFields.f0;
-        fields = tableNameAndFields.f1;
+
         if (config.hasPath(PASSWORD)) {
             password = config.getString(PASSWORD);
         }
@@ -140,26 +108,26 @@ public class JdbcSource implements FlinkBatchSource {
         }
 
         try (Connection connection = DriverManager.getConnection(dbUrl, username, password)) {
-            tableFieldInfo = initTableField(connection);
+            tableFieldInfo = initTableField(connection, query);
             RowTypeInfo rowTypeInfo = getRowTypeInfo();
             JdbcInputFormat.JdbcInputFormatBuilder builder = JdbcInputFormat.buildFlinkJdbcInputFormat();
             if (config.hasPath(PARTITION_COLUMN)) {
-                if (!tableFieldInfo.containsKey(config.getString(PARTITION_COLUMN))) {
-                    throw new IllegalArgumentException(String.format("field %s not contain in table %s",
-                            config.getString(PARTITION_COLUMN), tableName));
+                String partitionColumn = config.getString(PARTITION_COLUMN);
+                if (!tableFieldInfo.containsKey(partitionColumn)) {
+                    throw new IllegalArgumentException(String.format("field %s not contain in query sql %s",
+                            partitionColumn, query));
                 }
-                if (!isNumericType(rowTypeInfo.getTypeAt(config.getString(PARTITION_COLUMN)))) {
-                    throw new IllegalArgumentException(String.format("%s is not numeric type", PARTITION_COLUMN));
+                if (!isNumericType(rowTypeInfo.getTypeAt(partitionColumn))) {
+                    throw new IllegalArgumentException(String.format("%s is not numeric type", partitionColumn));
                 }
                 JdbcParameterValuesProvider jdbcParameterValuesProvider =
-                        initPartition(config.getString(PARTITION_COLUMN), connection);
+                        initPartition(partitionColumn, connection, query);
                 builder.setParametersProvider(jdbcParameterValuesProvider);
-                query = extendPartitionQuerySql(query, config.getString(PARTITION_COLUMN));
+                query = String.format("SELECT * FROM (%s) tt where " + partitionColumn + " >= ? AND " + partitionColumn + " < ?", query);
             }
             builder.setDrivername(driverName).setDBUrl(dbUrl).setUsername(username)
                     .setPassword(password).setQuery(query).setFetchSize(fetchSize)
                     .setRowTypeInfo(rowTypeInfo);
-
             jdbcInputFormat = builder.finish();
         } catch (SQLException e) {
             throw new RuntimeException("jdbc connection init failed.", e);
@@ -171,23 +139,7 @@ public class JdbcSource implements FlinkBatchSource {
         return "JdbcSource";
     }
 
-    private String extendPartitionQuerySql(String query, String column) {
-        Matcher matcher = COMPILE.matcher(query);
-        if (matcher.find()) {
-            String where = matcher.group(Integer.parseInt("3"));
-            if (where != null && where.trim().toLowerCase().startsWith("where")) {
-                // contain where
-                return query + " AND \"" + column + "\" BETWEEN ? AND ?";
-            } else {
-                // not contain where
-                return query + " WHERE \"" + column + "\" BETWEEN ? AND ?";
-            }
-        } else {
-            throw new IllegalArgumentException("sql statement format is incorrect :" + query);
-        }
-    }
-
-    private JdbcParameterValuesProvider initPartition(String columnName, Connection connection) throws SQLException {
+    private JdbcParameterValuesProvider initPartition(String columnName, Connection connection, String query) throws SQLException {
         long max = Long.MAX_VALUE;
         long min = Long.MIN_VALUE;
         if (config.hasPath(PARTITION_UPPER_BOUND) && config.hasPath(PARTITION_LOWER_BOUND)) {
@@ -196,7 +148,7 @@ public class JdbcSource implements FlinkBatchSource {
             return new JdbcNumericBetweenParametersProvider(min, max).ofBatchNum(parallelism * 2);
         }
         try (ResultSet rs = connection.createStatement().executeQuery(String.format("SELECT MAX(%s),MIN(%s) " +
-                "FROM %s", columnName, columnName, tableName))) {
+                "FROM (%s) tt", columnName, columnName, query))) {
             if (rs.next()) {
                 max = config.hasPath(PARTITION_UPPER_BOUND) ? config.getLong(PARTITION_UPPER_BOUND) :
                         Long.parseLong(rs.getString(1));
@@ -212,59 +164,47 @@ public class JdbcSource implements FlinkBatchSource {
                 || type.equals(LONG_TYPE_INFO) || type.equals(BIG_INT_TYPE_INFO);
     }
 
-    private Map<String, TypeInformation<?>> initTableField(Connection connection) {
-        Map<String, TypeInformation<?>> map = new LinkedHashMap<>();
-
+    private Map<String, TypeInformation<?>> initTableField(Connection connection, String selectSql) {
         try {
-            TypeInformationMap informationMapping = getTypeInformationMap(driverName);
-            DatabaseMetaData metaData = connection.getMetaData();
-            ResultSet columns = metaData.getColumns(connection.getCatalog(), connection.getSchema(), tableName, "%");
-            while (columns.next()) {
-                String columnName = columns.getString("COLUMN_NAME");
-                String dataTypeName = columns.getString("TYPE_NAME");
-                if (fields == null || fields.contains(columnName)) {
-                    map.put(columnName, informationMapping.getInformation(dataTypeName));
-                }
+            String databaseDialect = connection.getMetaData().getDatabaseProductName();
+            PreparedStatement preparedStatement = connection.prepareStatement(selectSql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            preparedStatement.setMaxRows(1);
+            ResultSetMetaData rsMeta = preparedStatement.getMetaData();
+            try {
+                return getRowInfo(rsMeta, databaseDialect);
+            } catch (SQLException e) {
+                ResultSet rs = preparedStatement.executeQuery();
+                return getRowInfo(rs.getMetaData(), databaseDialect);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             LOGGER.warn("get row type info exception", e);
+        }
+        return new LinkedHashMap<>();
+    }
+
+    private Map<String, TypeInformation<?>> getRowInfo(ResultSetMetaData rsMeta, String databaseDialect) throws SQLException {
+        Map<String, TypeInformation<?>> map = new LinkedHashMap<>();
+        if (rsMeta == null) {
+            throw new SQLException("No result set metadata available to resolver row info!");
+        }
+        TypeInformationMap informationMapping = getTypeInformationMap(databaseDialect);
+        for (int i = 1; i < rsMeta.getColumnCount(); i++) {
+            String columnName = rsMeta.getColumnLabel(i);
+            String columnTypeName = rsMeta.getColumnTypeName(i);
+            if (columnTypeName == null) {
+                throw new SQLException("Unsupported to get type info from result set metadata!");
+            }
+            map.put(columnName, informationMapping.getInformation(columnTypeName));
         }
         return map;
     }
 
-    private Tuple2<String, Set<String>> getTableNameAndFields(Pattern regex, String selectSql) {
-        Matcher matcher = regex.matcher(selectSql);
-        String tableName;
-        Set<String> fields = null;
-        if (matcher.find()) {
-            String var = matcher.group(1);
-            tableName = matcher.group(2);
-            if (!"*".equals(var.trim())) {
-                LinkedHashSet<String> vars = new LinkedHashSet<>();
-                String[] split = var.split(",");
-                for (String s : split) {
-                    vars.add(s.trim());
-                }
-                fields = vars;
-            }
-            return new Tuple2<>(tableName, fields);
-        } else {
-            throw new IllegalArgumentException("can't find tableName and fields in sql :" + selectSql);
-        }
-    }
-
     private RowTypeInfo getRowTypeInfo() {
         int size = tableFieldInfo.size();
-        if (fields != null && fields.size() > 0) {
-            size = fields.size();
-        } else {
-            fields = tableFieldInfo.keySet();
-        }
-
+        Set<String> fields = tableFieldInfo.keySet();
         TypeInformation<?>[] typeInformation = new TypeInformation<?>[size];
         String[] names = new String[size];
         int i = 0;
-
         for (String field : fields) {
             typeInformation[i] = tableFieldInfo.get(field);
             names[i] = field;
@@ -273,12 +213,13 @@ public class JdbcSource implements FlinkBatchSource {
         return new RowTypeInfo(typeInformation, names);
     }
 
-    private TypeInformationMap getTypeInformationMap(String driverName) {
-        driverName = driverName.toLowerCase();
-        if (driverName.contains("mysql")) {
+    private TypeInformationMap getTypeInformationMap(String databaseDialect) {
+        if ("mysql".equalsIgnoreCase(databaseDialect)) {
             return new MysqlTypeInformationMap();
-        } else if (driverName.contains("postgresql")) {
+        } else if ("postgresql".equalsIgnoreCase(databaseDialect)) {
             return new PostgresTypeInformationMap();
+        } else if ("oracle".equalsIgnoreCase(databaseDialect)) {
+            return new OracleTypeInformationMap();
         } else {
             return new DefaultTypeInformationMap();
         }
