ba1bd2f0c4523ea6d98dd45bddfae66b6bbdb402
fanjia <1095948736@qq.com>
2022-05-13 10:16:00 +0800

    fix checkstyle

--- seatunnel-translation/seatunnel-translation-spark/src/main/java/org/apache/seatunnel/translation/spark/sink/SparkSinkConverter.java
+++ seatunnel-translation/seatunnel-translation-spark/src/main/java/org/apache/seatunnel/translation/spark/sink/SparkSinkConverter.java
@@ -24,11 +24,12 @@ import org.apache.spark.sql.sources.v2.WriteSupport;
 
 import java.util.Map;
 
-public class SparkSinkConverter<Input, StateT, CommitInfoT, AggregatedCommitInfoT>
-        implements SinkConverter<SeaTunnelSink<Input, StateT, CommitInfoT, AggregatedCommitInfoT>, WriteSupport> {
+public class SparkSinkConverter<I, StateT, CommitInfoT, AggregatedCommitInfoT>
+        implements SinkConverter<SeaTunnelSink<I, StateT, CommitInfoT, AggregatedCommitInfoT>, WriteSupport> {
 
     @Override
-    public WriteSupport convert(SeaTunnelSink<Input, StateT, CommitInfoT, AggregatedCommitInfoT> sink, Map<String, String> configuration) {
+    public WriteSupport convert(SeaTunnelSink<I, StateT, CommitInfoT, AggregatedCommitInfoT> sink,
+                                Map<String, String> configuration) {
         throw new UnsupportedOperationException("Do not use SinkConverter to convert SeaTunnel Sink in " +
                 "Spark engine. Because Spark use refactor and SPI to load sink class. Use SparkSinkInjector" +
                 " instead.");
