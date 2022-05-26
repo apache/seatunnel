00a460f99a5177628c08b20f6b609bb75cac3ea4
fanjia <1095948736@qq.com>
2022-05-26 15:17:59 +0800

    fix ParallelSource can't stop when thread pool have error.

--- seatunnel-connectors/seatunnel-connectors-seatunnel/seatunnel-connectors-seatunnel-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/source/KafkaSourceSplitEnumerator.java
+++ seatunnel-connectors/seatunnel-connectors-seatunnel/seatunnel-connectors-seatunnel-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/source/KafkaSourceSplitEnumerator.java
@@ -126,7 +126,7 @@ public class KafkaSourceSplitEnumerator implements SourceSplitEnumerator<KafkaSo
     }
 
     private Set<KafkaSourceSplit> getTopicInfo() throws ExecutionException, InterruptedException {
-        Collection<String> topics = null;
+        Collection<String> topics;
         if (this.metadata.isPattern()) {
             Pattern pattern = Pattern.compile(this.metadata.getTopic());
             topics = this.adminClient.listTopics().names().get().stream()
