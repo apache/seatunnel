7d9210c39cd3657159c201e8870369de4bd66332
liuli <m_liuli@163.com>
2022-06-23 16:57:08 +0800

    delete unused parameter

--- seatunnel-connectors/seatunnel-connectors-seatunnel/seatunnel-connector-seatunnel-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/source/KafkaSourceSplitEnumerator.java
+++ seatunnel-connectors/seatunnel-connectors-seatunnel/seatunnel-connector-seatunnel-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/source/KafkaSourceSplitEnumerator.java
@@ -73,7 +73,7 @@ public class KafkaSourceSplitEnumerator implements SourceSplitEnumerator<KafkaSo
     @Override
     public void run() throws ExecutionException, InterruptedException {
         pendingSplit = getTopicInfo();
-        assignSplit(context.registeredReaders());
+        assignSplit();
     }
 
     @Override
@@ -87,7 +87,7 @@ public class KafkaSourceSplitEnumerator implements SourceSplitEnumerator<KafkaSo
     public void addSplitsBack(List<KafkaSourceSplit> splits, int subtaskId) {
         if (!splits.isEmpty()) {
             pendingSplit.addAll(splits);
-            assignSplit(Collections.singletonList(subtaskId));
+            assignSplit();
         }
     }
 
@@ -104,7 +104,7 @@ public class KafkaSourceSplitEnumerator implements SourceSplitEnumerator<KafkaSo
     @Override
     public void registerReader(int subtaskId) {
         if (!pendingSplit.isEmpty()) {
-            assignSplit(Collections.singletonList(subtaskId));
+            assignSplit();
         }
     }
 
@@ -145,7 +145,7 @@ public class KafkaSourceSplitEnumerator implements SourceSplitEnumerator<KafkaSo
                 }).collect(Collectors.toSet());
     }
 
-    private void assignSplit(Collection<Integer> taskIDList) {
+    private void assignSplit() {
         Map<Integer, List<KafkaSourceSplit>> readySplit = new HashMap<>(Common.COLLECTION_SIZE);
         for (int taskID = 0;  taskID < context.currentParallelism(); taskID++) {
             readySplit.computeIfAbsent(taskID, id -> new ArrayList<>());
