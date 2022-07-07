62ff08e6d83131a320052e6bb5cb21856fd3caba
Eric <gaojun2048@gmail.com>
2022-07-07 10:05:10 +0800

    [Connector-V2] Add File Sink Connector (#2117)
    * tmp commit
    
    * add hadoop2 and hadoop3 shade jar
    
    * add hadoop2 and hadoop3 shade jar
    
    * add license head
    
    * change know denpendencies
    
    * tmp commit
    
    * tmp commit
    
    * change hadoop dependency scope to provide
    
    * back pom
    
    * fix checkstyle
    
    * add example
    
    * fix example bug
    
    * remove file connector from example and e2e because hadoop2 can not compile with jdk11
    
    * no need jdk8 and jdk11 profile because we don't use hadoop shade jar
    
    * change hadoop jar dependency scope to provided
    
    * back
    
    * file connector can not build in jdk11
    
    * drop hadoop shade
    
    * add gitignore item
    
    * add hadoop and local file sink
    
    * fix pom error
    
    * fix pom error
    
    * fix pom error
    
    * implement new interface
    
    * fix UT error
    
    * fix e2e error
    
    * update build timeout from 30min to 40min
    
    * fix e2e error
    
    * remove auto service
    
    * fix e2e error
    
    * fix e2e error
    
    * fix e2e error
    
    * found e2e error
    
    * fix e2e error
    
    * fix e2e error
    
    * fix e2e error
    
    * merge from upstream
    
    * merge from upstream
    
    * merge from upstream
    
    * merge from upstream
    
    * merge from upstream
    
    * add mvn jvm option
    
    * add mvn jvm option
    
    * add license
    
    * add licnese
    
    * add licnese
    
    * fix dependency
    
    * fix build jvm oom
    
    * fix build jvm oom
    
    * fix build jvm oom
    
    * fix dependency
    
    * fix dependency
    
    * fix e2e error
    
    * add codeql check timeout from 30min to 60min
    
    * merge from dev
    
    * merge from dev
    
    * fix ci error
    
    * fix checkstyle
    
    * fix ci
    
    * fix ci
    
    * aa
    
    * aa
    
    * aa
    
    * add .idea
    
    * del .idea
    
    * del .idea
    
    * del .idea
    
    * del .idea
    
    * remove no use license
    
    * remove no use before and after method in test
    
    * fix license; remove dependency
    
    * fix review
    
    * fix build order
    
    * fix license
    
    * fix license
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * fix review
    
    * add code-analysys timeout to 120
    
    * retry ci
    
    * update license and remove no use jar from LICENSE file
    
    * retry ci
    
    Co-authored-by: Hisoka <fanjiaeminem@qq.com>

---
+++ seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/sink/TransactionStateFileSinkWriter.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.file.sink;
+
+import org.apache.seatunnel.api.sink.SinkWriter;
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.config.TextFileSinkConfig;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.spi.SinkFileSystemPlugin;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.transaction.TransactionStateFileWriter;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.writer.FileSinkPartitionDirNameGenerator;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.writer.FileSinkTransactionFileNameGenerator;
+
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
+
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class TransactionStateFileSinkWriter implements SinkWriter<SeaTunnelRow, FileCommitInfo, FileSinkState> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionStateFileSinkWriter.class);
+
+    private SeaTunnelRowType seaTunnelRowTypeInfo;
+    private Config pluginConfig;
+    private Context context;
+    private String jobId;
+
+    private TransactionStateFileWriter fileWriter;
+
+    private TextFileSinkConfig textFileSinkConfig;
+
+    public TransactionStateFileSinkWriter(@NonNull SeaTunnelRowType seaTunnelRowTypeInfo,
+                                          @NonNull Config pluginConfig,
+                                          @NonNull SinkWriter.Context context,
+                                          @NonNull TextFileSinkConfig textFileSinkConfig,
+                                          @NonNull String jobId,
+                                          @NonNull SinkFileSystemPlugin sinkFileSystemPlugin) {
+        this.seaTunnelRowTypeInfo = seaTunnelRowTypeInfo;
+        this.pluginConfig = pluginConfig;
+        this.context = context;
+        this.jobId = jobId;
+        this.textFileSinkConfig = textFileSinkConfig;
+
+        Optional<TransactionStateFileWriter> transactionStateFileWriter = sinkFileSystemPlugin.getTransactionStateFileWriter(this.seaTunnelRowTypeInfo,
+            new FileSinkTransactionFileNameGenerator(
+                this.textFileSinkConfig.getFileFormat(),
+                this.textFileSinkConfig.getFileNameExpression(),
+                this.textFileSinkConfig.getFileNameTimeFormat()),
+            new FileSinkPartitionDirNameGenerator(
+                this.textFileSinkConfig.getPartitionFieldList(),
+                this.textFileSinkConfig.getPartitionFieldsIndexInRow(),
+                this.textFileSinkConfig.getPartitionDirExpression()),
+            this.textFileSinkConfig.getSinkColumnsIndexInRow(),
+            this.textFileSinkConfig.getTmpPath(),
+            this.textFileSinkConfig.getPath(),
+            this.jobId,
+            this.context.getIndexOfSubtask(),
+            this.textFileSinkConfig.getFieldDelimiter(),
+            this.textFileSinkConfig.getRowDelimiter(),
+            sinkFileSystemPlugin.getFileSystem().get());
+
+        if (!transactionStateFileWriter.isPresent()) {
+            throw new RuntimeException("A TransactionStateFileWriter is need");
+        }
+
+        this.fileWriter = transactionStateFileWriter.get();
+
+        fileWriter.beginTransaction(1L);
+    }
+
+    public TransactionStateFileSinkWriter(@NonNull SeaTunnelRowType seaTunnelRowTypeInfo,
+                                          @NonNull Config pluginConfig,
+                                          @NonNull SinkWriter.Context context,
+                                          @NonNull TextFileSinkConfig textFileSinkConfig,
+                                          @NonNull String jobId,
+                                          @NonNull List<FileSinkState> fileSinkStates,
+                                          @NonNull SinkFileSystemPlugin sinkFileSystemPlugin) {
+        this.seaTunnelRowTypeInfo = seaTunnelRowTypeInfo;
+        this.pluginConfig = pluginConfig;
+        this.context = context;
+        this.jobId = jobId;
+
+        Optional<TransactionStateFileWriter> transactionStateFileWriter = sinkFileSystemPlugin.getTransactionStateFileWriter(this.seaTunnelRowTypeInfo,
+            new FileSinkTransactionFileNameGenerator(
+                this.textFileSinkConfig.getFileFormat(),
+                this.textFileSinkConfig.getFileNameExpression(),
+                this.textFileSinkConfig.getFileNameTimeFormat()),
+            new FileSinkPartitionDirNameGenerator(
+                this.textFileSinkConfig.getPartitionFieldList(),
+                this.textFileSinkConfig.getPartitionFieldsIndexInRow(),
+                this.textFileSinkConfig.getPartitionDirExpression()),
+            this.textFileSinkConfig.getSinkColumnsIndexInRow(),
+            this.textFileSinkConfig.getTmpPath(),
+            this.textFileSinkConfig.getPath(),
+            this.jobId,
+            this.context.getIndexOfSubtask(),
+            this.textFileSinkConfig.getFieldDelimiter(),
+            this.textFileSinkConfig.getRowDelimiter(),
+            sinkFileSystemPlugin.getFileSystem().get());
+
+        if (!transactionStateFileWriter.isPresent()) {
+            throw new RuntimeException("A TransactionStateFileWriter is need");
+        }
+
+        this.fileWriter = transactionStateFileWriter.get();
+
+        // Rollback dirty transaction
+        if (fileSinkStates.size() > 0) {
+            List<String> transactionAfter = fileWriter.getTransactionAfter(fileSinkStates.get(0).getTransactionId());
+            fileWriter.abortTransactions(transactionAfter);
+        }
+        fileWriter.beginTransaction(fileSinkStates.get(0).getCheckpointId() + 1);
+    }
+
+    @Override
+    public void write(SeaTunnelRow element) throws IOException {
+        fileWriter.write(element);
+    }
+
+    @Override
+    public Optional<FileCommitInfo> prepareCommit() throws IOException {
+        return fileWriter.prepareCommit();
+    }
+
+    @Override
+    public void abortPrepare() {
+        fileWriter.abortTransaction();
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileWriter.finishAndCloseWriteFile();
+    }
+
+    @Override
+    public List<FileSinkState> snapshotState(long checkpointId) throws IOException {
+        List<FileSinkState> fileSinkStates = fileWriter.snapshotState(checkpointId);
+        fileWriter.beginTransaction(checkpointId);
+        return fileSinkStates;
+    }
+}
---
+++ seatunnel-connectors-v2/connector-file/connector-file-hadoop/src/test/java/org/apache/seatunnel/connectors/seatunnel/file/sink/hdfs/FileSinkAggregatedCommitterTest.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.file.sink.hdfs;
+
+import org.apache.seatunnel.connectors.seatunnel.file.sink.FileAggregatedCommitInfo;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.FileCommitInfo;
+import org.apache.seatunnel.connectors.seatunnel.file.sink.FileSinkAggregatedCommitter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class FileSinkAggregatedCommitterTest {
+    @Test
+    public void testCommit() throws Exception {
+        FileSinkAggregatedCommitter fileSinkAggregatedCommitter = new FileSinkAggregatedCommitter(new HdfsFileSystemCommitter());
+        Map<String, Map<String, String>> transactionFiles = new HashMap<>();
+        Random random = new Random();
+        Long jobId = random.nextLong();
+        String transactionDir = String.format("/tmp/seatunnel/seatunnel/%s/T_%s_0_1", jobId, jobId);
+        String targetDir = String.format("/tmp/hive/warehouse/%s", jobId);
+        Map<String, String> needMoveFiles = new HashMap<>();
+        needMoveFiles.put(transactionDir + "/c3=4/c4=rrr/test1.txt", targetDir + "/c3=4/c4=rrr/test1.txt");
+        needMoveFiles.put(transactionDir + "/c3=4/c4=bbb/test1.txt", targetDir + "/c3=4/c4=bbb/test1.txt");
+        HdfsUtils.createFile(transactionDir + "/c3=4/c4=rrr/test1.txt");
+        HdfsUtils.createFile(transactionDir + "/c3=4/c4=bbb/test1.txt");
+
+        transactionFiles.put(transactionDir, needMoveFiles);
+        FileAggregatedCommitInfo fileAggregatedCommitInfo = new FileAggregatedCommitInfo(transactionFiles);
+        List<FileAggregatedCommitInfo> fileAggregatedCommitInfoList = new ArrayList<>();
+        fileAggregatedCommitInfoList.add(fileAggregatedCommitInfo);
+        fileSinkAggregatedCommitter.commit(fileAggregatedCommitInfoList);
+
+        Assert.assertTrue(HdfsUtils.fileExist(targetDir + "/c3=4/c4=bbb/test1.txt"));
+        Assert.assertTrue(HdfsUtils.fileExist(targetDir + "/c3=4/c4=rrr/test1.txt"));
+        Assert.assertTrue(!HdfsUtils.fileExist(transactionDir));
+    }
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Test
+    public void testCombine() throws Exception {
+        FileSinkAggregatedCommitter fileSinkAggregatedCommitter = new FileSinkAggregatedCommitter(new HdfsFileSystemCommitter());
+        Map<String, Map<String, String>> transactionFiles = new HashMap<>();
+        Random random = new Random();
+        Long jobId = random.nextLong();
+        String transactionDir = String.format("/tmp/seatunnel/seatunnel/%s/T_%s_0_1", jobId, jobId);
+        String targetDir = String.format("/tmp/hive/warehouse/%s", jobId);
+        Map<String, String> needMoveFiles = new HashMap<>();
+        needMoveFiles.put(transactionDir + "/c3=4/c4=rrr/test1.txt", targetDir + "/c3=4/c4=rrr/test1.txt");
+        needMoveFiles.put(transactionDir + "/c3=4/c4=bbb/test1.txt", targetDir + "/c3=4/c4=bbb/test1.txt");
+        HdfsUtils.createFile(transactionDir + "/c3=4/c4=rrr/test1.txt");
+        HdfsUtils.createFile(transactionDir + "/c3=4/c4=bbb/test1.txt");
+
+        Map<String, String> needMoveFiles1 = new HashMap<>();
+        needMoveFiles1.put(transactionDir + "/c3=4/c4=rrr/test2.txt", targetDir + "/c3=4/c4=rrr/test2.txt");
+        needMoveFiles1.put(transactionDir + "/c3=4/c4=bbb/test2.txt", targetDir + "/c3=4/c4=bbb/test2.txt");
+        FileCommitInfo fileCommitInfo = new FileCommitInfo(needMoveFiles, transactionDir);
+        FileCommitInfo fileCommitInfo1 = new FileCommitInfo(needMoveFiles1, transactionDir);
+        List<FileCommitInfo> fileCommitInfoList = new ArrayList<>();
+        fileCommitInfoList.add(fileCommitInfo);
+        fileCommitInfoList.add(fileCommitInfo1);
+        FileAggregatedCommitInfo combine = fileSinkAggregatedCommitter.combine(fileCommitInfoList);
+        Assert.assertEquals(1, combine.getTransactionMap().size());
+        Assert.assertEquals(4, combine.getTransactionMap().get(transactionDir).size());
+        Assert.assertEquals(targetDir + "/c3=4/c4=rrr/test1.txt", combine.getTransactionMap().get(transactionDir).get(transactionDir + "/c3=4/c4=rrr/test1.txt"));
+        Assert.assertEquals(targetDir + "/c3=4/c4=bbb/test1.txt", combine.getTransactionMap().get(transactionDir).get(transactionDir + "/c3=4/c4=bbb/test1.txt"));
+        Assert.assertEquals(targetDir + "/c3=4/c4=rrr/test2.txt", combine.getTransactionMap().get(transactionDir).get(transactionDir + "/c3=4/c4=rrr/test2.txt"));
+        Assert.assertEquals(targetDir + "/c3=4/c4=bbb/test2.txt", combine.getTransactionMap().get(transactionDir).get(transactionDir + "/c3=4/c4=bbb/test2.txt"));
+    }
+
+    @Test
+    public void testAbort() throws Exception {
+        FileSinkAggregatedCommitter fileSinkAggregatedCommitter = new FileSinkAggregatedCommitter(new HdfsFileSystemCommitter());
+        Map<String, Map<String, String>> transactionFiles = new HashMap<>();
+        Random random = new Random();
+        Long jobId = random.nextLong();
+        String transactionDir = String.format("/tmp/seatunnel/seatunnel/%s/T_%s_0_1", jobId, jobId);
+        String targetDir = String.format("/tmp/hive/warehouse/%s", jobId);
+        Map<String, String> needMoveFiles = new HashMap<>();
+        needMoveFiles.put(transactionDir + "/c3=4/c4=rrr/test1.txt", targetDir + "/c3=4/c4=rrr/test1.txt");
+        needMoveFiles.put(transactionDir + "/c3=4/c4=bbb/test1.txt", targetDir + "/c3=4/c4=bbb/test1.txt");
+        HdfsUtils.createFile(transactionDir + "/c3=4/c4=rrr/test1.txt");
+        HdfsUtils.createFile(transactionDir + "/c3=4/c4=bbb/test1.txt");
+
+        transactionFiles.put(transactionDir, needMoveFiles);
+        FileAggregatedCommitInfo fileAggregatedCommitInfo = new FileAggregatedCommitInfo(transactionFiles);
+        List<FileAggregatedCommitInfo> fileAggregatedCommitInfoList = new ArrayList<>();
+        fileAggregatedCommitInfoList.add(fileAggregatedCommitInfo);
+        fileSinkAggregatedCommitter.commit(fileAggregatedCommitInfoList);
+
+        Assert.assertTrue(HdfsUtils.fileExist(targetDir + "/c3=4/c4=bbb/test1.txt"));
+        Assert.assertTrue(HdfsUtils.fileExist(targetDir + "/c3=4/c4=rrr/test1.txt"));
+        Assert.assertTrue(!HdfsUtils.fileExist(transactionDir));
+
+        fileSinkAggregatedCommitter.abort(fileAggregatedCommitInfoList);
+        Assert.assertTrue(!HdfsUtils.fileExist(targetDir + "/c3=4/c4=bbb/test1.txt"));
+        Assert.assertTrue(!HdfsUtils.fileExist(targetDir + "/c3=4/c4=rrr/test1.txt"));
+
+        // transactionDir will being delete when abort
+        Assert.assertTrue(!HdfsUtils.fileExist(transactionDir));
+    }
+}
