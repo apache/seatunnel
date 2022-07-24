b43668444c27b08ce9a021b1efb2986676a6f670
zhangyuge1 <1728375982@qq.com>
2022-07-24 16:59:05 +0800

    add jdbc e2e test

---
+++ seatunnel-e2e/seatunnel-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/jdbc/FakeSourceToJdbcIT.java
@@ -0,0 +1,77 @@
+package org.apache.seatunnel.e2e.spark.jdbc;
+
+import org.apache.seatunnel.e2e.spark.SparkContainer;
+
+import com.google.common.collect.Lists;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FakeSourceToJdbcIT extends SparkContainer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FakeSourceToJdbcIT.class);
+    private MySQLContainer<?> mysql;
+    private Connection connection;
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Before
+    public void startMysqlContainer() throws InterruptedException, ClassNotFoundException, SQLException {
+        mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+                .withNetwork(NETWORK)
+                .withNetworkAliases("jdbc")
+                .withLogConsumer(new Slf4jLogConsumer(LOGGER));
+        mysql.setPortBindings(Lists.newArrayList("33306:3306"));
+        Startables.deepStart(Stream.of(mysql)).join();
+        LOGGER.info("Jdbc container started");
+        Thread.sleep(5000L);
+        Class.forName(mysql.getDriverClassName());
+        connection = DriverManager.getConnection(mysql.getJdbcUrl(), mysql.getUsername(), mysql.getPassword());
+        initializeJdbcTable();
+    }
+
+    private void initializeJdbcTable() throws SQLException {
+        Statement statement = connection.createStatement();
+        String sql = "CREATE TABLE test (\n" +
+                "  name varchar(255) NOT NULL\n" +
+                ")";
+        statement.executeUpdate(sql);
+        statement.close();
+    }
+
+    @Test
+    public void testFakeSourceToJdbcSink() throws SQLException, IOException, InterruptedException {
+        Container.ExecResult execResult = executeSeaTunnelSparkJob("/jdbc/fakesource_to_jdbc.conf");
+        Assert.assertEquals(0, execResult.getExitCode());
+        String sql = "select * from test";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        List<String> result = Lists.newArrayList();
+        while (resultSet.next()) {
+            result.add(resultSet.getString("name"));
+        }
+        Assert.assertFalse(result.isEmpty());
+    }
+
+    @After
+    public void closeMysqlContainer() {
+        if (mysql != null) {
+            mysql.stop();
+        }
+    }
+}
