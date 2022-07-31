5b86429dbf3ae9ded33269bc9d1bb0d18315fbed
Zongwen Li <zongwen.li.tech@gmail.com>
2022-07-31 23:32:00 +0800

    [hotfix][spark] Fix STRING type reverse conversion error

--- seatunnel-translation/seatunnel-translation-spark/src/main/java/org/apache/seatunnel/translation/spark/serialization/InternalRowConverter.java
+++ seatunnel-translation/seatunnel-translation-spark/src/main/java/org/apache/seatunnel/translation/spark/serialization/InternalRowConverter.java
@@ -162,6 +162,8 @@ public final class InternalRowConverter extends RowConverter<InternalRow> {
                 return ((Timestamp) field).toLocalDateTime();
             case MAP:
                 return convertMap((Map<?, ?>) field, (MapType<?, ?>) dataType, InternalRowConverter::reconvert);
+            case STRING:
+                return field.toString();
             default:
                 return field;
         }
