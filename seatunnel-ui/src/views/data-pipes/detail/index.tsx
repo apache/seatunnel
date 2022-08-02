d9667648bf153aefbb75aef5bfdb30bbd108e8ac
songjianet <1778651752@qq.com>
2022-08-02 14:33:32 +0800

    [Feat][UI] Add the script in the data pipes detail. (#2335)

--- seatunnel-ui/src/views/data-pipes/detail/index.tsx
+++ seatunnel-ui/src/views/data-pipes/detail/index.tsx
@@ -16,12 +16,57 @@
  */
 
 import { defineComponent } from 'vue'
+import { NSpace, NCard, NButton, NBreadcrumb, NBreadcrumbItem, NTabs, NTabPane } from 'naive-ui'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+import MonacoEditor from '@/components/monaco-editor'
+import type { Router } from 'vue-router'
 
 const DataPipesDetail = defineComponent({
-  setup() {},
+  setup() {
+    const { t } = useI18n()
+    const router: Router = useRouter()
+
+    const handleClickDataPipes = () => {
+      router.push({ path: '/data-pipes/list' })
+    }
+
+    return { t, handleClickDataPipes }
+  },
   render() {
     return (
-      <div></div>
+      <NSpace vertical>
+        <NCard>
+          {{
+            header: () => <NSpace align='center'>
+              <NBreadcrumb>
+                <NBreadcrumbItem onClick={this.handleClickDataPipes}>{this.t('data_pipes.data_pipes')}</NBreadcrumbItem>
+                <NBreadcrumbItem>user-order-tables-10</NBreadcrumbItem>
+              </NBreadcrumb>
+              <div class={['w-3', 'h-3', 'rounded-full', 'bg-green-400']}></div>
+              <span style={{
+                fontSize: 'var(--n-font-size)',
+                color: 'var(--n-item-text-color-active)'
+              }}>{this.t('data_pipes.stop')}</span>
+            </NSpace>,
+            'header-extra': () => <NSpace>
+              <NButton secondary>{this.t('data_pipes.execute')}</NButton>
+              <NButton secondary>{this.t('data_pipes.kill')}</NButton>
+              <NButton secondary>{this.t('data_pipes.stop')}</NButton>
+            </NSpace>
+          }}
+        </NCard>
+        <NTabs type='segment' class='mt-7'>
+          <NTabPane name='overview' tab={this.t('data_pipes.overview')}>
+            <NCard>overview</NCard>
+          </NTabPane>
+          <NTabPane name='script' tab={this.t('data_pipes.script')}>
+            <NCard>
+              <MonacoEditor />
+            </NCard>
+          </NTabPane>
+        </NTabs>
+      </NSpace>
     )
   }
 })
