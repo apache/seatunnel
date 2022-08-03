96f2a15e4d30db38b24266d5253c7e7b64db22ec
liuyehan <81779971+2013650523@users.noreply.github.com>
2022-08-03 12:23:55 +0800

    [Connector-V2] Add Email sink connector (#2304)
    * 0
    
    * Update pom.xml
    
    add kudu dependency
    
    * Update plugin-mapping.properties
    
    add kudu config
    
    * Update pom.xml
    
    add kudu
    
    * add email sink connector
    
    * Delete seatunnel-connectors-v2/connector-email directory
    
    * Update plugin-mapping.properties
    
    * Update plugin-mapping.properties
    
    * Update pom.xml
    
    * Create pom.xml
    
    * [Connector-V2] Add Kudu source and sink connector
    
    * [Connector-V2] Add Kudu source and sink connector
    
    * [Connector-V2] Add Email sink connector
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Delete seatunnel-connectors-v2/connector-kudu directory
    
    * Update pom.xml
    
    * Update plugin-mapping.properties
    
    * Update pom.xml
    
    * [Connector-V2] update codestyle pom.xml
    
    * [Connector-V2] update config
    
    * [Connector-V2] update license
    
    * [Connector-V2] update email package version 1.5.6
    
    * [Connector-V2] fix problem on code review
    
    * [Connector-V2] add Email usage document
    
    * Update Email.md

---
+++ seatunnel-connectors-v2/connector-email/src/main/java/org/apache/seatunnel/connectors/seatunnel/email/sink/EmailSink.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.email.sink;
+
+import org.apache.seatunnel.api.sink.SeaTunnelSink;
+import org.apache.seatunnel.api.sink.SinkWriter;
+import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
+import org.apache.seatunnel.connectors.seatunnel.common.sink.AbstractSimpleSink;
+import org.apache.seatunnel.connectors.seatunnel.common.sink.AbstractSinkWriter;
+
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
+
+import com.google.auto.service.AutoService;
+
+@AutoService(SeaTunnelSink.class)
+public class EmailSink extends AbstractSimpleSink<SeaTunnelRow, Void> {
+
+    private Config pluginConfig;
+    private SeaTunnelRowType seaTunnelRowType;
+
+    @Override
+    public void setTypeInfo(SeaTunnelRowType seaTunnelRowType) {
+        this.seaTunnelRowType = seaTunnelRowType;
+    }
+
+    @Override
+    public SeaTunnelDataType<SeaTunnelRow> getConsumedType() {
+        return this.seaTunnelRowType;
+    }
+
+    @Override
+    public AbstractSinkWriter<SeaTunnelRow, Void> createWriter(SinkWriter.Context context) {
+        return new EmailSinkWriter(seaTunnelRowType, pluginConfig);
+    }
+
+    @Override
+    public String getPluginName() {
+        return "EmailSink";
+    }
+
+    @Override
+    public void prepare(Config pluginConfig) {
+        this.pluginConfig = pluginConfig;
+    }
+
+}
