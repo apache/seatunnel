49784a4bc5957d422311a43e31b39a1130b3936a
wanghailin <hailin@fiture.com>
2022-08-11 22:05:34 +0800

    [Feature][Connector-V2] Support IoTDB sink

---
+++ docs/en/connector-v2/sink/IoTDB.md
@@ -0,0 +1,111 @@
+# IoTDB
+
+> IoTDB sink connector
+
+## Description
+
+Used to write data to IoTDB. Supports Batch and Streaming mode.
+
+## Options
+
+| name                          | type              | required | default value |
+|-------------------------------|-------------------|----------|---------------|
+| node_urls                     | list<string>      | yes      | -             |
+| username                      | string            | yes      | -             |
+| password                      | string            | yes      | -             |
+| batch_size                    | int               | no       | 1024          |
+| batch_interval_ms             | int               | no       | -             |
+| max_retries                   | int               | no       | -             |
+| retry_backoff_multiplier_ms   | int               | no       | -             |
+| max_retry_backoff_ms          | int               | no       | -             |
+| default_thrift_buffer_size    | int               | no       | -             |
+| max_thrift_frame_size         | int               | no       | -             |
+| zone_id                       | string            | no       | -             |
+| enable_rpc_compression        | boolean           | no       | -             |
+| connection_timeout_in_ms      | int               | no       | -             |
+| timeseries_options            | list              | no       | -             |
+| timeseries_options.path       | string            | no       | -             |
+| timeseries_options.data_type  | string            | no       | -             |
+| common-options                | string            | no       | -             |
+
+### node_urls [list<string>]
+
+`IoTDB` cluster address, the format is `host:port` , allowing multiple `hosts` to be specified. Such as `"host1:6667,host2:6667"`
+
+### username [string]
+
+`IoTDB` user username
+
+### password [string]
+
+`IoTDB` user password
+
+### batch_size [int]
+
+For batch writing, when the number of buffers reaches the number of `batch_size` or the time reaches `batch_interval_ms`, the data will be flushed into the IoTDB
+
+### batch_interval_ms [int]
+
+For batch writing, when the number of buffers reaches the number of `batch_size` or the time reaches `batch_interval_ms`, the data will be flushed into the IoTDB
+
+### max_retries [int]
+
+The number of retries to flush failed
+
+### retry_backoff_multiplier_ms [int]
+
+Using as a multiplier for generating the next delay for backoff
+
+### max_retry_backoff_ms [int]
+
+The amount of time to wait before attempting to retry a request to `IoTDB`
+
+### default_thrift_buffer_size [int]
+
+Thrift init buffer size in `IoTDB` client
+
+### max_thrift_frame_size [int]
+
+Thrift max frame size in `IoTDB` client
+
+### zone_id [string]
+
+java.time.ZoneId in `IoTDB` client
+
+### enable_rpc_compression [boolean]
+
+Enable rpc compression in `IoTDB` client
+
+### connection_timeout_in_ms [int]
+
+The maximum time (in ms) to wait when connect `IoTDB`
+
+### timeseries_options [list]
+
+Timeseries options
+
+### timeseries_options.path [string]
+
+Timeseries path
+
+### timeseries_options.data_type [string]
+
+Timeseries data type
+
+### common options [string]
+
+Sink plugin common parameters, please refer to [Sink Common Options](common-options.md) for details
+
+## Examples
+
+```hocon
+sink {
+  Clickhouse {
+    node_urls = ["localhost:6667"]
+    username = "root"
+    password = "root"
+    batch_size = 1024
+    batch_interval_ms = 1000
+  }
+}
+```
---
+++ seatunnel-e2e/seatunnel-spark-connector-v2-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/iotdb/FakeSourceToIoTDBIT.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.e2e.spark.v2.iotdb;
+
+import org.apache.seatunnel.e2e.spark.SparkContainer;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.iotdb.session.Session;
+import org.apache.iotdb.session.SessionDataSet;
+import org.apache.iotdb.tsfile.file.metadata.enums.CompressionType;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
+import org.apache.iotdb.tsfile.read.common.Field;
+import org.apache.iotdb.tsfile.read.common.RowRecord;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Slf4j
+public class FakeSourceToIoTDBIT extends SparkContainer {
+
+    private static final String IOTDB_DOCKER_IMAGE = "apache/iotdb:0.13.1-node";
+    private static final String IOTDB_DOCKER_NETWORK_ALIASES = "iotdb";
+    private static final int IOTDB_DOCKER_NETWORK_PORT = 6667;
+    private static final String IOTDB_USERNAME = "root";
+    private static final String IOTDB_PASSWORD = "root";
+
+    private GenericContainer<?> iotdbServer;
+    private Session session;
+
+    @BeforeEach
+    public void startIoTDBContainer() throws Exception {
+        iotdbServer = new GenericContainer<>(IOTDB_DOCKER_IMAGE)
+                .withNetwork(NETWORK)
+                .withNetworkAliases(IOTDB_DOCKER_NETWORK_ALIASES)
+                .withLogConsumer(new Slf4jLogConsumer(log));
+        iotdbServer.setPortBindings(Lists.newArrayList(
+                String.format("%s:%s", IOTDB_DOCKER_NETWORK_PORT, IOTDB_DOCKER_NETWORK_PORT)));
+        Startables.deepStart(Stream.of(iotdbServer)).join();
+        log.info("IoTDB container started");
+        // wait for IoTDB fully start
+        Thread.sleep(5000L);
+        session = createSession();
+        initIoTDBTimeseries();
+    }
+
+    /**
+     * fake source -> IoTDB sink
+     */
+    @Test
+    public void testFakeSourceToIoTDB() throws Exception {
+        Container.ExecResult execResult = executeSeaTunnelSparkJob("/iotdb/fakesource_to_iotdb.conf");
+        Assertions.assertEquals(0, execResult.getExitCode());
+
+        // query result
+        SessionDataSet dataSet = session.executeQueryStatement("select status, value from root.ln.d1");
+        List<Object> actual = new ArrayList<>();
+        while (dataSet.hasNext()) {
+            RowRecord row = dataSet.next();
+            List<Field> fields = row.getFields();
+            Field status = fields.get(0);
+            Field val = fields.get(1);
+            actual.add(Arrays.asList(status.getBoolV(), val.getLongV()));
+        }
+        List<Object> expected = Arrays.asList(
+                Arrays.asList(Boolean.TRUE, Long.valueOf(1001)),
+                Arrays.asList(Boolean.FALSE, Long.valueOf(1002)));
+        Assertions.assertIterableEquals(expected, actual);
+    }
+
+    private Session createSession() {
+        return new Session.Builder()
+                .host("localhost")
+                .port(IOTDB_DOCKER_NETWORK_PORT)
+                .username(IOTDB_USERNAME)
+                .password(IOTDB_PASSWORD)
+                .build();
+    }
+
+    private void initIoTDBTimeseries() throws Exception {
+        session.open();
+        session.setStorageGroup("root.ln");
+        session.createTimeseries("root.ln.d1.status",
+                TSDataType.BOOLEAN, TSEncoding.PLAIN, CompressionType.SNAPPY);
+        session.createTimeseries("root.ln.d1.value",
+                TSDataType.INT64, TSEncoding.PLAIN, CompressionType.SNAPPY);
+    }
+
+    @AfterEach
+    public void closeIoTDBContainer() {
+        if (iotdbServer != null) {
+            iotdbServer.stop();
+        }
+    }
+}
