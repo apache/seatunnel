c1bbbd59d55b82d93b4dbc3d0aa3450cc2c91b7b
hailin0 <hailin088@gmail.com>
2022-08-17 17:30:41 +0800

    [Feature][Connector-V2] Support IoTDB sink (#2407)
    * [Feature][Connector-V2] Support IoTDB sink
    
    
    * update
    
    * update docs
    
    Co-authored-by: wanghailin <hailin@fiture.com>

---
+++ seatunnel-connectors-v2/connector-iotdb/src/main/java/org/apache/seatunnel/connectors/seatunnel/iotdb/config/CommonConfig.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.iotdb.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class CommonConfig {
+
+    public static final String NODE_URLS = "node_urls";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+    private final List<String> nodeUrls;
+    private final String username;
+    private final String password;
+}
