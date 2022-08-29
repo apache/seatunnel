ba113d5497a1adf525ba43c931c39526fe62647e
Hisoka <fanjiaeminem@qq.com>
2022-08-29 16:54:39 +0800

    [Engine][Task] Fix multi SourceReader use same TaskGroupID (#2533)
    * [Engine][Task] Fix multi SourceReader use same TaskGroupID
    
    * [Engine][Task] Fix multi SourceReader use same TaskGroupID

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/flow/SourceFlowLifeCycle.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/flow/SourceFlowLifeCycle.java
@@ -80,8 +80,9 @@ public class SourceFlowLifeCycle<T, SplitT extends SourceSplit> extends Abstract
 
     @Override
     public void close() throws IOException {
-        super.close();
+        collector.close();
         reader.close();
+        super.close();
     }
 
     public void collect() throws Exception {
@@ -91,10 +92,9 @@ public class SourceFlowLifeCycle<T, SplitT extends SourceSplit> extends Abstract
     }
 
     public void signalNoMoreElement() {
-        // Close this reader
+        // ready close this reader
         try {
             this.closed = true;
-            collector.close();
             runningTask.getExecutionContext().sendToMaster(new SourceNoMoreElementOperation(currentTaskID,
                     enumeratorTaskID)).get();
         } catch (Exception e) {
