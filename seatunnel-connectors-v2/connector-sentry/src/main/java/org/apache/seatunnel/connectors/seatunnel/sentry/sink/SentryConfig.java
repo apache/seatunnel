2da23ddce1115295cd9683e3117915c7e844afd0
Saintyang <shengjie_yang@foxmail.com>
2022-09-06 14:11:02 +0800

    [Feature][Connector-V2] Add sentry sink connector #2244 (#2584)
    * [Feature][Connector-V2] Add sentry sink connector #2244
    
    * [Feature][Connector-V2] Add sentry sink connector: modify pom.xml and doc, remove example class
    
    Co-authored-by: yangshengjie <yangshengjie@analysys.com.cn>

---
+++ seatunnel-connectors-v2/connector-sentry/src/main/java/org/apache/seatunnel/connectors/seatunnel/sentry/sink/SentryConfig.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.sentry.sink;
+
+public class SentryConfig {
+
+    public static final String SENTRY = "sentry";
+    public static final String DSN = "dsn";
+    public static final String ENV = "env";
+    public static final String RELEASE = "release";
+    public static final String CACHE_DIRPATH = "cacheDirPath";
+    public static final String ENABLE_EXTERNAL_CONFIGURATION = "enableExternalConfiguration";
+    public static final String MAX_CACHEITEMS = "maxCacheItems";
+    public static final String FLUSH_TIMEOUTMILLIS = "flushTimeoutMillis";
+    public static final String MAX_QUEUESIZE = "maxQueueSize";
+
+}
