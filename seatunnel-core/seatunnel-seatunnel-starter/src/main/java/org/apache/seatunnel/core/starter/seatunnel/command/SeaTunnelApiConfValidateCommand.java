c7e92f01928e33a92dd9525200097579dd21db1a
Hisoka <fanjiaeminem@qq.com>
2022-09-08 21:51:01 +0800

    [ST-Engine][Example] Add seatunnel example module (#2659)
    * [Engine][Example] Add seatunnel example module
    
    * [Engine] [Core] Add seatunnel engine example
    
    * [Engine] [Core] Add seatunnel engine example
    
    * [Engine] [Example] Add random cluster name when use local mode
    
    * [Engine] [Example] Fix review

---
+++ seatunnel-core/seatunnel-seatunnel-starter/src/main/java/org/apache/seatunnel/core/starter/seatunnel/command/SeaTunnelApiConfValidateCommand.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.core.starter.seatunnel.command;
+
+import org.apache.seatunnel.core.starter.command.Command;
+import org.apache.seatunnel.core.starter.config.ConfigBuilder;
+import org.apache.seatunnel.core.starter.exception.ConfigCheckException;
+import org.apache.seatunnel.core.starter.seatunnel.args.SeaTunnelCommandArgs;
+import org.apache.seatunnel.core.starter.seatunnel.config.SeaTunnelApiConfigChecker;
+import org.apache.seatunnel.core.starter.utils.FileUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+
+/**
+ * Use to validate the configuration of the SeaTunnel API.
+ */
+public class SeaTunnelApiConfValidateCommand implements Command<SeaTunnelCommandArgs> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SeaTunnelApiConfValidateCommand.class);
+
+    private final SeaTunnelCommandArgs seaTunnelCommandArgs;
+
+    public SeaTunnelApiConfValidateCommand(SeaTunnelCommandArgs seaTunnelCommandArgs) {
+        this.seaTunnelCommandArgs = seaTunnelCommandArgs;
+    }
+
+    @Override
+    public void execute() throws ConfigCheckException {
+        Path configPath = FileUtils.getConfigPath(seaTunnelCommandArgs);
+        ConfigBuilder configBuilder = new ConfigBuilder(configPath);
+        new SeaTunnelApiConfigChecker().checkConfig(configBuilder.getConfig());
+        LOGGER.info("config OK !");
+    }
+}
