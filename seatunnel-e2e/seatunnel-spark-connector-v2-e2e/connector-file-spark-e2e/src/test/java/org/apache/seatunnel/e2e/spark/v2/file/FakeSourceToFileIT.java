42304d88fe21bbdf50b72b84a7f9c43b546b8196
tyrantlucifer <tyrantlucifer@gmail.com>
2022-09-09 17:14:03 +0800

    [Improve][Connector-V2] Remove useless test cases

--- seatunnel-e2e/seatunnel-spark-connector-v2-e2e/connector-file-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/file/FakeSourceToFileIT.java
+++ seatunnel-e2e/seatunnel-spark-connector-v2-e2e/connector-file-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/file/FakeSourceToFileIT.java
@@ -57,32 +57,4 @@ public class FakeSourceToFileIT extends SparkContainer {
         Container.ExecResult execResult = executeSeaTunnelSparkJob("/file/fakesource_to_local_json.conf");
         Assertions.assertEquals(0, execResult.getExitCode());
     }
-
-
-    /**
-     * fake source -> hdfs text file sink
-     */
-    @Test
-    public void testFakeSourceToHdfsFileText() throws IOException, InterruptedException {
-        Container.ExecResult execResult = executeSeaTunnelSparkJob("/file/fakesource_to_hdfs_text.conf");
-        Assertions.assertEquals(0, execResult.getExitCode());
-    }
-
-    /**
-     * fake source -> hdfs parquet file sink
-     */
-    @Test
-    public void testFakeSourceToHdfsFileParquet() throws IOException, InterruptedException {
-        Container.ExecResult execResult = executeSeaTunnelSparkJob("/file/fakesource_to_hdfs_parquet.conf");
-        Assertions.assertEquals(0, execResult.getExitCode());
-    }
-
-    /**
-     * fake source -> hdfs json file sink
-     */
-    @Test
-    public void testFakeSourceToHdfsFileJson() throws IOException, InterruptedException {
-        Container.ExecResult execResult = executeSeaTunnelSparkJob("/file/fakesource_to_hdfs_json.conf");
-        Assertions.assertEquals(0, execResult.getExitCode());
-    }
 }
