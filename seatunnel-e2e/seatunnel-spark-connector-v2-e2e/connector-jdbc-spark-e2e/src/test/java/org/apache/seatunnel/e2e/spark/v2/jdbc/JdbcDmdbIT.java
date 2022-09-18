f04b8ca561e12e5f5d36e10a89ab2ee63a9a86a2
laglangyue <laglangyue@qq.com>
2022-09-18 13:00:24 +0800

    [Improve][e2e] modify DM-driver by downLoad and add the value comparison of all columns

--- seatunnel-e2e/seatunnel-spark-connector-v2-e2e/connector-jdbc-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/jdbc/JdbcDmdbIT.java
+++ seatunnel-e2e/seatunnel-spark-connector-v2-e2e/connector-jdbc-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/jdbc/JdbcDmdbIT.java
@@ -52,13 +52,13 @@ public class JdbcDmdbIT extends SparkContainer {
     private static final String DM_DOCKER_IMAGE = "laglangyue/dmdb8";
     private static final String DRIVER_CLASS = "dm.jdbc.driver.DmDriver";
     private static final String HOST = "spark_e2e_dmdb";
-    private static final String LOCAL_HOST = "localhost";
     private static final String URL = "jdbc:dm://%s:5236";
     private static final String USERNAME = "SYSDBA";
     private static final String PASSWORD = "SYSDBA";
     private static final String DATABASE = "SYSDBA";
     private static final String SOURCE_TABLE = "e2e_table_source";
     private static final String SINK_TABLE = "e2e_table_sink";
+    public static final String DM_DRIVER_JAR = "https://repo1.maven.org/maven2/com/dameng/DmJdbcDriver18/8.1.1.193/DmJdbcDriver18-8.1.1.193.jar";
     private GenericContainer<?> dbServer;
     private Connection jdbcConnection;
 
@@ -94,6 +94,13 @@ public class JdbcDmdbIT extends SparkContainer {
         }
     }
 
+    @Override
+    protected void executeExtraCommands(GenericContainer<?> container) throws IOException, InterruptedException {
+        Container.ExecResult extraCommands = container.execInContainer("bash", "-c", "mkdir -p /tmp/seatunnel/plugins/Jdbc/lib "
+            + "&& cd /tmp/seatunnel/plugins/Jdbc/lib && curl -O " + DM_DRIVER_JAR);
+        Assertions.assertEquals(0, extraCommands.getExitCode());
+    }
+
     private void initializeJdbcConnection() throws SQLException {
         jdbcConnection = DriverManager.getConnection(String.format(
             URL, dbServer.getHost()), USERNAME, PASSWORD);
@@ -125,7 +132,7 @@ public class JdbcDmdbIT extends SparkContainer {
     }
 
     private void assertHasData(String table) {
-        try (Statement statement = jdbcConnection.createStatement();) {
+        try (Statement statement = jdbcConnection.createStatement()) {
             String sql = String.format("select * from %s.%s limit 1", DATABASE, table);
             ResultSet source = statement.executeQuery(sql);
             Assertions.assertTrue(source.next());
@@ -145,7 +152,33 @@ public class JdbcDmdbIT extends SparkContainer {
     public void testDMDBSourceToJdbcSink() throws SQLException, IOException, InterruptedException {
         Container.ExecResult execResult = executeSeaTunnelSparkJob("/jdbc/jdbc_dm_source_and_sink.conf");
         Assertions.assertEquals(0, execResult.getExitCode());
+        // assert
         assertHasData(SINK_TABLE);
+        JdbcE2eUtil.compare(jdbcConnection, String.format("select * from %s.%s limit 1", DATABASE, SOURCE_TABLE),
+            String.format("select * from %s.%s limit 1", DATABASE, SINK_TABLE),
+            Lists.newArrayList("DM_BIT",
+                "DM_INT",
+                "DM_INTEGER",
+                "DM_PLS_INTEGER",
+                "DM_TINYINT",
+                "DM_BYTE",
+                "DM_SMALLINT",
+                "DM_BIGINT",
+                "DM_NUMERIC",
+                "DM_NUMBER",
+                "DM_DECIMAL",
+                "DM_DEC",
+                "DM_REAL",
+                "DM_FLOAT",
+                "DM_DOUBLE_PRECISION",
+                "DM_DOUBLE",
+                "DM_CHAR",
+                "DM_CHARACTER",
+                "DM_VARCHAR",
+                "DM_VARCHAR2",
+                "DM_TEXT",
+                "DM_LONG",
+                "DM_LONGVARCHAR"));
     }
 
 }
