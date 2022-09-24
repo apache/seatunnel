24d0d192938a1805af7c3a52d7d1e08958b08154
tyrantlucifer <tyrantlucifer@gmail.com>
2022-09-24 23:35:04 +0800

    [Feature][text-format] Completed test format deserialization schema

--- seatunnel-formats/seatunnel-format-text/src/main/java/org/apache/seatunnel/format/text/TextDeserializationSchema.java
+++ seatunnel-formats/seatunnel-format-text/src/main/java/org/apache/seatunnel/format/text/TextDeserializationSchema.java
@@ -17,29 +17,40 @@
 
 package org.apache.seatunnel.format.text;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import org.apache.seatunnel.api.serialization.DeserializationSchema;
+import org.apache.seatunnel.api.table.type.ArrayType;
+import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.api.table.type.MapType;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
+import org.apache.seatunnel.common.utils.DateTimeUtils;
+import org.apache.seatunnel.common.utils.DateUtils;
+import org.apache.seatunnel.common.utils.JsonUtils;
+import org.apache.seatunnel.common.utils.TimeUtils;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import lombok.Builder;
+import lombok.NonNull;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+@Builder
 public class TextDeserializationSchema implements DeserializationSchema<SeaTunnelRow> {
-    private final SeaTunnelRowType seaTunnelRowType;
-    private final String delimiter;
-
-    public TextDeserializationSchema(SeaTunnelRowType seaTunnelRowType, String delimiter) {
-        checkNotNull(seaTunnelRowType);
-        checkNotNull(delimiter);
-        this.seaTunnelRowType = seaTunnelRowType;
-        this.delimiter = delimiter;
-    }
+    @NonNull
+    private SeaTunnelRowType seaTunnelRowType;
+    @NonNull
+    private String delimiter;
+    @Builder.Default
+    private DateUtils.Formatter dateFormatter = DateUtils.Formatter.YYYY_MM_DD;
+    @Builder.Default
+    private DateTimeUtils.Formatter dateTimeFormatter = DateTimeUtils.Formatter.YYYY_MM_DD_HH_MM_SS;
+    @Builder.Default
+    private TimeUtils.Formatter timeFormatter = TimeUtils.Formatter.HH_MM_SS;
 
     @Override
     public SeaTunnelRow deserialize(byte[] message) throws IOException {
@@ -63,9 +74,45 @@ public class TextDeserializationSchema implements DeserializationSchema<SeaTunne
     private Object convert(String field, SeaTunnelDataType<?> fieldType) {
         switch (fieldType.getSqlType()) {
             case ARRAY:
+                BasicType<?> elementType = ((ArrayType<?, ?>) fieldType).getElementType();
+                ArrayNode jsonNodes = JsonUtils.parseArray(field);
+                ArrayList<Object> objectArrayList = new ArrayList<>();
+                jsonNodes.forEach(jsonNode -> objectArrayList.add(convert(jsonNode.toString(), elementType)));
+                switch (elementType.getSqlType()) {
+                    case STRING:
+                        return objectArrayList.toArray(new String[0]);
+                    case BOOLEAN:
+                        return objectArrayList.toArray(new Boolean[0]);
+                    case TINYINT:
+                        return objectArrayList.toArray(new Byte[0]);
+                    case SMALLINT:
+                        return objectArrayList.toArray(new Short[0]);
+                    case INT:
+                        return objectArrayList.toArray(new Integer[0]);
+                    case BIGINT:
+                        return objectArrayList.toArray(new Long[0]);
+                    case FLOAT:
+                        return objectArrayList.toArray(new Float[0]);
+                    case DOUBLE:
+                        return objectArrayList.toArray(new Double[0]);
+                    default:
+                        String errorMsg = String.format("SeaTunnel array not support this data type [%s]", elementType.getSqlType());
+                        throw new UnsupportedOperationException(errorMsg);
+                }
             case MAP:
+                SeaTunnelDataType<?> keyType = ((MapType<?, ?>) fieldType).getKeyType();
+                SeaTunnelDataType<?> valueType = ((MapType<?, ?>) fieldType).getValueType();
+                LinkedHashMap<Object, Object> objectMap = new LinkedHashMap<>();
+                Map<String, String> fieldsMap = JsonUtils.toMap(field);
+                fieldsMap.forEach((key, value) -> objectMap.put(convert(key, keyType), convert(value, valueType)));
+                return objectMap;
             case ROW:
-                throw new UnsupportedOperationException("SeaTunnel text format does not support parsing these types [array, map, row]");
+                String[] splits = field.split(delimiter);
+                Object[] objects = new Object[splits.length];
+                for (int i = 0; i < splits.length; i++) {
+                    objects[i] = convert(splits[i], seaTunnelRowType.getFieldType(i));
+                }
+                return new SeaTunnelRow(objects);
             case STRING:
                 return field;
             case BOOLEAN:
@@ -89,12 +136,11 @@ public class TextDeserializationSchema implements DeserializationSchema<SeaTunne
             case BYTES:
                 return field.getBytes();
             case DATE:
-                // TODO: setting time formatter
-                return LocalDate.parse(field);
+                return DateUtils.parse(field, dateFormatter);
             case TIME:
-                return LocalTime.parse(field);
+                return TimeUtils.parse(field, timeFormatter);
             case TIMESTAMP:
-                return LocalDateTime.parse(field);
+                return DateTimeUtils.parse(field, dateTimeFormatter);
             default:
                 // do nothing
                 // never get in there
