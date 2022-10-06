7f555bd1df011226b598db8f6742427569ba3999
liugddx <804167098@qq.com>
2022-10-06 17:49:02 +0800

    add oracle test case

---
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/AbstractJdbcIT.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.jdbc;
+
+import static org.awaitility.Awaitility.given;
+
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+import org.apache.seatunnel.e2e.common.TestResource;
+import org.apache.seatunnel.e2e.common.TestSuiteBase;
+import org.apache.seatunnel.e2e.common.container.TestContainer;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestTemplate;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+@Slf4j
+public abstract class AbstractJdbcIT extends TestSuiteBase implements TestResource {
+
+    protected Connection jdbcConnection;
+    protected GenericContainer<?> dbServer;
+    private JdbcCase jdbcCase;
+
+    abstract JdbcCase getJdbcCase();
+
+    abstract int compareResult() throws SQLException;
+
+    abstract SeaTunnelRow initTestData();
+
+    private void getContainer() throws ClassNotFoundException, SQLException {
+        jdbcCase = this.getJdbcCase();
+        dbServer = new GenericContainer<>(jdbcCase.getDockerImage())
+            .withNetwork(NETWORK)
+            .withNetworkAliases(jdbcCase.getHost())
+            .withLogConsumer(new Slf4jLogConsumer(log));
+        dbServer.setPortBindings(Lists.newArrayList(
+            String.format("%s:%s", jdbcCase.getPort(), jdbcCase.getPort())));
+        Startables.deepStart(Stream.of(dbServer)).join();
+        Class.forName(jdbcCase.getDriverClass());
+        given().ignoreExceptions()
+            .await()
+            .atMost(180, TimeUnit.SECONDS)
+            .untilAsserted(this::initializeJdbcConnection);
+        initializeJdbcTable();
+    }
+
+    protected Connection initializeJdbcConnection() throws SQLException {
+        jdbcConnection = DriverManager.getConnection(jdbcCase.getJdbcUrl(), jdbcCase.getUserName(), jdbcCase.getPassword());
+        return jdbcConnection;
+    }
+
+    private void batchInsertData() throws SQLException {
+        try {
+            jdbcConnection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(jdbcCase.getInitDataSql())) {
+
+                for (int index = 0; index < jdbcCase.getSeaTunnelRow().getFields().length; index++) {
+                    preparedStatement.setObject(index + 1, jdbcCase.getSeaTunnelRow().getFields()[index]);
+                }
+                preparedStatement.execute();
+            }
+            jdbcConnection.commit();
+        } catch (SQLException e) {
+            jdbcConnection.rollback();
+            throw e;
+        }
+    }
+
+    private void initializeJdbcTable() throws SQLException {
+        try (Statement statement = jdbcConnection.createStatement()) {
+            String createSource = jdbcCase.getDdlSource();
+            String createSink = jdbcCase.getDdlSink();
+            statement.execute(createSource);
+            statement.execute(createSink);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        this.batchInsertData();
+    }
+
+    @BeforeAll
+    @Override
+    public void startUp() throws Exception {
+        this.getContainer();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        if (jdbcConnection != null) {
+            jdbcConnection.close();
+        }
+        if (dbServer != null) {
+            dbServer.close();
+        }
+    }
+
+    @TestTemplate
+    public void testJdbcDb(TestContainer container) throws IOException, InterruptedException {
+        Container.ExecResult execResult = container.executeJob(jdbcCase.getConfigFile());
+        Assertions.assertEquals(0, execResult.getExitCode(), execResult.getStderr());
+        Assertions.assertEquals(0, this.compareResult());
+    }
+
+}
---
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcOracledbIT.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.seatunnel.jdbc;
+
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JdbcOracledbIT extends AbstractJdbcIT {
+
+    private static final String DOCKER_IMAGE = "gvenzl/oracle-xe:18.4.0-slim";
+    private static final String DRIVER_CLASS = "oracle.jdbc.OracleDriver";
+    private static final String HOST = "e2e_oracleDb";
+    private static final int PORT = 1521;
+    private static final String URL = "jdbc:oracle:thin:@%s:%s/%s";
+    private static final String USERNAME = "test";
+    private static final String PASSWORD = "test";
+    private static final String DATABASE = "xepdb1";
+    private static final String SOURCE_TABLE = "e2e_table_source";
+    private static final String SINK_TABLE = "e2e_table_sink";
+    private static final String DRIVER_JAR = "https://repo1.maven.org/maven2/com/oracle/database/jdbc/ojdbc8/12.2.0.1/ojdbc8-12.2.0.1.jar";
+    private static final String CONFIG_FILE = "/jdbc_oracle_source_and_sink.conf";
+    private static final String DDL_SOURCE = "CREATE TABLE source (a VARCHAR(5) NOT NULL,\n" +
+        "                b VARCHAR(30) UNIQUE,\n" +
+        "                c VARCHAR(20));  ";
+    private static final String DDL_SINK = "CREATE TABLE sink (a VARCHAR(5) NOT NULL,\n" +
+        "                b VARCHAR(30) UNIQUE,\n" +
+        "                c VARCHAR(20));  ";
+    private static final String INIT_DATA_SQL = "insert into source values(?,?,?);";
+
+    @Override
+    JdbcCase getJdbcCase() {
+        String jdbcUrl = String.format(URL, HOST, PORT, DATABASE);
+        return JdbcCase.builder().dockerImage(DOCKER_IMAGE).driverClass(DRIVER_CLASS)
+            .host(HOST).jdbcUrl(jdbcUrl).userName(USERNAME).password(PASSWORD).dataBase(DATABASE)
+            .sourceTable(SOURCE_TABLE).sinkTable(SINK_TABLE).driverJar(DRIVER_JAR)
+            .ddlSource(DDL_SOURCE).ddlSink(DDL_SINK).initDataSql(INIT_DATA_SQL).configFile(CONFIG_FILE).seaTunnelRow(initTestData()).build();
+    }
+
+    @Override
+    int compareResult() throws SQLException {
+        String sql = "select a,b,c from sinks";
+        List<Object> result = new ArrayList<>();
+        try (Statement statement = jdbcConnection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                for (int index = 0; index < this.getJdbcCase().getSeaTunnelRow().getFields().length; index++) {
+                    result.add(resultSet.getObject(index));
+                }
+            }
+        }
+        Assertions.assertIterableEquals(Arrays.asList(this.getJdbcCase().getSeaTunnelRow().getFields()), result);
+        return 0;
+    }
+
+    @Override
+    SeaTunnelRow initTestData() {
+        return new SeaTunnelRow(new Object[]{"a", "b", "c"});
+    }
+}
