63b7a1d89cff8b15e547b0cbf914127a027505eb
wenwei <zhouwenwei@Jwenwei-MacBook-Pro.local>
2022-10-14 19:00:20 +0800

    [Feature][Connector-V2-Clickhouse] Clickhouse Source random use host when config multi-host

--- seatunnel-connectors-v2/connector-clickhouse/src/main/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/source/ClickhouseSource.java
+++ seatunnel-connectors-v2/connector-clickhouse/src/main/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/source/ClickhouseSource.java
@@ -72,7 +72,7 @@ public class ClickhouseSource implements SeaTunnelSource<SeaTunnelRow, Clickhous
                 config.getString(USERNAME), config.getString(PASSWORD));
 
         sql = config.getString(SQL);
-        ClickHouseNode currentServer = servers.get(threadLocalRandom.nextInt(servers.size() + 1));
+        ClickHouseNode currentServer = servers.get(threadLocalRandom.nextInt(servers.size()));
         try (ClickHouseClient client = ClickHouseClient.newInstance(currentServer.getProtocol());
              ClickHouseResponse response =
                      client.connect(currentServer).format(ClickHouseFormat.RowBinaryWithNamesAndTypes)
