179aa2f090d0083afbd1a2aa254f3d9e403dafa2
liugddx <804167098@qq.com>
2022-10-18 20:00:20 +0800

    revert gbase8a date type.

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/gbase8a/Gbase8aJdbcRowConverter.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/gbase8a/Gbase8aJdbcRowConverter.java
@@ -17,19 +17,13 @@
 
 package org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.gbase8a;
 
-import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
-import org.apache.seatunnel.api.table.type.SqlType;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.converter.AbstractJdbcRowConverter;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Gbase8aJdbcRowConverter extends AbstractJdbcRowConverter {
     @Override
@@ -40,51 +34,7 @@ public class Gbase8aJdbcRowConverter extends AbstractJdbcRowConverter {
     @SuppressWarnings("checkstyle:MagicNumber")
     @Override
     public SeaTunnelRow toInternal(ResultSet rs, ResultSetMetaData metaData, SeaTunnelRowType typeInfo) throws SQLException {
-        List<Object> fields = new ArrayList<>(typeInfo.getFieldTypes().length);
-        SeaTunnelDataType<?>[] seaTunnelDataTypes = typeInfo.getFieldTypes();
-
-        for (int i = 1; i <= seaTunnelDataTypes.length; i++) {
-            Object seatunnelField;
-            SqlType sqlType = seaTunnelDataTypes[i - 1].getSqlType();
-            if (null == rs.getObject(i)) {
-                seatunnelField = null;
-            } else if (SqlType.BOOLEAN.equals(sqlType)) {
-                seatunnelField = rs.getBoolean(i);
-            } else if (SqlType.TINYINT.equals(sqlType)) {
-                seatunnelField = rs.getByte(i);
-            } else if (SqlType.SMALLINT.equals(sqlType)) {
-                seatunnelField = rs.getShort(i);
-            } else if (SqlType.INT.equals(sqlType)) {
-                seatunnelField = rs.getInt(i);
-            } else if (SqlType.BIGINT.equals(sqlType)) {
-                seatunnelField = rs.getLong(i);
-            } else if (SqlType.DECIMAL.equals(sqlType)) {
-                Object value = rs.getObject(i);
-                seatunnelField = value instanceof BigInteger ?
-                    new BigDecimal((BigInteger) value, 0)
-                    : value;
-            } else if (SqlType.FLOAT.equals(sqlType)) {
-                seatunnelField = rs.getFloat(i);
-            } else if (SqlType.DOUBLE.equals(sqlType)) {
-                seatunnelField = rs.getDouble(i);
-            } else if (SqlType.STRING.equals(sqlType)) {
-                seatunnelField = rs.getString(i);
-            } else if (SqlType.TIME.equals(sqlType)) {
-                seatunnelField = rs.getTime(i);
-            } else if (SqlType.DATE.equals(sqlType)) {
-                seatunnelField = rs.getDate(i);
-            } else if (SqlType.TIMESTAMP.equals(sqlType)) {
-                seatunnelField = rs.getTimestamp(i);
-            } else if (SqlType.BYTES.equals(sqlType)) {
-                seatunnelField = rs.getBytes(i);
-            } else {
-                throw new IllegalStateException("Unexpected value: " + sqlType);
-            }
-
-            fields.add(seatunnelField);
-        }
-
-        return new SeaTunnelRow(fields.toArray());
+        return super.toInternal(rs, metaData, typeInfo);
     }
 
 }
--- seatunnel-translation/seatunnel-translation-flink/src/main/java/org/apache/seatunnel/translation/flink/utils/TypeConverterUtils.java
+++ seatunnel-translation/seatunnel-translation-flink/src/main/java/org/apache/seatunnel/translation/flink/utils/TypeConverterUtils.java
@@ -25,22 +25,17 @@ import org.apache.seatunnel.api.table.type.MapType;
 import org.apache.seatunnel.api.table.type.PrimitiveByteArrayType;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
-import org.apache.seatunnel.api.table.type.SqlDateType;
 
 import org.apache.flink.api.common.typeinfo.BasicArrayTypeInfo;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.LocalTimeTypeInfo;
 import org.apache.flink.api.common.typeinfo.PrimitiveArrayTypeInfo;
-import org.apache.flink.api.common.typeinfo.SqlTimeTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.MapTypeInfo;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.table.runtime.typeutils.BigDecimalTypeInfo;
 
 import java.math.BigDecimal;
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -69,9 +64,6 @@ public class TypeConverterUtils {
         BRIDGED_TYPES.put(LocalDate.class, BridgedType.of(LocalTimeType.LOCAL_DATE_TYPE, LocalTimeTypeInfo.LOCAL_DATE));
         BRIDGED_TYPES.put(LocalTime.class, BridgedType.of(LocalTimeType.LOCAL_TIME_TYPE, LocalTimeTypeInfo.LOCAL_TIME));
         BRIDGED_TYPES.put(LocalDateTime.class, BridgedType.of(LocalTimeType.LOCAL_DATE_TIME_TYPE, LocalTimeTypeInfo.LOCAL_DATE_TIME));
-        BRIDGED_TYPES.put(Date.class, BridgedType.of(SqlDateType.SQL_DATE_TYPE, SqlTimeTypeInfo.DATE));
-        BRIDGED_TYPES.put(Time.class, BridgedType.of(SqlDateType.SQL_TIME_TYPE, SqlTimeTypeInfo.TIME));
-        BRIDGED_TYPES.put(Timestamp.class, BridgedType.of(SqlDateType.SQL_DATE_TIME_TYPE, SqlTimeTypeInfo.TIMESTAMP));
 
         // basic array types
         BRIDGED_TYPES.put(byte[].class, BridgedType.of(PrimitiveByteArrayType.INSTANCE, PrimitiveArrayTypeInfo.BYTE_PRIMITIVE_ARRAY_TYPE_INFO));
