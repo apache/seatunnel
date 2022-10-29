c5c67f81025410bbecc0a97bcd1f8a5a82a6ffcf
yangbinbin <yangbinbin@aspirecn.com>
2022-10-30 00:50:28 +0800

    update

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-clickhouse-e2e/src/test/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/ClickhouseIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-clickhouse-e2e/src/test/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/ClickhouseIT.java
@@ -329,7 +329,7 @@ public class ClickhouseIT extends TestSuiteBase implements TestResource {
                     "57896044618658097711785492504343953926634992332820282019728792003956564819967",
                     "115792089237316195423570985008687907853269984665640564039457584007913129639935",
                     new double[]{1, 2},
-                    new double[][]{{2, 3}, {3, 4}}
+                    new double[][]{{2, 3}, {4, 5}}
                 });
             rows.add(row);
         }
