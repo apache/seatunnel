22cf058efbe6d9df47fbae88d9deefe10ca6d298
gaojun <gaojun2048@gmail.com>
2022-11-07 18:46:21 +0800

    Add pulsar source option rules and improve doc

--- seatunnel-connectors-v2/connector-pulsar/src/main/java/org/apache/seatunnel/connectors/seatunnel/pulsar/source/PulsarSource.java
+++ seatunnel-connectors-v2/connector-pulsar/src/main/java/org/apache/seatunnel/connectors/seatunnel/pulsar/source/PulsarSource.java
@@ -53,6 +53,7 @@ import org.apache.seatunnel.common.constants.PluginType;
 import org.apache.seatunnel.connectors.seatunnel.common.schema.SeaTunnelSchema;
 import org.apache.seatunnel.connectors.seatunnel.pulsar.config.PulsarAdminConfig;
 import org.apache.seatunnel.connectors.seatunnel.pulsar.config.PulsarClientConfig;
+import org.apache.seatunnel.connectors.seatunnel.pulsar.config.PulsarConfigUtil;
 import org.apache.seatunnel.connectors.seatunnel.pulsar.config.PulsarConsumerConfig;
 import org.apache.seatunnel.connectors.seatunnel.pulsar.config.SourceProperties;
 import org.apache.seatunnel.connectors.seatunnel.pulsar.source.enumerator.PulsarSplitEnumerator;
@@ -78,7 +79,6 @@ import java.util.regex.Pattern;
 
 @AutoService(SeaTunnelSource.class)
 public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit, PulsarSplitEnumeratorState> {
-    public static final String IDENTIFIER = "pulsar";
     private DeserializationSchema<T> deserialization;
 
     private PulsarAdminConfig adminConfig;
@@ -95,55 +95,55 @@ public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit,
 
     @Override
     public String getPluginName() {
-        return IDENTIFIER;
+        return PulsarConfigUtil.IDENTIFIER;
     }
 
     @SuppressWarnings("checkstyle:MagicNumber")
     @Override
     public void prepare(Config config) throws PrepareFailException {
-        CheckResult result = CheckConfigUtil.checkAllExists(config, SUBSCRIPTION_NAME, CLIENT_SERVICE_URL, ADMIN_SERVICE_URL);
+        CheckResult result = CheckConfigUtil.checkAllExists(config, SUBSCRIPTION_NAME.key(), CLIENT_SERVICE_URL.key(), ADMIN_SERVICE_URL.key());
         if (!result.isSuccess()) {
             throw new PrepareFailException(getPluginName(), PluginType.SOURCE, result.getMsg());
         }
 
         // admin config
         PulsarAdminConfig.Builder adminConfigBuilder = PulsarAdminConfig.builder()
-            .adminUrl(config.getString(ADMIN_SERVICE_URL));
-        setOption(config, AUTH_PLUGIN_CLASS, config::getString, adminConfigBuilder::authPluginClassName);
-        setOption(config, AUTH_PARAMS, config::getString, adminConfigBuilder::authParams);
+            .adminUrl(config.getString(ADMIN_SERVICE_URL.key()));
+        setOption(config, AUTH_PLUGIN_CLASS.key(), config::getString, adminConfigBuilder::authPluginClassName);
+        setOption(config, AUTH_PARAMS.key(), config::getString, adminConfigBuilder::authParams);
         this.adminConfig = adminConfigBuilder.build();
 
         // client config
         PulsarClientConfig.Builder clientConfigBuilder = PulsarClientConfig.builder()
-            .serviceUrl(config.getString(CLIENT_SERVICE_URL));
-        setOption(config, AUTH_PLUGIN_CLASS, config::getString, clientConfigBuilder::authPluginClassName);
-        setOption(config, AUTH_PARAMS, config::getString, clientConfigBuilder::authParams);
+            .serviceUrl(config.getString(CLIENT_SERVICE_URL.key()));
+        setOption(config, AUTH_PLUGIN_CLASS.key(), config::getString, clientConfigBuilder::authPluginClassName);
+        setOption(config, AUTH_PARAMS.key(), config::getString, clientConfigBuilder::authParams);
         this.clientConfig = clientConfigBuilder.build();
 
         // consumer config
         PulsarConsumerConfig.Builder consumerConfigBuilder = PulsarConsumerConfig.builder()
-            .subscriptionName(config.getString(SUBSCRIPTION_NAME));
+            .subscriptionName(config.getString(SUBSCRIPTION_NAME.key()));
         this.consumerConfig = consumerConfigBuilder.build();
 
         // source properties
         setOption(config,
-            TOPIC_DISCOVERY_INTERVAL,
-            -1L,
+            TOPIC_DISCOVERY_INTERVAL.key(),
+            TOPIC_DISCOVERY_INTERVAL.defaultValue(),
             config::getLong,
             v -> this.partitionDiscoveryIntervalMs = v);
         setOption(config,
-            POLL_TIMEOUT,
-            100,
+            POLL_TIMEOUT.key(),
+            POLL_TIMEOUT.defaultValue(),
             config::getInt,
             v -> this.pollTimeout = v);
         setOption(config,
-            POLL_INTERVAL,
-            50L,
+            POLL_INTERVAL.key(),
+            POLL_INTERVAL.defaultValue(),
             config::getLong,
             v -> this.pollInterval = v);
         setOption(config,
-            POLL_BATCH_SIZE,
-            500,
+            POLL_BATCH_SIZE.key(),
+            POLL_BATCH_SIZE.defaultValue(),
             config::getInt,
             v -> this.batchSize = v);
 
@@ -160,7 +160,7 @@ public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit,
     }
 
     private void setStartCursor(Config config) {
-        StartMode startMode = getEnum(config, CURSOR_STARTUP_MODE, StartMode.class, LATEST);
+        StartMode startMode = getEnum(config, CURSOR_STARTUP_MODE.key(), StartMode.class, CURSOR_STARTUP_MODE.defaultValue());
         switch (startMode) {
             case EARLIEST:
                 this.startCursor = StartCursor.earliest();
@@ -170,16 +170,16 @@ public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit,
                 break;
             case SUBSCRIPTION:
                 SubscriptionStartCursor.CursorResetStrategy resetStrategy = getEnum(config,
-                    CURSOR_RESET_MODE,
+                    CURSOR_RESET_MODE.key(),
                     SubscriptionStartCursor.CursorResetStrategy.class,
                     SubscriptionStartCursor.CursorResetStrategy.LATEST);
                 this.startCursor = StartCursor.subscription(resetStrategy);
                 break;
             case TIMESTAMP:
-                if (StringUtils.isBlank(config.getString(CURSOR_STARTUP_TIMESTAMP))) {
-                    throw new IllegalArgumentException(String.format("The '%s' property is required when the '%s' is 'timestamp'.", CURSOR_STARTUP_TIMESTAMP, CURSOR_STARTUP_MODE));
+                if (StringUtils.isBlank(config.getString(CURSOR_STARTUP_TIMESTAMP.key()))) {
+                    throw new IllegalArgumentException(String.format("The '%s' property is required when the '%s' is 'timestamp'.", CURSOR_STARTUP_TIMESTAMP.key(), CURSOR_STARTUP_MODE.key()));
                 }
-                setOption(config, CURSOR_STARTUP_TIMESTAMP, config::getLong, timestamp -> this.startCursor = StartCursor.timestamp(timestamp));
+                setOption(config, CURSOR_STARTUP_TIMESTAMP.key(), config::getLong, timestamp -> this.startCursor = StartCursor.timestamp(timestamp));
                 break;
             default:
                 throw new IllegalArgumentException(String.format("The %s mode is not supported.", startMode));
@@ -187,7 +187,7 @@ public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit,
     }
 
     private void setStopCursor(Config config) {
-        SourceProperties.StopMode stopMode = getEnum(config, CURSOR_STOP_MODE, SourceProperties.StopMode.class, NEVER);
+        SourceProperties.StopMode stopMode = getEnum(config, CURSOR_STOP_MODE.key(), SourceProperties.StopMode.class, CURSOR_STOP_MODE.defaultValue());
         switch (stopMode) {
             case LATEST:
                 this.stopCursor = StopCursor.latest();
@@ -196,10 +196,10 @@ public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit,
                 this.stopCursor = StopCursor.never();
                 break;
             case TIMESTAMP:
-                if (StringUtils.isBlank(config.getString(CURSOR_STOP_TIMESTAMP))) {
-                    throw new IllegalArgumentException(String.format("The '%s' property is required when the '%s' is 'timestamp'.", CURSOR_STOP_TIMESTAMP, CURSOR_STOP_MODE));
+                if (StringUtils.isBlank(config.getString(CURSOR_STOP_TIMESTAMP.key()))) {
+                    throw new IllegalArgumentException(String.format("The '%s' property is required when the '%s' is 'timestamp'.", CURSOR_STOP_TIMESTAMP.key(), CURSOR_STOP_MODE.key()));
                 }
-                setOption(config, CURSOR_STARTUP_TIMESTAMP, config::getLong, timestamp -> this.stopCursor = StopCursor.timestamp(timestamp));
+                setOption(config, CURSOR_STARTUP_TIMESTAMP.key(), config::getLong, timestamp -> this.stopCursor = StopCursor.timestamp(timestamp));
                 break;
             default:
                 throw new IllegalArgumentException(String.format("The %s mode is not supported.", stopMode));
@@ -207,19 +207,19 @@ public class PulsarSource<T> implements SeaTunnelSource<T, PulsarPartitionSplit,
     }
 
     private void setPartitionDiscoverer(Config config) {
-        String topic = config.getString(TOPIC);
+        String topic = config.getString(TOPIC.key());
         if (StringUtils.isNotBlank(topic)) {
             this.partitionDiscoverer = new TopicListDiscoverer(Arrays.asList(StringUtils.split(topic, ",")));
         }
-        String topicPattern = config.getString(TOPIC_PATTERN);
+        String topicPattern = config.getString(TOPIC_PATTERN.key());
         if (StringUtils.isNotBlank(topicPattern)) {
             if (this.partitionDiscoverer != null) {
-                throw new IllegalArgumentException(String.format("The properties '%s' and '%s' is exclusive.", TOPIC, TOPIC_PATTERN));
+                throw new IllegalArgumentException(String.format("The properties '%s' and '%s' is exclusive.", TOPIC.key(), TOPIC_PATTERN.key()));
             }
             this.partitionDiscoverer = new TopicPatternDiscoverer(Pattern.compile(topicPattern));
         }
         if (this.partitionDiscoverer == null) {
-            throw new IllegalArgumentException(String.format("The properties '%s' or '%s' is required.", TOPIC, TOPIC_PATTERN));
+            throw new IllegalArgumentException(String.format("The properties '%s' or '%s' is required.", TOPIC.key(), TOPIC_PATTERN.key()));
         }
     }
 
