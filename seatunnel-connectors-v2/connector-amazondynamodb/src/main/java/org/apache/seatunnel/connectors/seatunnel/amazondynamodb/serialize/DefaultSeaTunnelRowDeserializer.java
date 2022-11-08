8f19d399a91301a25d00a45f96fb61c996b60e7c
Hisoka <fanjiaeminem@qq.com>
2022-11-08 10:58:57 +0800

    [Connector-V2] [Amazon DynamoDB] Change Convert logic

--- seatunnel-connectors-v2/connector-amazondynamodb/src/main/java/org/apache/seatunnel/connectors/seatunnel/amazondynamodb/serialize/DefaultSeaTunnelRowDeserializer.java
+++ seatunnel-connectors-v2/connector-amazondynamodb/src/main/java/org/apache/seatunnel/connectors/seatunnel/amazondynamodb/serialize/DefaultSeaTunnelRowDeserializer.java
@@ -18,11 +18,7 @@
 package org.apache.seatunnel.connectors.seatunnel.amazondynamodb.serialize;
 
 import org.apache.seatunnel.api.table.type.ArrayType;
-import org.apache.seatunnel.api.table.type.BasicType;
-import org.apache.seatunnel.api.table.type.DecimalType;
-import org.apache.seatunnel.api.table.type.LocalTimeType;
 import org.apache.seatunnel.api.table.type.MapType;
-import org.apache.seatunnel.api.table.type.PrimitiveByteArrayType;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
@@ -37,6 +33,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -67,68 +64,72 @@ public class DefaultSeaTunnelRowDeserializer implements SeaTunnelRowDeserializer
     private Object convert(SeaTunnelDataType<?> seaTunnelDataType, AttributeValue attributeValue) {
         if (attributeValue.type().equals(AttributeValue.Type.NUL)) {
             return null;
-        } else if (BasicType.BOOLEAN_TYPE.equals(seaTunnelDataType)) {
-            return attributeValue.bool();
-        } else if (BasicType.BYTE_TYPE.equals(seaTunnelDataType)) {
-            if (attributeValue.n() != null) {
-                return Byte.parseByte(attributeValue.n());
-            }
-            return attributeValue.s().getBytes(StandardCharsets.UTF_8)[0];
-        } else if (BasicType.SHORT_TYPE.equals(seaTunnelDataType)) {
-            return Short.parseShort(attributeValue.n());
-        } else if (BasicType.INT_TYPE.equals(seaTunnelDataType)) {
-            return Integer.parseInt(attributeValue.n());
-        } else if (BasicType.LONG_TYPE.equals(seaTunnelDataType)) {
-            return Long.parseLong(attributeValue.n());
-        } else if (seaTunnelDataType instanceof DecimalType) {
-            return new BigDecimal(attributeValue.n());
-        } else if (BasicType.FLOAT_TYPE.equals(seaTunnelDataType)) {
-            return Float.parseFloat(attributeValue.n());
-        } else if (BasicType.DOUBLE_TYPE.equals(seaTunnelDataType)) {
-            return Double.parseDouble(attributeValue.n());
-        } else if (BasicType.STRING_TYPE.equals(seaTunnelDataType)) {
-            return attributeValue.s();
-        } else if (LocalTimeType.LOCAL_TIME_TYPE.equals(seaTunnelDataType)) {
-            return LocalTime.parse(attributeValue.s());
-        } else if (LocalTimeType.LOCAL_DATE_TYPE.equals(seaTunnelDataType)) {
-            return LocalDate.parse(attributeValue.s());
-        } else if (LocalTimeType.LOCAL_DATE_TIME_TYPE.equals(seaTunnelDataType)) {
-            return LocalDateTime.parse(attributeValue.s());
-        } else if (PrimitiveByteArrayType.INSTANCE.equals(seaTunnelDataType)) {
-            return attributeValue.b().asByteArray();
-        } else if (seaTunnelDataType instanceof MapType) {
-            Map<String, Object> seatunnelMap = new HashMap<>();
-            attributeValue.m().forEach((s, attributeValueInfo) -> {
-                seatunnelMap.put(s, convert(((MapType) seaTunnelDataType).getValueType(), attributeValueInfo));
-            });
-            return seatunnelMap;
-        } else if (seaTunnelDataType instanceof ArrayType) {
-            Object array = Array.newInstance(String.class, attributeValue.l().size());
-            if (attributeValue.hasL()) {
-                List<AttributeValue> datas = attributeValue.l();
-                array = Array.newInstance(((ArrayType<?, ?>) seaTunnelDataType).getElementType().getTypeClass(), attributeValue.l().size());
-                for (int index = 0; index < datas.size(); index++) {
-                    Array.set(array, index, convert(((ArrayType<?, ?>) seaTunnelDataType).getElementType(), datas.get(index)));
+        }
+        switch (seaTunnelDataType.getSqlType()) {
+            case BOOLEAN:
+                return attributeValue.bool();
+            case TINYINT:
+                if (attributeValue.n() != null) {
+                    return Byte.parseByte(attributeValue.n());
                 }
-            } else if (attributeValue.hasSs()) {
-                List<String> datas = attributeValue.ss();
-                for (int index = 0; index < datas.size(); index++) {
-                    Array.set(array, index, AttributeValue.fromS(datas.get(index)));
+                return attributeValue.s().getBytes(StandardCharsets.UTF_8)[0];
+            case SMALLINT:
+                return Short.parseShort(attributeValue.n());
+            case INT:
+                return Integer.parseInt(attributeValue.n());
+            case BIGINT:
+                return Long.parseLong(attributeValue.n());
+            case DECIMAL:
+                return new BigDecimal(attributeValue.n());
+            case FLOAT:
+                return Float.parseFloat(attributeValue.n());
+            case DOUBLE:
+                return Double.parseDouble(attributeValue.n());
+            case STRING:
+                return attributeValue.s();
+            case TIME:
+                return LocalTime.parse(attributeValue.s());
+            case DATE:
+                return LocalDate.parse(attributeValue.s());
+            case TIMESTAMP:
+                return LocalDateTime.parse(attributeValue.s()).atZone(ZoneOffset.UTC)
+                    .toInstant()
+                    .toEpochMilli();
+            case BYTES:
+                return attributeValue.b().asByteArray();
+            case MAP:
+                Map<String, Object> seatunnelMap = new HashMap<>();
+                attributeValue.m().forEach((s, attributeValueInfo) -> {
+                    seatunnelMap.put(s, convert(((MapType) seaTunnelDataType).getValueType(), attributeValueInfo));
+                });
+                return seatunnelMap;
+            case ARRAY:
+                Object array = Array.newInstance(String.class, attributeValue.l().size());
+                if (attributeValue.hasL()) {
+                    List<AttributeValue> datas = attributeValue.l();
+                    array = Array.newInstance(((ArrayType<?, ?>) seaTunnelDataType).getElementType().getTypeClass(), attributeValue.l().size());
+                    for (int index = 0; index < datas.size(); index++) {
+                        Array.set(array, index, convert(((ArrayType<?, ?>) seaTunnelDataType).getElementType(), datas.get(index)));
+                    }
+                } else if (attributeValue.hasSs()) {
+                    List<String> datas = attributeValue.ss();
+                    for (int index = 0; index < datas.size(); index++) {
+                        Array.set(array, index, AttributeValue.fromS(datas.get(index)));
+                    }
+                } else if (attributeValue.hasNs()) {
+                    List<String> datas = attributeValue.ns();
+                    for (int index = 0; index < datas.size(); index++) {
+                        Array.set(array, index, AttributeValue.fromS(datas.get(index)));
+                    }
+                } else if (attributeValue.hasBs()) {
+                    List<SdkBytes> datas = attributeValue.bs();
+                    for (int index = 0; index < datas.size(); index++) {
+                        Array.set(array, index, AttributeValue.fromB(datas.get(index)));
+                    }
                 }
-            } else if (attributeValue.hasNs()) {
-                List<String> datas = attributeValue.ns();
-                for (int index = 0; index < datas.size(); index++) {
-                    Array.set(array, index, AttributeValue.fromS(datas.get(index)));
-                }
-            } else if (attributeValue.hasBs()) {
-                List<SdkBytes> datas = attributeValue.bs();
-                for (int index = 0; index < datas.size(); index++) {
-                    Array.set(array, index, AttributeValue.fromB(datas.get(index)));
-                }
-            }
-            return array;
-        } else {
-            throw new IllegalStateException("Unexpected value: " + seaTunnelDataType);
+                return array;
+            default:
+                throw new IllegalStateException("Unexpected value: " + seaTunnelDataType);
         }
     }
 
