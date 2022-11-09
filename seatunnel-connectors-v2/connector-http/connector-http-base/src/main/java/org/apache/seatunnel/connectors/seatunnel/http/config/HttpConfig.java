908f41cb9a17f16cb4fe4c5a21b3e744896f47fd
TaoZex <2633363995@qq.com>
2022-11-10 00:20:04 +0800

    [Feature][Connector-V2][Lemlist]Add Lemlist e2e test

--- seatunnel-connectors-v2/connector-http/connector-http-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/http/config/HttpConfig.java
+++ seatunnel-connectors-v2/connector-http/connector-http-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/http/config/HttpConfig.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class HttpConfig {
     public static final String METHOD_DEFAULT_VALUE = "GET";
     public static final String DEFAULT_FORMAT = "json";
+    public static final String BASIC = "Basic";
     public static final int DEFAULT_RETRY_BACKOFF_MULTIPLIER_MS = 100;
     public static final int DEFAULT_RETRY_BACKOFF_MAX_MS = 10000;
     public static final Option<String> URL = Options.key("url")
