ab1826c7e982a99524389bfdf55e4ae0dff909e8
tyrantlucifer <tyrantlucifer@gmail.com>
2022-11-09 19:41:53 +0800

    [Feature][Connector-V2][File] Add source options

--- seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/BaseSourceConfig.java
+++ seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/BaseSourceConfig.java
@@ -17,13 +17,39 @@
 
 package org.apache.seatunnel.connectors.seatunnel.file.config;
 
+import org.apache.seatunnel.api.configuration.Option;
+import org.apache.seatunnel.api.configuration.Options;
+import org.apache.seatunnel.common.utils.DateTimeUtils;
+import org.apache.seatunnel.common.utils.DateUtils;
+import org.apache.seatunnel.common.utils.TimeUtils;
+
 public class BaseSourceConfig {
-    public static final String FILE_TYPE = "type";
-    public static final String FILE_PATH = "path";
-    public static final String SCHEMA = "schema";
-    public static final String DELIMITER = "delimiter";
-    public static final String DATE_FORMAT = "date_format";
-    public static final String DATETIME_FORMAT = "datetime_format";
-    public static final String TIME_FORMAT = "time_format";
-    public static final String PARSE_PARTITION_FROM_PATH = "parse_partition_from_path";
+    public static final Option<String> FILE_TYPE = Options.key("type")
+            .stringType()
+            .noDefaultValue()
+            .withDescription("File type");
+    public static final Option<String> FILE_PATH = Options.key("path")
+            .stringType()
+            .noDefaultValue()
+            .withDescription("File path");
+    public static final Option<String> DELIMITER = Options.key("delimiter")
+            .stringType()
+            .defaultValue(String.valueOf('\001'))
+            .withDescription("Filed delimiter");
+    public static final Option<String> DATE_FORMAT = Options.key("date_format")
+            .stringType()
+            .defaultValue(DateUtils.Formatter.YYYY_MM_DD.getValue())
+            .withDescription("Date format");
+    public static final Option<String> DATETIME_FORMAT = Options.key("datetime_format")
+            .stringType()
+            .defaultValue(DateTimeUtils.Formatter.YYYY_MM_DD_HH_MM_SS.getValue())
+            .withDescription("Datetime format");
+    public static final Option<String> TIME_FORMAT = Options.key("time_format")
+            .stringType()
+            .defaultValue(TimeUtils.Formatter.HH_MM_SS.getValue())
+            .withDescription("Time format");
+    public static final Option<Boolean> PARSE_PARTITION_FROM_PATH = Options.key("parse_partition_from_path")
+            .booleanType()
+            .defaultValue(true)
+            .withDescription("Is or not parse partition fields from file path");
 }
