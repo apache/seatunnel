8b337a7dfaaad84df3c78c0b66a9e149c482d465
lianghuan <Coderlianghuan@gmail.com>
2022-11-10 01:40:17 +0800

    [Feature][Connector-V2][Slack] Fix Code Style

--- seatunnel-connectors-v2/connector-slack/src/main/java/org/apache/seatunnel/connectors/seatunnel/sink/SlackSink.java
+++ seatunnel-connectors-v2/connector-slack/src/main/java/org/apache/seatunnel/connectors/seatunnel/sink/SlackSink.java
@@ -17,12 +17,19 @@
 
 package org.apache.seatunnel.connectors.seatunnel.sink;
 
+import static org.apache.seatunnel.connectors.seatunnel.config.SlackConfig.OAUTH_TOKEN;
+import static org.apache.seatunnel.connectors.seatunnel.config.SlackConfig.SLACK_CHANNEL;
+import static org.apache.seatunnel.connectors.seatunnel.config.SlackConfig.WEBHOOKS_URL;
+
 import org.apache.seatunnel.api.common.PrepareFailException;
 import org.apache.seatunnel.api.sink.SeaTunnelSink;
 import org.apache.seatunnel.api.sink.SinkWriter;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
+import org.apache.seatunnel.common.config.CheckConfigUtil;
+import org.apache.seatunnel.common.config.CheckResult;
+import org.apache.seatunnel.common.constants.PluginType;
 import org.apache.seatunnel.connectors.seatunnel.common.sink.AbstractSimpleSink;
 import org.apache.seatunnel.connectors.seatunnel.common.sink.AbstractSinkWriter;
 
@@ -31,6 +38,7 @@ import org.apache.seatunnel.shade.com.typesafe.config.Config;
 import com.google.auto.service.AutoService;
 
 import java.io.IOException;
+
 
 
 /**
@@ -64,6 +72,10 @@ public class SlackSink extends AbstractSimpleSink<SeaTunnelRow, Void> {
 
     @Override
     public void prepare(Config pluginConfig) throws PrepareFailException {
+        CheckResult checkResult = CheckConfigUtil.checkAllExists(pluginConfig, WEBHOOKS_URL, OAUTH_TOKEN, SLACK_CHANNEL);
+        if (!checkResult.isSuccess()) {
+            throw new PrepareFailException("Slack", PluginType.SINK, checkResult.getMsg());
+        }
         this.pluginConfig = pluginConfig;
     }
 }
