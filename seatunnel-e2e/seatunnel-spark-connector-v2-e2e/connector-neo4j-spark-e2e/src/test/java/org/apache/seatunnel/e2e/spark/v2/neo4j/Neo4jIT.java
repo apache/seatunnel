f97ef4029aa99dc9b96840f7ba8b9b2185bea36f
gaojun <gaojun2048@gmail.com>
2022-11-11 14:40:04 +0800

    update e2e port

--- seatunnel-e2e/seatunnel-spark-connector-v2-e2e/connector-neo4j-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/neo4j/Neo4jIT.java
+++ seatunnel-e2e/seatunnel-spark-connector-v2-e2e/connector-neo4j-spark-e2e/src/test/java/org/apache/seatunnel/e2e/spark/v2/neo4j/Neo4jIT.java
@@ -59,7 +59,7 @@ public class Neo4jIT extends SparkContainer {
 
     private static final String CONTAINER_IMAGE = "neo4j:latest";
     private static final String CONTAINER_HOST = "neo4j-host";
-    private static final int CONTAINER_PORT = 7687;
+    private static final int CONTAINER_PORT = 9688;
     private static final String CONTAINER_NEO4J_USERNAME = "neo4j";
     private static final String CONTAINER_NEO4J_PASSWORD = "1234";
     private static final URI CONTAINER_URI = URI.create("neo4j://localhost:" + CONTAINER_PORT);
