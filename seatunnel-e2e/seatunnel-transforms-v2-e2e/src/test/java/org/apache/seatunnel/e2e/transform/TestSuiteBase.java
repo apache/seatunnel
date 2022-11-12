541ae295e7e5609168cff9fa9a4178b827c6513c
yangbinbin <yangbinbin@aspirecn.com>
2022-11-12 19:39:35 +0800

    update

--- seatunnel-e2e/seatunnel-transforms-v2-e2e/src/test/java/org/apache/seatunnel/e2e/transform/TestSuiteBase.java
+++ seatunnel-e2e/seatunnel-transforms-v2-e2e/src/test/java/org/apache/seatunnel/e2e/transform/TestSuiteBase.java
@@ -37,7 +37,7 @@ import org.testcontainers.containers.Network;
     TestCaseInvocationContextProvider.class
 })
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DisabledOnContainer(value = {}, type = {EngineType.SEATUNNEL, EngineType.FLINK}, disabledReason = "TODO: Transform v2 translation to flink isn't completed")
+@DisabledOnContainer(value = {}, type = {EngineType.FLINK}, disabledReason = "TODO: Transform v2 translation to flink isn't completed")
 public abstract class TestSuiteBase {
 
     protected static final Network NETWORK = TestContainer.NETWORK;
