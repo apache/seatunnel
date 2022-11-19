372589e6e7a84ce9de72dc524d77b9d8f5514608
TaoZex <2633363995@qq.com>
2022-11-19 21:37:41 +0800

    [Feature][Connector-V2][Doris]update code

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcDorisdbIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcDorisdbIT.java
@@ -58,7 +58,7 @@ public class JdbcDorisdbIT extends AbstractJdbcIT {
         "  CHAR_COL       CHAR,\n" +
         "  VARCHAR_11_COL VARCHAR(11),\n" +
         "  STRING_COL     STRING,\n" +
-        "  DATETIME_COL   DATETIME,\n" +
+        "  DATETIME_COL   DATETIME\n" +
         ")ENGINE=OLAP\n" +
         "DUPLICATE KEY(`BIGINT_COL`)\n" +
         "DISTRIBUTED BY HASH(`BIGINT_COL`) BUCKETS 1\n" +
