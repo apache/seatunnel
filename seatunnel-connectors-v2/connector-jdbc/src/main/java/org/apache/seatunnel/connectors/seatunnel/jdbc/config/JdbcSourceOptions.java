72b8a73cab159fe5a1fba950c60d5de24838cffa
john <wenwei8268@gmail.com>
2022-11-20 00:02:42 +0800

    [feature][connector][jdbc] expose configurable options in JDBC (#3410)
    * [Feature][Connector V2] expose configurable options in JDBC
    
    * Update seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/sink/JdbcSinkFactory.java
    
    Co-authored-by: hailin0 <hailin088@gmail.com>
    
    * [Feature][Connector V2] expose configurable options in jdbc
    
    Co-authored-by: wenwei <zhouwenwei@Jwenwei-MacBook-Pro.local>
    Co-authored-by: hailin0 <hailin088@gmail.com>

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/config/JdbcSourceOptions.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/config/JdbcSourceOptions.java
@@ -41,18 +41,18 @@ public class JdbcSourceOptions implements Serializable {
 
     public JdbcSourceOptions(Config config) {
         this.jdbcConnectionOptions = buildJdbcConnectionOptions(config);
-        this.query = config.getString(JdbcConfig.QUERY);
-        if (config.hasPath(JdbcConfig.PARTITION_COLUMN)) {
-            this.partitionColumn = config.getString(JdbcConfig.PARTITION_COLUMN);
+        this.query = config.getString(JdbcConfig.QUERY.key());
+        if (config.hasPath(JdbcConfig.PARTITION_COLUMN.key())) {
+            this.partitionColumn = config.getString(JdbcConfig.PARTITION_COLUMN.key());
         }
-        if (config.hasPath(JdbcConfig.PARTITION_UPPER_BOUND)) {
-            this.partitionUpperBound = config.getLong(JdbcConfig.PARTITION_UPPER_BOUND);
+        if (config.hasPath(JdbcConfig.PARTITION_UPPER_BOUND.key())) {
+            this.partitionUpperBound = config.getLong(JdbcConfig.PARTITION_UPPER_BOUND.key());
         }
-        if (config.hasPath(JdbcConfig.PARTITION_LOWER_BOUND)) {
-            this.partitionLowerBound = config.getLong(JdbcConfig.PARTITION_LOWER_BOUND);
+        if (config.hasPath(JdbcConfig.PARTITION_LOWER_BOUND.key())) {
+            this.partitionLowerBound = config.getLong(JdbcConfig.PARTITION_LOWER_BOUND.key());
         }
-        if (config.hasPath(JdbcConfig.PARTITION_NUM)) {
-            this.partitionNumber = config.getInt(JdbcConfig.PARTITION_NUM);
+        if (config.hasPath(JdbcConfig.PARTITION_NUM.key())) {
+            this.partitionNumber = config.getInt(JdbcConfig.PARTITION_NUM.key());
         }
     }
 
