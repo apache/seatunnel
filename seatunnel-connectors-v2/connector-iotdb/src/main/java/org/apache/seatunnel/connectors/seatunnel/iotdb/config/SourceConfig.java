bc88fd10145a91afdfef7fb6fa13d22a521a1cce
john <wenwei8268@gmail.com>
2022-11-21 09:57:38 +0800

    [Feature][Connector V2] expose configurable options in IoTDB (#3387)
    Co-authored-by: wenwei <zhouwenwei@Jwenwei-MacBook-Pro.local>

--- seatunnel-connectors-v2/connector-iotdb/src/main/java/org/apache/seatunnel/connectors/seatunnel/iotdb/config/SourceConfig.java
+++ seatunnel-connectors-v2/connector-iotdb/src/main/java/org/apache/seatunnel/connectors/seatunnel/iotdb/config/SourceConfig.java
@@ -17,6 +17,9 @@
 
 package org.apache.seatunnel.connectors.seatunnel.iotdb.config;
 
+import org.apache.seatunnel.api.configuration.Option;
+import org.apache.seatunnel.api.configuration.Options;
+
 /**
  * SourceConfig is the configuration for the IotDBSource.
  * <p>
@@ -25,19 +28,19 @@ package org.apache.seatunnel.connectors.seatunnel.iotdb.config;
  */
 public class SourceConfig {
 
-    public static final String SQL = "sql";
+    public static final Option<String> SQL = Options.key("sql").stringType().noDefaultValue().withDescription("sql");
 
     /*---------------------- single node configurations -------------------------*/
 
     /**
      * The host of the IotDB server.
      */
-    public static final String HOST = "host";
+    public static final Option<String> HOST = Options.key("host").stringType().noDefaultValue().withDescription("host");
 
     /*
      * The port of the IotDB server.
      */
-    public static final String PORT = "port";
+    public static final Option<Integer> PORT = Options.key("port").intType().noDefaultValue().withDescription("port");
 
 
     /*---------------------- multiple node configurations -------------------------*/
@@ -45,58 +48,58 @@ public class SourceConfig {
     /**
      * Username for the source.
      */
-    public static final String USERNAME = "username";
+    public static final Option<String> USERNAME = Options.key("username").stringType().noDefaultValue().withDescription("usernam");
 
     /**
      * Password for the source.
      */
-    public static final String PASSWORD = "password";
+    public static final Option<String> PASSWORD = Options.key("password").stringType().noDefaultValue().withDescription("password");
 
     /**
      * multiple nodes
      */
-    public static final String NODE_URLS = "node_urls";
+    public static final Option<String> NODE_URLS = Options.key("node_urls").stringType().noDefaultValue().withDescription("node urls");
 
     /*---------------------- other configurations -------------------------*/
 
     /**
      * Fetches the next batch of data from the source.
      */
-    public static final String FETCH_SIZE = "fetch_size";
+    public static final Option<Integer> FETCH_SIZE = Options.key("fetch_size").intType().noDefaultValue().withDescription("fetch size");
 
     /**
      * thrift default buffer size
      */
-    public static final String THRIFT_DEFAULT_BUFFER_SIZE = "thrift_default_buffer_size";
+    public static final Option<Integer> THRIFT_DEFAULT_BUFFER_SIZE = Options.key("thrift_default_buffer_size").intType().noDefaultValue().withDescription(" default thrift buffer size of iot db ");
 
     /**
      * thrift max frame size
      */
-    public static final String THRIFT_MAX_FRAME_SIZE = "thrift_max_frame_size";
+    public static final Option<Integer> THRIFT_MAX_FRAME_SIZE = Options.key("thrift_max_frame_size").intType().noDefaultValue().withDescription("thrift max frame size ");
 
     /**
      * cassandra default buffer size
      */
-    public static final String ENABLE_CACHE_LEADER = "enable_cache_leader";
+    public static final Option<Boolean> ENABLE_CACHE_LEADER = Options.key("enable_cache_leader").booleanType().noDefaultValue().withDescription("enable cache leader ");
 
     /**
      * Version represents the SQL semantic version used by the client, which is used to be compatible with the SQL semantics of 0.12 when upgrading 0.13. The possible values are: V_0_12, V_0_13.
      */
-    public static final String VERSION = "version";
+    public static final Option<String> VERSION = Options.key("version").stringType().noDefaultValue().withDescription("version");
 
     /**
      * Query lower bound of the time range to be read.
      */
-    public static final String LOWER_BOUND = "lower_bound";
+    public static final Option<Long> LOWER_BOUND = Options.key("lower_bound").longType().noDefaultValue().withDescription("low bound");
 
     /**
      * Query upper bound of the time range to be read.
      */
-    public static final String UPPER_BOUND = "upper_bound";
+    public static final Option<Long> UPPER_BOUND = Options.key("upper_bound").longType().noDefaultValue().withDescription("upper bound");
 
     /**
      * Query num partitions to be read.
      */
-    public static final String NUM_PARTITIONS = "num_partitions";
+    public static final Option<Integer> NUM_PARTITIONS = Options.key("num_partitions").intType().noDefaultValue().withDescription("num partitions");
 
 }
