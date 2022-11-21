efa04b38fe3cb893083d8c3577734f5c7b96fa5d
Eric <gaojun2048@gmail.com>
2022-11-21 22:42:43 +0800

    [Feature][Connector][Neo4j] expose configurable options in Neo4j (#3342)
    * Update Neo4j Source and Sink and Split Config Options and QueryInfo

--- seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/config/Neo4jSinkQueryInfo.java
+++ seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/config/Neo4jSinkQueryInfo.java
@@ -17,16 +17,11 @@
 
 package org.apache.seatunnel.connectors.seatunnel.neo4j.config;
 
-import org.apache.seatunnel.api.configuration.Option;
-import org.apache.seatunnel.api.configuration.Options;
+import lombok.Data;
 
 import java.util.Map;
 
-public class Neo4jSinkConfig extends Neo4jCommonConfig {
-    public static final Option<Map<String, String>> QUERY_PARAM_POSITION =
-        Options.key("queryParamPosition")
-            .mapType()
-            .noDefaultValue()
-            .withDescription(
-                "position mapping information for query parameters. key name is parameter placeholder name. associated value is position of field in input data row.");
+@Data
+public class Neo4jSinkQueryInfo extends Neo4jQueryInfo {
+    private Map<String, Object> queryParamPosition;
 }
--- seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/config/Neo4jSourceQueryInfo.java
+++ seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/config/Neo4jSourceQueryInfo.java
@@ -17,16 +17,5 @@
 
 package org.apache.seatunnel.connectors.seatunnel.neo4j.config;
 
-import org.apache.seatunnel.api.configuration.Option;
-import org.apache.seatunnel.api.configuration.Options;
-
-import java.util.Map;
-
-public class Neo4jSinkConfig extends Neo4jCommonConfig {
-    public static final Option<Map<String, String>> QUERY_PARAM_POSITION =
-        Options.key("queryParamPosition")
-            .mapType()
-            .noDefaultValue()
-            .withDescription(
-                "position mapping information for query parameters. key name is parameter placeholder name. associated value is position of field in input data row.");
+public class Neo4jSourceQueryInfo extends Neo4jQueryInfo {
 }
