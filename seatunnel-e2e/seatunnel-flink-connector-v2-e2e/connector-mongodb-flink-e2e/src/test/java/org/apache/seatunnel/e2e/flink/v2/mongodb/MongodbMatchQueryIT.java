b34a20f248fa0c7a5f66a48ebda37dfefce969c6
shihong90 <2572805166@qq.com>
2022-12-13 14:14:57 +0800

    fix

--- seatunnel-e2e/seatunnel-flink-connector-v2-e2e/connector-mongodb-flink-e2e/src/test/java/org/apache/seatunnel/e2e/flink/v2/mongodb/MongodbMatchQueryIT.java
+++ seatunnel-e2e/seatunnel-flink-connector-v2-e2e/connector-mongodb-flink-e2e/src/test/java/org/apache/seatunnel/e2e/flink/v2/mongodb/MongodbMatchQueryIT.java
@@ -76,6 +76,7 @@ public class MongodbMatchQueryIT extends FlinkContainer {
     private static final String MONGODB_SINK_TABLE = "sink_table";
 
     private static final List<Document> TEST_DATASET = generateTestDataSet();
+    private static final List<Document> RESULT_DATASET = generateResultDataSet();
 
     private GenericContainer<?> mongodbContainer;
     private MongoClient client;
@@ -108,7 +109,7 @@ public class MongodbMatchQueryIT extends FlinkContainer {
         Container.ExecResult execResult = executeSeaTunnelFlinkJob("/mongodb/mongodb_source_matchQuery_and_sink.conf");
         Assertions.assertEquals(0, execResult.getExitCode());
         Assertions.assertIterableEquals(
-            TEST_DATASET.stream()
+            RESULT_DATASET.stream()
                 .map(e -> {
                     e.remove("_id");
                     return e;
@@ -216,6 +217,72 @@ public class MongodbMatchQueryIT extends FlinkContainer {
         }
         return documents;
     }
+
+    private static List<Document> generateResultDataSet() {
+        SeaTunnelRowType seatunnelRowType = new SeaTunnelRowType(
+            new String[]{
+                "id",
+                "c_map",
+                "c_array",
+                "c_string",
+                "c_boolean",
+                "c_tinyint",
+                "c_smallint",
+                "c_int",
+                "c_bigint",
+                "c_float",
+                "c_double",
+                "c_decimal",
+                "c_bytes",
+                "c_date",
+                "c_timestamp"
+            },
+            new SeaTunnelDataType[]{
+                BasicType.LONG_TYPE,
+                new MapType(BasicType.STRING_TYPE, BasicType.SHORT_TYPE),
+                ArrayType.BYTE_ARRAY_TYPE,
+                BasicType.STRING_TYPE,
+                BasicType.BOOLEAN_TYPE,
+                BasicType.BYTE_TYPE,
+                BasicType.SHORT_TYPE,
+                BasicType.INT_TYPE,
+                BasicType.LONG_TYPE,
+                BasicType.FLOAT_TYPE,
+                BasicType.DOUBLE_TYPE,
+                new DecimalType(2, 1),
+                PrimitiveByteArrayType.INSTANCE,
+                LocalTimeType.LOCAL_DATE_TYPE,
+                LocalTimeType.LOCAL_DATE_TIME_TYPE
+            }
+        );
+        Serializer serializer = new DefaultSerializer(seatunnelRowType);
+
+        List<Document> documents = new ArrayList<>();
+        for (int i = 4; i < 100; i++) {
+            SeaTunnelRow row = new SeaTunnelRow(
+                new Object[]{
+                    Long.valueOf(i),
+                    Collections.singletonMap("key", Short.parseShort("1")),
+                    new Byte[]{Byte.parseByte("1")},
+                    "string",
+                    Boolean.FALSE,
+                    Byte.parseByte("1"),
+                    Short.parseShort("1"),
+                    Integer.parseInt("1"),
+                    Long.parseLong("1"),
+                    Float.parseFloat("1.1"),
+                    Double.parseDouble("1.1"),
+                    BigDecimal.valueOf(11, 1),
+                    "test".getBytes(),
+                    LocalDate.now(),
+                    LocalDateTime.now()
+                });
+            documents.add(serializer.serialize(row));
+        }
+        return documents;
+    }
+
+
 
     @AfterEach
     public void closeMongoContainer() {
