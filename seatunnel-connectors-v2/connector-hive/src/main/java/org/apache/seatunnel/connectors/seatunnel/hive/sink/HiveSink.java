ac4e880fb58ece7e0920c962c11f4d768d6519b1
Tyrantlucifer <TyrantLucifer@gmail.com>
2022-12-17 20:31:21 +0800

    [Feature][Connector-V2][File] Optimize filesystem utils (#3749)

--- seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/sink/HiveSink.java
+++ seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/sink/HiveSink.java
@@ -130,6 +130,6 @@ public class HiveSink extends BaseHdfsFileSink {
 
     @Override
     public Optional<SinkAggregatedCommitter<FileCommitInfo, FileAggregatedCommitInfo>> createAggregatedCommitter() throws IOException {
-        return Optional.of(new HiveSinkAggregatedCommitter(pluginConfig, dbName, tableName, hadoopConf));
+        return Optional.of(new HiveSinkAggregatedCommitter(pluginConfig, dbName, tableName, fileSystemUtils));
     }
 }
