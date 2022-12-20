8838eb7a59c6ccfe97e78f214dfc36292c3fe9b1
tyrantlucifer <tyrantlucifer@gmail.com>
2022-12-20 16:55:58 +0800

    [Improve][Connector-V2][HDFS] Support setting hdfs-site.xml

--- seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/BaseSourceConfig.java
+++ seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/BaseSourceConfig.java
@@ -52,4 +52,8 @@ public class BaseSourceConfig {
             .booleanType()
             .defaultValue(true)
             .withDescription("Whether parse partition fields from file path");
+    public static final Option<String> HDFS_SITE_PATH = Options.key("hdfs_site_path")
+            .stringType()
+            .noDefaultValue()
+            .withDescription("The path of hdfs-site.xml");
 }
