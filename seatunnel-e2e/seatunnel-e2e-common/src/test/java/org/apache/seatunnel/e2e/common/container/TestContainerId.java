de68a39d29b732e1fa97d818d3de862a8d688a98
tyrantlucifer <tyrantlucifer@gmail.com>
2023-01-21 17:36:52 +0800

    [Feature][Core][Spark3] Update dist and support spark3 e2e

--- seatunnel-e2e/seatunnel-e2e-common/src/test/java/org/apache/seatunnel/e2e/common/container/TestContainerId.java
+++ seatunnel-e2e/seatunnel-e2e-common/src/test/java/org/apache/seatunnel/e2e/common/container/TestContainerId.java
@@ -31,6 +31,7 @@ public enum TestContainerId {
     FLINK_1_15(FLINK, "1.15.3"),
     FLINK_1_16(FLINK, "1.16.0"),
     SPARK_2_4(SPARK, "2.4.6"),
+    SPARK_3_3(SPARK, "3.3.0"),
     SEATUNNEL(EngineType.SEATUNNEL, "2.2.0");
 
     private final EngineType engineType;
