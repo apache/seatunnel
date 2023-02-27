bfca344ecb64410f78b6c09fa7514eabb3db78c0
gaojun <gaojun2048@gmail.com>
2023-02-27 16:15:08 +0800

    add database name and table name to error log

--- seatunnel-connectors-v2/connector-starrocks/src/main/java/org/apache/seatunnel/connectors/seatunnel/starrocks/client/StarRocksStreamLoadVisitor.java
+++ seatunnel-connectors-v2/connector-starrocks/src/main/java/org/apache/seatunnel/connectors/seatunnel/starrocks/client/StarRocksStreamLoadVisitor.java
@@ -105,7 +105,11 @@ public class StarRocksStreamLoadVisitor {
                             .toString());
         }
         if (RESULT_FAILED.equals(loadResult.get(keyStatus))) {
-            StringBuilder errorBuilder = new StringBuilder("Failed to flush data to StarRocks.\n");
+            StringBuilder errorBuilder = new StringBuilder("Failed to flush data to StarRocks \n");
+            errorBuilder.append(sinkConfig.getDatabase())
+                .append("/")
+                .append(sinkConfig.getTable())
+                .append("\n");
             if (loadResult.containsKey("Message")) {
                 errorBuilder.append(loadResult.get("Message"));
                 errorBuilder.append('\n');
