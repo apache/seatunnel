e809d87750c69f56b90215eb0c437f0dd21d9fec
Guangdong Liu <804167098@qq.com>
2023-03-20 18:43:04 +0800

    [Improve][Seatunnel-Cluster] Run the server through  daemon (#4161)
    * Add Support SeaTunnel Zeta Cluster Node Run Through A Daemon

--- seatunnel-core/seatunnel-starter/src/main/bin/seatunnel-cluster.sh
+++ seatunnel-core/seatunnel-starter/src/main/bin/seatunnel-cluster.sh
@@ -38,6 +38,7 @@ APP_DIR=`cd "$PRG_DIR/.." >/dev/null; pwd`
 CONF_DIR=${APP_DIR}/config
 APP_JAR=${APP_DIR}/starter/seatunnel-starter.jar
 APP_MAIN="org.apache.seatunnel.core.starter.seatunnel.SeaTunnelServer"
+OUT="${APP_DIR}/logs/seatunnel-server.out"
 
 if [ -f "${CONF_DIR}/seatunnel-env.sh" ]; then
     . "${CONF_DIR}/seatunnel-env.sh"
@@ -69,7 +70,10 @@ do
   if [[ "${i}" == *"JvmOption"* ]]; then
     JVM_OPTION="${i}"
     JAVA_OPTS="${JAVA_OPTS} ${JVM_OPTION#*=}"
-    break
+  elif [[ "${i}" == "-d" || "${i}" == "--daemon" ]]; then
+    DAEMON=true
+  elif [[ "${i}" == "-h" || "${i}" == "--help" ]]; then
+    HELP=true
   fi
 done
 
@@ -92,4 +96,10 @@ JVM_OPTIONS=`java -cp ${CLASS_PATH} org.apache.seatunnel.core.starter.seatunnel.
 JAVA_OPTS="${JAVA_OPTS} ${JVM_OPTIONS//\$\{loggc\}/${ST_TMPDIR}}"
 echo "JAVA_OPTS:" ${JAVA_OPTS}
 
-java ${JAVA_OPTS} -cp ${CLASS_PATH} ${APP_MAIN} ${args}
+if [[ $DAEMON == true && $HELP == false ]]; then
+ touch $SEATUNNEL_HOME/logs/seatunnel-server.out
+ java ${JAVA_OPTS} -cp ${CLASS_PATH} ${APP_MAIN} ${args} > "$OUT" 200<&- 2>&1 < /dev/null &
+ else
+ java ${JAVA_OPTS} -cp ${CLASS_PATH} ${APP_MAIN} ${args}
+fi
+
