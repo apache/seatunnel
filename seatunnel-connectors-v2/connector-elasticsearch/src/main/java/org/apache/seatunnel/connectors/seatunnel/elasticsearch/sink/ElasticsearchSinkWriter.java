d7461e9fe79a29aeb76874db3e98962cd84a21ad
zhilinli <lzl15844876351@163.com>
2023-03-22 17:42:30 +0800

    [Bug] [connector-v2] [Eleasticsearch] document description error #4370

--- seatunnel-connectors-v2/connector-elasticsearch/src/main/java/org/apache/seatunnel/connectors/seatunnel/elasticsearch/sink/ElasticsearchSinkWriter.java
+++ seatunnel-connectors-v2/connector-elasticsearch/src/main/java/org/apache/seatunnel/connectors/seatunnel/elasticsearch/sink/ElasticsearchSinkWriter.java
@@ -93,9 +93,9 @@ public class ElasticsearchSinkWriter
         this.retryMaterial =
                 new RetryMaterial(maxRetryCount, true, exception -> true, DEFAULT_SLEEP_TIME_MS);
         // Initialize the interval flush
-        if(this.batchIntervalMs>0){
+        if (this.batchIntervalMs > 0) {
             open();
-            log.info("The initial scheduling is complete batch_interval_ms :"+batchIntervalMs);
+            log.info("The initial scheduling is complete batch_interval_ms :" + batchIntervalMs);
         }
     }
 
