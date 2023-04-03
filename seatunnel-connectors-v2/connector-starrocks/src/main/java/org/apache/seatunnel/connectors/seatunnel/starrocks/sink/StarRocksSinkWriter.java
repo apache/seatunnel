9ca5a6d87928e2142644f0739d798cf7c88ca29f
Hisoka <fanjiaeminem@qq.com>
2023-04-03 18:10:40 +0800

    [Improve] [Connector-V2] Throw StarRocks Serialize Error To Client

--- seatunnel-connectors-v2/connector-starrocks/src/main/java/org/apache/seatunnel/connectors/seatunnel/starrocks/sink/StarRocksSinkWriter.java
+++ seatunnel-connectors-v2/connector-starrocks/src/main/java/org/apache/seatunnel/connectors/seatunnel/starrocks/sink/StarRocksSinkWriter.java
@@ -61,8 +61,8 @@ public class StarRocksSinkWriter extends AbstractSinkWriter<SeaTunnelRow, Void> 
             record = serializer.serialize(element);
         } catch (Exception e) {
             throw new StarRocksConnectorException(
-                    CommonErrorCode.WRITER_OPERATION_FAILED,
-                    "serialize failed. Row={" + element + "}");
+                CommonErrorCode.WRITER_OPERATION_FAILED,
+                "serialize failed. Row={" + element + "}", e);
         }
         manager.write(record);
     }
