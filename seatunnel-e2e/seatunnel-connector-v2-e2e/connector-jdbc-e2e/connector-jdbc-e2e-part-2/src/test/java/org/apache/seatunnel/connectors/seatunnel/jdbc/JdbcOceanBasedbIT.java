a18844f58ae0f566d5abbc59f0dd9def61a34261
silenceland <silenceland23@163.com>
2023-04-17 08:50:10 +0800

    [Feature][Connector-V2][OceanBase]fix column float or double

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/connector-jdbc-e2e-part-2/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcOceanBasedbIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/connector-jdbc-e2e-part-2/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcOceanBasedbIT.java
@@ -42,127 +42,46 @@ public class JdbcOceanBasedbIT extends AbstractJdbcIT {
     private static final int PORT = 2881;
     private static final String DATABASE = "seatunnel";
     private static final String URL =
-            "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?createDatabaseIfNotExist=true";
+            "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?createDatabaseIfNotExist=true&_enable_convert_real_to_decimal=true";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "";
     private static final String SOURCE_TABLE = "e2e_ob_source";
     private static final String SINK_TABLE = "e2e_ob_sink";
     private static final String DRIVER_JAR =
             "https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.16/mysql-connector-java-8.0.16.jar";
     private static final List<String> CONFIG_FILE =
             Lists.newArrayList("/jdbc_oceanbase_source_and_sink.conf");
-    private static final String COLUMN_STRING =
-            "col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col16,col17,col18,col19,col20,col21,col22,col23,col24,col25,col26,col27";
-
-    private static final String DDL_SOURCE =
-            "CREATE TABLE `"
-                    + DATABASE
-                    + "."
-                    + SOURCE_TABLE
-                    + "` (\n"
-                    + "  `col1` int(11) NOT NULL AUTO_INCREMENT COMMENT '第一列', \n"
-                    + "  `col2` integer DEFAULT NULL COMMENT '第二列',\n"
-                    + "  `col3` decimal(10,2) DEFAULT NULL COMMENT '第三列',\n"
-                    + "  `col4` number DEFAULT NULL COMMENT '第四列',\n"
-                    + "  `col5` tinyint(4) DEFAULT NULL COMMENT '第五列',\n"
-                    + "  `col6` smallint(6) DEFAULT NULL COMMENT '第六列',\n"
-                    + "  `col7` mediumint(9) DEFAULT NULL COMMENT '第七列',\n"
-                    + "  `col8` bigint(20) DEFAULT NULL COMMENT '第八列',\n"
-                    + "  `col9` float(10,2) DEFAULT NULL COMMENT '第九列',\n"
-                    + "  `col10` double(10,2) DEFAULT NULL COMMENT '第十列',\n"
-                    + "  `col11` varchar(10) DEFAULT NULL COMMENT '第十一列',\n"
-                    + "  `col12` char(10) NOT NULL DEFAULT '中文字段' COMMENT '第十二列',\n"
-                    + "  `col13` text DEFAULT NULL COMMENT '第十三列',\n"
-                    + "  `col14` tinytext DEFAULT NULL COMMENT '第十四列',\n"
-                    + "  `col15` mediumtext DEFAULT NULL COMMENT '第十五列',\n"
-                    + "  `col16` longtext DEFAULT NULL COMMENT '第十六列',\n"
-                    + "  `col17` blob DEFAULT NULL COMMENT '第十七列',\n"
-                    + "  `col18` tinyblob DEFAULT NULL COMMENT '第十八列',\n"
-                    + "  `col19` longblob DEFAULT NULL COMMENT '第十九列',\n"
-                    + "  `col20` mediumblob DEFAULT NULL COMMENT '第二十列',\n"
-                    + "  `col21` binary(16) DEFAULT NULL COMMENT '第二十一列',\n"
-                    + "  `col22` varbinary(16) DEFAULT NULL COMMENT '第二十二列',\n"
-                    + "  `col23` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '第二十三列',\n"
-                    + "  `col24` time DEFAULT NULL COMMENT '第二十四列',\n"
-                    + "  `col25` date DEFAULT NULL COMMENT '第二十五列',\n"
-                    + "  `col26` datetime DEFAULT NULL COMMENT '第二十六列',\n"
-                    + "  `col27` year(4) DEFAULT NULL COMMENT '第二十七列',\n"
-                    + "  PRIMARY KEY (`col1`)\n"
-                    + ") AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC COMPRESSION = 'zstd_1.0' REPLICA_NUM = 1 BLOCK_SIZE = 16384 USE_BLOOM_FILTER = FALSE TABLET_SIZE = 134217728 PCTFREE = 0 COMMENT = '"
-                    + SOURCE_TABLE
-                    + "'";
 
     private static final String CREATE_SQL =
-            "CREATE TABLE `"
-                    + DATABASE
-                    + "."
-                    + SINK_TABLE
-                    + "` (\n"
-                    + "  `col1` int(11) NOT NULL AUTO_INCREMENT COMMENT '第一列', \n"
-                    + "  `col2` integer DEFAULT NULL COMMENT '第二列',\n"
-                    + "  `col3` decimal(10,2) DEFAULT NULL COMMENT '第三列',\n"
-                    + "  `col4` number DEFAULT NULL COMMENT '第四列',\n"
-                    + "  `col5` tinyint(4) DEFAULT NULL COMMENT '第五列',\n"
-                    + "  `col6` smallint(6) DEFAULT NULL COMMENT '第六列',\n"
-                    + "  `col7` mediumint(9) DEFAULT NULL COMMENT '第七列',\n"
-                    + "  `col8` bigint(20) DEFAULT NULL COMMENT '第八列',\n"
-                    + "  `col9` float(10,2) DEFAULT NULL COMMENT '第九列',\n"
-                    + "  `col10` double(10,2) DEFAULT NULL COMMENT '第十列',\n"
-                    + "  `col11` varchar(10) DEFAULT NULL COMMENT '第十一列',\n"
-                    + "  `col12` char(10) NOT NULL DEFAULT '中文字段' COMMENT '第十二列',\n"
-                    + "  `col13` text DEFAULT NULL COMMENT '第十三列',\n"
-                    + "  `col14` tinytext DEFAULT NULL COMMENT '第十四列',\n"
-                    + "  `col15` mediumtext DEFAULT NULL COMMENT '第十五列',\n"
-                    + "  `col16` longtext DEFAULT NULL COMMENT '第十六列',\n"
-                    + "  `col17` blob DEFAULT NULL COMMENT '第十七列',\n"
-                    + "  `col18` tinyblob DEFAULT NULL COMMENT '第十八列',\n"
-                    + "  `col19` longblob DEFAULT NULL COMMENT '第十九列',\n"
-                    + "  `col20` mediumblob DEFAULT NULL COMMENT '第二十列',\n"
-                    + "  `col21` binary(16) DEFAULT NULL COMMENT '第二十一列',\n"
-                    + "  `col22` varbinary(16) DEFAULT NULL COMMENT '第二十二列',\n"
-                    + "  `col23` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '第二十三列',\n"
-                    + "  `col24` time DEFAULT NULL COMMENT '第二十四列',\n"
-                    + "  `col25` date DEFAULT NULL COMMENT '第二十五列',\n"
-                    + "  `col26` datetime DEFAULT NULL COMMENT '第二十六列',\n"
-                    + "  `col27` year(4) DEFAULT NULL COMMENT '第二十七列',\n"
-                    + "  PRIMARY KEY (`col1`)\n"
-                    + ") AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC COMPRESSION = 'zstd_1.0' REPLICA_NUM = 1 BLOCK_SIZE = 16384 USE_BLOOM_FILTER = FALSE TABLET_SIZE = 134217728 PCTFREE = 0 COMMENT = '"
-                    + SINK_TABLE
-                    + "'";
-
-    private static final String INIT_DATA_SQL =
-            "insert into "
-                    + SOURCE_TABLE
+            "CREATE TABLE %s\n"
                     + " (\n"
-                    + "`col1` ,\n"
-                    + "`col2` ,\n"
-                    + "`col3` ,\n"
-                    + "`col4` ,\n"
-                    + "`col5` ,\n"
-                    + "`col6` ,\n"
-                    + "`col7` ,\n"
-                    + "`col8` ,\n"
-                    + "`col9` ,\n"
-                    + "`col10`,\n"
-                    + "`col11`,\n"
-                    + "`col12`,\n"
-                    + "`col13`,\n"
-                    + "`col14`,\n"
-                    + "`col15`,\n"
-                    + "`col16`,\n"
-                    + "`col17`,\n"
-                    + "`col18`,\n"
-                    + "`col19`,\n"
-                    + "`col20`,\n"
-                    + "`col21`,\n"
-                    + "`col22`,\n"
-                    + "`col23`,\n"
-                    + "`col24`,\n"
-                    + "`col25`,\n"
-                    + "`col26`,\n"
-                    + "`col27`,"
-                    + ")values(\n"
-                    + "\t?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?\n"
+                    + "  col1 INT PRIMARY KEY COMMENT '第一列', \n"
+                    + "  col2 integer DEFAULT NULL COMMENT '第二列',\n"
+                    + "  col3 decimal(10,2) DEFAULT NULL COMMENT '第三列',\n"
+                    + "  col4 number DEFAULT NULL COMMENT '第四列',\n"
+                    + "  col5 tinyint(4) DEFAULT NULL COMMENT '第五列',\n"
+                    + "  col6 smallint(6) DEFAULT NULL COMMENT '第六列',\n"
+                    + "  col7 mediumint(9) DEFAULT NULL COMMENT '第七列',\n"
+                    + "  col8 bigint(20) DEFAULT NULL COMMENT '第八列',\n"
+                    + "  col9 varchar(10) DEFAULT NULL COMMENT '第九列',\n"
+                    + "  col10 varchar(10) DEFAULT NULL COMMENT '第十列',\n"
+                    + "  col11 varchar(10) DEFAULT NULL COMMENT '第十一列',\n"
+                    + "  col12 char(10) NOT NULL DEFAULT '中文字段' COMMENT '第十二列',\n"
+                    + "  col13 text DEFAULT NULL COMMENT '第十三列',\n"
+                    + "  col14 tinytext DEFAULT NULL COMMENT '第十四列',\n"
+                    + "  col15 mediumtext DEFAULT NULL COMMENT '第十五列',\n"
+                    + "  col16 longtext DEFAULT NULL COMMENT '第十六列',\n"
+                    + "  col17 blob DEFAULT NULL COMMENT '第十七列',\n"
+                    + "  col18 tinyblob DEFAULT NULL COMMENT '第十八列',\n"
+                    + "  col19 longblob DEFAULT NULL COMMENT '第十九列',\n"
+                    + "  col20 mediumblob DEFAULT NULL COMMENT '第二十列',\n"
+                    + "  col21 binary(16) DEFAULT NULL COMMENT '第二十一列',\n"
+                    + "  col22 varbinary(16) DEFAULT NULL COMMENT '第二十二列',\n"
+                    + "  col23 timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '第二十三列',\n"
+                    + "  col24 time DEFAULT NULL COMMENT '第二十四列',\n"
+                    + "  col25 date DEFAULT NULL COMMENT '第二十五列',\n"
+                    + "  col26 datetime DEFAULT NULL COMMENT '第二十六列',\n"
+                    + "  col27 year(4) DEFAULT NULL COMMENT '第二十七列'\n"
                     + ")";
 
     @Override
@@ -170,6 +89,9 @@ public class JdbcOceanBasedbIT extends AbstractJdbcIT {
         Map<String, String> containerEnv = new HashMap<>();
         Pair<String[], List<SeaTunnelRow>> testDataSet = initTestData();
         String jdbcUrl = String.format(URL, PORT, DATABASE);
+        String[] fieldNames = testDataSet.getKey();
+        String insertSql = insertTable(DATABASE, SOURCE_TABLE, fieldNames);
+
         return JdbcCase.builder()
                 .dockerImage(OCEANBASE_IMAGE)
                 .networkAliases(OCEANBASE_CONTAINER_HOST)
@@ -187,7 +109,7 @@ public class JdbcOceanBasedbIT extends AbstractJdbcIT {
                 .sinkTable(SINK_TABLE)
                 .createSql(CREATE_SQL)
                 .configFile(CONFIG_FILE)
-                .insertSql(INIT_DATA_SQL)
+                .insertSql(insertSql)
                 .testData(testDataSet)
                 .build();
     }
@@ -223,8 +145,8 @@ public class JdbcOceanBasedbIT extends AbstractJdbcIT {
                                 6,
                                 7,
                                 8,
-                                9.1,
-                                10.1,
+                                "9.1",
+                                "10.1",
                                 "十一",
                                 "十二",
                                 "十三",
@@ -267,7 +189,7 @@ public class JdbcOceanBasedbIT extends AbstractJdbcIT {
 
     @Override
     protected void createSchemaIfNeeded() {
-        String sql = "CREATE DATABASE " + DATABASE;
+        String sql = "CREATE DATABASE IF NOT EXISTS " + DATABASE;
         try {
             connection.prepareStatement(sql).executeUpdate();
         } catch (Exception e) {
