c2b59a7411aaca6db72ab44d631d22a68b29cb85
changhuyan <877018069@qq.com>
2023-04-21 18:13:25 +0800

    [Feature][Jdbc-Connector] Add java.util.*

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/JdbcDialectLoader.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/JdbcDialectLoader.java
@@ -23,6 +23,7 @@ import org.apache.seatunnel.connectors.seatunnel.jdbc.exception.JdbcConnectorExc
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
 import java.util.stream.Collectors;
 
 /** Utility for working with {@link JdbcDialect}. */
