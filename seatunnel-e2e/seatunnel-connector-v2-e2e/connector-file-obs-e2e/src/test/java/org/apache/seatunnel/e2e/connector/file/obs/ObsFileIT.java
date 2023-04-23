c49aca99507d79b122339114b9d2e103879b2101
kim-up <enast@qq.com>
2023-04-23 17:10:40 +0800

    [Connector-v2][obs] Update e2e

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-file-obs-e2e/src/test/java/org/apache/seatunnel/e2e/connector/file/obs/ObsFileIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-file-obs-e2e/src/test/java/org/apache/seatunnel/e2e/connector/file/obs/ObsFileIT.java
@@ -28,7 +28,7 @@ import org.testcontainers.containers.Container;
 
 import java.io.IOException;
 
-@Disabled
+@Disabled("Please testing it in your local environment with obs account conf")
 public class ObsFileIT extends TestSuiteBase {
 
     @TestTemplate
@@ -37,16 +37,30 @@ public class ObsFileIT extends TestSuiteBase {
         if (container instanceof Flink13Container) {
             return;
         }
+        // test write obs csv file
+        Container.ExecResult csvWriteResult = container.executeJob("/csv/fake_to_obs_csv.conf");
+        Assertions.assertEquals(0, csvWriteResult.getExitCode(), csvWriteResult.getStderr());
+        // test read obs csv file
+        Container.ExecResult csvReadResult = container.executeJob("/csv/obs_csv_to_assert.conf");
+        Assertions.assertEquals(0, csvReadResult.getExitCode(), csvReadResult.getStderr());
+        // test read obs csv file with projection
+        Container.ExecResult csvProjectionReadResult =
+                container.executeJob("/csv/obs_csv_projection_to_assert.conf");
+        Assertions.assertEquals(
+                0, csvProjectionReadResult.getExitCode(), csvProjectionReadResult.getStderr());
+        // test write obs excel file
         Container.ExecResult excelWriteResult =
                 container.executeJob("/excel/fake_to_obs_excel.conf");
         Assertions.assertEquals(0, excelWriteResult.getExitCode(), excelWriteResult.getStderr());
+        // test read obs excel file
         Container.ExecResult excelReadResult =
                 container.executeJob("/excel/obs_excel_to_assert.conf");
         Assertions.assertEquals(0, excelReadResult.getExitCode(), excelReadResult.getStderr());
+        // test read obs excel file with projection
         Container.ExecResult excelProjectionReadResult =
                 container.executeJob("/excel/obs_excel_projection_to_assert.conf");
         Assertions.assertEquals(
-                0, excelReadResult.getExitCode(), excelProjectionReadResult.getStderr());
+                0, excelProjectionReadResult.getExitCode(), excelProjectionReadResult.getStderr());
         // test write obs text file
         Container.ExecResult textWriteResult =
                 container.executeJob("/text/fake_to_obs_file_text.conf");
