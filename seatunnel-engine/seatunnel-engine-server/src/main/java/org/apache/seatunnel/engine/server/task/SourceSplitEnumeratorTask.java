46a68d9abc4e2137aa6cfdc37f0ca76ef509cc9c
liugddx <liugddx@gmail.com>
2023-05-22 23:36:21 +0800

    fix ci error

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/SourceSplitEnumeratorTask.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/SourceSplitEnumeratorTask.java
@@ -90,8 +90,6 @@ public class SourceSplitEnumeratorTask<SplitT extends SourceSplit> extends Coord
 
     private volatile boolean prepareCloseTriggered;
 
-    private final Object lock = new Object();
-
     @Override
     public void init() throws Exception {
         currState = SeaTunnelTaskState.INIT;
@@ -143,7 +141,7 @@ public class SourceSplitEnumeratorTask<SplitT extends SourceSplit> extends Coord
         final long barrierId = barrier.getId();
         Serializable snapshotState = null;
         byte[] serialize = null;
-        synchronized (lock) {
+        synchronized (enumeratorContext) {
             if (barrier.snapshot()) {
                 snapshotState = enumerator.snapshotState(barrierId);
                 serialize = enumeratorStateSerializer.serialize(snapshotState);
