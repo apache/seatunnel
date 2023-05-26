01f937580255e0d52ac61f27c8743f7d6cfb5d16
FuYouJ <1247908487@qq.com>
2023-05-26 23:29:36 +0800

    [Feature][connectors]supports neo4j sink batch write

--- seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/sink/Neo4jSinkWriter.java
+++ seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/sink/Neo4jSinkWriter.java
@@ -17,10 +17,13 @@
 
 package org.apache.seatunnel.connectors.seatunnel.neo4j.sink;
 
+import org.apache.seatunnel.api.common.SeaTunnelAPIErrorCode;
 import org.apache.seatunnel.api.sink.SinkWriter;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
+import org.apache.seatunnel.common.constants.PluginType;
 import org.apache.seatunnel.connectors.seatunnel.neo4j.config.Neo4jSinkQueryInfo;
+import org.apache.seatunnel.connectors.seatunnel.neo4j.exception.Neo4jConnectorException;
 import org.apache.seatunnel.connectors.seatunnel.neo4j.internal.SeatunnelRowNeo4jValue;
 
 import org.neo4j.driver.Driver;
@@ -29,6 +32,7 @@ import org.neo4j.driver.Session;
 import org.neo4j.driver.SessionConfig;
 import org.neo4j.driver.Value;
 import org.neo4j.driver.Values;
+import org.neo4j.driver.exceptions.ClientException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,6 +42,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static org.apache.seatunnel.connectors.seatunnel.neo4j.config.Neo4jCommonConfig.PLUGIN_NAME;
 
 @Slf4j
 public class Neo4jSinkWriter implements SinkWriter<SeaTunnelRow, Void, Void> {
@@ -97,8 +103,18 @@ public class Neo4jSinkWriter implements SinkWriter<SeaTunnelRow, Void, Void> {
     }
 
     private Query batchQuery() {
-        Value batchValues = Values.parameters(writeBuffer);
-        return new Query(neo4jSinkQueryInfo.getQuery(), batchValues);
+        try {
+            Value batchValues =
+                    Values.parameters(neo4jSinkQueryInfo.getBatchVariable(), writeBuffer);
+            return new Query(neo4jSinkQueryInfo.getQuery(), batchValues);
+        } catch (ClientException e) {
+            log.error("Failed to build cypher statement", e);
+            throw new Neo4jConnectorException(
+                    SeaTunnelAPIErrorCode.CONFIG_VALIDATION_FAILED,
+                    String.format(
+                            "PluginName: %s, PluginType: %s, Message: %s",
+                            PLUGIN_NAME, PluginType.SINK, e.getMessage()));
+        }
     }
 
     private void writeByQuery(Query query) {
@@ -119,8 +135,16 @@ public class Neo4jSinkWriter implements SinkWriter<SeaTunnelRow, Void, Void> {
 
     @Override
     public void close() throws IOException {
-        tryWriteByBatchSize();
+        flushWriteBuffer();
         session.close();
         driver.close();
+    }
+
+    private void flushWriteBuffer() {
+        while (!writeBuffer.isEmpty()) {
+            Query query = batchQuery();
+            writeByQuery(query);
+            writeBuffer.clear();
+        }
     }
 }
