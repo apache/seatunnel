37657008fb72da1844d66376507320e08084e918
Jia Fan <fanjiaeminem@qq.com>
2023-05-29 15:02:47 +0800

    improve

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/TaskExecutionService.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/TaskExecutionService.java
@@ -24,6 +24,7 @@ import org.apache.seatunnel.engine.common.Constant;
 import org.apache.seatunnel.engine.common.config.ConfigProvider;
 import org.apache.seatunnel.engine.common.config.SeaTunnelConfig;
 import org.apache.seatunnel.engine.common.config.server.ThreadShareMode;
+import org.apache.seatunnel.engine.common.exception.JobNotFoundException;
 import org.apache.seatunnel.engine.common.loader.SeaTunnelChildFirstClassLoader;
 import org.apache.seatunnel.engine.common.utils.PassiveCompletableFuture;
 import org.apache.seatunnel.engine.server.exception.TaskGroupContextNotFoundException;
@@ -384,6 +385,9 @@ public class TaskExecutionService implements DynamicMetricsProvider {
                 notifyStateSuccess = true;
             } catch (InterruptedException e) {
                 logger.severe("send notify task status failed", e);
+            } catch (JobNotFoundException e) {
+                logger.warning("send notify task status failed because can't find job", e);
+                notifyStateSuccess = true;
             } catch (ExecutionException e) {
                 if (e.getCause() instanceof JobNotFoundException) {
                     logger.warning("send notify task status failed because can't find job", e);
