acabe809da3945fabac6fa14f966032eabb0b9b2
Eric <gaojun2048@gmail.com>
2023-05-30 17:24:34 +0800

    [Hotfix][Zeta] fix pipeline state not right bug (#4823)

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/dag/physical/PhysicalPlan.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/dag/physical/PhysicalPlan.java
@@ -140,10 +140,6 @@ public class PhysicalPlan {
                 pipelineState -> {
                     try {
                         if (PipelineStatus.CANCELED.equals(pipelineState.getPipelineStatus())) {
-                            if (subPlan.canRestorePipeline()) {
-                                subPlan.restorePipeline();
-                                return;
-                            }
                             canceledPipelineNum.incrementAndGet();
                             if (makeJobEndWhenPipelineEnded) {
                                 LOGGER.info(
@@ -154,14 +150,6 @@ public class PhysicalPlan {
                             }
                         } else if (PipelineStatus.FAILED.equals(
                                 pipelineState.getPipelineStatus())) {
-                            if (subPlan.canRestorePipeline()) {
-                                LOGGER.info(
-                                        String.format(
-                                                "Can restore pipeline %s",
-                                                subPlan.getPipelineFullName()));
-                                subPlan.restorePipeline();
-                                return;
-                            }
                             failedPipelineNum.incrementAndGet();
                             errorBySubPlan.compareAndSet(null, pipelineState.getThrowableMsg());
                             if (makeJobEndWhenPipelineEnded) {
