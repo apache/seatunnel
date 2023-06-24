f54545afdcf1e4c61ca8ed751affe75f2cfc957f
FuYouJ <1247908487@qq.com>
2023-06-24 22:34:02 +0800

    [Feature][Zeta] Configuration files support user variable replacement

---
+++ seatunnel-core/seatunnel-starter/src/test/java/org/apache/seatunnel/core/starter/seatunnel/args/ClientCommandArgsTest.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.core.starter.seatunnel.args;
+
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigFactory;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigObject;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigResolveOptions;
+
+import org.apache.seatunnel.core.starter.utils.CommandLineUtils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ClientCommandArgsTest {
+    @Test
+    public void test_user_defined_paramsCommand() throws URISyntaxException {
+        String[] args = {
+            "-c", "/args/user_defined_params.conf", "-e", "local", "-i", "fake_table=tttt"
+        };
+        ClientCommandArgs clientCommandArgs =
+                CommandLineUtils.parse(args, new ClientCommandArgs(), "seatunnel-zeta", true);
+        clientCommandArgs.buildCommand();
+        URL resource = ClientCommandArgsTest.class.getResource("/args/user_defined_params.conf");
+
+        Config config =
+                ConfigFactory.parseFile(Paths.get(resource.toURI()).toFile())
+                        .resolve(ConfigResolveOptions.defaults().setAllowUnresolved(true))
+                        .resolveWith(
+                                ConfigFactory.systemProperties(),
+                                ConfigResolveOptions.defaults().setAllowUnresolved(true));
+        List<? extends ConfigObject> sourceConfig = config.getObjectList("source");
+        for (ConfigObject configObject : sourceConfig) {
+            String tableName = configObject.toConfig().getString("result_table_name");
+            Assertions.assertEquals(tableName, "tttt");
+        }
+        List<? extends ConfigObject> sinkConfig = config.getObjectList("sink");
+        for (ConfigObject sinkObject : sinkConfig) {
+            String tableName = sinkObject.toConfig().getString("result_table_name");
+            Assertions.assertEquals(tableName, "tttt");
+        }
+    }
+}
