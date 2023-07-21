b48aadc865a15e510c155e2173671380e9c336a8
monster <60029759+MonsterChenzhuo@users.noreply.github.com>
2023-07-21 15:00:13 +0800

    [Hotfix]Fix mongodb cdc e2e instability (#5128)
    Co-authored-by: chenzy15 <chenzy15@ziroom.com>

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-cdc-mongodb-e2e/src/test/java/mongodb/MongodbCDCIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-cdc-mongodb-e2e/src/test/java/mongodb/MongodbCDCIT.java
@@ -34,7 +34,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestTemplate;
 import org.testcontainers.containers.Container;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.utility.DockerLoggerFactory;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -78,7 +80,7 @@ public class MongodbCDCIT extends TestSuiteBase implements TestResource {
 
     // ----------------------------------------------------------------------------
     // mysql
-    private static final String MYSQL_HOST = "mysql_cdc_e2e";
+    private static final String MYSQL_HOST = "mysql_e2e";
 
     private static final String MYSQL_USER_NAME = "st_user";
 
@@ -104,8 +106,10 @@ public class MongodbCDCIT extends TestSuiteBase implements TestResource {
         mySqlContainer.withDatabaseName(MYSQL_DATABASE);
         mySqlContainer.withUsername(MYSQL_USER_NAME);
         mySqlContainer.withPassword(MYSQL_USER_PASSWORD);
+        mySqlContainer.withLogConsumer(
+                new Slf4jLogConsumer(DockerLoggerFactory.getLogger("Mysql-Docker-Image")));
         // For local test use
-        // mySqlContainer.setPortBindings(Collections.singletonList("3308:3306"));
+        mySqlContainer.setPortBindings(Collections.singletonList("3310:3306"));
         return mySqlContainer;
     }
 
@@ -134,6 +138,9 @@ public class MongodbCDCIT extends TestSuiteBase implements TestResource {
         mongodbContainer = new MongoDBContainer(NETWORK);
         // For local test use
         mongodbContainer.setPortBindings(Collections.singletonList("27017:27017"));
+        mongodbContainer.withLogConsumer(
+                new Slf4jLogConsumer(DockerLoggerFactory.getLogger("Mongodb-Docker-Image")));
+
         Startables.deepStart(Stream.of(mongodbContainer)).join();
         mongodbContainer.executeCommandFileInSeparateDatabase(MONGODB_DATABASE);
         initConnection();
@@ -213,6 +220,7 @@ public class MongodbCDCIT extends TestSuiteBase implements TestResource {
                 for (int i = 1; i <= columnCount; i++) {
                     objects.add(resultSet.getObject(i));
                 }
+                log.info("Print mysql sink data:" + objects);
                 result.add(objects);
             }
             return result;
