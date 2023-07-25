53a1f0c6c14190c37c0b28320db049f822436a4f
Xiaojian Sun <sunxiaojian926@163.com>
2023-07-25 10:09:24 +0800

    [Feature][connector][kafka] Support read debezium format message from kafka (#5066)

--- seatunnel-connectors-v2/connector-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/config/Config.java
+++ seatunnel-connectors-v2/connector-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/config/Config.java
@@ -26,8 +26,6 @@ import java.util.Map;
 public class Config {
 
     public static final String CONNECTOR_IDENTITY = "Kafka";
-    public static final String REPLICATION_FACTOR = "replication.factor";
-
     /** The default field delimiter is “,” */
     public static final String DEFAULT_FIELD_DELIMITER = ",";
 
@@ -98,6 +96,12 @@ public class Config {
                     .withDescription(
                             "Data format. The default format is json. Optional text format. The default field separator is \", \". "
                                     + "If you customize the delimiter, add the \"field_delimiter\" option.");
+
+    public static final Option<Boolean> DEBEZIUM_RECORD_INCLUDE_SCHEMA =
+            Options.key("debezium_record_include_schema")
+                    .booleanType()
+                    .defaultValue(true)
+                    .withDescription("Does the debezium record carry a schema.");
 
     public static final Option<String> FIELD_DELIMITER =
             Options.key("field_delimiter")
