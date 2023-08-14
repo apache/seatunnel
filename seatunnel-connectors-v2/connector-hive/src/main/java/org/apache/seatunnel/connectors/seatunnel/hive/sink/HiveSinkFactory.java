14d8a6c08c5b14a34bbfa710daca0cbca8050b1b
lightzhao <40714172+lightzhao@users.noreply.github.com>
2023-08-14 15:58:10 +0800

    [Improve][Connector-v2][HiveSink]remove drop partition when abort. (#4940)
    Co-authored-by: lightzhao <zhaolianyong777@gmail.com>
    Co-authored-by: liuli <m_liuli@163.com>
    Co-authored-by: ic4y <83933160+ic4y@users.noreply.github.com>

--- seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/sink/HiveSinkFactory.java
+++ seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/sink/HiveSinkFactory.java
@@ -24,6 +24,8 @@ import org.apache.seatunnel.connectors.seatunnel.hive.config.HiveConfig;
 
 import com.google.auto.service.AutoService;
 
+import static org.apache.seatunnel.connectors.seatunnel.hive.config.HiveConfig.ABORT_DROP_PARTITION_METADATA;
+
 @AutoService(Factory.class)
 public class HiveSinkFactory implements TableSinkFactory {
     @Override
@@ -36,6 +38,7 @@ public class HiveSinkFactory implements TableSinkFactory {
         return OptionRule.builder()
                 .required(HiveConfig.TABLE_NAME)
                 .required(HiveConfig.METASTORE_URI)
+                .optional(ABORT_DROP_PARTITION_METADATA)
                 .build();
     }
 }
