34188ed0385ef536b95ba449434358657a22008f
cmuhao <sduxuhao@gmail.com>
2023-09-10 22:27:20 -0700

    fix style

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-amazonsqs-e2e/src/test/java/org/apache/seatunnel/e2e/connector/amazonsqs/AmazonsqsIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-amazonsqs-e2e/src/test/java/org/apache/seatunnel/e2e/connector/amazonsqs/AmazonsqsIT.java
@@ -38,7 +38,6 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
-import java.net.ConnectException;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
