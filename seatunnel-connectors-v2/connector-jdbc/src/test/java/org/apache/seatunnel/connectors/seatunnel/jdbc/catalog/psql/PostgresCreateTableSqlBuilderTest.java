b98b6bcee3a83490dd62d18544d295eea5b89a56
Jia Fan <fanjiaeminem@qq.com>
2023-11-03 10:53:51 +0800

    [Fix] Fix Postgres create table test case failed (#5778)

--- seatunnel-connectors-v2/connector-jdbc/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/psql/PostgresCreateTableSqlBuilderTest.java
+++ seatunnel-connectors-v2/connector-jdbc/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/psql/PostgresCreateTableSqlBuilderTest.java
@@ -18,54 +18,111 @@
 package org.apache.seatunnel.connectors.seatunnel.jdbc.catalog.psql;
 
 import org.apache.seatunnel.api.table.catalog.CatalogTable;
+import org.apache.seatunnel.api.table.catalog.Column;
 import org.apache.seatunnel.api.table.catalog.ConstraintKey;
 import org.apache.seatunnel.api.table.catalog.PhysicalColumn;
 import org.apache.seatunnel.api.table.catalog.PrimaryKey;
 import org.apache.seatunnel.api.table.catalog.TableIdentifier;
 import org.apache.seatunnel.api.table.catalog.TableSchema;
 import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.DatabaseIdentifier;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.Lists;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 class PostgresCreateTableSqlBuilderTest {
 
     @Test
     void build() {
-        CatalogTable catalogTable = catalogTable();
-        PostgresCreateTableSqlBuilder postgresCreateTableSqlBuilder =
-                new PostgresCreateTableSqlBuilder(catalogTable);
-        String createTableSql =
-                postgresCreateTableSqlBuilder.build(catalogTable.getTableId().toTablePath());
-        Assertions.assertEquals(
-                "CREATE TABLE \"test\" (\n"
-                        + "\"id\" int4 NOT NULL PRIMARY KEY,\n"
-                        + "\"name\" text NOT NULL,\n"
-                        + "\"age\" int4 NOT NULL,\n"
-                        + "\tCONSTRAINT unique_name UNIQUE (\"name\")\n"
-                        + ");",
-                createTableSql);
-        Assertions.assertEquals(
-                Lists.newArrayList("CREATE INDEX test_index_age ON \"test\"(\"age\");"),
-                postgresCreateTableSqlBuilder.getCreateIndexSqls());
+        Arrays.asList(true, false)
+                .forEach(
+                        otherDB -> {
+                            CatalogTable catalogTable = catalogTable(otherDB);
+                            PostgresCreateTableSqlBuilder postgresCreateTableSqlBuilder =
+                                    new PostgresCreateTableSqlBuilder(catalogTable);
+                            String createTableSql =
+                                    postgresCreateTableSqlBuilder.build(
+                                            catalogTable.getTableId().toTablePath());
+                            Assertions.assertEquals(
+                                    "CREATE TABLE \"test\" (\n"
+                                            + "\"id\" int4 NOT NULL PRIMARY KEY,\n"
+                                            + "\"name\" text NOT NULL,\n"
+                                            + "\"age\" int4 NOT NULL,\n"
+                                            + "\tCONSTRAINT unique_name UNIQUE (\"name\")\n"
+                                            + ");",
+                                    createTableSql);
+                            Assertions.assertEquals(
+                                    Lists.newArrayList(
+                                            "CREATE INDEX test_index_age ON \"test\"(\"age\");"),
+                                    postgresCreateTableSqlBuilder.getCreateIndexSqls());
+                        });
     }
 
-    private CatalogTable catalogTable() {
-        TableIdentifier tableIdentifier = TableIdentifier.of("postgres", "public", "test");
+    private CatalogTable catalogTable(boolean otherDB) {
+        TableIdentifier tableIdentifier =
+                TableIdentifier.of(
+                        otherDB ? DatabaseIdentifier.MYSQL : DatabaseIdentifier.POSTGRESQL,
+                        "public",
+                        "test");
+        List<Column> columns;
+        if (otherDB) {
+            columns =
+                    Lists.newArrayList(
+                            PhysicalColumn.of("id", BasicType.INT_TYPE, 0, false, null, ""),
+                            PhysicalColumn.of("name", BasicType.STRING_TYPE, 0, false, null, ""),
+                            PhysicalColumn.of("age", BasicType.INT_TYPE, 0, false, null, ""));
+        } else {
+            columns =
+                    Lists.newArrayList(
+                            PhysicalColumn.of(
+                                    "id",
+                                    BasicType.INT_TYPE,
+                                    0,
+                                    false,
+                                    null,
+                                    "",
+                                    "int4",
+                                    false,
+                                    false,
+                                    null,
+                                    Collections.emptyMap(),
+                                    null),
+                            PhysicalColumn.of(
+                                    "name",
+                                    BasicType.STRING_TYPE,
+                                    0,
+                                    false,
+                                    null,
+                                    "",
+                                    "text",
+                                    false,
+                                    false,
+                                    null,
+                                    Collections.emptyMap(),
+                                    null),
+                            PhysicalColumn.of(
+                                    "age",
+                                    BasicType.INT_TYPE,
+                                    0,
+                                    false,
+                                    null,
+                                    "",
+                                    "int4",
+                                    false,
+                                    false,
+                                    null,
+                                    Collections.emptyMap(),
+                                    null));
+        }
         TableSchema tableSchema =
                 TableSchema.builder()
-                        .columns(
-                                Lists.newArrayList(
-                                        PhysicalColumn.of(
-                                                "id", BasicType.INT_TYPE, 0, false, null, ""),
-                                        PhysicalColumn.of(
-                                                "name", BasicType.STRING_TYPE, 0, false, null, ""),
-                                        PhysicalColumn.of(
-                                                "age", BasicType.INT_TYPE, 0, false, null, "")))
+                        .columns(columns)
                         .primaryKey(PrimaryKey.of("pk_id", Lists.newArrayList("id")))
                         .constraintKey(
                                 Lists.newArrayList(
