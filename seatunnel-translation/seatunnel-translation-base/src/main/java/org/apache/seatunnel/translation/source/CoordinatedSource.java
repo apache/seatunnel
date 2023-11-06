b360608aa5a7e34044cc3bee8d912659428e4d6b
zhilinli <zhilinli0706@gmail.com>
2023-11-06 16:22:42 +0800

    update

--- seatunnel-translation/seatunnel-translation-base/src/main/java/org/apache/seatunnel/translation/source/CoordinatedSource.java
+++ seatunnel-translation/seatunnel-translation-base/src/main/java/org/apache/seatunnel/translation/source/CoordinatedSource.java
@@ -62,7 +62,7 @@ public class CoordinatedSource<T, SplitT extends SourceSplit, StateT extends Ser
     protected final Map<Integer, List<SplitT>> restoredSplitStateMap = new HashMap<>();
 
     protected transient volatile SourceSplitEnumerator<SplitT, StateT> splitEnumerator;
-    // task and Reader Map Info
+    // the mapping is from subtask id to the reader info
     protected transient Map<Integer, SourceReader<T, SplitT>> readerMap = new ConcurrentHashMap<>();
     // Control the running status of each Task globally
     protected final Map<Integer, AtomicBoolean> readerRunningMap;
@@ -101,17 +101,21 @@ public class CoordinatedSource<T, SplitT extends SourceSplit, StateT extends Ser
                         enumeratorStateSerializer.deserialize(restoredState.get(-1).get(0));
             }
 
-            /*  Based on the last saved state Add a Split in a Pending State to a Pending split Re-instantiate the SourceSplitEnumerator */
+            /* Based on the last saved state Add a Split in a Pending State to a Pending split Re-instantiate the SourceSplitEnumerator */
             splitEnumerator =
                     source.restoreEnumerator(coordinatedEnumeratorContext, restoredEnumeratorState);
+            // The status of readers corresponding to all tasks is restored
             restoredState.forEach(
                     (subtaskId, splitBytes) -> {
+                       /* If the Map Task contains -1, it means that there are unread areas of
+                     split that we need to skip */
                         if (subtaskId == -1) {
                             return;
                         }
                         List<SplitT> restoredSplitState = new ArrayList<>(splitBytes.size());
                         for (byte[] splitByte : splitBytes) {
                             try {
+                          /* Deserialize the recovery read state in each Task Finally, the  state of all tasks is added to the new */
                                 restoredSplitState.add(splitSerializer.deserialize(splitByte));
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
