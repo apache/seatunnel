a31f761b4caf44d41e9d2f547d287e189b3810ec
Jia Fan <fanjiaeminem@qq.com>
2023-11-24 10:06:27 +0800

    update

--- tools/update_modules_check/update_modules_check.py
+++ tools/update_modules_check/update_modules_check.py
@@ -167,6 +167,8 @@ def get_sub_update_it_modules(modules, total_num, current_num):
         module_list.remove("connector-kudu-e2e")
     if "connector-amazonsqs-e2e" in module_list:
         module_list.remove("connector-amazonsqs-e2e")
+    if "seatunnel-engine-k8s-e2e" in module_list:
+        module_list.remove("seatunnel-engine-k8s-e2e")
     for i, module in enumerate(module_list):
         if len(module) > 0 and i % int(total_num) == int(current_num):
             final_modules.append(":" + module)
