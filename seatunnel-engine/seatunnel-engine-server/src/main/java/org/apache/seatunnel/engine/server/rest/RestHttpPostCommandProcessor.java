a2ac2d691a9e9def775f2f9e95895c2acbb8a979
liugddx <liugddx@gmail.com>
2023-12-19 23:53:01 +0800

    1

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/RestHttpPostCommandProcessor.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/RestHttpPostCommandProcessor.java
@@ -130,7 +130,7 @@ public class RestHttpPostCommandProcessor extends HttpCommandProcessor<HttpPostC
 
         JobConfig jobConfig = new JobConfig();
         jobConfig.setName(
-                requestParams.get(StringUtils.isEmpty(jobName) ? RestConstant.JOB_NAME : jobName));
+                requestParams.get(StringUtils.isEmpty(jobName) ? requestParams.get(RestConstant.JOB_NAME) : jobName));
 
         boolean startWithSavePoint =
                 Boolean.parseBoolean(requestParams.get(RestConstant.IS_START_WITH_SAVE_POINT));
