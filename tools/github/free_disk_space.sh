901d2a969cfa8c23eb302c5cafcaf2c74eb27fff
Jia Fan <fanjiaeminem@qq.com>
2024-02-20 20:06:34 +0800

    update

--- tools/github/free_disk_space.sh
+++ tools/github/free_disk_space.sh
@@ -14,30 +14,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "=============================================================================="
-echo "Freeing up disk space on CI system"
-echo "=============================================================================="
-
-echo "Listing 100 largest packages"
-dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
-df -h
-echo "Removing large packages"
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y '^mongodb-.*'
-sudo apt-get remove -y '^mysql-.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
-sudo apt-get autoremove -y
-sudo apt-get clean
-df -h
-echo "Removing large directories"
-
-sudo rm -rf /usr/share/dotnet/
-sudo rm -rf /usr/local/graalvm/
-sudo rm -rf /usr/local/.ghcup/
-sudo rm -rf /usr/local/share/powershell
-sudo rm -rf /usr/local/share/chromium
-sudo rm -rf /usr/local/lib/android
-sudo rm -rf /usr/local/lib/node_modules
-df -h
+#echo "=============================================================================="
+#echo "Freeing up disk space on CI system"
+#echo "=============================================================================="
+#
+#echo "Listing 100 largest packages"
+#dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+#df -h
+#echo "Removing large packages"
+#sudo apt-get remove -y '^dotnet-.*'
+#sudo apt-get remove -y '^llvm-.*'
+#sudo apt-get remove -y 'php.*'
+#sudo apt-get remove -y '^mongodb-.*'
+#sudo apt-get remove -y '^mysql-.*'
+#sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
+#sudo apt-get autoremove -y
+#sudo apt-get clean
+#df -h
+#echo "Removing large directories"
+#
+#sudo rm -rf /usr/share/dotnet/
+#sudo rm -rf /usr/local/graalvm/
+#sudo rm -rf /usr/local/.ghcup/
+#sudo rm -rf /usr/local/share/powershell
+#sudo rm -rf /usr/local/share/chromium
+#sudo rm -rf /usr/local/lib/android
+#sudo rm -rf /usr/local/lib/node_modules
+#df -h
