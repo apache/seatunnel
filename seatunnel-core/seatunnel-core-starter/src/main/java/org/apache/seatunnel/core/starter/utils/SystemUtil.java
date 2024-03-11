ca4c88f04147e00154246baf5240f965162d3c6b
Leoli <krislee@163.com>
2024-03-11 21:04:55 +0800

    fix issue6386 and windows specific launch cmd script error

--- seatunnel-core/seatunnel-core-starter/src/main/java/org/apache/seatunnel/core/starter/utils/SystemUtil.java
+++ seatunnel-core/seatunnel-core-starter/src/main/java/org/apache/seatunnel/core/starter/utils/SystemUtil.java
@@ -3,22 +3,22 @@ package org.apache.seatunnel.core.starter.utils;
 import org.apache.commons.lang3.SystemUtils;
 
 public class SystemUtil {
-      
+
     public static String GetOsType() {
-        String os_type="";
-        
+        String os_type = "";
+
         if (SystemUtils.IS_OS_WINDOWS) {
-           os_type="Windows";
+            os_type = "Windows";
         } else if (SystemUtils.IS_OS_MAC) {
-           os_type="Mac";
+            os_type = "Mac";
         } else if (SystemUtils.IS_OS_LINUX) {
-           os_type="Linux";
+            os_type = "Linux";
         } else if (SystemUtils.IS_OS_SOLARIS) {
-           os_type="Solaris";
+            os_type = "Solaris";
         } else {
-          os_type="Unknown";
+            os_type = "Unknown";
         }
-        
+
         return os_type;
-    }    
+    }
 }
