7133386bcb25d1a39eee95f6d5ada755fa51ed28
luops <luopsh@qq.com>
2024-03-26 11:52:14 +0800

    [feature][connectors-v2][jdbc] add xugu jdbc connector

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/xugu/XuguCatalog.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/xugu/XuguCatalog.java
@@ -133,6 +133,16 @@ public class XuguCatalog extends AbstractJdbcCatalog {
     }
 
     @Override
+    protected String getCreateDatabaseSql(String databaseName) {
+        return String.format("CREATE DATABASE \"%s\"", databaseName);
+    }
+
+    @Override
+    protected String getDropDatabaseSql(String databaseName) {
+        return String.format("DROP DATABASE \"%s\"", databaseName);
+    }
+
+    @Override
     protected String getListTableSql(String databaseName) {
         return "SELECT user_name ,table_name FROM all_users au \n"
                 + "INNER JOIN all_tables at ON au.user_id=at.user_id AND au.db_id=at.db_id";
