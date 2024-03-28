2d3b9f7d7c7f2a35e68912ddc66b9f8675b2c348
zhuqy <zhuqy@dtjoy.com>
2024-03-28 17:03:43 +0800

    [Bug] [connector-jdbc] Catalog's SELECT_COLUMNS_SQL_TEMPLATE have some issues. update e2e test. (#6054)

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/connector-jdbc-e2e-part-3/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcSqlServerIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/connector-jdbc-e2e-part-3/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcSqlServerIT.java
@@ -368,15 +368,23 @@ public class JdbcSqlServerIT extends AbstractJdbcIT {
                                 Assertions.assertEquals(column.getColumnLength(), 255L);
                             }
                             if (column.getName().equals("DECIMAL_TEST")) {
-                                Assertions.assertEquals(((DecimalType)column.getDataType()).getPrecision(), 18L);
-                                Assertions.assertEquals(((DecimalType)column.getDataType()).getScale(), 2L);
+                                Assertions.assertEquals(
+                                        ((DecimalType)column.getDataType()).getPrecision(),
+                                        18L);
+                                Assertions.assertEquals(
+                                        ((DecimalType)column.getDataType()).getScale(),
+                                        2L);
                             }
                             if (column.getName().equals("NCHAR_TEST")) {
                                 Assertions.assertEquals(column.getColumnLength(), 2L);
                             }
                             if (column.getName().equals("NUMERIC_TEST")) {
-                                Assertions.assertEquals(((DecimalType)column.getDataType()).getPrecision(), 18L);
-                                Assertions.assertEquals(((DecimalType)column.getDataType()).getScale(), 2L);
+                                Assertions.assertEquals(
+                                        ((DecimalType)column.getDataType()).getPrecision(),
+                                        18L);
+                                Assertions.assertEquals(
+                                        ((DecimalType)column.getDataType()).getScale(),
+                                        2L);
                             }
                             if (column.getName().equals("NVARCHAR_TEST")) {
                                 Assertions.assertEquals(column.getColumnLength(), 32L);
