58b468f6d70844a3925e45066758e5f749fe01f2
dailai <dailai@chinatelecom.cn>
2024-06-13 13:52:30 +0800

    [Hotfix][Seatunnel-engine] Optimize the code

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/flow/SourceFlowLifeCycle.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/flow/SourceFlowLifeCycle.java
@@ -310,18 +310,11 @@ public class SourceFlowLifeCycle<T, SplitT extends SourceSplit> extends ActionFl
                         "lock checkpoint[{}] waiting for complete..., phase: [{}]",
                         barrier.getId(),
                         schemaChangePhase.get().getPhase());
-            } else if (prepareClose) {
-                TaskLocation taskLocation = runningTask.getTaskLocation();
-                log.info(
-                        "Task[{}]({}/{}) prepare close",
-                        taskLocation.getTaskID(),
-                        taskLocation.getPipelineId(),
-                        taskLocation.getJobId());
             } else {
-                throw new IllegalStateException(
-                        String.format(
-                                "schema-change checkpoint[%s] and phase[%s] is not matched",
-                                barrier.getId(), checkpointType));
+                log.debug(
+                        "Ignore schema-change checkpoint[{}] on idle task, phase: [{}]",
+                        barrier.getId(),
+                        checkpointType);
             }
         }
     }
