4532a7ef68836ccfa8f2a68d03df744781e62a84
dailai <dailai@chinatelecom.cn>
2024-06-17 16:37:37 +0800

    [Improve][Connector-V2] Support table filter

--- seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/base/schema/AbstractSchemaChangeResolver.java
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/base/schema/AbstractSchemaChangeResolver.java
@@ -17,20 +17,20 @@
 
 package org.apache.seatunnel.connectors.cdc.base.schema;
 
-import org.apache.seatunnel.api.table.catalog.TablePath;
 import org.apache.seatunnel.connectors.cdc.base.config.JdbcSourceConfig;
 import org.apache.seatunnel.connectors.cdc.base.utils.SourceRecordUtils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 
 import com.google.common.collect.Lists;
-import io.debezium.relational.RelationalTableFilters;
-import io.debezium.relational.TableId;
-import io.debezium.relational.Tables;
+import io.debezium.relational.history.HistoryRecord;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 public abstract class AbstractSchemaChangeResolver implements SchemaChangeResolver {
 
     protected static final List<String> SUPPORT_DDL = Lists.newArrayList("ALTER TABLE");
@@ -43,21 +43,16 @@ public abstract class AbstractSchemaChangeResolver implements SchemaChangeResolv
 
     @Override
     public boolean support(SourceRecord record) {
-        RelationalTableFilters tableFilters =
-                jdbcSourceConfig.getDbzConnectorConfig().getTableFilters();
-        Tables.TableFilter tableFilter = tableFilters.dataCollectionFilter();
-        TablePath tablePath = SourceRecordUtils.getTablePath(record);
         String ddl = SourceRecordUtils.getDdl(record);
-        return StringUtils.isNotEmpty(tablePath.getTableName())
-                && tableFilter.isIncluded(toTableId(tablePath))
-                && StringUtils.isNotBlank(ddl)
+        Struct value = (Struct) record.value();
+        List<Struct> tableChanges = value.getArray(HistoryRecord.Fields.TABLE_CHANGES);
+        if (tableChanges == null || tableChanges.isEmpty()) {
+            log.warn("Ignoring statement for non-captured table {}", ddl);
+            return false;
+        }
+        return StringUtils.isNotBlank(ddl)
                 && SUPPORT_DDL.stream()
                         .map(String::toUpperCase)
                         .anyMatch(prefix -> ddl.toUpperCase().contains(prefix));
-    }
-
-    private TableId toTableId(TablePath tablePath) {
-        return new TableId(
-                tablePath.getDatabaseName(), tablePath.getSchemaName(), tablePath.getTableName());
     }
 }
