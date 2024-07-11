c8cbcba0520192062a42e5f23ae865b8f2a16108
gdliu3 <gdliu3@iflytek.com>
2024-07-11 22:52:49 +0800

    1

--- seatunnel-e2e/seatunnel-engine-e2e/connector-seatunnel-e2e-base/src/test/java/org/apache/seatunnel/engine/e2e/ClusterSeaTunnelContainer.java
+++ seatunnel-e2e/seatunnel-engine-e2e/connector-seatunnel-e2e-base/src/test/java/org/apache/seatunnel/engine/e2e/ClusterSeaTunnelContainer.java
@@ -64,7 +64,7 @@ public class ClusterSeaTunnelContainer extends SeaTunnelContainer {
     private static final Path binPath = Paths.get(SEATUNNEL_HOME, "bin", SERVER_SHELL);
     private static final Path config = Paths.get(SEATUNNEL_HOME, "config");
     private static final Path hadoopJar =
-            Paths.get(SEATUNNEL_HOME, "lib/seatunnel-hadoop3-3.1.4-uber.jar");
+            Paths.get(SEATUNNEL_HOME, "lib/seatunnel-hadoop3-3.3.4-uber.jar");
 
     private static final long CUSTOM_JOB_ID = 862969647010611201L;
 
@@ -364,7 +364,7 @@ public class ClusterSeaTunnelContainer extends SeaTunnelContainer {
         server.withCopyFileToContainer(
                 MountableFile.forHostPath(
                         PROJECT_ROOT_PATH
-                                + "/seatunnel-shade/seatunnel-hadoop3-3.1.4-uber/target/seatunnel-hadoop3-3.1.4-uber.jar"),
+                                + "/seatunnel-shade/seatunnel-hadoop3-3.3.4-uber/target/seatunnel-hadoop3-3.3.4-uber.jar"),
                 hadoopJar.toString());
         server.start();
         // execute extra commands
