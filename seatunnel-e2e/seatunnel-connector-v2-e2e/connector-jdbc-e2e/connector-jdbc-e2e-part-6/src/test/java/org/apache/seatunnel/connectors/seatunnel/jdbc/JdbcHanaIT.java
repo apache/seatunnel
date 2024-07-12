1fe9990e2616983c83d5cd34c7f054ff2cc6a875
Jia Fan <fanjiaeminem@qq.com>
2024-07-12 16:54:48 +0800

    [Improve][CI] Add auto label action (#7174)

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/connector-jdbc-e2e-part-6/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcHanaIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-jdbc-e2e/connector-jdbc-e2e-part-6/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/JdbcHanaIT.java
@@ -45,7 +45,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcHanaIT extends AbstractJdbcIT {
-    private static final String HANA_IMAGE = "saplabs/hanaexpress:2.00.072.00.20231123.1";
+    private static final String HANA_IMAGE = "saplabs/hanaexpress:2.00.076.00.20240701.1";
     private static final String HANA_NETWORK_ALIASES = "e2e_saphana";
     private static final String DRIVER_CLASS = "com.sap.db.jdbc.Driver";
     private static final int HANA_PORT = 39017;
