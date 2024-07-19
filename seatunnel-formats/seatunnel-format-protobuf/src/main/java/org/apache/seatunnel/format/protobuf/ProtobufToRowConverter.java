0d5f4af6522e2d7ef10e61bd1a4c7cb38e538887
zhangshenghang <admin@hadoop.wiki>
2024-07-19 16:44:35 +0800

    [feature]add protobuf

--- seatunnel-formats/seatunnel-format-protobuf/src/main/java/org/apache/seatunnel/format/protobuf/ProtobufToRowConverter.java
+++ seatunnel-formats/seatunnel-format-protobuf/src/main/java/org/apache/seatunnel/format/protobuf/ProtobufToRowConverter.java
@@ -73,37 +73,6 @@ public class ProtobufToRowConverter implements Serializable {
         return CompileDescriptor.compileDescriptorTempFile(protoContent,messageName);
     }
 
-    private String compileDescriptorTempFile() throws IOException, InterruptedException {
-        // Because Protobuf can only be dynamically parsed through the descriptor file, the file
-        // needs to be compiled and generated. The following method is used here to solve the
-        // problem: generate a temporary directory and compile .proto into a descriptor temporary
-        // file. The temporary file and directory are deleted after the JVM runs.
-        File tmpDir = File.createTempFile("tmp_protobuf_", "_proto");
-        tmpDir.delete();
-        tmpDir.mkdirs();
-        tmpDir.deleteOnExit();
-        File protoFile = new File(tmpDir, ".proto");
-        protoFile.deleteOnExit();
-        FileUtils.writeStringToFile(protoFile.getPath(), protoContent);
-        String targetDesc = tmpDir + "/.desc";
-        new File(targetDesc).deleteOnExit();
-
-        int exitCode =
-                Protoc.runProtoc(
-                        new String[] {
-                            "--proto_path=" + protoFile.getParent(),
-                            "--descriptor_set_out=" + targetDesc,
-                            protoFile.getPath()
-                        });
-
-        if (exitCode != 0) {
-            throw new SeaTunnelProtobufFormatException(
-                    ProtobufFormatErrorCode.DESCRIPTOR_CONVERT_FAILED,
-                    "Protoc compile error, exit code: " + exitCode);
-        }
-        return targetDesc;
-    }
-
     public SeaTunnelRow converter(
             Descriptors.Descriptor descriptor,
             DynamicMessage dynamicMessage,
