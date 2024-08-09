1302e147946b59449636cc1a167fcef4cc1641d1
lizhenglei <673421862@qq.com>
2024-08-09 12:23:21 +0800

    1

--- seatunnel-e2e/seatunnel-transforms-v2-e2e/seatunnel-transforms-v2-e2e-part-2/src/test/java/org/apache/seatunnel/e2e/transform/TestDynamicCompileIT.java
+++ seatunnel-e2e/seatunnel-transforms-v2-e2e/seatunnel-transforms-v2-e2e-part-2/src/test/java/org/apache/seatunnel/e2e/transform/TestDynamicCompileIT.java
@@ -19,7 +19,9 @@ package org.apache.seatunnel.e2e.transform;
 
 import org.apache.seatunnel.e2e.common.TestResource;
 import org.apache.seatunnel.e2e.common.container.ContainerExtendedFactory;
+import org.apache.seatunnel.e2e.common.container.EngineType;
 import org.apache.seatunnel.e2e.common.container.TestContainer;
+import org.apache.seatunnel.e2e.common.junit.DisabledOnContainer;
 import org.apache.seatunnel.e2e.common.junit.TestContainerExtension;
 
 import org.junit.jupiter.api.AfterAll;
@@ -87,7 +89,7 @@ public class TestDynamicCompileIT extends TestSuiteBase implements TestResource 
                         container.execInContainer(
                                 "bash",
                                 "-c",
-                                "mkdir -p /tmp/seatunnel/plugins/Fake/lib && cd /tmp/seatunnel/plugins/Fake/lib && wget  "
+                                "mkdir -p /tmp/seatunnel/lib && cd /tmp/seatunnel/lib && wget  "
                                         + "https://repo1.maven.org/maven2/cn/hutool/hutool-all/5.3.6/hutool-all-5.3.6.jar");
                 Assertions.assertEquals(0, extraCommands.getExitCode(), extraCommands.getStderr());
             };
@@ -159,6 +161,10 @@ public class TestDynamicCompileIT extends TestSuiteBase implements TestResource 
     }
 
     @TestTemplate
+    @DisabledOnContainer(
+            value = {},
+            type = {EngineType.FLINK, EngineType.SPARK},
+            disabledReason = "The hbase container authentication configuration is incorrect.")
     public void testHttpDynamic(TestContainer container) throws IOException, InterruptedException {
         Container.ExecResult execResult =
                 container.executeJob(basePath + "single_dynamic_http_compile_transform.conf");
