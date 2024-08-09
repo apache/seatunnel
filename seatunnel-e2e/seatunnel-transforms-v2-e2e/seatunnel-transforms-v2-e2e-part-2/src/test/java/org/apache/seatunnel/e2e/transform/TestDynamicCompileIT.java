b6772fc0df9e1fccd4237a0d5e185731d334d857
lizhenglei <673421862@qq.com>
2024-08-09 18:31:11 +0800

    fix

--- seatunnel-e2e/seatunnel-transforms-v2-e2e/seatunnel-transforms-v2-e2e-part-2/src/test/java/org/apache/seatunnel/e2e/transform/TestDynamicCompileIT.java
+++ seatunnel-e2e/seatunnel-transforms-v2-e2e/seatunnel-transforms-v2-e2e-part-2/src/test/java/org/apache/seatunnel/e2e/transform/TestDynamicCompileIT.java
@@ -19,9 +19,7 @@ package org.apache.seatunnel.e2e.transform;
 
 import org.apache.seatunnel.e2e.common.TestResource;
 import org.apache.seatunnel.e2e.common.container.ContainerExtendedFactory;
-import org.apache.seatunnel.e2e.common.container.EngineType;
 import org.apache.seatunnel.e2e.common.container.TestContainer;
-import org.apache.seatunnel.e2e.common.junit.DisabledOnContainer;
 import org.apache.seatunnel.e2e.common.junit.TestContainerExtension;
 
 import org.junit.jupiter.api.AfterAll;
@@ -161,10 +159,6 @@ public class TestDynamicCompileIT extends TestSuiteBase implements TestResource 
     }
 
     @TestTemplate
-    @DisabledOnContainer(
-            value = {},
-            type = {EngineType.FLINK, EngineType.SPARK},
-            disabledReason = "The hbase container authentication configuration is incorrect.")
     public void testHttpDynamic(TestContainer container) throws IOException, InterruptedException {
         Container.ExecResult execResult =
                 container.executeJob(basePath + "single_dynamic_http_compile_transform.conf");
