6c4a4b5fc9fb1cd4d06ec062f929d49ee81c38be
wangchao <wangchao001@zhongan.com>
2024-08-16 11:15:18 +0800

    [Improve][Connector-V2] Email write verification

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-email-e2e/src/test/java/org/apache/seatunnel/e2e/connector/email/EmailWithMultiIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-email-e2e/src/test/java/org/apache/seatunnel/e2e/connector/email/EmailWithMultiIT.java
@@ -36,14 +36,20 @@ import org.testcontainers.utility.DockerImageName;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.mail.Folder;
+import javax.mail.Session;
+import javax.mail.Store;
+
 import java.io.IOException;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 @Slf4j
 public class EmailWithMultiIT extends TestSuiteBase implements TestResource {
     private static final String IMAGE = "greenmail/standalone";
     private static final String HOST = "email-e2e";
-    private static final int PORT = 3025;
+    private static final int STMP_PORT = 3025;
+    private static final int IMAP_PORT = 3143;
 
     private GenericContainer<?> smtpContainer;
 
@@ -54,7 +60,7 @@ public class EmailWithMultiIT extends TestSuiteBase implements TestResource {
                 new GenericContainer<>(DockerImageName.parse(IMAGE))
                         .withNetwork(NETWORK)
                         .withNetworkAliases(HOST)
-                        .withExposedPorts(PORT)
+                        .withExposedPorts(STMP_PORT, IMAP_PORT)
                         .withLogConsumer(
                                 new Slf4jLogConsumer(LoggerFactory.getLogger("email-service")));
         Startables.deepStart(Stream.of(smtpContainer)).join();
@@ -69,8 +75,9 @@ public class EmailWithMultiIT extends TestSuiteBase implements TestResource {
     }
 
     @TestTemplate
-    public void testEmailSink(TestContainer container) throws IOException, InterruptedException {
+    public void testEmailSink(TestContainer container) throws Exception {
         Container.ExecResult textWriteResult = container.executeJob("/fake_to_email.conf");
+        testEMailSuccess(1, "receiver-1@example.com");
         Assertions.assertEquals(0, textWriteResult.getExitCode());
     }
 
@@ -79,10 +86,40 @@ public class EmailWithMultiIT extends TestSuiteBase implements TestResource {
             value = {},
             type = {EngineType.FLINK},
             disabledReason = "Currently FLINK do not support multi-table")
-    public void testMultipleTableEmailSink(TestContainer container)
-            throws IOException, InterruptedException {
+    public void testMultipleTableEmailSink(TestContainer container) throws Exception {
         Container.ExecResult textWriteResult = container.executeJob("/fake_to_multiemailsink.conf");
+        testEMailSuccess(2, "receiver-1@example.com", "receiver-2@example.com");
         Assertions.assertEquals(0, textWriteResult.getExitCode());
+    }
+
+    private Session setupImap() {
+        log.info("in setupImap");
+        Properties props = new Properties();
+        props.setProperty("mail.store.protocol", "imap");
+        props.put("mail.imap.host", smtpContainer.getHost());
+        props.put("mail.imap.port", smtpContainer.getMappedPort(IMAP_PORT));
+        props.put("mail.imap.localaddress", smtpContainer.getHost());
+        return Session.getInstance(props, null);
+    }
+
+    private void testEMailSuccess(int receivedNum, String... users) throws Exception {
+        Session sessionIMAP = setupImap();
+        for (String user : users) {
+            Store store = sessionIMAP.getStore("imap");
+            store.connect(
+                    smtpContainer.getHost(), smtpContainer.getMappedPort(IMAP_PORT), user, "");
+            if (store.isConnected()) {
+                log.info("IMAP is connected");
+                Folder folder = store.getFolder("INBOX");
+                if (folder != null) {
+                    folder.open(Folder.READ_ONLY);
+                    log.info("mail messages.length: {}", folder.getMessageCount());
+                    Assertions.assertEquals(receivedNum, folder.getMessageCount());
+                }
+            } else {
+                log.info("IMAP is not connected");
+            }
+        }
     }
 
     @Disabled("Email authentication address and authentication information need to be configured")
