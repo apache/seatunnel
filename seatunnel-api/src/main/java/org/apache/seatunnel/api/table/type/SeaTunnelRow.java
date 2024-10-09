33a9d2a09cf8b741ca58905b66134d9eddd7330c
CosmosNi <627165587@qq.com>
2024-10-09 09:55:29 +0800

    [Fix][Connector-V2] Fix local file source catalog transform ex

--- seatunnel-api/src/main/java/org/apache/seatunnel/api/table/type/SeaTunnelRow.java
+++ seatunnel-api/src/main/java/org/apache/seatunnel/api/table/type/SeaTunnelRow.java
@@ -118,7 +118,7 @@ public final class SeaTunnelRow implements Serializable {
         SqlType sqlType = dataType.getSqlType();
         switch (sqlType) {
             case STRING:
-                return (String.valueOf(v)).length();
+                return ((String) v).length();
             case BOOLEAN:
             case TINYINT:
                 return 1;
