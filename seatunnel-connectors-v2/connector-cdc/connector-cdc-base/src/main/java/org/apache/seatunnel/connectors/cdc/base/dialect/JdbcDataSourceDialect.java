768ca6635e7c72d882620eddde266aebb233e9ff
XenosK <xiangqiang.kong@sunmi.com>
2024-10-16 13:39:55 +0800

    [Feature][Connector-V2]  StringUtils.isNotEmpty

--- seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/base/dialect/JdbcDataSourceDialect.java
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/base/dialect/JdbcDataSourceDialect.java
@@ -17,6 +17,7 @@
 
 package org.apache.seatunnel.connectors.cdc.base.dialect;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.seatunnel.api.table.catalog.ConstraintKey;
 import org.apache.seatunnel.api.table.catalog.PrimaryKey;
 import org.apache.seatunnel.connectors.cdc.base.config.JdbcSourceConfig;
