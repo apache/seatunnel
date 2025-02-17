062c0d182fd0ccda0d6a55e0db91debca4adb761
baolin.guo <1245682673@qq.com>
2025-02-17 08:19:01 +0800

    selectDB code format

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-selectdb-cloud-e2e/src/test/java/org/apache/seatunnel/e2e/connector/selectdb/SelectDBSchemaChangeIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-selectdb-cloud-e2e/src/test/java/org/apache/seatunnel/e2e/connector/selectdb/SelectDBSchemaChangeIT.java
@@ -42,10 +42,10 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
