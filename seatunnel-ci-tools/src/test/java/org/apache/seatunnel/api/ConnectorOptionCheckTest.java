1f4847c3c89178c6c22fde3dee359d843967e0cc
lyne <33561138+lyne7-sc@users.noreply.github.com>
2025-02-18 09:51:37 +0800

    Update ConnectorOptionCheckTest.java

--- seatunnel-ci-tools/src/test/java/org/apache/seatunnel/api/ConnectorOptionCheckTest.java
+++ seatunnel-ci-tools/src/test/java/org/apache/seatunnel/api/ConnectorOptionCheckTest.java
@@ -205,8 +205,6 @@ public class ConnectorOptionCheckTest {
         whiteList.add("InfluxDBSinkOptions");
         whiteList.add("KuduSourceOptions");
         whiteList.add("SocketSinkOptions");
-        whiteList.add("DataHubSinkOptions");
-        whiteList.add("ClickhouseSinkOptions");
         whiteList.add("SelectDBSinkOptions");
         whiteList.add("PrometheusSinkOptions");
         whiteList.add("FirestoreSinkOptions");
