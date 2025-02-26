694e11de646d4fd6f94ae8e5ee1c31dccb3420ff
zeng <gientech.com>
2025-02-26 09:57:12 +0800

    [improve] adjust parameter relationship

--- seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/FileBaseSinkOptions.java
+++ seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/FileBaseSinkOptions.java
@@ -46,6 +46,14 @@ public class FileBaseSinkOptions extends FileBaseOptions {
     public static final String DEFAULT_FILE_NAME_EXPRESSION = "${transactionId}";
     public static final int DEFAULT_BATCH_SIZE = 1000000;
 
+    public static final Option<CompressFormat> TXT_COMPRESS =
+            Options.key("compress_codec")
+                    .singleChoice(
+                            CompressFormat.class,
+                            Arrays.asList(CompressFormat.NONE, CompressFormat.LZO))
+                    .defaultValue(CompressFormat.NONE)
+                    .withDescription("Txt file supported compression");
+
     public static final Option<CompressFormat> PARQUET_COMPRESS =
             Options.key("compress_codec")
                     .singleChoice(
