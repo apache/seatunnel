d7515e196ff33438dd5748a3254bba3a165f7936
lee <liqifan@cvte.com>
2025-03-04 16:17:28 +0800

    [Fix][MongoDB] The Long type cannot handle string values in scientific notation.

--- seatunnel-connectors-v2/connector-mongodb/src/test/java/org/apache/seatunnel/connectors/seatunnel/mongodb/source/split/SamplingSplitStrategyTest.java
+++ seatunnel-connectors-v2/connector-mongodb/src/test/java/org/apache/seatunnel/connectors/seatunnel/mongodb/source/split/SamplingSplitStrategyTest.java
@@ -1,11 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.seatunnel.connectors.seatunnel.mongodb.source.split;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
-import com.mongodb.MongoNamespace;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.seatunnel.connectors.seatunnel.mongodb.internal.MongodbClientProvider;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
 import org.bson.BsonDocument;
 import org.bson.BsonString;
 import org.bson.Document;
@@ -13,25 +28,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import com.mongodb.MongoNamespace;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class SamplingSplitStrategyTest {
 
-    @Mock
-    private MongodbClientProvider clientProvider;
+    @Mock private MongodbClientProvider clientProvider;
 
-    @Mock
-    private MongoCollection<BsonDocument> collection;
+    @Mock private MongoCollection<BsonDocument> collection;
 
-    @Mock
-    private MongoDatabase database;
+    @Mock private MongoDatabase database;
 
-    @Mock
-    private BsonDocument matchQuery;
+    @Mock private BsonDocument matchQuery;
 
     private SamplingSplitStrategy strategy;
 
@@ -47,7 +63,8 @@ public class SamplingSplitStrategyTest {
     }
 
     @Test
-    public void testGetDocumentNumAndAvgSize() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testGetDocumentNumAndAvgSize()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         BsonDocument statsCmd = new BsonDocument("collStats", new BsonString("collectionName"));
         Document res = new Document();
         res.put("count", "1.3360484963E10");
