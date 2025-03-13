d2b060b0209c4c52293f56e73296c26c859fcb89
Jarvis <jarvis@apache.org>
2025-03-13 09:24:06 +0800

    upd

--- seatunnel-connectors-v2/connector-http/connector-http-gitlab/src/main/java/org/apache/seatunnel/connectors/seatunnel/gitlab/source/config/GitlabSourceParameter.java
+++ seatunnel-connectors-v2/connector-http/connector-http-gitlab/src/main/java/org/apache/seatunnel/connectors/seatunnel/gitlab/source/config/GitlabSourceParameter.java
@@ -17,6 +17,7 @@
 
 package org.apache.seatunnel.connectors.seatunnel.gitlab.source.config;
 
+import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.shade.com.typesafe.config.Config;
 
 import org.apache.seatunnel.connectors.seatunnel.http.config.HttpParameter;
@@ -26,12 +27,12 @@ import java.util.HashMap;
 public class GitlabSourceParameter extends HttpParameter {
 
     @Override
-    public void buildWithConfig(Config pluginConfig) {
+    public void buildWithConfig(ReadonlyConfig pluginConfig) {
         super.buildWithConfig(pluginConfig);
         this.headers = this.getHeaders() == null ? new HashMap<>() : this.getHeaders();
         this.headers.put(
                 GitlabSourceOptions.PRIVATE_TOKEN,
-                pluginConfig.getString(GitlabSourceOptions.ACCESS_TOKEN.key()));
+                pluginConfig.get(GitlabSourceOptions.ACCESS_TOKEN));
         this.setHeaders(this.headers);
     }
 }
