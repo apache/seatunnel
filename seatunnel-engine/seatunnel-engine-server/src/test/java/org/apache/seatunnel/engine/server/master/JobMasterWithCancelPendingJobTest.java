1d76149c42f0e74048b1bcfdcfddec7fc7f82081
shenghang <shenghang@apache.org>
2025-03-16 22:20:17 +0800

    commit

---
+++ seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/master/JobMasterWithCancelPendingJobTest.java
@@ -0,0 +1,184 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.engine.server.master;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.internal.serialization.Data;
+import com.hazelcast.map.IMap;
+import org.apache.poi.ss.formula.functions.T;
+import org.apache.seatunnel.engine.common.Constant;
+import org.apache.seatunnel.engine.common.config.EngineConfig;
+import org.apache.seatunnel.engine.common.config.SeaTunnelConfig;
+import org.apache.seatunnel.engine.common.config.server.ScheduleStrategy;
+import org.apache.seatunnel.engine.common.runtime.ExecutionMode;
+import org.apache.seatunnel.engine.common.utils.PassiveCompletableFuture;
+import org.apache.seatunnel.engine.core.dag.logical.LogicalDag;
+import org.apache.seatunnel.engine.core.job.JobImmutableInformation;
+import org.apache.seatunnel.engine.core.job.JobInfo;
+import org.apache.seatunnel.engine.core.job.JobResult;
+import org.apache.seatunnel.engine.core.job.JobStatus;
+import org.apache.seatunnel.engine.core.job.PipelineStatus;
+import org.apache.seatunnel.engine.server.AbstractSeaTunnelServerTest;
+import org.apache.seatunnel.engine.server.SeaTunnelServer;
+import org.apache.seatunnel.engine.server.SeaTunnelServerStarter;
+import org.apache.seatunnel.engine.server.TestUtils;
+import org.apache.seatunnel.engine.server.checkpoint.CheckpointCloseReason;
+import org.apache.seatunnel.engine.server.checkpoint.CheckpointCoordinator;
+import org.apache.seatunnel.engine.server.dag.physical.PhysicalVertex;
+import org.apache.seatunnel.engine.server.dag.physical.PipelineLocation;
+import org.apache.seatunnel.engine.server.dag.physical.SubPlan;
+import org.apache.seatunnel.engine.server.execution.TaskGroupLocation;
+import org.apache.seatunnel.engine.server.resourcemanager.resource.SlotProfile;
+import org.apache.seatunnel.engine.server.service.slot.SlotService;
+import org.apache.seatunnel.engine.server.task.CoordinatorTask;
+import org.apache.seatunnel.engine.server.task.SeaTunnelTask;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+/** JobMaster Tester. */
+@DisabledOnOs(OS.WINDOWS)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class JobMasterWithCancelPendingJobTest extends AbstractSeaTunnelServerTest {
+    /**
+     * IMap key is jobId and value is a Tuple2 Tuple2 key is JobMaster init timestamp and value is
+     * the jobImmutableInformation which is sent by client when submit job
+     *
+     * <p>This IMap is used to recovery runningJobInfoIMap in JobMaster when a new master node
+     * active
+     */
+    private IMap<Long, JobInfo> runningJobInfoIMap;
+
+    /**
+     * IMap key is one of jobId {@link
+     * PipelineLocation} and {@link
+     * TaskGroupLocation}
+     *
+     * <p>The value of IMap is one of {@link JobStatus} {@link PipelineStatus} {@link
+     * org.apache.seatunnel.engine.server.execution.ExecutionState}
+     *
+     * <p>This IMap is used to recovery runningJobStateIMap in JobMaster when a new master node
+     * active
+     */
+    IMap<Object, Object> runningJobStateIMap;
+
+    /**
+     * IMap key is one of jobId {@link
+     * PipelineLocation} and {@link
+     * TaskGroupLocation}
+     *
+     * <p>The value of IMap is one of {@link
+     * org.apache.seatunnel.engine.server.dag.physical.PhysicalPlan} stateTimestamps {@link
+     * SubPlan} stateTimestamps {@link
+     * PhysicalVertex} stateTimestamps
+     *
+     * <p>This IMap is used to recovery runningJobStateTimestampsIMap in JobMaster when a new master
+     * node active
+     */
+    IMap<Object, Long[]> runningJobStateTimestampsIMap;
+
+    /**
+     * IMap key is {@link PipelineLocation}
+     *
+     * <p>The value of IMap is map of {@link TaskGroupLocation} and the {@link SlotProfile} it used.
+     *
+     * <p>This IMap is used to recovery ownedSlotProfilesIMap in JobMaster when a new master node
+     * active
+     */
+    private IMap<PipelineLocation, Map<TaskGroupLocation, SlotProfile>> ownedSlotProfilesIMap;
+
+    @BeforeAll
+    public void before() {
+        String name = this.getClass().getName();
+        Config hazelcastConfig = Config.loadFromString(getHazelcastConfig());
+        hazelcastConfig.setClusterName(
+                TestUtils.getClusterName("AbstractSeaTunnelServerTest_" + name));
+        SeaTunnelConfig seaTunnelConfig = loadSeaTunnelConfig();
+        seaTunnelConfig.setHazelcastConfig(hazelcastConfig);
+        EngineConfig engineConfig = seaTunnelConfig.getEngineConfig();
+        engineConfig.setMode(ExecutionMode.LOCAL);
+        engineConfig.setScheduleStrategy(ScheduleStrategy.WAIT);
+        engineConfig.getSlotServiceConfig().setDynamicSlot(false);
+        engineConfig.getSlotServiceConfig().setSlotNum(1);
+        instance = SeaTunnelServerStarter.createHazelcastInstance(seaTunnelConfig);
+        nodeEngine = instance.node.nodeEngine;
+        server = nodeEngine.getService(SeaTunnelServer.SERVICE_NAME);
+        LOGGER = nodeEngine.getLogger(AbstractSeaTunnelServerTest.class);
+//        cancel_pending_job.conf
+    }
+
+    @Test
+    public void testCancelPendingJob() throws InterruptedException {
+
+
+        long jobId = instance.getFlakeIdGenerator(Constant.SEATUNNEL_ID_GENERATOR_NAME).newId();
+        JobMaster jobMaster = newJobInstanceWithRunningState(jobId);
+        Assertions.assertEquals(JobStatus.RUNNING, jobMaster.getJobStatus());
+
+    }
+
+
+    private JobMaster newJobInstanceWithRunningState(long jobId) throws InterruptedException {
+        return newJobInstanceWithRunningState(jobId, false);
+    }
+
+    private JobMaster newJobInstanceWithRunningState(long jobId, boolean restore)
+            throws InterruptedException {
+        LogicalDag testLogicalDag =
+                TestUtils.createTestLogicalPlan(
+                        "cancel_pending_job.conf", "cancel_pending_job", jobId);
+
+        JobImmutableInformation jobImmutableInformation =
+                new JobImmutableInformation(
+                        jobId,
+                        "Test",
+                        restore,
+                        nodeEngine.getSerializationService(),
+                        testLogicalDag,
+                        Collections.emptyList(),
+                        Collections.emptyList());
+
+        Data data = nodeEngine.getSerializationService().toData(jobImmutableInformation);
+
+        PassiveCompletableFuture<Void> voidPassiveCompletableFuture =
+                server.getCoordinatorService()
+                        .submitJob(jobId, data, jobImmutableInformation.isStartWithSavePoint());
+        voidPassiveCompletableFuture.join();
+
+        JobMaster jobMaster = server.getCoordinatorService().getJobMaster(jobId);
+
+        // waiting for job status turn to running
+        await().atMost(120000, TimeUnit.MILLISECONDS)
+                .untilAsserted(
+                        () -> Assertions.assertEquals(JobStatus.PENDING, jobMaster.getJobStatus()));
+
+        // Because handleCheckpointTimeout is an async method, so we need sleep 5s to waiting job
+        // status become running again
+        Thread.sleep(5000);
+        return jobMaster;
+    }
+}
