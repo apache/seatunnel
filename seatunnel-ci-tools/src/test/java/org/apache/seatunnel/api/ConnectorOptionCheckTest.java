c802ff7ad288c97972a5a08a4ab9f76ec0ab48fc
Jarvis <jarvis@apache.org>
2025-03-26 09:40:48 +0800

    update

--- seatunnel-ci-tools/src/test/java/org/apache/seatunnel/api/ConnectorOptionCheckTest.java
+++ seatunnel-ci-tools/src/test/java/org/apache/seatunnel/api/ConnectorOptionCheckTest.java
@@ -189,7 +189,6 @@ public class ConnectorOptionCheckTest {
 
     private Set<String> buildWhiteList() {
         Set<String> whiteList = new HashSet<>();
-        whiteList.add("JdbcSinkOptions");
         whiteList.add("TypesenseSourceOptions");
         whiteList.add("TypesenseSinkOptions");
         whiteList.add("PulsarSinkOptions");
