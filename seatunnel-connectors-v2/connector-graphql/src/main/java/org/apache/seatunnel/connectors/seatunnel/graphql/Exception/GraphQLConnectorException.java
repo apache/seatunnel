adb109e370c7a943404f7cc37eaccb5a79f57e5f
hailin0 <hailin088@gmail.com>
2025-04-14 10:48:46 +0800

    Update seatunnel-connectors-v2/connector-graphql/src/main/java/org/apache/seatunnel/connectors/seatunnel/graphql/Exception/GraphQLConnectorException.java

--- seatunnel-connectors-v2/connector-graphql/src/main/java/org/apache/seatunnel/connectors/seatunnel/graphql/Exception/GraphQLConnectorException.java
+++ seatunnel-connectors-v2/connector-graphql/src/main/java/org/apache/seatunnel/connectors/seatunnel/graphql/Exception/GraphQLConnectorException.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.seatunnel.connectors.seatunnel.graphql.Exception;
+package org.apache.seatunnel.connectors.seatunnel.graphql.exception;
 
 import org.apache.seatunnel.common.exception.SeaTunnelErrorCode;
 import org.apache.seatunnel.common.exception.SeaTunnelRuntimeException;
