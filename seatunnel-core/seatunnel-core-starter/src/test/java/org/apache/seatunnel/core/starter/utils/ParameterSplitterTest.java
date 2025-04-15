300e0059dbeb8c97d40934b8cf3477d667a7c612
lee <liqifan@cvte.com>
2025-04-15 17:30:03 +0800

    [Fix][Common] Problems with comma-separated logic in variable values during variable substitution.(#9151)

--- seatunnel-core/seatunnel-core-starter/src/test/java/org/apache/seatunnel/core/starter/utils/ParameterSplitterTest.java
+++ seatunnel-core/seatunnel-core-starter/src/test/java/org/apache/seatunnel/core/starter/utils/ParameterSplitterTest.java
@@ -21,6 +21,8 @@ import org.apache.seatunnel.core.starter.command.ParameterSplitter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParameterSplitterTest {
