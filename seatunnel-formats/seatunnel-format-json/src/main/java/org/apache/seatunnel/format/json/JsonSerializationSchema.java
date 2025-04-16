fe68f643343e52f56a460e80396704108f4fd287
misi <gientech.com>
2025-04-16 11:25:40 +0800

    [improve] remove annotation

--- seatunnel-formats/seatunnel-format-json/src/main/java/org/apache/seatunnel/format/json/JsonSerializationSchema.java
+++ seatunnel-formats/seatunnel-format-json/src/main/java/org/apache/seatunnel/format/json/JsonSerializationSchema.java
@@ -18,6 +18,7 @@
 
 package org.apache.seatunnel.format.json;
 
+import org.apache.seatunnel.shade.com.fasterxml.jackson.core.JsonGenerator;
 import org.apache.seatunnel.shade.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.seatunnel.shade.com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -49,9 +50,9 @@ public class JsonSerializationSchema implements SerializationSchema {
 
     private final RowToJsonConverters.RowToJsonConverter runtimeConverter;
 
-    //    {
-    //        mapper.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
-    //    }
+    {
+        mapper.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
+    }
 
     public JsonSerializationSchema(SeaTunnelRowType rowType) {
         this(rowType, StandardCharsets.UTF_8);
