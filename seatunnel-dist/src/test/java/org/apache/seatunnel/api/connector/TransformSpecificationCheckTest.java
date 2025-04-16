5d9b73a4b5a780d3b239a423bd86443af1a74ad4
hailin0 <wanghailin@apache.org>
2025-04-16 23:01:00 +0800

    [Feature][Transform] Add table filter transform

--- seatunnel-dist/src/test/java/org/apache/seatunnel/api/connector/TransformSpecificationCheckTest.java
+++ seatunnel-dist/src/test/java/org/apache/seatunnel/api/connector/TransformSpecificationCheckTest.java
@@ -43,7 +43,7 @@ public class TransformSpecificationCheckTest {
                 FactoryUtil.discoverFactories(
                         Thread.currentThread().getContextClassLoader(),
                         TableTransformFactory.class);
-        Assertions.assertEquals(16, factories.size());
+        Assertions.assertEquals(17, factories.size());
     }
 
     @Test
