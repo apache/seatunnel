c1b4118c2211a45064b2fae337cdc0e9da379048
njh_cmss <nijiahui_yewu@cmss.chinamobile.com>
2025-04-21 10:12:54 +0800

    [Improve][Zeta] Add log to print the each task execute location

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/TaskExecutionService.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/TaskExecutionService.java
@@ -686,6 +686,14 @@ public class TaskExecutionService implements DynamicMetricsProvider {
             ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
             Thread.currentThread().setContextClassLoader(classLoader);
             final Task t = tracker.task;
+
+            // Log worker address for task execution
+            String workerAddress = nodeEngine.getThisAddress().toString();
+            logger.info(
+                    String.format(
+                            "Starting task [%s] execution on worker [%s]",
+                            t.getTaskID(), workerAddress));
+
             ProgressState result = null;
             try {
                 startedLatch.countDown();
@@ -796,6 +804,14 @@ public class TaskExecutionService implements DynamicMetricsProvider {
                                     .get(taskGroupExecutionTracker.taskGroup.getTaskGroupLocation())
                                     .getClassLoaders()
                                     .get(taskTracker.task.getTaskID()));
+
+                    // Log worker address for task execution
+                    String workerAddress = nodeEngine.getThisAddress().toString();
+                    logger.info(
+                            String.format(
+                                    "Starting task [%s] execution on worker [%s]",
+                                    taskTracker.task.getTaskID(), workerAddress));
+
                     call = taskTracker.task.call();
                     synchronized (timer) {
                         timer.timerStop();
@@ -938,10 +954,11 @@ public class TaskExecutionService implements DynamicMetricsProvider {
 
         void taskDone(Task task) {
             TaskGroupLocation taskGroupLocation = taskGroup.getTaskGroupLocation();
+            String workerAddress = nodeEngine.getThisAddress().toString();
             logger.info(
                     String.format(
-                            "taskDone, taskId = %d, taskGroup = %s",
-                            task.getTaskID(), taskGroupLocation));
+                            "Task [%d] completed on worker [%s], taskGroup = %s",
+                            task.getTaskID(), workerAddress, taskGroupLocation));
             Throwable ex = executionException.get();
             if (completionLatch.decrementAndGet() == 0) {
                 recycleClassLoader(taskGroupLocation);
--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/AbstractTask.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/task/AbstractTask.java
@@ -27,6 +27,8 @@ import org.apache.seatunnel.engine.server.execution.TaskExecutionContext;
 import org.apache.seatunnel.engine.server.execution.TaskLocation;
 import org.apache.seatunnel.engine.server.task.statemachine.SeaTunnelTaskState;
 
+import com.hazelcast.cluster.Address;
+import com.hazelcast.logging.ILogger;
 import lombok.NonNull;
 
 import java.io.IOException;
@@ -81,6 +83,16 @@ public abstract class AbstractTask implements Task {
     public void init() throws Exception {
         this.restoreComplete = new CompletableFuture<>();
         progress.start();
+
+        // 记录任务执行位置的日志
+        if (executionContext != null) {
+            ILogger logger = executionContext.getLogger();
+            Address workerAddress =
+                    executionContext.getInstance().getCluster().getLocalMember().getAddress();
+            logger.info(
+                    String.format(
+                            "Task [%s] executing on worker [%s]", getTaskID(), workerAddress));
+        }
     }
 
     @NonNull @Override
