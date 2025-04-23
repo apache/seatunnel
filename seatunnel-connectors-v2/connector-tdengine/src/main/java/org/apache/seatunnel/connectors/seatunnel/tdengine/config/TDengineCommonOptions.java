90278f7e30a3dbacdc722377d0a09d88224b8137
jia zhang <keven.jia.zhang@qq.com>
2025-04-23 10:23:44 +0800

    [Improve][Connector-V2]Adjust some common options in source and sink for tdengine

--- seatunnel-connectors-v2/connector-tdengine/src/main/java/org/apache/seatunnel/connectors/seatunnel/tdengine/config/TDengineCommonOptions.java
+++ seatunnel-connectors-v2/connector-tdengine/src/main/java/org/apache/seatunnel/connectors/seatunnel/tdengine/config/TDengineCommonOptions.java
@@ -20,22 +20,37 @@ package org.apache.seatunnel.connectors.seatunnel.tdengine.config;
 import org.apache.seatunnel.api.configuration.Option;
 import org.apache.seatunnel.api.configuration.Options;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
-public class TDengineSourceOptions extends TDengineCommonOptions {
-
-    public static final Option<String> LOWER_BOUND =
-            Options.key("lowerBound")
+public abstract class TDengineCommonOptions {
+    public static final Option<String> URL =
+            Options.key("url")
                     .stringType()
                     .noDefaultValue()
-                    .withDescription("The lower bound for data query range");
+                    .withDescription("The TDengine server URL, format: jdbc:TAOS-RS://host:port");
 
-    public static final Option<String> UPPER_BOUND =
-            Options.key("upperBound")
+    public static final Option<String> USERNAME =
+            Options.key("username")
                     .stringType()
                     .noDefaultValue()
-                    .withDescription("The upper bound for data query range");
+                    .withDescription("The username for TDengine authentication");
+
+    public static final Option<String> PASSWORD =
+            Options.key("password")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription("The password for TDengine authentication");
+
+    public static final Option<String> DATABASE =
+            Options.key("database")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription("The TDengine database name");
+
+    public static final Option<String> STABLE =
+            Options.key("stable")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription("The TDengine super table name");
 }
--- seatunnel-connectors-v2/connector-tdengine/src/main/java/org/apache/seatunnel/connectors/seatunnel/tdengine/config/TDengineSinkOptions.java
+++ seatunnel-connectors-v2/connector-tdengine/src/main/java/org/apache/seatunnel/connectors/seatunnel/tdengine/config/TDengineSinkOptions.java
@@ -25,37 +25,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class TDengineSinkOptions {
-
-    public static final Option<String> URL =
-            Options.key("url")
-                    .stringType()
-                    .noDefaultValue()
-                    .withDescription("The TDengine server URL, format: jdbc:TAOS-RS://host:port");
-
-    public static final Option<String> USERNAME =
-            Options.key("username")
-                    .stringType()
-                    .noDefaultValue()
-                    .withDescription("The username for TDengine authentication");
-
-    public static final Option<String> PASSWORD =
-            Options.key("password")
-                    .stringType()
-                    .noDefaultValue()
-                    .withDescription("The password for TDengine authentication");
-
-    public static final Option<String> DATABASE =
-            Options.key("database")
-                    .stringType()
-                    .noDefaultValue()
-                    .withDescription("The TDengine database name");
-
-    public static final Option<String> STABLE =
-            Options.key("stable")
-                    .stringType()
-                    .noDefaultValue()
-                    .withDescription("The TDengine super table name");
+public class TDengineSinkOptions extends TDengineCommonOptions {
 
     public static final Option<String> TIMEZONE =
             Options.key("timezone")
