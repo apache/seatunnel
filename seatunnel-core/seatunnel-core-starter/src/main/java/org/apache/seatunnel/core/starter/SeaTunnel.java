9312afdfa2fd150346b18564a5544661791e2e0c
njh_cmss <nijiahui_yewu@cmss.chinamobile.com>
2025-05-12 15:05:17 +0800

    [Bug][Zeta] SeaTunnelClient can not exit with error

--- seatunnel-core/seatunnel-core-starter/src/main/java/org/apache/seatunnel/core/starter/SeaTunnel.java
+++ seatunnel-core/seatunnel-core-starter/src/main/java/org/apache/seatunnel/core/starter/SeaTunnel.java
@@ -55,6 +55,7 @@ public class SeaTunnel {
         String errorMsg = throwable.getMessage();
         log.error("Config Error:\n");
         log.error("Reason: {} \n", errorMsg);
+        log.error("Exception StackTrace:{} ", ExceptionUtils.getStackTrace(throwable));
         log.error(
                 "\n===============================================================================\n\n\n");
     }
