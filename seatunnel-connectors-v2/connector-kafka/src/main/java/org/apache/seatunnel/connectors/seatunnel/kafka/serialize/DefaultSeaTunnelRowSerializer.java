02eb5cea130f0048cc1767d42869d0949bf6804f
WenDing-Y <1062698930@qq.com>
2025-05-29 13:25:30 +0800

    Update DefaultSeaTunnelRowSerializer.java

--- seatunnel-connectors-v2/connector-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/serialize/DefaultSeaTunnelRowSerializer.java
+++ seatunnel-connectors-v2/connector-kafka/src/main/java/org/apache/seatunnel/connectors/seatunnel/kafka/serialize/DefaultSeaTunnelRowSerializer.java
@@ -100,7 +100,7 @@ public class DefaultSeaTunnelRowSerializer implements SeaTunnelRowSerializer {
             ReadonlyConfig pluginConfig) {
         return new DefaultSeaTunnelRowSerializer(
                 topicExtractor(topic, rowType, format),
-                partitionExtractor(null),
+                partitionNativeExtractor(rowType),
                 timestampExtractor(rowType),
                 keyExtractor(null, rowType, format, null, null),
                 valueExtractor(rowType, format, delimiter, pluginConfig),
