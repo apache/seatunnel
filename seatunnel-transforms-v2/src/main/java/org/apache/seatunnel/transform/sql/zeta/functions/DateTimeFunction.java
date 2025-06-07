6583bf8168adc791b05be24157067bd3b98b2752
corgy-w <corgy-w@qq.com>
2025-06-07 14:43:09 +0800

    [Fix][Transform-V2] correct handling of 'T' in date format strings

--- seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/sql/zeta/functions/DateTimeFunction.java
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/sql/zeta/functions/DateTimeFunction.java
@@ -634,8 +634,8 @@ public class DateTimeFunction {
             return null;
         }
         String format = (String) args.get(1);
-        if (format.contains("T") && !format.contains("'T'")) {
-            format = format.replace("T", "'T'");
+        if (format.contains("''T''")) {
+            format = format.replace("''T''", "'T'");
         }
         if (format.contains("yy") && format.contains("mm")) {
             DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
