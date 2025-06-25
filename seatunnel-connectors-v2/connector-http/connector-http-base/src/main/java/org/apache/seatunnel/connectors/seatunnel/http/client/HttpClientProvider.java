77ecc44da15cf3adf3969f5dcde6252db4ee13d9
xinlong <xinlong.wang@hehemobi.com>
2025-06-25 09:44:59 +0800

    [Bug] [seatunnel-connector-http-base] should check whether it already exists Content-Type in header, to avoid duplicate additions #9476

--- seatunnel-connectors-v2/connector-http/connector-http-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/http/client/HttpClientProvider.java
+++ seatunnel-connectors-v2/connector-http/connector-http-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/http/client/HttpClientProvider.java
@@ -489,7 +489,9 @@ public class HttpClientProvider implements AutoCloseable {
                 request.setEntity(new UrlEncodedFormEntity(parameters, ENCODING));
             }
         } else {
-            request.addHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON);
+            if (!request.containsHeader(HTTP.CONTENT_TYPE)) {
+                request.addHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON);
+            }
             StringEntity entity =
                     new StringEntity(JsonUtils.toJsonString(body), ContentType.APPLICATION_JSON);
             entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON));
