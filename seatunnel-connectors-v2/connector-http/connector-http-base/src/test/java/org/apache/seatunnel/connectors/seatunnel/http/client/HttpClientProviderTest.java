dee976acffcb83e8afd72aef1f18131d05019c6f
xinlong <xinlong.wang@hehemobi.com>
2025-07-10 16:29:20 +0800

    modified test case code

--- seatunnel-connectors-v2/connector-http/connector-http-base/src/test/java/org/apache/seatunnel/connectors/seatunnel/http/client/HttpClientProviderTest.java
+++ seatunnel-connectors-v2/connector-http/connector-http-base/src/test/java/org/apache/seatunnel/connectors/seatunnel/http/client/HttpClientProviderTest.java
@@ -19,36 +19,20 @@ package org.apache.seatunnel.connectors.seatunnel.http.client;
 import org.apache.http.Header;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 
 class HttpClientProviderTest {
 
-    private HttpPost mockRequest;
-
-    @BeforeEach
-    void setUp() {
-
-        mockRequest = new HttpPost("http://localhost:8080");
-    }
-
-    @AfterEach
-    void tearDown() {
-        mockRequest = null;
-    }
-
     @Test
     void testAddDefaultJsonContentTypeWhenNotPresent() throws Exception {
+        HttpPost mockRequest = new HttpPost("http://localhost:8080");
         Map<String, Object> body = new HashMap<>();
         body.put("key", "value");
 
@@ -62,6 +46,7 @@ class HttpClientProviderTest {
 
     @Test
     void testPreserveExistingContentType() throws Exception {
+        HttpPost mockRequest = new HttpPost("http://localhost:8080");
         mockRequest.addHeader(new BasicHeader("Content-Type", "text/plain"));
 
         Map<String, Object> body = new HashMap<>();
@@ -77,15 +62,9 @@ class HttpClientProviderTest {
 
     @Test
     void addBody() throws Exception {
-        HttpPost post = mockRequest;
-
-        Header[] originalHeaders =
-                new Header[] {new BasicHeader(HTTP.CONTENT_TYPE, "application/json;utf-8")};
-        when(mockRequest.getAllHeaders()).thenReturn(originalHeaders);
-        when(mockRequest.getHeaders(HTTP.CONTENT_TYPE)).thenReturn(new Header[0]);
-
+        HttpPost post = new HttpPost("http://localhost:8080");
         Map<String, Object> body = new HashMap<>();
-        //        Header[] originalHeaders = post.getAllHeaders();
+        Header[] originalHeaders = post.getAllHeaders();
         HttpClientProvider.addBody(post, body);
 
         // ensure the original headers are preserved
@@ -102,7 +81,6 @@ class HttpClientProviderTest {
                     "Header value mismatch at index " + i);
         }
         // ensure no manually set content type or encoding
-        // test case fix, content type support user manually  set
-        //        Assertions.assertNull(post.getEntity().getContentEncoding());
+        Assertions.assertNull(post.getEntity().getContentEncoding());
     }
 }
