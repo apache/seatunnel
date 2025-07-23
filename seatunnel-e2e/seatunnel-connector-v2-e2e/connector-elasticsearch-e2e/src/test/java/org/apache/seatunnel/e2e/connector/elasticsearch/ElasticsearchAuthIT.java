0a8b17b3e39a7f3cda9cea1c04bdd2797ab85c8c
njh_cmss <nijiahui_yewu@cmss.chinamobile.com>
2025-07-23 15:26:55 +0800

    [Feature][elasticsearch-connector] Add API key authentication method

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-elasticsearch-e2e/src/test/java/org/apache/seatunnel/e2e/connector/elasticsearch/ElasticsearchAuthIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-elasticsearch-e2e/src/test/java/org/apache/seatunnel/e2e/connector/elasticsearch/ElasticsearchAuthIT.java
@@ -203,19 +203,19 @@ public class ElasticsearchAuthIT extends TestSuiteBase implements TestResource {
 
         log.info("Creating real API key at: {}", apiKeyUrl);
 
-        // Create API key request body
+        // Create API key request body with broader permissions
         String requestBody =
                 "{\n"
                         + "  \"name\": \"seatunnel-test-api-key\",\n"
                         + "  \"role_descriptors\": {\n"
                         + "    \"seatunnel_test_role\": {\n"
-                        + "      \"cluster\": [\"monitor\", \"manage_index_templates\"],\n"
+                        + "      \"cluster\": [\"monitor\", \"manage_index_templates\", \"create_index\"],\n"
                         + "      \"indices\": [\n"
                         + "        {\n"
                         + "          \"names\": [\""
                         + TEST_INDEX
-                        + "\", \"test_*\"],\n"
-                        + "          \"privileges\": [\"all\"]\n"
+                        + "\", \"auth_test_*\", \"test_*\", \"*_target\"],\n"
+                        + "          \"privileges\": [\"all\", \"create\", \"create_index\", \"delete\", \"index\", \"manage\", \"read\", \"write\"]\n"
                         + "        }\n"
                         + "      ]\n"
                         + "    }\n"
