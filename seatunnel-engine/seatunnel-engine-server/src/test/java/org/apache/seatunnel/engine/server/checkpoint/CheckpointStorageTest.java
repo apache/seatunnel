e25ffdcc69652c6d6fc7906b7bd1f55a6cd05d54
chestnufang <chestnufang@tencent.com>
2025-07-24 16:00:24 +0800

    fix unit case test

--- seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/checkpoint/CheckpointStorageTest.java
+++ seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/checkpoint/CheckpointStorageTest.java
@@ -89,9 +89,7 @@ public class CheckpointStorageTest extends AbstractSeaTunnelServerTest {
                                         server.getCoordinatorService().getJobStatus(jobId),
                                         JobStatus.SAVEPOINT_DONE));
         List<PipelineState> savepoint1 = checkpointStorage.getAllCheckpoints(String.valueOf(jobId));
-
-        await().atMost(5000, TimeUnit.MILLISECONDS)
-                .untilAsserted(() -> Assertions.assertEquals(1, savepoint1.size()));
+        Assertions.assertEquals(1, savepoint1.size());
     }
 
     @Test
