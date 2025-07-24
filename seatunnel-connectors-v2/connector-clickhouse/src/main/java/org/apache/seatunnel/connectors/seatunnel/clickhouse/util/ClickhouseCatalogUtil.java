9875a706e34f150c5aa38735c6900c5e5343fe46
lixuekun <lixuekun820@126.com>
2025-07-24 09:47:54 +0800

    fix clickHouse createTable column nullable

--- seatunnel-connectors-v2/connector-clickhouse/src/main/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/util/ClickhouseCatalogUtil.java
+++ seatunnel-connectors-v2/connector-clickhouse/src/main/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/util/ClickhouseCatalogUtil.java
@@ -36,8 +36,11 @@ public class ClickhouseCatalogUtil extends CatalogUtil {
         if (column.getSinkType() != null) {
             columnType = column.getSinkType();
         } else {
-            columnType = column.isNullable() ? "Nullable("+ ClickhouseTypeConverter.INSTANCE.reconvert(column).getColumnType() + ")":
-                    ClickhouseTypeConverter.INSTANCE.reconvert(column).getColumnType();
+            if (column.isNullable()) {
+                columnType = "Nullable(" + ClickhouseTypeConverter.INSTANCE.reconvert(column).getColumnType() + ")";
+            } else {
+                columnType = ClickhouseTypeConverter.INSTANCE.reconvert(column).getColumnType();
+            }
         }
         return String.format(
                 "`%s` %s %s",
