61c542987dc904745a71471445a1e0866d835941
liucong <kevinliu57@163.com>
2025-07-30 13:24:10 +0800

    [Fix][engine-server] Fix the issue of garbled characters in log output, remove unuse import in BaseServlet

--- seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/rest/BaseServletTest.java
+++ seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/rest/BaseServletTest.java
@@ -40,7 +40,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 class BaseServletTest extends AbstractSeaTunnelServerTest {
 
