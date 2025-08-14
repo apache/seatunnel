d47f24464cbb2e312222fa1f4ac90cea482dcda7
zhangqingsong <zhangqs0205@126.com>
2025-08-14 19:22:09 +0800

    [Feature][connector-v2] Add Sensorsdata Connector Support #9323 (#9432)

---
+++ seatunnel-connectors-v2/connector-sensorsdata/src/main/java/org/apache/seatunnel/connectors/sensorsdata/format/record/SensorsDataJsonKeys.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.sensorsdata.format.record;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SensorsDataJsonKeys {
+
+    public static final String TRACK_ID = "_track_id";
+
+    public static final String VERSION = "version";
+
+    public static final String TYPE = "type";
+
+    public static final String LIB = "lib";
+
+    public static final String TIME = "time";
+
+    public static final String PROJECT = "project";
+
+    public static final String PROPERTIES = "properties";
+
+    public static final String TOKEN = "token";
+
+    public static final String SCHEMA = "schema";
+
+    public static final String EVENT = "event";
+
+    public static final String ID = "id";
+
+    public static final String TIME_FREE = "time_free";
+
+    public static final String IDENTITIES = "identities";
+
+    public static final String DISTINCT_ID = "distinct_id";
+
+    public static final String ITEM_ID = "item_id";
+
+    public static final String ITEM_TYPE = "item_type";
+}
