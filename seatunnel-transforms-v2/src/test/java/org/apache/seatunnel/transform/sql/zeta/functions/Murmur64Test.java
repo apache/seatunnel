2ddf7df97cb7017e23a32f4c375e9449a4d070e6
David Zollo <davidzollo365@gmail.com>
2025-08-25 17:18:32 +0800

    Update seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/sql/zeta/functions/Murmur64Test.java
    Co-authored-by: Jia Fan <fanjiaeminem@qq.com>

--- seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/sql/zeta/functions/Murmur64Test.java
+++ seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/sql/zeta/functions/Murmur64Test.java
@@ -70,12 +70,6 @@ public class Murmur64Test {
         Assertions.assertEquals(murmur64Direct("unicode_test"), outRow.getField(3));
         Assertions.assertEquals(murmur64Direct(null), outRow.getField(4));
 
-        log.info("SQL Engine test results:");
-        log.info("'hello world' -> {}", outRow.getField(0));
-        log.info("'' -> {}", outRow.getField(1));
-        log.info("'test123' -> {}", outRow.getField(2));
-        log.info("'unicode_test' -> {}", outRow.getField(3));
-        log.info("null -> {}", outRow.getField(4));
     }
 
     /**
