f4575d0556525d99b14241bac89d484afdfa0493
zhangdonghao <hawk9821@qq.com>
2025-09-08 18:16:53 +0800

    [Fix][Zeta] Fix pendingJobMasterMap resource leak

--- seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/master/JobMasterTest.java
+++ seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/master/JobMasterTest.java
@@ -51,6 +51,7 @@ import com.hazelcast.map.IMap;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.awaitility.Awaitility.await;
 
@@ -310,17 +311,28 @@ public class JobMasterTest extends AbstractSeaTunnelServerTest {
                         .submitJob(jobId, data, jobImmutableInformation.isStartWithSavePoint());
         voidPassiveCompletableFuture.join();
 
-        JobMaster jobMaster = server.getCoordinatorService().getJobMaster(jobId);
-        Assertions.assertNotNull(jobMaster);
+        AtomicReference<JobMaster> jobMasterReference = new AtomicReference<>();
+
+        await().atMost(30000, TimeUnit.MILLISECONDS)
+                .untilAsserted(
+                        () -> {
+                            JobMaster jobMaster =
+                                    server.getCoordinatorService().getJobMaster(jobId);
+                            Assertions.assertNotNull(jobMaster);
+                            jobMasterReference.set(jobMaster);
+                        });
 
         // waiting for job status turn to running
         await().atMost(120000, TimeUnit.MILLISECONDS)
                 .untilAsserted(
-                        () -> Assertions.assertEquals(JobStatus.RUNNING, jobMaster.getJobStatus()));
+                        () ->
+                                Assertions.assertEquals(
+                                        JobStatus.RUNNING,
+                                        jobMasterReference.get().getJobStatus()));
 
         // Because handleCheckpointTimeout is an async method, so we need sleep 5s to waiting job
         // status become running again
         Thread.sleep(5000);
-        return jobMaster;
+        return jobMasterReference.get();
     }
 }
