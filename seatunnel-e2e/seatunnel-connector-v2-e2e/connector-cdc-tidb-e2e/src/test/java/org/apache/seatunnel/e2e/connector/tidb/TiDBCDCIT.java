e396e7cb17027423a08852d4421941f4494b4ef2
Jast <shenghang@apache.org>
2025-10-07 22:39:19 +0800

    refactor(jdbc): remove unused DockerClient and cleanup test code- Removed unused DockerClient and DockerClientFactory imports - Removed unused URL and SQL constants- Removed redundant docker image removal code - Renamed test method to follow camelCase convention - Removed commented out super.startUp() call

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-cdc-tidb-e2e/src/test/java/org/apache/seatunnel/e2e/connector/tidb/TiDBCDCIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-cdc-tidb-e2e/src/test/java/org/apache/seatunnel/e2e/connector/tidb/TiDBCDCIT.java
@@ -48,7 +48,7 @@ import static org.awaitility.Awaitility.await;
 @Slf4j
 @DisabledOnContainer(
         value = {},
-        type = {EngineType.SPARK, EngineType.FLINK},
+        type = {EngineType.SPARK},
         disabledReason = "Currently SPARK do not support cdc")
 public class TiDBCDCIT extends TiDBTestBase implements TestResource {
 
