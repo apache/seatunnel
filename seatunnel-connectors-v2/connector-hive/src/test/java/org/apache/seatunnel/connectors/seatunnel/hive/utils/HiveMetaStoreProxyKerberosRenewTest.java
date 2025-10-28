242c16e1eb2fb29de182da9b636de6dffa31bb8f
wangxiaogang <wangxiaogang@chinatelecom.cn>
2025-10-28 17:08:00 +0800

    BDPL-25768 Add UT for hive kerberos renew

--- seatunnel-connectors-v2/connector-hive/src/test/java/org/apache/seatunnel/connectors/seatunnel/hive/utils/HiveMetaStoreProxyKerberosRenewTest.java
+++ seatunnel-connectors-v2/connector-hive/src/test/java/org/apache/seatunnel/connectors/seatunnel/hive/utils/HiveMetaStoreProxyKerberosRenewTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.seatunnel.connectors.seatunnel.hive.utils;
 
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
@@ -10,13 +27,26 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
-static class HiveMetaStoreProxyKerberosRenewTest {
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class HiveMetaStoreProxyKerberosRenewTest {
 
     private static void set(Object target, String field, Object value) throws Exception {
         Field f = target.getClass().getDeclaredField(field);
         f.setAccessible(true);
         f.set(target, value);
+    }
+
+    private static Object invoke(Object target, String method) throws Exception {
+        Method m = target.getClass().getDeclaredMethod(method);
+        m.setAccessible(true);
+        return m.invoke(target);
     }
 
     @Test
@@ -32,7 +62,7 @@ static class HiveMetaStoreProxyKerberosRenewTest {
         set(proxy, "userGroupInformation", ugi);
         set(proxy, "kerberosEnabled", true);
 
-        HiveMetaStoreClient out = proxy.getClient();
+        HiveMetaStoreClient out = (HiveMetaStoreClient) invoke(proxy, "getClient");
         Assertions.assertNotNull(out);
         verify(ugi, times(1)).checkTGTAndReloginFromKeytab();
     }
@@ -50,7 +80,7 @@ static class HiveMetaStoreProxyKerberosRenewTest {
         set(proxy, "userGroupInformation", ugi);
         set(proxy, "kerberosEnabled", true);
 
-        HiveMetaStoreClient out = proxy.getClient();
+        HiveMetaStoreClient out = (HiveMetaStoreClient) invoke(proxy, "getClient");
         Assertions.assertNotNull(out);
         verify(ugi, never()).checkTGTAndReloginFromKeytab();
     }
@@ -69,7 +99,14 @@ static class HiveMetaStoreProxyKerberosRenewTest {
         set(proxy, "userGroupInformation", ugi);
         set(proxy, "kerberosEnabled", true);
 
-        Assertions.assertDoesNotThrow(proxy::getClient);
+        Assertions.assertDoesNotThrow(
+                () -> {
+                    try {
+                        invoke(proxy, "getClient");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
         verify(ugi, times(1)).checkTGTAndReloginFromKeytab();
     }
 }
