5863f99eba760a44c653f74bc1ca1658a8f1db30
zengyi <zengyi@chinatelecom.cn>
2025-10-31 17:58:11 +0800

    [Feature] Oracle Source/Sink support read/write TIMESTAMP_TZ

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/oracle/OracleJdbcRowConverter.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/oracle/OracleJdbcRowConverter.java
@@ -17,16 +17,23 @@
 
 package org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.oracle;
 
+import org.apache.seatunnel.api.table.catalog.TableSchema;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SqlType;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.converter.AbstractJdbcRowConverter;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.DatabaseIdentifier;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.utils.JdbcFieldTypeUtils;
+
+import lombok.var;
 
 import javax.annotation.Nullable;
 
 import java.io.ByteArrayInputStream;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.OffsetDateTime;
 
 import static org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.oracle.OracleTypeConverter.ORACLE_BLOB;
 
@@ -35,6 +42,74 @@ public class OracleJdbcRowConverter extends AbstractJdbcRowConverter {
     @Override
     public String converterName() {
         return DatabaseIdentifier.ORACLE;
+    }
+
+    @Override
+    public SeaTunnelRow toInternal(ResultSet rs, TableSchema tableSchema) throws SQLException {
+        SeaTunnelRow row = super.toInternal(rs, tableSchema);
+        // Handle TIMESTAMP_TZ types for Oracle
+        var rowType = tableSchema.toPhysicalRowDataType();
+        for (int fieldIndex = 0; fieldIndex < rowType.getTotalFields(); fieldIndex++) {
+            SeaTunnelDataType<?> seaTunnelDataType = rowType.getFieldType(fieldIndex);
+            if (seaTunnelDataType.getSqlType().equals(SqlType.TIMESTAMP_TZ)) {
+                int resultSetIndex = fieldIndex + 1;
+                OffsetDateTime offsetDateTime = getOracleOffsetDateTime(rs, resultSetIndex);
+                row.setField(fieldIndex, offsetDateTime);
+            }
+        }
+        return row;
+    }
+
+    /**
+     * Get OffsetDateTime from Oracle TIMESTAMP WITH TIME ZONE column. Oracle stores TIMESTAMP WITH
+     * TIME ZONE as a proprietary TIMESTAMPTZ object.
+     */
+    private OffsetDateTime getOracleOffsetDateTime(ResultSet rs, int columnIndex)
+            throws SQLException {
+        Object obj = rs.getObject(columnIndex);
+        if (obj == null) {
+            return null;
+        }
+
+        // Handle OffsetDateTime directly
+        if (obj instanceof OffsetDateTime) {
+            return (OffsetDateTime) obj;
+        }
+
+        // Handle other time types
+        if (obj instanceof java.time.ZonedDateTime) {
+            return ((java.time.ZonedDateTime) obj).toOffsetDateTime();
+        }
+
+        if (obj instanceof java.time.Instant) {
+            return ((java.time.Instant) obj).atOffset(java.time.ZoneOffset.UTC);
+        }
+
+        if (obj instanceof java.sql.Timestamp) {
+            return ((java.sql.Timestamp) obj).toLocalDateTime().atOffset(java.time.ZoneOffset.UTC);
+        }
+
+        if (obj instanceof java.util.Date) {
+            return ((java.util.Date) obj).toInstant().atOffset(java.time.ZoneOffset.UTC);
+        }
+
+        if (obj instanceof Long) {
+            return java.time.Instant.ofEpochMilli((Long) obj).atOffset(java.time.ZoneOffset.UTC);
+        }
+
+        // Try to parse as string (Oracle TIMESTAMPTZ string representation)
+        String str = obj.toString();
+        try {
+            return JdbcFieldTypeUtils.parseOffsetDateTimeFromString(str);
+        } catch (Exception e) {
+            // Last resort: try standard OffsetDateTime parsing
+            try {
+                return OffsetDateTime.parse(str);
+            } catch (Exception ex) {
+                throw new SQLException(
+                        "Failed to parse Oracle TIMESTAMP WITH TIME ZONE value: " + str, ex);
+            }
+        }
     }
 
     @Override
