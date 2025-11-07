5dc461a4b9abf943ae3d31500dd19ab75222e7f9
zengyi <zengyi@chinatelecom.cn>
2025-11-07 16:45:56 +0800

    Revert "[feature] fix ci bug"
    This reverts commit d56dd8e76c0716c3989adbaee9e81ef00ce24a27.

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/mysql/MySqlTypeConverter.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/mysql/MySqlTypeConverter.java
@@ -513,7 +513,6 @@ public class MySqlTypeConverter implements TypeConverter<BasicTypeDefine<MysqlTy
                 }
                 break;
             case TIMESTAMP:
-            case TIMESTAMP_TZ:
                 builder.nativeType(MysqlType.DATETIME);
                 builder.dataType(MYSQL_DATETIME);
                 if (version.isAtOrBefore(MySqlVersion.V_5_5)) {
