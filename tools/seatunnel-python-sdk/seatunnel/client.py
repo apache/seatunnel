1988249d3406e8796cae1d713ba1b52490b1780a
Lucio Junior <luciobvjr.dev@gmail.com>
2025-12-11 13:19:45 -0300

    refactor: move `SeatunnelClient`

--- tools/seatunnel-python-sdk/seatunnel/client.py
+++ tools/seatunnel-python-sdk/seatunnel/client.py
@@ -1,10 +1,13 @@
 import httpx
 from .helpers.httpMethod import HttpMethod
+from .endpoints.cluster import ClusterApi
+from .endpoints.jobs import JobsApi
+from .endpoints.config import ConfigApi
+from .endpoints.system import SystemApi
 
 class Client:
-    def __init__(self, base_url: str, token: str | None = None, timeout: float = 10):
+    def __init__(self, base_url: str, timeout: float = 10):
         self.base_url = base_url
-        self.token = token
         self.timeout = timeout
         self.session = httpx.Client(timeout=timeout)
 
@@ -20,3 +23,12 @@ class Client:
             return resp.json()
         else:
             return resp.text
+        
+class SeaTunnelClient:
+    def __init__(self, base_url):
+        self.client = Client(base_url)
+
+        self.cluster = ClusterApi(self.client)
+        self.jobs = JobsApi(self.client)
+        self.config = ConfigApi(self.client)
+        self.system = SystemApi(self.client)
