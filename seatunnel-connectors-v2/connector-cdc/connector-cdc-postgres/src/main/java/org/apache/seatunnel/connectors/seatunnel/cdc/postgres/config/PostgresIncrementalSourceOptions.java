9c8fdd058f4f7a2b8120464569cd31fbaa4acd31
jarvis <jarvis@apache.org>
2025-12-19 14:48:20 +0800

    postgres

---
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-postgres/src/main/java/org/apache/seatunnel/connectors/seatunnel/cdc/postgres/config/PostgresIncrementalSourceOptions.java
@@ -0,0 +1,6 @@
+package org.apache.seatunnel.connectors.seatunnel.cdc.postgres.config;
+
+import org.apache.seatunnel.connectors.cdc.base.option.JdbcSourceOptions;
+
+public class PostgresIncrementalSourceOptions extends JdbcSourceOptions {
+}
