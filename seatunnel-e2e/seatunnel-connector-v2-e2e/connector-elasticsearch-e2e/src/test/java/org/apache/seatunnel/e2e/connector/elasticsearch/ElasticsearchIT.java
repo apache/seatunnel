91972882069cdd844dce44c016c0f184aceb3e3b
njh_cmss <nijiahui_yewu@cmss.chinamobile.com>
2025-12-26 17:10:21 +0800

    [Feature][Connector-V2][Elasticsearch] Add Runtime Fields support (Elasticsearch 7.11+)

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-elasticsearch-e2e/src/test/java/org/apache/seatunnel/e2e/connector/elasticsearch/ElasticsearchIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-elasticsearch-e2e/src/test/java/org/apache/seatunnel/e2e/connector/elasticsearch/ElasticsearchIT.java
@@ -1140,7 +1140,6 @@ public class ElasticsearchIT extends TestSuiteBase implements TestResource {
     private List<String> generateRuntimeTestData() throws IOException {
         List<String> testData = new ArrayList<>();
 
-        // Use a fixed date: 2024-01-15 (Monday) for predictable day_of_week
         Map<String, Object> doc = new HashMap<>();
         doc.put("c_string", "test_1");
         doc.put("c_int", 10);
