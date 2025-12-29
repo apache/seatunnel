96b5873175cb59847dff162091940ca7a0a9d8e2
老王 <58297137+chl-wxp@users.noreply.github.com>
2025-12-29 09:46:46 +0800

    [feature] add issue

--- seatunnel-api/src/main/java/org/apache/seatunnel/api/configuration/util/issue/DefaultEnhancedConfigurationValidator.java
+++ seatunnel-api/src/main/java/org/apache/seatunnel/api/configuration/util/issue/DefaultEnhancedConfigurationValidator.java
@@ -1,27 +1,43 @@
 package org.apache.seatunnel.api.configuration.util.issue;
 
+import lombok.AllArgsConstructor;
+import org.apache.seatunnel.api.configuration.Option;
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.api.configuration.util.EnhancedConfigurationValidator;
+import org.apache.seatunnel.common.constants.PluginType;
 
 import java.util.Collections;
 import java.util.List;
 
+@AllArgsConstructor
 public abstract class DefaultEnhancedConfigurationValidator
         implements EnhancedConfigurationValidator {
 
+    protected final String identifier;
+    protected final PluginType pluginType;
+    //protected final
+
     @Override
     public List<ConfigurationVerificationIssue> validateDeprecatedRules(ReadonlyConfig context) {
-        return Collections.emptyList();
+        final List<Option<?>> deprecateOptions = deprecatedOptions(context);
+        if (deprecateOptions.isEmpty()) {
+            return Collections.emptyList();
+        }
+        deprecateOptions.forEach(option -> {
+
+        });
     }
+
+    protected abstract List<Option<?>> deprecatedOptions(ReadonlyConfig context);
 
     @Override
     public List<ConfigurationVerificationIssue> validateConflictRules(ReadonlyConfig context) {
-        return Collections.emptyList();
+
     }
 
     @Override
     public List<ConfigurationVerificationIssue> validateVersionCompatibilityRules(
             ReadonlyConfig context) {
-        return Collections.emptyList();
+
     }
 }
--- seatunnel-api/src/main/java/org/apache/seatunnel/api/configuration/util/issue/DeprecatedConfigurationIssue.java
+++ seatunnel-api/src/main/java/org/apache/seatunnel/api/configuration/util/issue/DeprecatedConfigurationIssue.java
@@ -1,3 +1,20 @@
 package org.apache.seatunnel.api.configuration.util.issue;
 
-public class DeprecatedConfigurationIssue {}
+import org.apache.seatunnel.api.configuration.Option;
+import org.apache.seatunnel.common.constants.PluginType;
+
+public class DeprecatedConfigurationIssue extends ConfigurationVerificationIssue{
+
+
+    private DeprecatedConfigurationIssue(Level level, String identifier, PluginType pluginType, Option<?> option) {
+        super(level, identifier, pluginType, option);
+
+    }
+
+    @Override
+    public void log() {
+
+    }
+
+
+}
