b1a55744697c5297c6280d3c300399b307391081
LiuZhengYong <502684289@qq.com>
2026-01-04 23:05:46 +0800

    Change the Chinese describe into English

--- seatunnel-examples/seatunnel-engine-examples/src/main/java/org/apache/seatunnel/example/engine/SeaTunnelEngineEmbeddedExample.java
+++ seatunnel-examples/seatunnel-engine-examples/src/main/java/org/apache/seatunnel/example/engine/SeaTunnelEngineEmbeddedExample.java
@@ -54,7 +54,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-/** 将SeaTunnel引擎当作工具的方式嵌入到项目中使用 */
+/** Embed the Zeta engine as a tool in the project. Start a local cluster instance in Cluster mode and use it to submit tasks, manage tasks, and query task metrics, etc. */
 public class SeaTunnelEngineEmbeddedExample implements AutoCloseable {
 
     public static void main(String[] args) {
