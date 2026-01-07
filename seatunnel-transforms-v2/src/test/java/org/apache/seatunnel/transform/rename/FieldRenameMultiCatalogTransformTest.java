75a8d555a11702ae786057123b6de9be4d09d183
dy102 <ekagnlek2@gmail.com>
2026-01-08 02:14:57 +0900

    [Fix][Connector-V2] refactor tests

--- seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/rename/FieldRenameMultiCatalogTransformTest.java
+++ seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/rename/FieldRenameMultiCatalogTransformTest.java
@@ -21,7 +21,6 @@ import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.api.table.catalog.CatalogTable;
 import org.apache.seatunnel.api.table.catalog.CatalogTableUtil;
 import org.apache.seatunnel.api.table.type.BasicType;
-import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
 import org.apache.seatunnel.api.transform.SeaTunnelTransform;
@@ -39,30 +38,28 @@ class FieldRenameMultiCatalogTransformTest {
 
     @Test
     void testCreateIdentityTransform() {
-        String tableName = "test";
-        String[] fields = new String[] {"id", "int", "string"};
-        CatalogTable table =
-                CatalogTableUtil.getCatalogTable(
-                        tableName,
-                        "test",
-                        "test",
-                        "test",
-                        new SeaTunnelRowType(
-                                fields,
-                                new SeaTunnelDataType[] {
-                                    BasicType.INT_TYPE, BasicType.INT_TYPE, BasicType.STRING_TYPE
-                                }));
+        SeaTunnelRowType rowType =
+                new SeaTunnelRowType(
+                        new String[] {"id", "name"},
+                        new org.apache.seatunnel.api.table.type.SeaTunnelDataType[] {
+                            BasicType.INT_TYPE, BasicType.STRING_TYPE
+                        });
+        CatalogTable catalogTable =
+                CatalogTableUtil.getCatalogTable("test", "test", "test", "test", rowType);
+        List<CatalogTable> tables = Collections.singletonList(catalogTable);
         ReadonlyConfig config =
                 ReadonlyConfig.fromMap(
                         Collections.singletonMap(
                                 TransformCommonOptions.TABLE_MATCH_REGEX.key(), ".exclude"));
 
         FakeRenameMultiCatalogTransform transform =
-                new FakeRenameMultiCatalogTransform(Collections.singletonList(table), config);
+                new FakeRenameMultiCatalogTransform(tables, config);
 
         Assertions.assertInstanceOf(
                 IdentityMapTransform.class,
-                transform.getTransformMap().get(table.getTableId().toTablePath().toString()));
+                transform
+                        .getTransformMap()
+                        .get(tables.get(0).getTableId().toTablePath().toString()));
     }
 
     private static class FakeRenameMultiCatalogTransform extends FieldRenameMultiCatalogTransform {
