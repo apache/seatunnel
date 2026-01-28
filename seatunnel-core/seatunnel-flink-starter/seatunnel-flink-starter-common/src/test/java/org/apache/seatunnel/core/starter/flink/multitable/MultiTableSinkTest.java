45706d0bcd7735ab9bf44c7d586bf24c4447d099
lijie <lijie2@cmss.chinamobile.com>
2026-01-28 09:38:59 +0800

    [Feature][seatunnel core][seatunnel-flink-start] Filnk engine supports task restore from checkpoint upon startup

--- seatunnel-core/seatunnel-flink-starter/seatunnel-flink-starter-common/src/test/java/org/apache/seatunnel/core/starter/flink/multitable/MultiTableSinkTest.java
+++ seatunnel-core/seatunnel-flink-starter/seatunnel-flink-starter-common/src/test/java/org/apache/seatunnel/core/starter/flink/multitable/MultiTableSinkTest.java
@@ -25,6 +25,7 @@ import org.apache.seatunnel.e2e.sink.inmemory.InMemorySinkWriter;
 import org.apache.seatunnel.e2e.source.inmemory.InMemorySourceSplitEnumerator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
@@ -35,6 +36,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+@Order(1)
 public class MultiTableSinkTest {
 
     @Test
