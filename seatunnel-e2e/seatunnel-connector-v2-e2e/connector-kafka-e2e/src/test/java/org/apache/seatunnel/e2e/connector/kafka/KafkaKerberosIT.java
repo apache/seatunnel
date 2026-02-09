7aab83c05ac467bdd70cf02d15769c01361b5e47
misi <wendi.zeng@gientech.com>
2026-02-09 09:49:03 +0800

    [improve] revert job status CANCELING to CANCELED

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-kafka-e2e/src/test/java/org/apache/seatunnel/e2e/connector/kafka/KafkaKerberosIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-kafka-e2e/src/test/java/org/apache/seatunnel/e2e/connector/kafka/KafkaKerberosIT.java
@@ -330,7 +330,7 @@ public class KafkaKerberosIT extends TestSuiteBase implements TestResource {
                 .untilAsserted(
                         () -> {
                             String jobStatus = container.getJobStatus(String.valueOf(jobId));
-                            Assertions.assertEquals("CANCELING", jobStatus);
+                            Assertions.assertEquals("CANCELED", jobStatus);
                         });
 
         // step 2. Verify that the program outputs retry logs
