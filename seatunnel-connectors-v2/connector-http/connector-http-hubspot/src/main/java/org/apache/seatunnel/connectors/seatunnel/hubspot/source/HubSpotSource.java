7429ae1db087cf8a087e903f624e12cf835efdf9
sukusuma <skkusuma98@gmail.com>
2026-02-23 16:19:30 -0500

    Test: Refactor HubSpotSourceFactoryTest to standard SeaTunnel patterns

--- seatunnel-connectors-v2/connector-http/connector-http-hubspot/src/main/java/org/apache/seatunnel/connectors/seatunnel/hubspot/source/HubSpotSource.java
+++ seatunnel-connectors-v2/connector-http/connector-http-hubspot/src/main/java/org/apache/seatunnel/connectors/seatunnel/hubspot/source/HubSpotSource.java
@@ -18,15 +18,47 @@
 package org.apache.seatunnel.connectors.seatunnel.hubspot.source;
 
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+import org.apache.seatunnel.connectors.seatunnel.common.source.AbstractSingleSplitReader;
+import org.apache.seatunnel.connectors.seatunnel.common.source.SingleSplitReaderContext;
 import org.apache.seatunnel.connectors.seatunnel.http.source.HttpSource;
+import org.apache.seatunnel.connectors.seatunnel.http.source.HttpSourceReader;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class HubSpotSource extends HttpSource {
 
+    private final HubSpotSourceParameter hubSpotSourceParameter = new HubSpotSourceParameter();
+
     public HubSpotSource(ReadonlyConfig config) {
         super(config);
+        // Build the custom parameter (injects tokens, builds URL)
+        hubSpotSourceParameter.buildWithConfig(config);
+
+        // Safely set the default contentField for the parent class if missing
+        boolean hasContentField =
+                config.getOptional(
+                                org.apache.seatunnel.api.configuration.Options.key("content_field")
+                                        .stringType()
+                                        .noDefaultValue())
+                        .isPresent();
+
+        if (!hasContentField) {
+            this.contentField = HubSpotSourceParameter.DEFAULT_CONTENT_FIELD;
+        }
+    }
+
+    @Override
+    public AbstractSingleSplitReader<SeaTunnelRow> createReader(
+            SingleSplitReaderContext readerContext) throws Exception {
+        // Use our custom parameter with the Auth headers instead of the parent's empty one
+        return new HttpSourceReader(
+                this.hubSpotSourceParameter,
+                readerContext,
+                this.deserializationSchema,
+                jsonField,
+                contentField);
     }
 
     @Override
