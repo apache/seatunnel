d483d5d9cfe9c2f721c468424b3fabf2be75301b
sukusuma <skkusuma98@gmail.com>
2026-04-12 13:57:22 -0400

    [Feature][Connector-V2][HubSpot] Fix E2E test annotations and MockServer implementation

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-http-e2e/src/test/java/org/apache/seatunnel/e2e/connector/http/HubSpotIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-http-e2e/src/test/java/org/apache/seatunnel/e2e/connector/http/HubSpotIT.java
@@ -17,94 +17,76 @@
 
 package org.apache.seatunnel.e2e.connector.http;
 
-import org.apache.seatunnel.e2e.common.TestResource;
 import org.apache.seatunnel.e2e.common.TestSuiteBase;
 import org.apache.seatunnel.e2e.common.container.TestContainer;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestTemplate;
-import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.Container;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.utility.DockerImageName;
 
-import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class HubSpotIT extends TestSuiteBase implements TestResource {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    private static final Logger LOG = LoggerFactory.getLogger(HubSpotIT.class);
-    private static final String MOCKSERVER_IMAGE = "mockserver/mockserver:5.14.0";
-    private GenericContainer<?> mockServerContainer;
-    private MockServerClient mockServerClient;
+public class HubSpotIT extends TestSuiteBase {
+
+    private static ClientAndServer mockServer;
 
     @BeforeAll
-    @Override
-    public void startUp() {
-        this.mockServerContainer =
-                new GenericContainer<>(DockerImageName.parse(MOCKSERVER_IMAGE))
-                        .withNetwork(NETWORK)
-                        .withNetworkAliases("mock-server")
-                        .withExposedPorts(1080)
-                        .waitingFor(
-                                Wait.forHttp("/mockserver/status")
-                                        .withMethod("PUT")
-                                        .forStatusCode(200))
-                        .withLogConsumer(new Slf4jLogConsumer(LOG));
+    public static void setup() throws Exception {
+        // Start MockServer on port 8089 to match our conf file
+        mockServer = ClientAndServer.startClientAndServer(8089);
 
-        this.mockServerContainer.start();
+        // Read mock JSON files
+        String page1Json =
+                new String(
+                        Files.readAllBytes(
+                                Paths.get(
+                                        HubSpotIT.class.getResource("/mock_page1.json").toURI())));
+        String page2Json =
+                new String(
+                        Files.readAllBytes(
+                                Paths.get(
+                                        HubSpotIT.class.getResource("/mock_page2.json").toURI())));
 
-        this.mockServerClient =
-                new MockServerClient(
-                        this.mockServerContainer.getHost(),
-                        this.mockServerContainer.getMappedPort(1080));
+        // Stub Page 2 FIRST (More specific match: expects the '?after=token-123' param)
+        mockServer
+                .when(
+                        HttpRequest.request()
+                                .withMethod("GET")
+                                .withPath("/crm/v3/objects/contacts")
+                                .withQueryStringParameter("after", "token-123"))
+                .respond(
+                        HttpResponse.response()
+                                .withStatusCode(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody(page2Json));
 
-        this.initMockServer();
-    }
-
-    @AfterAll
-    @Override
-    public void tearDown() {
-        if (this.mockServerContainer != null) {
-            this.mockServerContainer.close();
-        }
-        if (this.mockServerClient != null) {
-            this.mockServerClient.close();
-        }
-    }
-
-    private void initMockServer() {
-        this.mockServerClient
+        // Stub Page 1 SECOND (Fallback match: initial request with no token param)
+        mockServer
                 .when(HttpRequest.request().withMethod("GET").withPath("/crm/v3/objects/contacts"))
                 .respond(
                         HttpResponse.response()
                                 .withStatusCode(200)
                                 .withHeader("Content-Type", "application/json")
-                                .withBody(
-                                        "{\n"
-                                                + "  \"results\": [\n"
-                                                + "    {\n"
-                                                + "      \"id\": \"101\",\n"
-                                                + "      \"properties\": \"simple_test_value\"\n"
-                                                + "    }\n"
-                                                + "  ]\n"
-                                                + "}"));
+                                .withBody(page1Json));
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (mockServer != null) {
+            mockServer.stop();
+        }
     }
 
     @TestTemplate
-    public void testHubSpotSource(TestContainer container)
-            throws IOException, InterruptedException {
-        Container.ExecResult result = container.executeJob("/hubspot_source_case.conf");
-        Assertions.assertEquals(
-                0,
-                result.getExitCode(),
-                "Job failed to execute. Check logs: " + result.getStderr());
+    public void testHubspotSourcePagination(TestContainer container) throws Exception {
+        Container.ExecResult execResult = container.executeJob("/hubspot_source_to_assert.conf");
+        assertEquals(0, execResult.getExitCode(), "SeaTunnel job should exit with code 0");
     }
 }
