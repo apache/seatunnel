a5fd5e316b0fb58ac2ef6c2696676e51cc07138c
Jarvis <jarvis@apache.org>
2026-04-13 15:34:20 +0800

    update

--- seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/source/HiveSource.java
+++ seatunnel-connectors-v2/connector-hive/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/source/HiveSource.java
@@ -19,13 +19,13 @@ package org.apache.seatunnel.connectors.seatunnel.hive.source;
 
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.api.source.Boundedness;
+import org.apache.seatunnel.api.source.SeaTunnelSource;
 import org.apache.seatunnel.api.source.SourceReader;
 import org.apache.seatunnel.api.source.SourceSplitEnumerator;
+import org.apache.seatunnel.api.source.SupportColumnProjection;
+import org.apache.seatunnel.api.source.SupportParallelism;
 import org.apache.seatunnel.api.table.catalog.CatalogTable;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
-import org.apache.seatunnel.connectors.seatunnel.file.config.FileBaseSourceOptions;
-import org.apache.seatunnel.connectors.seatunnel.file.config.HadoopConf;
-import org.apache.seatunnel.connectors.seatunnel.file.source.BaseFileSource;
 import org.apache.seatunnel.connectors.seatunnel.file.source.split.FileSourceSplit;
 import org.apache.seatunnel.connectors.seatunnel.file.source.state.FileSourceState;
 import org.apache.seatunnel.connectors.seatunnel.hive.config.HiveConstants;
@@ -37,39 +37,15 @@ import org.apache.seatunnel.connectors.seatunnel.hive.source.split.MultipleTable
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class HiveSource extends BaseFileSource {
+public class HiveSource
+        implements SeaTunnelSource<SeaTunnelRow, FileSourceSplit, FileSourceState>,
+                SupportParallelism,
+                SupportColumnProjection {
 
     private final MultipleTableHiveSourceConfig multipleTableHiveSourceConfig;
 
     public HiveSource(ReadonlyConfig readonlyConfig) {
-        super(readonlyConfig);
         this.multipleTableHiveSourceConfig = new MultipleTableHiveSourceConfig(readonlyConfig);
-    }
-
-    @Override
-    protected HadoopConf initHadoopConf() {
-        HadoopConf conf = new HadoopConf(pluginConfig.get(FileBaseSourceOptions.DEFAULT_FS));
-
-        if (pluginConfig.getOptional(FileBaseSourceOptions.HDFS_SITE_PATH).isPresent()) {
-            conf.setHdfsSitePath(pluginConfig.get(FileBaseSourceOptions.HDFS_SITE_PATH));
-        }
-
-        if (pluginConfig.getOptional(FileBaseSourceOptions.REMOTE_USER).isPresent()) {
-            conf.setRemoteUser(pluginConfig.get(FileBaseSourceOptions.REMOTE_USER));
-        }
-
-        if (pluginConfig.getOptional(FileBaseSourceOptions.KRB5_PATH).isPresent()) {
-            conf.setKrb5Path(pluginConfig.get(FileBaseSourceOptions.KRB5_PATH));
-        }
-
-        if (pluginConfig.getOptional(FileBaseSourceOptions.KERBEROS_PRINCIPAL).isPresent()) {
-            conf.setKerberosPrincipal(pluginConfig.get(FileBaseSourceOptions.KERBEROS_PRINCIPAL));
-        }
-        if (pluginConfig.getOptional(FileBaseSourceOptions.KERBEROS_KEYTAB_PATH).isPresent()) {
-            conf.setKerberosKeytabPath(
-                    pluginConfig.get(FileBaseSourceOptions.KERBEROS_KEYTAB_PATH));
-        }
-        return conf;
     }
 
     @Override
