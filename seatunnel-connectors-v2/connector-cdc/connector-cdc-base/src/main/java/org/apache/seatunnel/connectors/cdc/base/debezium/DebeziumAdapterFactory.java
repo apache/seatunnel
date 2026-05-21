d79f2a5f895a015d1d61e855e9ef1f46244a392c
vsantonastaso <vsantonastaso.dev@gmail.com>
2026-05-21 20:12:02 +0200

    [Feature][Connector-V2] Refactoring to have per-connector version management of Debezium
    Signed-off-by: vsantonastaso <vsantonastaso.dev@gmail.com>

---
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/base/debezium/DebeziumAdapterFactory.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.cdc.base.debezium;
+
+import org.apache.seatunnel.api.annotation.Experimental;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ServiceLoader;
+
+/**
+ * Factory for loading connector-specific {@link DebeziumAdapter} instances via {@link
+ * ServiceLoader}.
+ */
+@Experimental
+public class DebeziumAdapterFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DebeziumAdapterFactory.class);
+
+    private DebeziumAdapterFactory() {}
+
+    /**
+     * Returns the {@link DebeziumAdapter} whose {@link DebeziumAdapter#supports(String)} method
+     * returns {@code true} for the given Debezium connector fully-qualified class name (e.g.,
+     * {@code "io.debezium.connector.mysql.MySqlConnector"}).
+     *
+     * @param connectorClassName the fully-qualified Debezium connector class name, matching the
+     *     {@code connector.class} Debezium property
+     * @param classLoader the class loader used to discover {@link DebeziumAdapter} registrations
+     *     from {@code META-INF/services}
+     * @throws IllegalStateException if no matching adapter is found
+     */
+    public static DebeziumAdapter getAdapter(String connectorClassName, ClassLoader classLoader) {
+        LOG.info("Loading DebeziumAdapter for connector class: {}", connectorClassName);
+        ServiceLoader<DebeziumAdapter> loader =
+                ServiceLoader.load(DebeziumAdapter.class, classLoader);
+
+        for (DebeziumAdapter adapter : loader) {
+            if (adapter.supports(connectorClassName)) {
+                LOG.info(
+                        "Found DebeziumAdapter for {}: {} targeting Debezium {}",
+                        connectorClassName,
+                        adapter.getClass().getName(),
+                        adapter.getDebeziumVersion());
+                return adapter;
+            }
+        }
+
+        throw new IllegalStateException(
+                "No DebeziumAdapter found for connector class: "
+                        + connectorClassName
+                        + ". Ensure a META-INF/services/"
+                        + DebeziumAdapter.class.getName()
+                        + " registration is present in the connector module.");
+    }
+}
---
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/test/java/org/apache/seatunnel/connectors/cdc/base/debezium/TestDebeziumAdapter.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.connectors.cdc.base.debezium;
+
+/** Test-only {@link DebeziumAdapter} implementation registered via META-INF/services. */
+public class TestDebeziumAdapter implements DebeziumAdapter {
+
+    static final String TEST_CONNECTOR_CLASS = "io.debezium.connector.test.TestConnector";
+    static final String TEST_DEBEZIUM_VERSION = "1.9.8.Final";
+
+    @Override
+    public String getDebeziumVersion() {
+        return TEST_DEBEZIUM_VERSION;
+    }
+
+    @Override
+    public boolean supports(String connectorClassName) {
+        return TEST_CONNECTOR_CLASS.equals(connectorClassName);
+    }
+}
