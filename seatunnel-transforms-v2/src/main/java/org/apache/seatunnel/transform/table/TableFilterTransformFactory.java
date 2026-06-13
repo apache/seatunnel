e0afa6a3ed3c7086730663bc760b545e96a70c40
zhiwei.niu <zniu70696@gmail.com>
2026-06-13 14:20:01 +0800

    [Improve][Transform-V2] Migrate TableFilter validation to declarative OptionRule

--- seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/table/TableFilterTransformFactory.java
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/table/TableFilterTransformFactory.java
@@ -17,7 +17,11 @@
 
 package org.apache.seatunnel.transform.table;
 
+import org.apache.seatunnel.api.configuration.ReadonlyConfig;
+import org.apache.seatunnel.api.configuration.util.ConditionExtension;
+import org.apache.seatunnel.api.configuration.util.Conditions;
 import org.apache.seatunnel.api.configuration.util.OptionRule;
+import org.apache.seatunnel.api.configuration.util.OptionValidationException;
 import org.apache.seatunnel.api.table.connector.TableTransform;
 import org.apache.seatunnel.api.table.factory.Factory;
 import org.apache.seatunnel.api.table.factory.TableTransformFactory;
@@ -25,6 +29,8 @@ import org.apache.seatunnel.api.table.factory.TableTransformFactoryContext;
 import org.apache.seatunnel.transform.common.TransformCommonOptions;
 
 import com.google.auto.service.AutoService;
+
+import java.util.regex.PatternSyntaxException;
 
 @AutoService(Factory.class)
 public class TableFilterTransformFactory implements TableTransformFactory {
@@ -38,8 +44,15 @@ public class TableFilterTransformFactory implements TableTransformFactory {
         return OptionRule.builder()
                 .optional(
                         TableFilterConfig.DATABASE_PATTERN,
+                        Conditions.extension(
+                                TableFilterConfig.DATABASE_PATTERN, new RegexValidator()))
+                .optional(
                         TableFilterConfig.SCHEMA_PATTERN,
-                        TableFilterConfig.TABLE_PATTERN)
+                        Conditions.extension(
+                                TableFilterConfig.SCHEMA_PATTERN, new RegexValidator()))
+                .optional(
+                        TableFilterConfig.TABLE_PATTERN,
+                        Conditions.extension(TableFilterConfig.TABLE_PATTERN, new RegexValidator()))
                 .optional(TableFilterConfig.PATTERN_MODE)
                 .optional(TransformCommonOptions.MULTI_TABLES)
                 .optional(TransformCommonOptions.TABLE_MATCH_REGEX)
@@ -51,5 +64,27 @@ public class TableFilterTransformFactory implements TableTransformFactory {
         return () ->
                 new TableFilterMultiCatalogTransform(
                         context.getCatalogTables(), context.getOptions());
+    }
+
+    static class RegexValidator implements ConditionExtension<String> {
+        @Override
+        public String description() {
+            return "must be a valid regular expression";
+        }
+
+        @Override
+        public boolean evaluate(ReadonlyConfig config, String value)
+                throws OptionValidationException {
+            if (value == null || value.isEmpty()) {
+                return true;
+            }
+            try {
+                java.util.regex.Pattern.compile(value);
+                return true;
+            } catch (PatternSyntaxException e) {
+                throw new OptionValidationException(
+                        String.format("Invalid regex pattern '%s': %s", value, e.getDescription()));
+            }
+        }
     }
 }
