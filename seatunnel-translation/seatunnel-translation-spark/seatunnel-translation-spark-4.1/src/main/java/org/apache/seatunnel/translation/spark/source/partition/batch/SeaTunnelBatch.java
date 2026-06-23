6832ca5cb1a44e177f759388a26eafd3dfd37da8
shangeyao <shangeyao@qq.com>
2026-06-23 16:44:43 +0800

    [Feature][Translation] Add Spark 4.1 support
    Closes #11173
    
    - Add seatunnel-translation-spark-4.1 module (Scala 2.13, Spark 4.1.2)
    - Add seatunnel-spark-4.1-starter module
    - Adapt to Spark 4.1 API: override useCommitCoordinator() in SeaTunnelBatchWrite
    - Source: SeaTunnelSparkSource, SeaTunnelSourceTable, SeaTunnelScan, SeaTunnelScanBuilder
    - Batch: SeaTunnelBatch, ParallelBatchPartitionReader, CoordinatedBatchPartitionReader
    - MicroBatch: SeaTunnelMicroBatch, SeaTunnelOffset, ParallelMicroBatchPartitionReader
    - Sink: SeaTunnelSparkSink, SeaTunnelSinkTable, SeaTunnelBatchWrite, SparkSinkInjector

---
+++ seatunnel-translation/seatunnel-translation-spark/seatunnel-translation-spark-4.1/src/main/java/org/apache/seatunnel/translation/spark/source/partition/batch/SeaTunnelBatch.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.translation.spark.source.partition.batch;
+
+import org.apache.seatunnel.api.source.SeaTunnelSource;
+import org.apache.seatunnel.api.source.SupportCoordinate;
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+import org.apache.seatunnel.translation.spark.execution.MultiTableManager;
+
+import org.apache.spark.sql.connector.read.Batch;
+import org.apache.spark.sql.connector.read.InputPartition;
+import org.apache.spark.sql.connector.read.PartitionReaderFactory;
+
+import java.util.Map;
+
+/** A physical plan of SeaTunnel source */
+public class SeaTunnelBatch implements Batch {
+
+    private final SeaTunnelSource<SeaTunnelRow, ?, ?> source;
+
+    private final int parallelism;
+    private final String jobId;
+    private final Map<String, String> envOptions;
+
+    private final MultiTableManager multiTableManager;
+
+    public SeaTunnelBatch(
+            SeaTunnelSource<SeaTunnelRow, ?, ?> source,
+            int parallelism,
+            String jobId,
+            Map<String, String> envOptions,
+            MultiTableManager multiTableManager) {
+        this.source = source;
+        this.parallelism = parallelism;
+        this.jobId = jobId;
+        this.envOptions = envOptions;
+        this.multiTableManager = multiTableManager;
+    }
+
+    @Override
+    public InputPartition[] planInputPartitions() {
+        InputPartition[] partitions;
+        if (source instanceof SupportCoordinate) {
+            partitions = new SeaTunnelBatchInputPartition[1];
+            partitions[0] = new SeaTunnelBatchInputPartition(0);
+        } else {
+            partitions = new SeaTunnelBatchInputPartition[parallelism];
+            for (int partitionId = 0; partitionId < parallelism; partitionId++) {
+                partitions[partitionId] = new SeaTunnelBatchInputPartition(partitionId);
+            }
+        }
+        return partitions;
+    }
+
+    @Override
+    public PartitionReaderFactory createReaderFactory() {
+        return new SeaTunnelBatchPartitionReaderFactory(
+                source, parallelism, jobId, envOptions, multiTableManager);
+    }
+}
---
+++ seatunnel-translation/seatunnel-translation-spark/seatunnel-translation-spark-4.1/src/main/java/org/apache/seatunnel/translation/spark/source/partition/micro/SeaTunnelMicroBatchPartitionReaderFactory.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.translation.spark.source.partition.micro;
+
+import org.apache.seatunnel.api.source.SeaTunnelSource;
+import org.apache.seatunnel.api.source.SupportCoordinate;
+import org.apache.seatunnel.api.table.type.SeaTunnelRow;
+import org.apache.seatunnel.translation.spark.execution.MultiTableManager;
+import org.apache.seatunnel.translation.spark.source.partition.batch.ParallelBatchPartitionReader;
+
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.connector.read.InputPartition;
+import org.apache.spark.sql.connector.read.PartitionReader;
+import org.apache.spark.sql.connector.read.PartitionReaderFactory;
+import org.apache.spark.sql.util.CaseInsensitiveStringMap;
+
+import java.util.Map;
+
+public class SeaTunnelMicroBatchPartitionReaderFactory implements PartitionReaderFactory {
+
+    private final SeaTunnelSource<SeaTunnelRow, ?, ?> source;
+
+    private final int parallelism;
+    private final String jobId;
+
+    private final String checkpointLocation;
+
+    private final CaseInsensitiveStringMap caseInsensitiveStringMap;
+
+    private final MultiTableManager multiTableManager;
+
+    public SeaTunnelMicroBatchPartitionReaderFactory(
+            SeaTunnelSource<SeaTunnelRow, ?, ?> source,
+            int parallelism,
+            String jobId,
+            String checkpointLocation,
+            CaseInsensitiveStringMap caseInsensitiveStringMap,
+            MultiTableManager multiTableManager) {
+        this.source = source;
+        this.parallelism = parallelism;
+        this.jobId = jobId;
+        this.checkpointLocation = checkpointLocation;
+        this.caseInsensitiveStringMap = caseInsensitiveStringMap;
+        this.multiTableManager = multiTableManager;
+    }
+
+    @Override
+    public PartitionReader<InternalRow> createReader(InputPartition partition) {
+        SeaTunnelMicroBatchInputPartition seaTunnelPartition =
+                (SeaTunnelMicroBatchInputPartition) partition;
+        ParallelBatchPartitionReader partitionReader;
+        Integer subtaskId = seaTunnelPartition.getSubtaskId();
+        Integer checkpointId = seaTunnelPartition.getCheckpointId();
+        Integer checkpointInterval = seaTunnelPartition.getCheckpointInterval();
+        String hdfsRoot = seaTunnelPartition.getHdfsRoot();
+        String hdfsUser = seaTunnelPartition.getHdfsUser();
+        Map<String, String> envOptions = caseInsensitiveStringMap.asCaseSensitiveMap();
+        if (source instanceof SupportCoordinate) {
+            partitionReader =
+                    new CoordinatedMicroBatchPartitionReader(
+                            source,
+                            parallelism,
+                            jobId,
+                            subtaskId,
+                            checkpointId,
+                            checkpointInterval,
+                            checkpointLocation,
+                            hdfsRoot,
+                            hdfsUser,
+                            envOptions,
+                            multiTableManager);
+        } else {
+            partitionReader =
+                    new ParallelMicroBatchPartitionReader(
+                            source,
+                            parallelism,
+                            jobId,
+                            subtaskId,
+                            checkpointId,
+                            checkpointInterval,
+                            checkpointLocation,
+                            hdfsRoot,
+                            hdfsUser,
+                            envOptions,
+                            multiTableManager);
+        }
+        return new SeaTunnelMicroBatchPartitionReader(partitionReader);
+    }
+}
