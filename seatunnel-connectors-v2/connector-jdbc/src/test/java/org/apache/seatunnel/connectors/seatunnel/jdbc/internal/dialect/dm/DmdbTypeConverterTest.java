0c83bcd6de3956689b86ee95aa6d54ac267e64ca
张万义 <arvin.zhang@htx-inc.com>
2026-08-18 14:38:18 +0800

    [Chore] Fix code style violations (spotless:apply)

--- seatunnel-connectors-v2/connector-jdbc/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/dm/DmdbTypeConverterTest.java
+++ seatunnel-connectors-v2/connector-jdbc/src/test/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/dm/DmdbTypeConverterTest.java
@@ -958,10 +958,7 @@ public class DmdbTypeConverterTest {
     @Test
     public void testReconvertDecimalBoundary() {
         Column column =
-                PhysicalColumn.builder()
-                        .name("test")
-                        .dataType(new DecimalType(40, 5))
-                        .build();
+                PhysicalColumn.builder().name("test").dataType(new DecimalType(40, 5)).build();
         BasicTypeDefine typeDefine = DmdbTypeConverter.INSTANCE.reconvert(column);
         Assertions.assertEquals(column.getName(), typeDefine.getName());
         Assertions.assertEquals(
@@ -969,11 +966,7 @@ public class DmdbTypeConverterTest {
                 typeDefine.getColumnType());
         Assertions.assertEquals(DmdbTypeConverter.DM_DECIMAL, typeDefine.getDataType());
 
-        column =
-                PhysicalColumn.builder()
-                        .name("test")
-                        .dataType(new DecimalType(10, -2))
-                        .build();
+        column = PhysicalColumn.builder().name("test").dataType(new DecimalType(10, -2)).build();
         typeDefine = DmdbTypeConverter.INSTANCE.reconvert(column);
         Assertions.assertEquals(column.getName(), typeDefine.getName());
         Assertions.assertEquals(
@@ -981,11 +974,7 @@ public class DmdbTypeConverterTest {
                 typeDefine.getColumnType());
         Assertions.assertEquals(DmdbTypeConverter.DM_DECIMAL, typeDefine.getDataType());
 
-        column =
-                PhysicalColumn.builder()
-                        .name("test")
-                        .dataType(new DecimalType(38, 38))
-                        .build();
+        column = PhysicalColumn.builder().name("test").dataType(new DecimalType(38, 38)).build();
         typeDefine = DmdbTypeConverter.INSTANCE.reconvert(column);
         Assertions.assertEquals(column.getName(), typeDefine.getName());
         Assertions.assertEquals(
@@ -1006,10 +995,10 @@ public class DmdbTypeConverterTest {
         BasicTypeDefine typeDefine = DmdbTypeConverter.INSTANCE.reconvert(column);
         Assertions.assertEquals(column.getName(), typeDefine.getName());
         Assertions.assertEquals(
-                String.format("%s(%s)", DmdbTypeConverter.DM_TIME, DmdbTypeConverter.MAX_TIME_SCALE),
+                String.format(
+                        "%s(%s)", DmdbTypeConverter.DM_TIME, DmdbTypeConverter.MAX_TIME_SCALE),
                 typeDefine.getColumnType());
-        Assertions.assertEquals(
-                DmdbTypeConverter.MAX_TIME_SCALE, typeDefine.getScale().intValue());
+        Assertions.assertEquals(DmdbTypeConverter.MAX_TIME_SCALE, typeDefine.getScale().intValue());
         Assertions.assertEquals(DmdbTypeConverter.DM_TIME, typeDefine.getDataType());
     }
 
@@ -1033,5 +1022,4 @@ public class DmdbTypeConverterTest {
                 DmdbTypeConverter.MAX_TIMESTAMP_SCALE, typeDefine.getScale().intValue());
         Assertions.assertEquals(DmdbTypeConverter.DM_TIMESTAMP, typeDefine.getDataType());
     }
-
 }
