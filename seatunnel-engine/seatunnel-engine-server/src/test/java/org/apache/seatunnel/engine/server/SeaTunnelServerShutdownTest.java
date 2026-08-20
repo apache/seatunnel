90b4690c56f2202a3f71ff384feee1855cb403b7
DanielLeens <escheduler@outlook.com>
2026-08-20 06:33:03 -0400

    [Test][Zeta] Cover graceful member removal marker

---
+++ seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/SeaTunnelServerShutdownTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.engine.server;
+
+import org.apache.seatunnel.engine.common.Constant;
+import org.apache.seatunnel.engine.common.config.SeaTunnelConfig;
+import org.apache.seatunnel.engine.server.common.SeaTunnelEngineContext;
+
+import org.junit.jupiter.api.Test;
+
+import com.hazelcast.cluster.Address;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import com.hazelcast.spi.impl.NodeEngineImpl;
+
+import java.lang.reflect.Field;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+class SeaTunnelServerShutdownTest {
+
+    @Test
+    void shouldMarkGracefulMemberRemovalOnGracefulShutdown() throws Exception {
+        NodeEngineImpl nodeEngine = mock(NodeEngineImpl.class);
+        HazelcastInstance hazelcastInstance = mock(HazelcastInstance.class);
+        IMap<Address, Long> gracefulMemberRemovalIMap = mock(IMap.class);
+        Address address = new Address("127.0.0.1", 5801);
+        when(nodeEngine.getThisAddress()).thenReturn(address);
+        when(nodeEngine.getHazelcastInstance()).thenReturn(hazelcastInstance);
+        when(hazelcastInstance.getMap(Constant.IMAP_GRACEFUL_MEMBER_REMOVAL))
+                .thenReturn(gracefulMemberRemovalIMap);
+
+        createServer(nodeEngine).shutdown(false);
+
+        verify(gracefulMemberRemovalIMap).put(eq(address), anyLong());
+        verify(gracefulMemberRemovalIMap, never()).remove(address);
+    }
+
+    @Test
+    void shouldNotMarkGracefulMemberRemovalOnForcedShutdown() throws Exception {
+        NodeEngineImpl nodeEngine = mock(NodeEngineImpl.class);
+
+        createServer(nodeEngine).shutdown(true);
+
+        verifyNoInteractions(nodeEngine);
+    }
+
+    private static SeaTunnelServer createServer(NodeEngineImpl nodeEngine) throws Exception {
+        SeaTunnelServer seaTunnelServer = new SeaTunnelServer(mock(SeaTunnelConfig.class));
+        setField(seaTunnelServer, "nodeEngine", nodeEngine);
+        setField(seaTunnelServer, "engineContext", mock(SeaTunnelEngineContext.class));
+        return seaTunnelServer;
+    }
+
+    private static void setField(Object target, String fieldName, Object value) throws Exception {
+        Field field = target.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(target, value);
+    }
+}
