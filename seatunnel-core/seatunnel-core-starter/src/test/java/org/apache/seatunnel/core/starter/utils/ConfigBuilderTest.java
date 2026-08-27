9f08278f6accfcfa1dbde88a956b483e806622e4
NganWave <nganwave@163.com>
2026-08-27 12:35:29 +0800

    [Test][seatunnel-core] Test parsing json in array value with typesafe api

--- seatunnel-core/seatunnel-core-starter/src/test/java/org/apache/seatunnel/core/starter/utils/ConfigBuilderTest.java
+++ seatunnel-core/seatunnel-core-starter/src/test/java/org/apache/seatunnel/core/starter/utils/ConfigBuilderTest.java
@@ -17,15 +17,21 @@
 
 package org.apache.seatunnel.core.starter.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.seatunnel.shade.com.typesafe.config.Config;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigFactory;
+import org.apache.seatunnel.shade.com.typesafe.config.ConfigValue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class ConfigBuilderTest {
 
     @Test
@@ -166,5 +172,34 @@ public class ConfigBuilderTest {
 
         Assertions.assertEquals("string", desensitizedFields.get("access_token"));
         Assertions.assertEquals("string", desensitizedFields.get("user-password"));
+    }
+
+
+    @Test
+    public void testJsonInArrayParsing(){
+        //table_list
+        String value="[{\"table_path\":\"json_test.ml_*\",\"use_regex\":\"true\"},{\"table_path\":\"json_test.ratings\"}]";
+        try {
+            Config parsed = ConfigFactory.parseString("v = " + value);
+            ConfigValue result= parsed.root().get("v");
+            Object obj=result.unwrapped();
+            Assertions.assertInstanceOf(List.class,obj);
+
+            List<Object> tableList=(List<Object>)obj;
+            tableList.forEach(tablePath->{
+                Assertions.assertInstanceOf(Map.class,tablePath);
+            });
+
+            List<Map<String,String>> list=new ArrayList<>();
+            Map<String,String> map= new LinkedHashMap<>();
+            map.put("table_path","json_test.ml_*");
+            map.put("use_regex","true");
+            list.add(map);
+            list.add(Collections.singletonMap("table_path","json_test.ratings"));
+
+            Assertions.assertArrayEquals(tableList.toArray(),list.toArray());
+        } catch (Exception e) {
+            log.warn("Failed to parse value as ConfigValue, fallback to plain string: {}", value);
+        }
     }
 }
