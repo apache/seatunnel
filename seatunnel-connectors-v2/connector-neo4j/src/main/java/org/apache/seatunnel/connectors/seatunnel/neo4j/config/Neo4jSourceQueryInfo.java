91d10450283ed1314c8cd98b45c40e9842006837
jjj-n <1357598741@qq.com>
2026-09-02 13:51:09 +0800

    [Improve][Connector-V2][Neo4j] Migrate configuration validation to OptionRule
    Signed-off-by: jjj-n <1357598741@qq.com>

--- seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/config/Neo4jSourceQueryInfo.java
+++ seatunnel-connectors-v2/connector-neo4j/src/main/java/org/apache/seatunnel/connectors/seatunnel/neo4j/config/Neo4jSourceQueryInfo.java
@@ -19,11 +19,9 @@ package org.apache.seatunnel.connectors.seatunnel.neo4j.config;
 
 import org.apache.seatunnel.shade.com.typesafe.config.Config;
 
-import org.apache.seatunnel.common.constants.PluginType;
-
 public class Neo4jSourceQueryInfo extends Neo4jQueryInfo {
 
     public Neo4jSourceQueryInfo(Config pluginConfig) {
-        super(pluginConfig, PluginType.SOURCE);
+        super(pluginConfig);
     }
 }
