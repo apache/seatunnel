a25dcc9866a32c799302e3a54ceaff72b6246345
Supidan <1328827476@qq.com>
2026-09-08 07:48:23 +0800

    [Test][Connector-V2] Improve Slack factory validation test organization

--- seatunnel-connectors-v2/connector-slack/src/test/java/org/apache/seatunnel/connectors/seatunnel/slack/sink/SlackFactoryTest.java
+++ seatunnel-connectors-v2/connector-slack/src/test/java/org/apache/seatunnel/connectors/seatunnel/slack/sink/SlackFactoryTest.java
@@ -15,20 +15,22 @@
  * limitations under the License.
  */
 
-package org.apache.seatunnel.connectors.seatunnel.slack;
+package org.apache.seatunnel.connectors.seatunnel.slack.sink;
 
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.api.configuration.util.ConfigValidator;
 import org.apache.seatunnel.api.configuration.util.OptionValidationException;
-import org.apache.seatunnel.connectors.seatunnel.slack.sink.SlackSinkFactory;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 class SlackFactoryTest {
 
@@ -45,14 +47,24 @@ class SlackFactoryTest {
         assertInvalid(options, key);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"webhooks_url", "oauth_token", "slack_channel"})
-    void blankRequiredOption(String key) {
-        for (String value : new String[] {"", " ", "\t", "\n", " \t\r\n "}) {
-            Map<String, Object> options = validOptions();
-            options.put(key, value);
-            assertInvalid(options, key);
-        }
+    @ParameterizedTest(name = "{0}: {1}")
+    @MethodSource("blankRequiredOptions")
+    void blankRequiredOption(String key, String description, String value) {
+        Map<String, Object> options = validOptions();
+        options.put(key, value);
+        assertInvalid(options, key);
+    }
+
+    private static Stream<Arguments> blankRequiredOptions() {
+        return Stream.of("webhooks_url", "oauth_token", "slack_channel")
+                .flatMap(
+                        key ->
+                                Stream.of(
+                                        Arguments.of(key, "empty", ""),
+                                        Arguments.of(key, "space", " "),
+                                        Arguments.of(key, "tab", "\t"),
+                                        Arguments.of(key, "newline", "\n"),
+                                        Arguments.of(key, "mixed whitespace", " \t\r\n ")));
     }
 
     @ParameterizedTest
