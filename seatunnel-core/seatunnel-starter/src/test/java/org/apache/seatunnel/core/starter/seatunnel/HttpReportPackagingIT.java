9e9dd08f9ab26c609d4673a8e208904b7a58f896
Goutam Adwant <workwithgoutam@gmail.com>
2026-09-10 20:06:09 -0700

    [Fix][Zeta] Isolate HTTP reporting client dependencies

---
+++ seatunnel-core/seatunnel-starter/src/test/java/org/apache/seatunnel/core/starter/seatunnel/HttpReportPackagingIT.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seatunnel.core.starter.seatunnel;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+
+/** Verifies the packaged HTTP client with the distribution's lib-before-starter classpath. */
+class HttpReportPackagingIT {
+
+    @Test
+    void isolatesHttpClientFromHadoopOkio() throws Exception {
+        Path starter = Paths.get("target", "seatunnel-starter.jar").toAbsolutePath();
+        Assertions.assertTrue(Files.isRegularFile(starter), "Run after the package phase");
+        URL starterJar = starter.toUri().toURL();
+        URL hadoopJar = findHadoopJar();
+        // Do not inherit Maven's dependency ordering, which places the newer Okio first.
+        try (URLClassLoader loader =
+                new URLClassLoader(
+                        new URL[] {hadoopJar, starterJar},
+                        ClassLoader.getSystemClassLoader().getParent())) {
+            Assertions.assertEquals(
+                    hadoopJar,
+                    Class.forName("okio.ByteString", false, loader)
+                            .getProtectionDomain()
+                            .getCodeSource()
+                            .getLocation());
+            Class<?> handler =
+                    Class.forName(
+                            "org.apache.seatunnel.engine.server.event.JobEventHttpReportHandler",
+                            false,
+                            loader);
+            Class<?> clientType = handler.getDeclaredField("httpClient").getType();
+            // Construct the client referenced by real packaged engine bytecode, not a test import.
+            Object client = clientType.getConstructor().newInstance();
+            Assertions.assertNotNull(client);
+            Assertions.assertEquals(
+                    "org.apache.seatunnel.shade.engine.okhttp3.OkHttpClient", clientType.getName());
+            Assertions.assertEquals(
+                    starterJar, clientType.getProtectionDomain().getCodeSource().getLocation());
+            Assertions.assertEquals(
+                    starterJar,
+                    Class.forName(
+                                    "org.apache.seatunnel.shade.engine.okio.ByteString",
+                                    false,
+                                    loader)
+                            .getProtectionDomain()
+                            .getCodeSource()
+                            .getLocation());
+        }
+    }
+
+    private URL findHadoopJar() throws Exception {
+        Enumeration<URL> resources =
+                getClass().getClassLoader().getResources("okio/ByteString.class");
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            if ("jar".equals(resource.getProtocol())
+                    && resource.toExternalForm().contains("/seatunnel-shade-hadoop3-uber-")) {
+                return ((JarURLConnection) resource.openConnection()).getJarFileURL();
+            }
+        }
+        throw new AssertionError("The Hadoop uber jar must be present on the test classpath");
+    }
+}
