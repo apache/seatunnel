6583b4835eff1a3871110126d0d8795857c557c4
Saloni Gupta <sgnl264@gmail.com>
2026-09-11 13:48:08 +0200

    fix(connector-http-linear): resolve options imports and update dependencies

--- seatunnel-connectors-v2/connector-http/connector-http-linear/src/main/java/org/apache/seatunnel/connectors/seatunnel/linear/config/LinearSourceOptions.java
+++ seatunnel-connectors-v2/connector-http/connector-http-linear/src/main/java/org/apache/seatunnel/connectors/seatunnel/linear/config/LinearSourceOptions.java
@@ -16,12 +16,12 @@
  */
 
 package org.apache.seatunnel.connectors.seatunnel.linear.config;
+import org.apache.seatunnel.connectors.seatunnel.http.config.HttpCommonOptions;
 
 import org.apache.seatunnel.api.configuration.Option;
 import org.apache.seatunnel.api.configuration.Options;
-import org.apache.seatunnel.connectors.seatunnel.http.config.HttpOption;
 
-public class LinearSourceOptions extends HttpOption {
+public class LinearSourceOptions extends HttpCommonOptions {
     public static final Option<String> API_KEY =
             Options.key("api_key").stringType().noDefaultValue().withDescription("Linear API Key");
 }
