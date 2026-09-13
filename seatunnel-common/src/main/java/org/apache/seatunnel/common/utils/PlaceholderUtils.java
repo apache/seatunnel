3fa44b14a3344495d12e5c25f89717ad769e9fb3
NganWave <nganwave@163.com>
2026-09-13 22:52:07 +0800

    [Improve][seatunnel-core] Optimize placeholders replacement priority logic

--- seatunnel-common/src/main/java/org/apache/seatunnel/common/utils/PlaceholderUtils.java
+++ seatunnel-common/src/main/java/org/apache/seatunnel/common/utils/PlaceholderUtils.java
@@ -21,15 +21,17 @@ import org.apache.seatunnel.shade.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.seatunnel.shade.org.apache.commons.lang3.StringUtils;
 
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class PlaceholderUtils {
 
     private static final Pattern PLACEHOLDER_PATTERN =
-            Pattern.compile("\\$\\{\\??(?:\"([^\"]+)\"|([^{}:]+))(?::([^{}\\[\\]]*))?\\}");
+            Pattern.compile("\\$\\{\\??" + "(?:\"([^\"]+)\"|([^{}:]+))" + "(?::(.+))?" + "\\}");
 
     public static String replacePlaceholders(String input, String placeholderName, String value) {
         return replacePlaceholders(input, placeholderName, value, null);
@@ -98,20 +100,18 @@ public class PlaceholderUtils {
         while (matcher.find()) {
             String pureKey = matcher.group(1) != null ? matcher.group(1) : matcher.group(2);
             String defaultValue = matcher.group(3);
-
             String resolvedValue = valueResolver.apply(pureKey);
             String replacement;
 
-            if (resolvedValue != null) {
-                replacement = resolvedValue;
-            } else if (defaultValue != null) {
-                replacement = defaultValue;
-            } else {
-                replacement = matcher.group(0);
-            }
+            replacement =
+                    Stream.of(resolvedValue, defaultValue, System.getProperty(pureKey))
+                            .filter(Objects::nonNull)
+                            .findFirst()
+                            .orElse(matcher.group(0));
 
             matcher.appendReplacement(result, Matcher.quoteReplacement(replacement));
         }
+
         matcher.appendTail(result);
         return result.toString();
     }
