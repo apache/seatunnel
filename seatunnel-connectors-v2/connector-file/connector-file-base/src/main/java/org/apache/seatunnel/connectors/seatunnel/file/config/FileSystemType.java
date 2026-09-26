fd524bc06f1a4c8296c5c0a77303839d844557c6
Sudeep Hazra <sudeephazra@gmail.com>
2026-09-26 23:55:20 +0530

    [Feature][Connector-V2][ADLS Gen2] Fix merge conflict errors

--- seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/FileSystemType.java
+++ seatunnel-connectors-v2/connector-file/connector-file-base/src/main/java/org/apache/seatunnel/connectors/seatunnel/file/config/FileSystemType.java
@@ -31,7 +31,7 @@ public enum FileSystemType implements Serializable {
     OBS("ObsFile"),
     BOS("BosFile"),
     GCS("GcsFile"),
-    ADLS("ADLSFile");
+    ADLS("ADLSFile"),
     SMB("SmbFile");
 
     private final String fileSystemPluginName;
